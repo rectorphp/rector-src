04302b385b25243654758b69209ac1e5402c3019
Sebastian Schreiber <me@schreibersebastian.de>
2021-05-17 22:43:15 +0200

    [TASK] Reduce test cases

--- rules-tests/Arguments/Rector/FuncCall/FunctionArgumentDefaultValueReplacerRector/config/configured_rule.php
+++ rules-tests/Arguments/Rector/FuncCall/FunctionArgumentDefaultValueReplacerRector/config/configured_rule.php
@@ -12,15 +12,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(Rector\Arguments\Rector\FuncCall\FunctionArgumentDefaultValueReplacerRector::class)
         ->call('configure', [[
             FunctionArgumentDefaultValueReplacerRector::REPLACED_ARGUMENTS => ValueObjectInliner::inline([
-                new FuncCallArgumentDefaultValueReplacer('version_compare', 2, 'gte', 'ge'),
                 new FuncCallArgumentDefaultValueReplacer('version_compare', 2, 'lte', 'le'),
                 new FuncCallArgumentDefaultValueReplacer('version_compare', 2, '', '!='),
-                new FuncCallArgumentDefaultValueReplacer('version_compare', 2, '!', '!='),
-                new FuncCallArgumentDefaultValueReplacer('version_compare', 2, 'g', 'gt'),
-                new FuncCallArgumentDefaultValueReplacer('version_compare', 2, 'l', 'lt'),
-                new FuncCallArgumentDefaultValueReplacer('version_compare', 2, 'gte', 'ge'),
-                new FuncCallArgumentDefaultValueReplacer('version_compare', 2, 'lte', 'le'),
-                new FuncCallArgumentDefaultValueReplacer('version_compare', 2, 'n', 'ne'),
                 new FuncCallArgumentDefaultValueReplacer(
                     'some_function',
                     0,
