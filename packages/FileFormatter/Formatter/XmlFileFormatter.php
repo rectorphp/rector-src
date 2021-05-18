9a7f8d9b849568c7dea821ef7d5500441db06ac6
Sebastian Schreiber <me@schreibersebastian.de>
2021-05-18 21:53:11 +0200

    [TASK] Remove shanethehat/pretty-xml

--- packages/FileFormatter/Formatter/XmlFileFormatter.php
+++ packages/FileFormatter/Formatter/XmlFileFormatter.php
@@ -16,6 +16,26 @@ use Rector\FileFormatter\ValueObjectFactory\EditorConfigConfigurationBuilder;
  */
 final class XmlFileFormatter implements FileFormatterInterface
 {
+    /**
+     * @var int
+     */
+    private $depth;
+
+    /**
+     * @var int
+     */
+    private $indent = 4;
+
+    /**
+     * @var string
+     */
+    private $padChar = ' ';
+
+    /**
+     * @var boolean
+     */
+    private $preserveWhitespace = false;
+
     public function __construct(
         private Formatter $xmlFormatter
     ) {
@@ -47,5 +67,129 @@ final class XmlFileFormatter implements FileFormatterInterface
         $editorConfigConfigurationBuilder->withIndent(Indent::createTabWithSize(1));
 
         return $editorConfigConfigurationBuilder;
+    }
+
+    /**
+     * @param string $xml
+     * @return string
+     */
+    private function formatXml($xml)
+    {
+        $output = '';
+        $this->depth = 0;
+
+        $parts = $this->getXmlParts($xml);
+
+        if (strpos($parts[0], '<?xml') === 0) {
+            $output = array_shift($parts) . PHP_EOL;
+        }
+
+        foreach ($parts as $part) {
+            $output .= $this->getOutputForPart($part);
+        }
+
+        return trim($output);
+    }
+
+    /**
+     * @param string $xml
+     * @return array
+     */
+    private function getXmlParts($xml)
+    {
+        $withNewLines = preg_replace('/(>)(<)(\/*)/', "$1\n$2$3", trim($xml));
+        return explode("\n", $withNewLines);
+    }
+
+    /**
+     * @param string $part
+     * @return string
+     */
+    private function getOutputForPart($part)
+    {
+        $output = '';
+        $this->runPre($part);
+
+        if ($this->preserveWhitespace) {
+            $output .= $part . PHP_EOL;
+        } else {
+            $part = trim($part);
+            $output .= $this->getPaddedString($part) . PHP_EOL;
+        }
+
+        $this->runPost($part);
+
+        return $output;
+    }
+
+    /**
+     * @param string $part
+     */
+    private function runPre($part)
+    {
+        if ($this->isClosingTag($part)) {
+            $this->depth--;
+        }
+    }
+
+    /**
+     * @param string $part
+     */
+    private function runPost($part)
+    {
+        if ($this->isOpeningTag($part)) {
+            $this->depth++;
+        }
+        if ($this->isClosingCdataTag($part)) {
+            $this->preserveWhitespace = false;
+        }
+        if ($this->isOpeningCdataTag($part)) {
+            $this->preserveWhitespace = true;
+        }
+    }
+
+    /**
+     * @param string $part
+     * @return string
+     */
+    private function getPaddedString($part)
+    {
+        return str_pad($part, strlen($part) + ($this->depth * $this->indent), $this->padChar, STR_PAD_LEFT);
+    }
+
+    /**
+     * @param string $part
+     * @return boolean
+     */
+    private function isOpeningTag($part)
+    {
+        return (bool) preg_match('/^<[^\/]*>$/', $part);
+    }
+
+    /**
+     * @param string $part
+     * @return boolean
+     */
+    private function isClosingTag($part)
+    {
+        return (bool) preg_match('/^\s*<\//', $part);
+    }
+
+    /**
+     * @param string $part
+     * @return boolean
+     */
+    private function isOpeningCdataTag($part)
+    {
+        return strpos($part, '<![CDATA[') !== false;
+    }
+
+    /**
+     * @param string $part
+     * @return boolean
+     */
+    private function isClosingCdataTag($part)
+    {
+        return strpos($part, ']]>') !== false;
     }
 }
