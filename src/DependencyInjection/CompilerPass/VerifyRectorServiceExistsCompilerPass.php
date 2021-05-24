469be0d6d286b8bbc10c7715ae63f21f03064027
Markus Staab <maggus.staab@googlemail.com>
2021-05-24 09:39:11 +0200

    Update src/DependencyInjection/CompilerPass/VerifyRectorServiceExistsCompilerPass.php

--- src/DependencyInjection/CompilerPass/VerifyRectorServiceExistsCompilerPass.php
+++ src/DependencyInjection/CompilerPass/VerifyRectorServiceExistsCompilerPass.php
@@ -27,7 +27,7 @@ final class VerifyRectorServiceExistsCompilerPass implements CompilerPassInterfa
 
             if (!class_exists($class)) {
                 throw new ShouldNotHappenException(
-                    sprintf('Rector rule "%s" not found, please verify that the rule exists.', $class)
+                    sprintf('Rector rule "%s" not found, please verify that the class exists.', $class)
                 );
             }
 
