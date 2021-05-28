79ab54aeef5b8dab9214cf19e0dbbadd16fd22d3
Markus Staab <maggus.staab@googlemail.com>
2021-05-28 15:40:19 +0200

    Update packages-tests/FileFormatter/ValueObject/NewLineTest.php

--- packages-tests/FileFormatter/ValueObject/NewLineTest.php
+++ packages-tests/FileFormatter/ValueObject/NewLineTest.php
@@ -8,7 +8,6 @@ use Iterator;
 use PHPUnit\Framework\TestCase;
 use Rector\FileFormatter\Exception\InvalidNewLineStringException;
 use Rector\FileFormatter\ValueObject\NewLine;
-use Rector\Testing\PHPUnit\AbstractTestCase;
 use Rector\Testing\PHPUnit\PlatformAgnosticAssertions;
 use Symplify\SmartFileSystem\SmartFileInfo;
 
