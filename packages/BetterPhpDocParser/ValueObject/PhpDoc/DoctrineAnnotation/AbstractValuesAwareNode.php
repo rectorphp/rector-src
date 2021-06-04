8e4ecd66b37bd2ee33d9a0914fae3e1d9a775f54
romainallanot <romain@llanot.com>
2021-06-04 10:08:48 +0200

    Fix failing test fixture for AnnotationToAttributeRector

--- packages/BetterPhpDocParser/ValueObject/PhpDoc/DoctrineAnnotation/AbstractValuesAwareNode.php
+++ packages/BetterPhpDocParser/ValueObject/PhpDoc/DoctrineAnnotation/AbstractValuesAwareNode.php
@@ -148,7 +148,7 @@ abstract class AbstractValuesAwareNode implements PhpDocTagValueNode
             if (is_int($key) && $this->silentKey !== null) {
                 $explicitKeysValues[$this->silentKey] = $valueWithoutQuotes;
             } else {
-                $explicitKeysValues[$key] = $valueWithoutQuotes;
+                $explicitKeysValues[$this->removeQuotes($key)] = $valueWithoutQuotes;
             }
         }
 
@@ -161,6 +161,10 @@ abstract class AbstractValuesAwareNode implements PhpDocTagValueNode
      */
     protected function removeQuotes($value)
     {
+        if (\is_array($value)) {
+            return $this->removeQuotesFromArray($value);
+        }
+
         if (! is_string($value)) {
             return $value;
         }
@@ -171,6 +175,21 @@ abstract class AbstractValuesAwareNode implements PhpDocTagValueNode
         }
 
         return $matches['content'];
+    }
+
+    /**
+     * @param mixed[] $values
+     */
+    protected function removeQuotesFromArray(array $values): array
+    {
+        $unquotedArray = [];
+        foreach ($values as $key => $value) {
+            $unquotedKey = $this->removeQuotes($key);
+            $unquotedValue = $this->removeQuotes($value);
+            $unquotedArray[$unquotedKey] = $unquotedValue;
+        }
+
+        return $unquotedArray;
     }
 
     /**
