ac77652a5b672a453b096685b1ff21ea87a19f1f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-06-05 01:22:54 +0700

    phpstan

--- rules/TypeDeclaration/TypeInferer/SilentVoidResolver.php
+++ rules/TypeDeclaration/TypeInferer/SilentVoidResolver.php
@@ -47,7 +47,9 @@ final class SilentVoidResolver
             return false;
         }
 
-        if ($this->externalFullyQualifiedAnalyzer->hasExternalClassOrInterface($classLike->extends)) {
+        /** @var FullyQualified[]|FullyQualified|null $extends */
+        $extends = $classLike->extends;
+        if ($this->externalFullyQualifiedAnalyzer->hasExternalClassOrInterface($extends)) {
             return false;
         }
 
