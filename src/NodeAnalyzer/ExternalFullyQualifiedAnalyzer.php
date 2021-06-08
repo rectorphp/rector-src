8dcae6a914e9667ae838ac9659ffbbb3c6c68a09
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-06-08 13:31:14 +0700

    phpstan

--- src/NodeAnalyzer/ExternalFullyQualifiedAnalyzer.php
+++ src/NodeAnalyzer/ExternalFullyQualifiedAnalyzer.php
@@ -22,7 +22,7 @@ final class ExternalFullyQualifiedAnalyzer
 
     public function hasExternalFullyQualifieds(ClassLike $classLike): bool
     {
-        /** @var FullyQualified|FullyQualified[]|null $extends */
+        /** @var FullyQualified|FullyQualified[] $extends */
         $extends = $classLike instanceof Trait_ ? [] : ($classLike->extends ?? []);
 
         /** @var FullyQualified[] $implements */
