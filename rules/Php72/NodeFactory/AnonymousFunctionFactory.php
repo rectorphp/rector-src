729c263eac66ef31d28dece1dd17d52b152c8c55
Tomas Votruba <tomas.vot@gmail.com>
2021-06-10 12:19:55 +0200

    merge AnonymousFunctionFactory to one

--- rules/Php72/NodeFactory/AnonymousFunctionFactory.php
+++ rules/Php72/NodeFactory/AnonymousFunctionFactory.php
@@ -5,25 +5,39 @@ declare(strict_types=1);
 namespace Rector\Php72\NodeFactory;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\Closure;
 use PhpParser\Node\Expr\ClosureUse;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\PropertyFetch;
 use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Identifier;
 use PhpParser\Node\Name;
 use PhpParser\Node\NullableType;
 use PhpParser\Node\Param;
 use PhpParser\Node\Stmt;
+use PhpParser\Node\Stmt\Expression;
+use PhpParser\Node\Stmt\Return_;
 use PhpParser\Node\UnionType;
+use PHPStan\Reflection\FunctionVariantWithPhpDocs;
+use PHPStan\Reflection\ParameterReflection;
+use PHPStan\Reflection\Php\PhpMethodReflection;
+use PHPStan\Type\MixedType;
+use PHPStan\Type\VoidType;
 use Rector\Core\PhpParser\Node\BetterNodeFinder;
+use Rector\Core\PhpParser\Node\NodeFactory;
 use Rector\NodeNameResolver\NodeNameResolver;
 use Rector\NodeTypeResolver\Node\AttributeKey;
+use Rector\StaticTypeMapper\StaticTypeMapper;
 
 final class AnonymousFunctionFactory
 {
     public function __construct(
         private NodeNameResolver $nodeNameResolver,
-        private BetterNodeFinder $betterNodeFinder
+        private BetterNodeFinder $betterNodeFinder,
+        private NodeFactory $nodeFactory,
+        private StaticTypeMapper $staticTypeMapper,
     ) {
     }
 
@@ -49,6 +63,44 @@ final class AnonymousFunctionFactory
 
         $anonymousFunctionNode->stmts = $stmts;
         return $anonymousFunctionNode;
+    }
+
+    /**
+     * @param Variable|PropertyFetch $expr
+     */
+    public function createFromPhpMethodReflection(PhpMethodReflection $phpMethodReflection, Expr $expr): Closure
+    {
+        /** @var FunctionVariantWithPhpDocs $functionVariantWithPhpDoc */
+        $functionVariantWithPhpDoc = $phpMethodReflection->getVariants()[0];
+
+        $anonymousFunction = new Closure();
+        $newParams = $this->createParams($functionVariantWithPhpDoc->getParameters());
+
+        $anonymousFunction->params = $newParams;
+
+        $innerMethodCall = new MethodCall($expr, $phpMethodReflection->getName());
+        $innerMethodCall->args = $this->nodeFactory->createArgsFromParams($newParams);
+
+        if (! $functionVariantWithPhpDoc->getReturnType() instanceof MixedType) {
+            $returnType = $this->staticTypeMapper->mapPHPStanTypeToPhpParserNode(
+                $functionVariantWithPhpDoc->getReturnType()
+            );
+            $anonymousFunction->returnType = $returnType;
+        }
+
+        // does method return something?
+
+        if (! $functionVariantWithPhpDoc->getReturnType() instanceof VoidType) {
+            $anonymousFunction->stmts[] = new Return_($innerMethodCall);
+        } else {
+            $anonymousFunction->stmts[] = new Expression($innerMethodCall);
+        }
+
+        if ($expr instanceof Variable && ! $this->nodeNameResolver->isName($expr, 'this')) {
+            $anonymousFunction->uses[] = new ClosureUse($expr);
+        }
+
+        return $anonymousFunction;
     }
 
     /**
@@ -96,5 +148,26 @@ final class AnonymousFunctionFactory
         }
 
         return $filteredVariables;
+    }
+
+    /**
+     * @param ParameterReflection[] $parameterReflections
+     * @return Param[]
+     */
+    private function createParams(array $parameterReflections): array
+    {
+        $params = [];
+        foreach ($parameterReflections as $parameterReflection) {
+            $param = new Param(new Variable($parameterReflection->getName()));
+
+            if (! $parameterReflection->getType() instanceof MixedType) {
+                $paramType = $this->staticTypeMapper->mapPHPStanTypeToPhpParserNode($parameterReflection->getType());
+                $param->type = $paramType;
+            }
+
+            $params[] = $param;
+        }
+
+        return $params;
     }
 }
