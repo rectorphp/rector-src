e4350c27a92d44244b40557e7a85abf5e14e27db
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-06-14 19:46:56 +0700

    Revert "[Php80] Test for misplaced } on multiline in AnnotationToAttributeRector (#218)"
    This reverts commit fc17cc90a9dbee71396866d1baf7c3fd3fef419b.

--- rules-tests/Php80/Rector/Class_/AnnotationToAttributeRector/config/configured_rule.php
+++ rules-tests/Php80/Rector/Class_/AnnotationToAttributeRector/config/configured_rule.php
@@ -18,8 +18,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
                 new AnnotationToAttribute('Symfony\Component\Routing\Annotation\Route'),
                 new AnnotationToAttribute('Symfony\Component\Validator\Constraints\Choice'),
 
-                new AnnotationToAttribute('Doctrine\ORM\Mapping\Entity'),
-                new AnnotationToAttribute('Doctrine\ORM\Mapping\Table'),
                 new AnnotationToAttribute('Doctrine\ORM\Mapping\Id'),
                 new AnnotationToAttribute('Doctrine\ORM\Mapping\Column'),
                 new AnnotationToAttribute('Doctrine\ORM\Mapping\OrderBy'),
