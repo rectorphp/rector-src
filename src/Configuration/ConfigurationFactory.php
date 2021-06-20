dade998c55216e52d5b2dea188682b88d06a4082
Tomas Votruba <tomas.vot@gmail.com>
2021-06-20 17:27:31 +0200

    create configuraiton via factory

---
+++ src/Configuration/ConfigurationFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Core\Configuration;
+
+final class ConfigurationFactory
+{
+    public function create(): Configuration
+    {
+        // @todo
+        die;
+    }
+}
