cfaee8f1d479d2b1f0ea14d3ec6d3e1d3bc15673
GitHub Action <action@github.com>
2021-06-24 16:06:31 +0000

    [ci-review] Rector Rectify

--- rules-tests/DeadCode/Rector/ClassMethod/RemoveUnusedPromotedPropertyRector/config/configured_rule.php
+++ rules-tests/DeadCode/Rector/ClassMethod/RemoveUnusedPromotedPropertyRector/config/configured_rule.php
@@ -2,9 +2,11 @@
 
 declare(strict_types=1);
 
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPromotedPropertyRector;
+
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
-    $services->set(Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPromotedPropertyRector::class);
+    $services->set(RemoveUnusedPromotedPropertyRector::class);
 };
