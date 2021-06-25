a1e946d85b30c26a8605b08153c33878c9fa4df8
GitHub Action <action@github.com>
2021-06-25 17:20:42 +0000

    [ci-review] Rector Rectify

--- rules-tests/Renaming/Rector/Name/RenameClassRector/config/rename_parent.php
+++ rules-tests/Renaming/Rector/Name/RenameClassRector/config/rename_parent.php
@@ -2,13 +2,15 @@
 
 declare(strict_types=1);
 
+use Rector\Renaming\Rector\Name\RenameClassRector;
+
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
-    $services->set(\Rector\Renaming\Rector\Name\RenameClassRector::class)
+    $services->set(RenameClassRector::class)
         ->call('configure', [[
-            \Rector\Renaming\Rector\Name\RenameClassRector::OLD_TO_NEW_CLASSES => [
+            RenameClassRector::OLD_TO_NEW_CLASSES => [
                 'ThisClassDoesNotExistAnymore' => 'NewClassThatDoesNotExistEither',
                 'App\NotHereClass\AndNamespace' => 'NewClassThatDoesNotExistEither',
             ],
