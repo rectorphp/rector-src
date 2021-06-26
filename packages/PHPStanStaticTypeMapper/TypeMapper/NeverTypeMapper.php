c4b3c0db9b7a2d993e5a33f5eea9602557964b58
Markus Staab <markus.staab@redaxo.de>
2021-06-26 21:58:19 +0200

    use TypeKind

--- packages/PHPStanStaticTypeMapper/TypeMapper/NeverTypeMapper.php
+++ packages/PHPStanStaticTypeMapper/TypeMapper/NeverTypeMapper.php
@@ -10,6 +10,7 @@ use PHPStan\PhpDocParser\Ast\Type\TypeNode;
 use PHPStan\Type\NeverType;
 use PHPStan\Type\Type;
 use Rector\PHPStanStaticTypeMapper\Contract\TypeMapperInterface;
+use Rector\PHPStanStaticTypeMapper\ValueObject\TypeKind;
 
 final class NeverTypeMapper implements TypeMapperInterface
 {
@@ -26,7 +27,7 @@ final class NeverTypeMapper implements TypeMapperInterface
      */
     public function mapToPHPStanPhpDocTypeNode(Type $type, ?string $kind = null): TypeNode
     {
-        if ($kind === 'return') {
+        if ($kind === TypeKind::KIND_RETURN) {
             return new IdentifierTypeNode('never');
         }
         return new IdentifierTypeNode('mixed');
