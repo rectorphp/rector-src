87500f6899fa6f1de3cc3f9c12665a1e2ec964ec
Tomas Votruba <tomas.vot@gmail.com>
2021-06-28 22:20:31 +0200

    Add enums

--- rules-tests/CodingStyle/Rector/MethodCall/PreferThisOrSelfMethodCallRector/config/configured_rule.php
+++ rules-tests/CodingStyle/Rector/MethodCall/PreferThisOrSelfMethodCallRector/config/configured_rule.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use Rector\CodingStyle\Enum\PreferenceSelfThis;
 use Rector\CodingStyle\Rector\MethodCall\PreferThisOrSelfMethodCallRector;
-use Rector\CodingStyle\ValueObject\PreferenceSelfThis;
 use Rector\Tests\CodingStyle\Rector\MethodCall\PreferThisOrSelfMethodCallRector\Source\AbstractTestCase;
 use Rector\Tests\CodingStyle\Rector\MethodCall\PreferThisOrSelfMethodCallRector\Source\BeLocalClass;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -14,9 +14,15 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(PreferThisOrSelfMethodCallRector::class)
         ->call('configure', [[
             PreferThisOrSelfMethodCallRector::TYPE_TO_PREFERENCE => [
-                AbstractTestCase::class => PreferenceSelfThis::PREFER_SELF,
-                BeLocalClass::class => PreferenceSelfThis::PREFER_THIS,
-                TestCase::class => PreferenceSelfThis::PREFER_SELF,
+                AbstractTestCase::class => \Symplify\SymfonyPhpConfig\ValueObjectInliner::inline(
+                    PreferenceSelfThis::PREFER_SELF()
+                ),
+                BeLocalClass::class => \Symplify\SymfonyPhpConfig\ValueObjectInliner::inline(
+                    PreferenceSelfThis::PREFER_THIS()
+                ),
+                TestCase::class => \Symplify\SymfonyPhpConfig\ValueObjectInliner::inline(
+                    PreferenceSelfThis::PREFER_SELF()
+                ),
             ],
         ]]);
 };
