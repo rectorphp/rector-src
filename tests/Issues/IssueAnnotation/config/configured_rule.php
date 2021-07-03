ac28dec5a9889802af3fce47406ec444855b9795
Tomas Votruba <tomas.vot@gmail.com>
2021-07-03 17:20:43 +0200

    fix default value completing in DowngradeAttributeToAnnotationRector

--- tests/Issues/IssueAnnotation/config/configured_rule.php
+++ tests/Issues/IssueAnnotation/config/configured_rule.php
@@ -2,24 +2,10 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
-use Rector\Core\ValueObject\PhpVersion;
-use Rector\DowngradePhp80\Rector\Class_\DowngradeAttributeToAnnotationRector;
 use Rector\DowngradePhp80\Rector\MethodCall\DowngradeNamedArgumentRector;
-use Rector\DowngradePhp80\ValueObject\DowngradeAttributeToAnnotation;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\SymfonyPhpConfig\ValueObjectInliner;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_74);
-
     $services = $containerConfigurator->services();
-    $services->set(DowngradeAttributeToAnnotation::class)
-        ->call('configure', [[
-            DowngradeAttributeToAnnotationRector::ATTRIBUTE_TO_ANNOTATION => ValueObjectInliner::inline([
-                new DowngradeAttributeToAnnotation('Attribute', 'annotation'),
-            ]),
-        ]]);
     $services->set(DowngradeNamedArgumentRector::class);
 };
