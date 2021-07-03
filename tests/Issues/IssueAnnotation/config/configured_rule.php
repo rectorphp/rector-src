0f3d68c920aed99e6d9759daffca071ec714eb9c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-07-03 19:23:10 +0700

    Failing test AutoconfigureTag

---
+++ tests/Issues/IssueAnnotation/config/configured_rule.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Rector\Core\ValueObject\PhpVersion;
+use Rector\DowngradePhp80\Rector\Class_\DowngradeAttributeToAnnotationRector;
+use Rector\DowngradePhp80\Rector\MethodCall\DowngradeNamedArgumentRector;
+use Rector\DowngradePhp80\ValueObject\DowngradeAttributeToAnnotation;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\SymfonyPhpConfig\ValueObjectInliner;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_74);
+
+    $services = $containerConfigurator->services();
+    $services->set(DowngradeAttributeToAnnotation::class)
+        ->call('configure', [[
+            DowngradeAttributeToAnnotationRector::ATTRIBUTE_TO_ANNOTATION => ValueObjectInliner::inline([
+                new DowngradeAttributeToAnnotation('Attribute', 'annotation'),
+                // Symfony
+                new DowngradeAttributeToAnnotation('Symfony\Contracts\Service\Attribute\Required', 'required'),
+                // Nette
+                new DowngradeAttributeToAnnotation('Nette\DI\Attributes\Inject', 'inject'),
+            ]),
+        ]]);
+    $services->set(DowngradeNamedArgumentRector::class);
+};
