577c061ed575ddfcc056a8564b59c31fa79f33b7
Tomas Votruba <tomas.vot@gmail.com>
2021-07-04 22:47:58 +0200

    void load on ConfigurableCallValuesCollectingPhpFileLoader

--- src/DependencyInjection/Loader/ConfigurableCallValuesCollectingPhpFileLoader.php
+++ src/DependencyInjection/Loader/ConfigurableCallValuesCollectingPhpFileLoader.php
@@ -23,7 +23,7 @@ final class ConfigurableCallValuesCollectingPhpFileLoader extends PhpFileLoader
     /**
      * @param mixed $resource
      */
-    public function load($resource, string $type = null)
+    public function load($resource, string $type = null): void
     {
         // this call collects root values
         $this->collectConfigureCallsFromJustImportedConfigurableRectorDefinitions();
