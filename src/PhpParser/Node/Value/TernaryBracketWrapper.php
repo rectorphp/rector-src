100bca8f037e0d0975b407d7f05e92b39cfcccc3
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-07-07 17:14:22 +0700

    move to TernaryBracketWrapper service

---
+++ src/PhpParser/Node/Value/TernaryBracketWrapper.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Core\PhpParser\Node\Value;
+
+use PhpParser\Node\Expr\ConstFetch;
+use PhpParser\Node\Expr\Ternary;
+use PhpParser\Node\Name;
+use Rector\Core\PhpParser\Printer\BetterStandardPrinter;
+
+final class TernaryBracketWrapper
+{
+    public function __construct(private BetterStandardPrinter $betterStandardPrinter)
+    {
+    }
+
+    public function getWrappedTernaryConstFetch(Ternary $ternary)
+    {
+        return new ConstFetch(new Name('(' . $this->betterStandardPrinter->print($ternary) . ')'));
+    }
+}
