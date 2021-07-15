bcea05608ce69097baca7b582957e9534271b021
Jess Archer <jess@jessarcher.com>
2021-07-15 14:04:29 +1000

    Add support for the recursive array syntax of compact()

--- src/NodeAnalyzer/CompactFuncCallAnalyzer.php
+++ src/NodeAnalyzer/CompactFuncCallAnalyzer.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Rector\Core\NodeAnalyzer;
 
+use PhpParser\Node\Expr\Array_;
 use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Scalar\String_;
@@ -29,11 +30,42 @@ final class CompactFuncCallAnalyzer
 
         $args = $funcCall->args;
         foreach ($args as $arg) {
+            if ($arg->value instanceof Array_) {
+                if ($this->isInArray($arg->value, $variableName)) {
+                    return true;
+                }
+
+                continue;
+            }
+
             if (! $arg->value instanceof String_) {
                 continue;
             }
 
             if ($arg->value->value === $variableName) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function isInArray(Array_ $array, string $variableName): bool
+    {
+        foreach ($array->items as $item) {
+            if ($item->value instanceof Array_) {
+                if ($this->isInArray($item->value, $variableName)) {
+                    return true;
+                }
+
+                continue;
+            }
+
+            if (! $item->value instanceof String_) {
+                continue;
+            }
+
+            if ($item->value->value === $variableName) {
                 return true;
             }
         }
