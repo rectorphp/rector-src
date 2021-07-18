1454c4f19e43679866b3e2a438d948dda10c34ad
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-07-18 20:28:22 +0700

    [Privatization][TypeDeclaration] Invalid return type on use RepeatedLiteralToClassConstantRector + ReturnTypeDeclarationRector

---
+++ tests/Issues/IssueImportedReturn/config/configured_rule.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Rector\Core\ValueObject\PhpVersion;
+use Rector\Privatization\Rector\Class_\RepeatedLiteralToClassConstantRector;
+use Rector\TypeDeclaration\Rector\FunctionLike\ReturnTypeDeclarationRector;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+    $services->set(RepeatedLiteralToClassConstantRector::class);
+    $services->set(ReturnTypeDeclarationRector::class);
+
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
+    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_80);
+};
