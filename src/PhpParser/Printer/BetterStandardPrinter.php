c0f860ea5a0bf812a02a51d3a3b09db87870fe90
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-07-22 22:56:50 +0700

    using mU

--- src/PhpParser/Printer/BetterStandardPrinter.php
+++ src/PhpParser/Printer/BetterStandardPrinter.php
@@ -62,7 +62,7 @@ final class BetterStandardPrinter extends Standard
      * @see https://regex101.com/r/qZiqGo/8
      * @var string
      */
-    private const REPLACE_COLON_WITH_SPACE_REGEX = '#(^((public|private|protected)\s+)?function .*?\(.*?\) : )#';
+    private const REPLACE_COLON_WITH_SPACE_REGEX = '#(^((public|private|protected)\s+)?function .*?\(.*?\)) : #mU';
 
     /**
      * Use space by default
