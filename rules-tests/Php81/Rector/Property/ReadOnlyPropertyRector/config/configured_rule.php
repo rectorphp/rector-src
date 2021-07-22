75394ec89a35f4503c55551c02e6008a4ed2adec
GitHub Action <action@github.com>
2021-07-22 22:03:59 +0000

    [ci-review] Rector Rectify

--- rules-tests/Php81/Rector/Property/ReadOnlyPropertyRector/config/configured_rule.php
+++ rules-tests/Php81/Rector/Property/ReadOnlyPropertyRector/config/configured_rule.php
@@ -2,9 +2,11 @@
 
 declare(strict_types=1);
 
+use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
+
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
-    $services->set(\Rector\Php81\Rector\Property\ReadOnlyPropertyRector::class);
+    $services->set(ReadOnlyPropertyRector::class);
 };
