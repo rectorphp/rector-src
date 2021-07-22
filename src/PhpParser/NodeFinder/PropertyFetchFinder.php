3e5157c9f32fd934e564a63b674309aedadaee42
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-07-22 12:12:07 +0700

    getting trait via ReflectionProvider

--- src/PhpParser/NodeFinder/PropertyFetchFinder.php
+++ src/PhpParser/NodeFinder/PropertyFetchFinder.php
@@ -10,17 +10,24 @@ use PhpParser\Node\Expr\StaticPropertyFetch;
 use PhpParser\Node\Param;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\Property;
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Reflection\ReflectionProvider;
 use Rector\Core\PhpParser\Node\BetterNodeFinder;
-use Rector\NodeCollector\NodeCollector\NodeRepository;
+use Rector\Core\Reflection\ReflectionResolver;
 use Rector\NodeNameResolver\NodeNameResolver;
 use Rector\NodeTypeResolver\Node\AttributeKey;
+use Symplify\SmartFileSystem\SmartFileSystem;
+use PhpParser\Parser;
 
 final class PropertyFetchFinder
 {
     public function __construct(
-        private NodeRepository $nodeRepository,
         private BetterNodeFinder $betterNodeFinder,
-        private NodeNameResolver $nodeNameResolver
+        private NodeNameResolver $nodeNameResolver,
+        private ReflectionProvider $reflectionProvider,
+        private ReflectionResolver $reflectionResolver,
+        private SmartFileSystem $smartFileSystem,
+        private Parser $parser
     ) {
     }
 
@@ -32,6 +39,57 @@ final class PropertyFetchFinder
         $classLike = $propertyOrPromotedParam->getAttribute(AttributeKey::CLASS_NODE);
         if (! $classLike instanceof Class_) {
             return [];
+        }
+
+        $propertyName = $this->resolvePropertyName($propertyOrPromotedParam);
+        if ($propertyName === null) {
+            return [];
+        }
+
+        $className       = $this->nodeNameResolver->getName($classLike);
+        $classReflection = $this->reflectionProvider->getClass($className);
+
+        if (! $classReflection instanceof ClassReflection) {
+            return [];
+        }
+
+        $traits = $classReflection->getTraits(true);
+        $propertyFetches = [];
+        foreach ($traits as $trait) {
+            $fileName = $trait->getFileName();
+            if (! $fileName) {
+                continue;
+            }
+
+            $fileContent = $this->smartFileSystem->readFile($fileName);
+            $nodes       = $this->parser->parse($fileContent);
+
+            $propertyFetches[] = $this->betterNodeFinder->findFirst($nodes, function (Node $node) use (
+                $propertyName
+            ): bool {
+                // property + static fetch
+                if (! $node instanceof PropertyFetch && ! $node instanceof StaticPropertyFetch) {
+                    return false;
+                }
+
+                return $this->nodeNameResolver->isName($node, $propertyName);
+            });
+        }
+
+        return $propertyFetches;
+
+        dump_node($traits);die;
+
+        $traits = [];
+        foreach ($classLike->getTraitUses() as $traitUse) {
+            foreach ($traitUse->traits as $trait) {
+                $traitName = $this->nodeNameResolver->getName($trait);
+                $traits[]  = $this->reflectionProvider->getClass($traitName);
+            }
+        }
+
+        foreach ($traits as $trait) {
+
         }
 
         $classLikesToSearch = $this->nodeRepository->findUsedTraitsInClass($classLike);
