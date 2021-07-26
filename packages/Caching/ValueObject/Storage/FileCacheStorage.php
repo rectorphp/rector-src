fa3b02ffb611358ab53762b0f3cd89ba8b94ffdc
Markus Staab <m.staab@complex-it.de>
2021-07-26 09:47:36 +0200

    sync FIleCacheStorage impl with upstream phpstan

--- packages/Caching/ValueObject/Storage/FileCacheStorage.php
+++ packages/Caching/ValueObject/Storage/FileCacheStorage.php
@@ -5,11 +5,9 @@ declare(strict_types=1);
 namespace Rector\Caching\ValueObject\Storage;
 
 use Nette\Utils\Random;
-use Nette\Utils\Strings;
-use Rector\Caching\ValueObject\CacheFilePaths;
-use Rector\Caching\ValueObject\CacheItem;
-use Symplify\EasyCodingStandard\Caching\Exception\CachingException;
-use Symplify\SmartFileSystem\SmartFileSystem;
+use PHPStan\Cache\CacheItem;
+use PHPStan\File\FileWriter;
+use PHPStan\ShouldNotHappenException;
 
 /**
  * Inspired by
@@ -23,66 +21,83 @@ final class FileCacheStorage
     ) {
     }
 
+    private function makeDir(string $directory) : void
+    {
+        if (\is_dir($directory)) {
+            return;
+        }
+        $result = @\mkdir($directory, 0777);
+        if ($result === \false) {
+            \clearstatcache();
+            if (\is_dir($directory)) {
+                return;
+            }
+            $error = \error_get_last();
+            throw new \InvalidArgumentException(\sprintf('Failed to create directory "%s" (%s).', $this->directory, $error !== null ? $error['message'] : 'unknown cause'));
+        }
+    }
+
     /**
+     * @param string $key
+     * @param string $variableKey
      * @return mixed|null
      */
     public function load(string $key, string $variableKey)
     {
-        $cacheFilePaths = $this->getCacheFilePaths($key);
-
-        $filePath = $cacheFilePaths->getFilePath();
-        if (! is_file($filePath)) {
-            return null;
-        }
-
-        $cacheItem = require $filePath;
-        if (! $cacheItem instanceof CacheItem) {
-            return null;
-        }
-
-        if (! $cacheItem->isVariableKeyValid($variableKey)) {
-            return null;
-        }
-
-        return $cacheItem->getData();
+        return (function (string $key, string $variableKey) {
+            [, , $filePath] = $this->getFilePaths($key);
+            if (!\is_file($filePath)) {
+                return null;
+            }
+            $cacheItem = (require $filePath);
+            if (!$cacheItem instanceof CacheItem) {
+                return null;
+            }
+            if (!$cacheItem->isVariableKeyValid($variableKey)) {
+                return null;
+            }
+            return $cacheItem->getData();
+        })($key, $variableKey);
     }
 
     /**
+     * @param string $key
+     * @param string $variableKey
      * @param mixed $data
+     * @return void
      */
-    public function save(string $key, string $variableKey, $data): void
+    public function save(string $key, string $variableKey, $data) : void
     {
-        $cacheFilePaths = $this->getCacheFilePaths($key);
-
-        $this->smartFileSystem->mkdir($cacheFilePaths->getFirstDirectory());
-        $this->smartFileSystem->mkdir($cacheFilePaths->getSecondDirectory());
-
-        $errorBefore = error_get_last();
-        $exported = @var_export(new CacheItem($variableKey, $data), true);
-        $errorAfter = error_get_last();
-
+        [$firstDirectory, $secondDirectory, $path] = $this->getFilePaths($key);
+        $this->makeDir($this->directory);
+        $this->makeDir($firstDirectory);
+        $this->makeDir($secondDirectory);
+        $tmpPath = \sprintf('%s/%s.tmp', $this->directory, Random::generate());
+        $errorBefore = \error_get_last();
+        $exported = @\var_export(new CacheItem($variableKey, $data), \true);
+        $errorAfter = \error_get_last();
         if ($errorAfter !== null && $errorBefore !== $errorAfter) {
-            $errorMessage = sprintf(
-                'Error occurred while saving item "%s" ("%s") to cache: "%s"',
-                $key,
-                $variableKey,
-                $errorAfter['message']
-            );
-            throw new CachingException($errorMessage);
+            throw new ShouldNotHappenException(\sprintf('Error occurred while saving item %s (%s) to cache: %s', $key, $variableKey, $errorAfter['message']));
         }
-
-        $variableFileContent = sprintf("<?php declare(strict_types = 1);\n\nreturn %s;", $exported);
-        $this->smartFileSystem->dumpFile($cacheFilePaths->getFilePath(), $variableFileContent);
+        FileWriter::write($tmpPath, \sprintf("<?php declare(strict_types = 1);\n\nreturn %s;", $exported));
+        $renameSuccess = @\rename($tmpPath, $path);
+        if ($renameSuccess) {
+            return;
+        }
+        @\unlink($tmpPath);
+        if (\DIRECTORY_SEPARATOR === '/' || !\file_exists($path)) {
+            throw new \InvalidArgumentException(\sprintf('Could not write data to cache file %s.', $path));
+        }
     }
 
     public function clean(string $cacheKey): void
     {
-        $cacheFilePaths = $this->getCacheFilePaths($cacheKey);
+        [$firstDirectory, $secondDirectory, $path] = $this->getFilePaths($cacheKey);
 
         $this->smartFileSystem->remove([
-            $cacheFilePaths->getFirstDirectory(),
-            $cacheFilePaths->getSecondDirectory(),
-            $cacheFilePaths->getFilePath(),
+            $firstDirectory,
+            $secondDirectory,
+            $path
         ]);
     }
 
@@ -91,13 +106,16 @@ final class FileCacheStorage
         $this->smartFileSystem->remove($this->directory);
     }
 
-    private function getCacheFilePaths(string $key): CacheFilePaths
+    /**
+     * @param string $key
+     * @return array{string, string, string}
+     */
+    private function getFilePaths(string $key) : array
     {
-        $keyHash = sha1($key);
-        $firstDirectory = sprintf('%s/%s', $this->directory, Strings::substring($keyHash, 0, 2));
-        $secondDirectory = sprintf('%s/%s', $firstDirectory, Strings::substring($keyHash, 2, 2));
-        $filePath = sprintf('%s/%s.php', $secondDirectory, $keyHash);
-
-        return new CacheFilePaths($firstDirectory, $secondDirectory, $filePath);
+        $keyHash = \sha1($key);
+        $firstDirectory = \sprintf('%s/%s', $this->directory, \substr($keyHash, 0, 2));
+        $secondDirectory = \sprintf('%s/%s', $firstDirectory, \substr($keyHash, 2, 2));
+        $filePath = \sprintf('%s/%s.php', $secondDirectory, $keyHash);
+        return [$firstDirectory, $secondDirectory, $filePath];
     }
 }
