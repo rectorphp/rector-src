96969693968588f946389772b4c3b9c437ce624c
Markus Staab <47448731+clxmstaab@users.noreply.github.com>
2021-07-27 15:45:55 +0200

    Update config/parameters.php

--- config/parameters.php
+++ config/parameters.php
@@ -31,7 +31,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     // cache
     $parameters->set(Option::CACHE_DIR, sys_get_temp_dir() . '/rector_cached_files');
 
-    if (false !== getenv('GITHUB_ACTION')) {
+    $runsInGithubAction = false !== getenv('GITHUB_ACTION');
+    if ($runsInGithubAction ) {
         $parameters->set(Option::CACHE_CLASS, MemoryCacheStorage::class);
     }
 };
