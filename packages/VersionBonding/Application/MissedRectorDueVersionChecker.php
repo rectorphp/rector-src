aa636d3a87a7dbc5f734060f559fb6b14c8f979e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-02 21:29:59 +0700

    final touch

--- packages/VersionBonding/Application/MissedRectorDueVersionChecker.php
+++ packages/VersionBonding/Application/MissedRectorDueVersionChecker.php
@@ -91,9 +91,10 @@ final class MissedRectorDueVersionChecker
         $phpVersion = new PhpVersion($minProjectPhpVersion);
 
         $warningMessage = sprintf(
-            'Your project requires min PHP version "%s".%sSome Rector rules defined in your configuration require higher PHP version and will not run,%sto avoid breaking your codebase, use -vvv for detailed info.',
+            'Your project requires min PHP version "%s". %s%d Rector rules defined in your configuration require higher PHP version and will not run,%sto avoid breaking your codebase, use -vvv for detailed info.',
             $phpVersion->getVersionString(),
             PHP_EOL . PHP_EOL,
+            count($missedRectors),
             PHP_EOL
         );
 
