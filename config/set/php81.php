bff652b71d1da93ec1d84e81b6dcafdfa68a7c4b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-02 13:10:31 +0700

    clean up

--- config/set/php81.php
+++ config/set/php81.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Php81\Rector\Class_\MyCLabsClassToEnumRector;
-
 use Rector\Php81\Rector\ClassConst\FinalizePublicClassConstantRector;
 use Rector\Php81\Rector\MethodCall\MyCLabsMethodCallToEnumConstRector;
 use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
