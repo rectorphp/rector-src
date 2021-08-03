b7403d4ad1161c0052e3a33d92099abf76c71192
Michal Lulco <lulco@efabrica.sk>
2021-08-03 23:29:48 +0200

    Separate test using multiple files changed trait

--- rules-tests/Restoration/Rector/ClassLike/UpdateFileNameByClassNameFileSystemRector/UpdateFileNameByClassNameFileSystemRectorTest.php
+++ rules-tests/Restoration/Rector/ClassLike/UpdateFileNameByClassNameFileSystemRector/UpdateFileNameByClassNameFileSystemRectorTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Rector\Tests\Restoration\Rector\ClassLike\UpdateFileNameByClassNameFileSystemRector;
 
 use Iterator;
+use Nette\Utils\FileSystem;
+use Rector\FileSystemRector\ValueObject\AddedFileWithContent;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
 use Symplify\SmartFileSystem\SmartFileInfo;
 
@@ -15,7 +17,13 @@ final class UpdateFileNameByClassNameFileSystemRectorTest extends AbstractRector
      */
     public function test(SmartFileInfo $smartFileInfo): void
     {
-        $this->doTestFileInfoWithAdditionalChanges($smartFileInfo);
+        $this->doTestFileInfo($smartFileInfo);
+
+        $expectedAddedFileWithContent = new AddedFileWithContent(
+            $this->originalTempFileInfo->getRealPathDirectory() . '/SkipDifferentClassName.php',
+            FileSystem::read(__DIR__ . '/Fixture/skip_different_class_name.php.inc')
+        );
+        $this->assertFileWasAdded($expectedAddedFileWithContent);
     }
 
     /**
---
+++ rules-tests/Restoration/Rector/ClassLike/UpdateFileNameByClassNameFileSystemRector/UpdateFileNameByClassNameFileSystemRectorUsingMultipleFilesChangedTraitTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\Restoration\Rector\ClassLike\UpdateFileNameByClassNameFileSystemRector;
+
+use Iterator;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class UpdateFileNameByClassNameFileSystemRectorUsingMultipleFilesChangedTraitTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $smartFileInfo): void
+    {
+        $this->doTestFileInfoWithAdditionalChanges($smartFileInfo);
+    }
+
+    /**
+     * @return Iterator<SmartFileInfo>
+     */
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/FixtureUsingMultipleFilesChangedTrait');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/configured_rule.php';
+    }
+}
