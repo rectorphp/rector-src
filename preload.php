f1a1030218c0f0c74fb21235a685e59d76d7622d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-04 02:26:26 +0700

    cs fix after preload

--- preload.php
+++ preload.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 require_once __DIR__ . '/vendor/nikic/php-parser/lib/PhpParser/NameContext.php';
 require_once __DIR__ . '/vendor/nikic/php-parser/lib/PhpParser/NodeVisitorAbstract.php';
