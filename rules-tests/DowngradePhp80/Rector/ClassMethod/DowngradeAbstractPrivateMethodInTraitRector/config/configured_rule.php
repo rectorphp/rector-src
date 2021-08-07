2206316f2d6e5fb615ddb6b0d6fabe1ef247c020
Bl00D4NGEL <kuhlesdominik@gmx.de>
2021-08-07 14:18:53 +0200

    implement feature

--- rules-tests/DowngradePhp80/Rector/ClassMethod/DowngradeAbstractPrivateMethodInTraitRector/config/configured_rule.php
+++ rules-tests/DowngradePhp80/Rector/ClassMethod/DowngradeAbstractPrivateMethodInTraitRector/config/configured_rule.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-use Rector\DowngradePhp80\Rector\ClassMethod\DowngradeStaticTypeDeclarationRector;
+use Rector\DowngradePhp80\Rector\ClassMethod\DowngradeAbstractPrivateMethodInTraitRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
-    $services->set(DowngradeStaticTypeDeclarationRector::class);
+    $services->set(DowngradeAbstractPrivateMethodInTraitRector::class);
 };
--- rules/DowngradePhp80/Rector/ClassMethod/DowngradeAbstractPrivateMethodInTraitRector.php
+++ rules/DowngradePhp80/Rector/ClassMethod/DowngradeAbstractPrivateMethodInTraitRector.php
@@ -5,7 +5,68 @@ declare(strict_types=1);
 
 namespace Rector\DowngradePhp80\Rector\ClassMethod;
 
-final class DowngradeAbstractPrivateMethodInTraitRector
-{
+use PhpParser\Node;
+use PhpParser\Node\Stmt\ClassMethod;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Core\ValueObject\Visibility;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
+/**
+ * @see \Rector\Tests\DowngradePhp80\Rector\ClassMethod\DowngradeAbstractPrivateMethodInTraitRector\DowngradeAbstractPrivateMethodInTraitRectorTest
+ */
+final class DowngradeAbstractPrivateMethodInTraitRector extends AbstractRector
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Remove "abstract" from private methods in traits and adds an empty function body',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+trait SomeTrait
+{
+    abstract private function someAbstractPrivateFunction();
+}
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+trait SomeTrait
+{
+    private function someAbstractPrivateFunction() {}
+}
+CODE_SAMPLE
+                ),
+            ]
+        );
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [ClassMethod::class];
+    }
+
+    /**
+     * @param ClassMethod $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $node->isAbstract()) {
+            return null;
+        }
+
+        if (! $node->isPrivate()) {
+            return null;
+        }
+
+        // Strip the abstract flag
+        $node->flags -= Visibility::ABSTRACT;
+        // Add empty array for stmts to generate empty function body
+        $node->stmts = [];
+
+        return $node;
+    }
 }
