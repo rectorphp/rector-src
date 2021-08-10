42333369838f40e0b16783e39b6582db78a1384e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-10 17:42:57 +0700

    [CodeQuality] Skip SimplifyUselessVariableRector on global variable

--- src/NodeAnalyzer/VariableAnalyzer.php
+++ src/NodeAnalyzer/VariableAnalyzer.php
@@ -6,6 +6,7 @@ namespace Rector\Core\NodeAnalyzer;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Stmt\Global_;
 use PhpParser\Node\Stmt\Static_;
 use Rector\Core\PhpParser\Comparing\NodeComparator;
 use Rector\Core\PhpParser\Node\BetterNodeFinder;
@@ -18,17 +19,21 @@ final class VariableAnalyzer
     ) {
     }
 
-    public function isStatic(Variable $variable): bool
+    public function isStaticOrGlobal(Variable $variable): bool
     {
         return (bool) $this->betterNodeFinder->findFirstPreviousOfNode($variable, function (Node $n) use (
             $variable
         ): bool {
-            if (! $n instanceof Static_) {
+            if (! $n instanceof Static_ && ! $n instanceof Global_) {
                 return false;
             }
 
             foreach ($n->vars as $staticVar) {
-                if ($this->nodeComparator->areNodesEqual($staticVar->var, $variable)) {
+                $staticVarVariable = $n instanceof Static_
+                    ? $staticVar->var
+                    : $staticVar;
+
+                if ($this->nodeComparator->areNodesEqual($staticVarVariable, $variable)) {
                     return true;
                 }
             }
