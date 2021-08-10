785f2a2c0020e605d910c1b8161990114b448d6b
Jess Archer <jess@jessarcher.com>
2021-08-10 11:44:19 +1000

    Replicate issue wit isDryRun false

--- tests/Issues/PhpTagsAddedToBlade/Fixture/php_tags_added_to_blade.input.php
+++ tests/Issues/PhpTagsAddedToBlade/Fixture/php_tags_added_to_blade.input.php
@@ -8,14 +8,3 @@ echo isset($value) ? $value : 10;
 @section('content')
     The value is <?= $something ?>
 @endsection
------
-@extends('layout')
-
-<?php
-// Some code to trigger rector to write the file
-echo $value ?? 10;
-?>
-
-@section('content')
-    The value is <?= $something ?>
-@endsection
