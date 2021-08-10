c08b765156d42c5294d349603a4695ae1654938b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-10 16:15:59 +0700

    rollback content to allow re-run test

--- tests/Issues/PhpTagsAddedToBlade/PhpTagsAddedToBladeTest.php
+++ tests/Issues/PhpTagsAddedToBlade/PhpTagsAddedToBladeTest.php
@@ -16,6 +16,7 @@ final class PhpTagsAddedToBladeTest extends AbstractRectorTestCase
     public function test(): void
     {
         $inputFileInfo = new SmartFileInfo(__DIR__ . '/Fixture/php_tags_added_to_blade.input.php');
+        $inputFileInfoContent = file_get_contents($inputFileInfo->getRealPath());
         $expectedFileInfo = new SmartFileInfo(__DIR__ . '/Fixture/php_tags_added_to_blade.expected.php');
 
         $configuration = new Configuration(isDryRun: false);
@@ -24,6 +25,7 @@ final class PhpTagsAddedToBladeTest extends AbstractRectorTestCase
         $file = new File($inputFileInfo, $inputFileInfo->getContents());
         $this->applicationFileProcessor->run([$file], $configuration);
 
+        file_put_contents($inputFileInfo->getRealPath(), $inputFileInfoContent);
         $this->assertStringEqualsFile($expectedFileInfo->getRealPath(), $file->getFileContent());
     }
 
