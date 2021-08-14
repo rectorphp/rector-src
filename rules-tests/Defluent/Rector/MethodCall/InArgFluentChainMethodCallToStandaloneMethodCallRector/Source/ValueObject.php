45280bb6ad9ea1fc3e31e1dd97e331124ee2584f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-14 18:23:39 +0700

    update

---
+++ rules-tests/Defluent/Rector/MethodCall/InArgFluentChainMethodCallToStandaloneMethodCallRector/Source/ValueObject.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\Defluent\Rector\MethodCall\InArgFluentChainMethodCallToStandaloneMethodCallRector\Source;
+
+class ValueObject
+{
+    public $class = FluentClass::class;
+}
