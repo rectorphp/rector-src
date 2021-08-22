e0da25353fda4aaaa32814bec4b3c11ce278e456
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-23 02:22:17 +0700

    [Core] Add Smarty Support for PhpFilesFinder check non-PHP files

--- src/FileSystem/PhpFilesFinder.php
+++ src/FileSystem/PhpFilesFinder.php
@@ -9,6 +9,16 @@ use Symplify\SmartFileSystem\SmartFileInfo;
 
 final class PhpFilesFinder
 {
+    /**
+     * @var string[]
+     */
+    private const NON_PHP_FILE_EXTENSIONS = [
+        // Laravel
+        '.blade.php',
+        // Smarty
+        '.tpl',
+    ];
+
     public function __construct(
         private FilesFinder $filesFinder,
         private UnchangedFilesFilter $unchangedFilesFilter
@@ -23,10 +33,19 @@ final class PhpFilesFinder
     {
         $phpFileInfos = $this->filesFinder->findInDirectoriesAndFiles($paths);
 
-        // filter out non-PHP php files, e.g. blade templates in Laravel
+        // filter out non-PHP files
         $phpFileInfos = array_filter(
             $phpFileInfos,
-            fn (SmartFileInfo $smartFileInfo): bool => ! \str_ends_with($smartFileInfo->getPathname(), '.blade.php')
+            function (SmartFileInfo $smartFileInfo): bool {
+                $pathName = $smartFileInfo->getPathname();
+                foreach (self::NON_PHP_FILE_EXTENSIONS as $nonPHPFileExtension) {
+                    if (str_ends_with($pathName, $nonPHPFileExtension)) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
         );
 
         return $this->unchangedFilesFilter->filterAndJoinWithDependentFileInfos($phpFileInfos);
