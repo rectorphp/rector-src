d4bf0d3d7bb414f7c155dfb7fc1c996688d3e4e5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-23 04:03:00 +0700

    Fixed :tada

--- rules/Php71/Rector/FuncCall/CountOnNullRector.php
+++ rules/Php71/Rector/FuncCall/CountOnNullRector.php
@@ -10,6 +10,7 @@ use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\BinaryOp\BooleanOr;
 use PhpParser\Node\Expr\BinaryOp\Identical;
 use PhpParser\Node\Expr\Cast\Array_;
+use PhpParser\Node\Expr\ClassConstFetch;
 use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Expr\Instanceof_;
 use PhpParser\Node\Expr\Ternary;
@@ -81,6 +82,10 @@ CODE_SAMPLE
         }
 
         $countedNode = $node->args[0]->value;
+        if ($countedNode instanceof ClassConstFetch) {
+            return null;
+        }
+
         if ($this->countableTypeAnalyzer->isCountableType($countedNode)) {
             return null;
         }
