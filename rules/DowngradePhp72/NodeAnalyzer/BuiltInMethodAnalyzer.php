93c069170f0b75d44301617d2e557c77d7ec71b0
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-26 05:54:48 +0700

    [DowngradePhp72] Skip implements built in interface method on DowngradeParameterTypeWideningRector

---
+++ rules/DowngradePhp72/NodeAnalyzer/BuiltInMethodAnalyzer.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DowngradePhp72\NodeAnalyzer;
+
+use PhpParser\Node\Stmt\ClassMethod;
+use PHPStan\Reflection\ClassReflection;
+use Rector\NodeNameResolver\NodeNameResolver;
+
+final class BuiltInMethodAnalyzer
+{
+    public function __construct(private NodeNameResolver $nodeNameResolver)
+    {
+    }
+
+    public function isImplementsBuiltInInterface(ClassReflection $classReflection, ClassMethod $classMethod): bool
+    {
+        $methodName = $this->nodeNameResolver->getName($classMethod);
+        if ($methodName === null) {
+            return false;
+        }
+
+        foreach ($classReflection->getInterfaces() as $interfaceReflection) {
+            if (! $interfaceReflection->hasMethod($methodName)) {
+                continue;
+            }
+
+            return $interfaceReflection->isBuiltIn();
+        }
+
+        return false;
+    }
+}
