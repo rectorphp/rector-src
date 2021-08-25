8c4ca240200b3458649ee4a9484bd9988754c3dc
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-25 09:52:27 +0700

    [DowngradePhp70] Add DowngradeParentTypeDeclarationRector

---
+++ rules/DowngradePhp70/Rector/ClassMethod/DowngradeParentTypeDeclarationRector.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DowngradePhp70\Rector\ClassMethod;
+
+use PhpParser\Node;
+use PhpParser\Node\Name;
+use PhpParser\Node\Stmt\ClassMethod;
+use PHPStan\Reflection\ReflectionProvider;
+use Rector\Core\Rector\AbstractRector;
+use Rector\DowngradePhp71\TypeDeclaration\PhpDocFromTypeDeclarationDecorator;
+use Rector\TypeDeclaration\TypeInferer\ReturnTypeInferer;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\Tests\DowngradePhp70\Rector\ClassMethod\DowngradeParentTypeDeclarationRector\DowngradeParentTypeDeclarationRectorTest
+ */
+final class DowngradeParentTypeDeclarationRector extends AbstractRector
+{
+    public function __construct(
+        private PhpDocFromTypeDeclarationDecorator $phpDocFromTypeDeclarationDecorator,
+        private ReflectionProvider $reflectionProvider,
+        private ReturnTypeInferer $returnTypeInferer
+    ) {
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [ClassMethod::class];
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Remove "parent" return type, add a "@return $this" tag instead',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+class ParentClass
+{
+}
+
+class SomeClass extends ParentClass
+{
+    public function foo(): parent
+    {
+        return $this;
+    }
+}
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+class ParentClass
+{
+}
+
+class SomeClass extends ParentClass
+{
+    /**
+     * @return parent
+     */
+    public function foo()
+    {
+        return $this;
+    }
+}
+CODE_SAMPLE
+                ),
+            ]
+        );
+    }
+
+    /**
+     * @param ClassMethod $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $node->returnType instanceof Name) {
+            return null;
+        }
+
+        if (! $this->nodeNameResolver->isName($node->returnType, 'parent')) {
+            return null;
+        }
+
+        $node->returnType = null;
+        return $node;
+    }
+}
