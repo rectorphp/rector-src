b354dd92e00882612e60ff9f336940de50902d60
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-27 06:33:06 +0700

    final touch

--- rules/DowngradePhp72/NodeAnalyzer/OverrideFromAnonymousClassMethodAnalyzer.php
+++ rules/DowngradePhp72/NodeAnalyzer/OverrideFromAnonymousClassMethodAnalyzer.php
@@ -8,9 +8,11 @@ use PhpParser\Node\Name\FullyQualified;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassLike;
 use PhpParser\Node\Stmt\ClassMethod;
+use PHPStan\Reflection\Php\PhpMethodReflection;
 use PHPStan\Reflection\ReflectionProvider;
 use Rector\Core\NodeAnalyzer\ClassAnalyzer;
 use Rector\NodeNameResolver\NodeNameResolver;
+use Rector\NodeTypeResolver\Node\AttributeKey;
 
 final class OverrideFromAnonymousClassMethodAnalyzer
 {
@@ -40,6 +42,17 @@ final class OverrideFromAnonymousClassMethodAnalyzer
         $classReflection = $this->reflectionProvider->getClass($extendsClass);
         $methodName = $this->nodeNameResolver->getName($classMethod);
 
-        return $classReflection->hasMethod($methodName);
+        if (! $classReflection->hasMethod($methodName)) {
+            return false;
+        }
+
+        $scope = $classMethod->getAttribute(AttributeKey::SCOPE);
+        $method = $classReflection->getMethod($methodName, $scope);
+
+        if (! $method instanceof PhpMethodReflection) {
+            return false;
+        }
+
+        return ! $method->isPrivate();
     }
 }
