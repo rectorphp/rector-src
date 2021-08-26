1aaabd6c009494ded7d2f090063bd5ed78a65e50
GitHub Action <action@github.com>
2021-08-26 12:04:08 +0000

    [ci-review] Rector Rectify

--- rules/DowngradePhp71/TypeDeclaration/PhpDocFromTypeDeclarationDecorator.php
+++ rules/DowngradePhp71/TypeDeclaration/PhpDocFromTypeDeclarationDecorator.php
@@ -127,12 +127,6 @@ final class PhpDocFromTypeDeclarationDecorator
      */
     private function isMatchingType(Type $type, array $requiredTypes): bool
     {
-        foreach ($requiredTypes as $requiredType) {
-            if ($type::class === $requiredType) {
-                return true;
-            }
-        }
-
-        return false;
+        return in_array($type::class, $requiredTypes, true);
     }
 }
