6ba2d7dcae79a0faa55dcfbf2373fabd4d80e2c8
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-30 06:58:22 +0700

    dbeug

--- rules/CodingStyle/Rector/Stmt/NewlineAfterStatementRector.php
+++ rules/CodingStyle/Rector/Stmt/NewlineAfterStatementRector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Rector\CodingStyle\Rector\Stmt;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Scalar\String_;
 use PhpParser\Node\Stmt;
 use PhpParser\Node\Stmt\ClassMethod;
@@ -14,6 +15,9 @@ use PhpParser\Node\Stmt\For_;
 use PhpParser\Node\Stmt\Nop;
 use PhpParser\Node\Stmt\While_;
 use PhpParser\Node\Stmt\Foreach_;
+use PhpParser\Node\Stmt\Property;
+use PhpParser\Node\Stmt\PropertyProperty;
+use PhpParser\NodeTraverser;
 use Rector\Core\Rector\AbstractRector;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
@@ -25,13 +29,15 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 final class NewlineAfterStatementRector extends AbstractRector
 {
     private const STMTS_TO_HAVE_NEXT_NEWLINE = [
-        ClassMethod::class,
-        If_::class,
-        Do_::class,
-        While_::class,
-        For_::class,
-        Foreach_::class,
+        //ClassMethod::class,
+        Property::class,
+        //Do_::class,
+        //While_::class,
+        //For_::class,
+        //Foreach_::class,
     ];
+
+    private array $stmtsHashed = [];
 
     public function getRuleDefinition(): RuleDefinition
     {
@@ -81,25 +87,45 @@ CODE_SAMPLE
      */
     public function refactor(Node $node): ?Node
     {
-        if (! in_array($node::class, self::STMTS_TO_HAVE_NEXT_NEWLINE, true)) {
+        $parent = $node->getAttribute(AttributeKey::PARENT_NODE);
+        if (! $parent instanceof Node) {
             return null;
         }
 
-        $nextNode = $node->getAttribute(AttributeKey::NEXT_NODE);
-        if ($nextNode instanceof Nop) {
-            return null;
-        }
+        $print = $this->betterStandardPrinter->prettyPrint([$parent]);
+        $this->traverseNodesWithCallable($parent, function (Node $subNode) use ($node, $print, $parent): void {
+            if (! in_array($node::class, self::STMTS_TO_HAVE_NEXT_NEWLINE, true)) {
+                return;
+            }
 
-        if (! $nextNode instanceof Nop) {
-            return null;
-        }
+            if ($subNode !== $node) {
+                return;
+            }
 
-        $currentStatement = $node->getAttribute(AttributeKey::CURRENT_STATEMENT);
-        if ($currentStatement instanceof Nop) {
-            return null;
-        }
+            $hash = spl_object_hash($node);
+            if (isset($this->stmtsHashed[$hash])) {
+                return;
+            }
 
-        $this->addNodeAfterNode(new Nop(), $node);
+            $nextNode = $subNode->getAttribute(AttributeKey::NEXT_NODE);
+            if ($nextNode instanceof Nop) {
+                return;
+            }
+
+            if (! $nextNode instanceof Node) {
+                return;
+            }
+
+            $parent = $subNode->getAttribute(AttributeKey::PARENT_NODE);
+            $newPrint = $this->betterStandardPrinter->prettyPrint([$parent]);
+            if ($newPrint !== $print) {
+                return;
+            }
+
+            $this->stmtsHashed[$hash] = true;
+            $this->addNodeAfterNode(new Nop(), $subNode);
+        });
+
         return $node;
     }
 }
