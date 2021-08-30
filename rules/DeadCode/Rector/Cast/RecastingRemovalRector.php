b3a5584f9dd781a0d34f41e3bc9bcd2e0a037250
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-30 12:49:27 +0700

    final touch

--- rules/DeadCode/Rector/Cast/RecastingRemovalRector.php
+++ rules/DeadCode/Rector/Cast/RecastingRemovalRector.php
@@ -128,7 +128,7 @@ CODE_SAMPLE
 
             // need to UnionType check due rectify with RecastingRemovalRector + CountOnNullRector
             // cause add (array) cast on $node->args
-            // on union $node type on combined with
+            // on union $node types FuncCall|MethodCall|StaticCall
             $varType = $this->nodeTypeResolver->resolve($expr->var);
             return ! $varType instanceof UnionType;
         }
