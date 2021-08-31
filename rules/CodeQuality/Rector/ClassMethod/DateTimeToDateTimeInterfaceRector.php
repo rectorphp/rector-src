f89097326e34fd48444c791fc0100b778a3f206a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-31 10:45:00 +0700

    comment

--- rules/CodeQuality/Rector/ClassMethod/DateTimeToDateTimeInterfaceRector.php
+++ rules/CodeQuality/Rector/ClassMethod/DateTimeToDateTimeInterfaceRector.php
@@ -97,6 +97,9 @@ CODE_SAMPLE
     {
         if ($node instanceof ClassMethod) {
             $this->refactorClassMethod($node);
+
+            // @see https://github.com/rectorphp/rector-src/pull/794
+            // avoid duplicated ifs and returns when combined with ChangeOrIfReturnToEarlyReturnRector and ChangeAndIfToEarlyReturnRector
             return null;
         }
 
