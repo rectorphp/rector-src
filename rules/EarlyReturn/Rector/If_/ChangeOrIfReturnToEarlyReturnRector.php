10e86a3f7398842dc2a84193c9ff24029561ef2e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-31 19:35:56 +0700

    phpstan

--- rules/EarlyReturn/Rector/If_/ChangeOrIfReturnToEarlyReturnRector.php
+++ rules/EarlyReturn/Rector/If_/ChangeOrIfReturnToEarlyReturnRector.php
@@ -91,21 +91,18 @@ CODE_SAMPLE
         }
 
         $nextNode = $node->getAttribute(AttributeKey::NEXT_NODE);
-        if ($nextNode === null) {
-            return null;
-        }
-
-        if ($nextNode instanceof Return_ && $nextNode->expr === null) {
-            return null;
-        }
-
-        // avoid repetitive ifs combined with other rules
-        if ($this->nodeComparator->areNodesEqual($nextNode->expr, $node->stmts[0]->expr)) {
+        if ($this->shouldSkip($node, $nextNode)) {
             return null;
         }
 
         /** @var Return_ $return */
         $return = $node->stmts[0];
+
+        // avoid repetitive ifs combined with other rules
+        if ($this->nodeComparator->areNodesEqual($nextNode->expr, $return->expr)) {
+            return null;
+        }
+
         $ifs = $this->createMultipleIfs($node->cond, $return, []);
 
         foreach ($ifs as $key => $if) {
@@ -118,6 +115,15 @@ CODE_SAMPLE
 
         $this->removeNode($node);
         return $node;
+    }
+
+    private function shouldSkip(If_ $if, ?Node $nextNode): bool
+    {
+        if ($nextNode === null) {
+            return true;
+        }
+
+        return $nextNode instanceof Return_ && $nextNode->expr === null;
     }
 
     /**
