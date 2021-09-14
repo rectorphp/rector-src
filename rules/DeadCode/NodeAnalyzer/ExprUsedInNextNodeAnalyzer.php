41a0a27f825448940502a08ae6680fdcb2cd54cf
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-09-15 01:36:03 +0700

    fix

--- rules/DeadCode/NodeAnalyzer/ExprUsedInNextNodeAnalyzer.php
+++ rules/DeadCode/NodeAnalyzer/ExprUsedInNextNodeAnalyzer.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace Rector\DeadCode\NodeAnalyzer;
 
 use PhpParser\Node;
+use PhpParser\Node\Arg;
 use PhpParser\Node\Expr;
+use PhpParser\Node\Name;
 use Rector\Core\PhpParser\Node\BetterNodeFinder;
+use Rector\NodeTypeResolver\Node\AttributeKey;
+use PHPStan\Analyser\Scope;
 
 final class ExprUsedInNextNodeAnalyzer
 {
@@ -16,11 +20,26 @@ final class ExprUsedInNextNodeAnalyzer
     ) {
     }
 
-    public function isUsed(Expr $expr): bool
+    /**
+     * $isCheckNameScope is used to whether to check scope of Name that may be renamed
+     * @see https://github.com/rectorphp/rector/issues/6675
+     */
+    public function isUsed(Expr $expr, bool $isCheckNameScope = false): bool
     {
         return (bool) $this->betterNodeFinder->findFirstNext(
             $expr,
-            fn (Node $node): bool => $this->exprUsedInNodeAnalyzer->isUsed($node, $expr)
+            function (Node $node) use ($expr, $isCheckNameScope): bool {
+                if ($isCheckNameScope && $node instanceof Name) {
+                    $scope = $node->getAttribute(AttributeKey::SCOPE);
+                    $resolvedName = $node->getAttribute(AttributeKey::RESOLVED_NAME);
+
+                    if (! $scope instanceof Scope && ! $resolvedName instanceof Name) {
+                        return true;
+                    }
+                }
+
+                return $this->exprUsedInNodeAnalyzer->isUsed($node, $expr);
+            }
         );
     }
 }
