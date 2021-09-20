bb63ce67ca99ca7426b308241985cdbaf43b5837
GitHub Action <action@github.com>
2021-09-20 16:58:17 +0000

    [ci-review] Rector Rectify

--- packages/NodeTypeResolver/NodeTypeCorrector/PregMatchTypeCorrector.php
+++ packages/NodeTypeResolver/NodeTypeCorrector/PregMatchTypeCorrector.php
@@ -57,9 +57,11 @@ final class PregMatchTypeCorrector
             if (! $this->nodeNameResolver->isNames($funcCallNode, ['preg_match', 'preg_match_all'])) {
                 continue;
             }
+
             if (! isset($funcCallNode->args[2])) {
                 continue;
             }
+
             if (! $funcCallNode->args[2] instanceof Arg) {
                 continue;
             }
