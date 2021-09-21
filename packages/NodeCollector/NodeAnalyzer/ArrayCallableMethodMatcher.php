57bfc190a5cb3948788ff106178c0be633eea45f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-09-21 23:51:15 +0700

    Fixed :tada:

--- packages/NodeCollector/NodeAnalyzer/ArrayCallableMethodMatcher.php
+++ packages/NodeCollector/NodeAnalyzer/ArrayCallableMethodMatcher.php
@@ -48,27 +48,30 @@ final class ArrayCallableMethodMatcher
 
         /** @var ArrayItem[] $items */
         $items = $array->items;
-        $secondItemValue = $items[1]->value;
+        $values = $this->valueResolver->getValue($array);
 
-        if (! $secondItemValue instanceof String_) {
+        if ($values === []) {
             return $array;
         }
 
-        if ($this->shouldSkipAssociativeArray($array)) {
+        if ($this->shouldSkipAssociativeArray($values)) {
             return null;
         }
 
         // $this, self, static, FQN
         $firstItemValue = $items[0]->value;
 
-        // static ::class reference?
-        if ($firstItemValue instanceof ClassConstFetch) {
-            $calleeType = $this->resolveClassConstFetchType($firstItemValue);
-        } else {
-            $calleeType = $this->nodeTypeResolver->resolve($firstItemValue);
-        }
+        $calleeType = $firstItemValue instanceof ClassConstFetch
+            // static ::class reference?
+            ? $this->resolveClassConstFetchType($firstItemValue)
+            : $this->nodeTypeResolver->resolve($firstItemValue);
 
         if (! $calleeType instanceof TypeWithClassName) {
+            return null;
+        }
+
+        $secondItemValue = $items[1]->value;
+        if (! $secondItemValue instanceof String_) {
             return null;
         }
 
@@ -95,9 +98,11 @@ final class ArrayCallableMethodMatcher
         return $array->items[1] === null;
     }
 
-    private function shouldSkipAssociativeArray(Array_ $array): bool
+    /**
+     * @param mixed $values
+     */
+    private function shouldSkipAssociativeArray($values): bool
     {
-        $values = $this->valueResolver->getValue($array);
         if (! is_array($values)) {
             return false;
         }
