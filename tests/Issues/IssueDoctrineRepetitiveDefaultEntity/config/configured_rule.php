833af1fe6fa9421eca6df869ffc3fe9faf480406
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-09-23 17:33:56 +0700

    failing test case for https://github.com/rectorphp/rector/issues/6697

---
+++ tests/Issues/IssueDoctrineRepetitiveDefaultEntity/config/configured_rule.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Rector\Doctrine\Rector\Class_\InitializeDefaultEntityCollectionRector;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
+
+    $services = $containerConfigurator->services();
+    $services->set(InitializeDefaultEntityCollectionRector::class);
+};
