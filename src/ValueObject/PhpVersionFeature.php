2c065340e8173176ce033135e128daecea6e7413
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-09-24 14:49:45 +0700

    apply to AssignArrayToStringRector

--- src/ValueObject/PhpVersionFeature.php
+++ src/ValueObject/PhpVersionFeature.php
@@ -195,6 +195,11 @@ final class PhpVersionFeature
     /**
      * @var int
      */
+    public const NO_ASSIGN_ARRAY_TO_STRING = PhpVersion::PHP_71;
+
+    /**
+     * @var int
+     */
     public const OBJECT_TYPE = PhpVersion::PHP_72;
 
     /**
