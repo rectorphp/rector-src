c5e87847a9f29692f6ba0c576aaf50b52be84f23
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-09-24 14:51:20 +0700

    apply to BinaryOpBetweenNumberAndStringRector

--- src/ValueObject/PhpVersionFeature.php
+++ src/ValueObject/PhpVersionFeature.php
@@ -200,6 +200,11 @@ final class PhpVersionFeature
     /**
      * @var int
      */
+    public const BINARY_OP_NUMBER_STRING = PhpVersion::PHP_71;
+
+    /**
+     * @var int
+     */
     public const OBJECT_TYPE = PhpVersion::PHP_72;
 
     /**
