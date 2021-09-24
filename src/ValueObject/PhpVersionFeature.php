94413dddbbc98e11d23af1a42391b390faa2beaa
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-09-24 15:56:35 +0700

    apply to StrContainsRector

--- src/ValueObject/PhpVersionFeature.php
+++ src/ValueObject/PhpVersionFeature.php
@@ -461,6 +461,11 @@ final class PhpVersionFeature
     public const STR_STARTS_WITH = PhpVersion::PHP_80;
 
     /**
+     * @var int
+     */
+    public const STR_CONTAINS = PhpVersion::PHP_80;
+
+    /**
      * @see https://wiki.php.net/rfc/noreturn_type
      * @var int
      */
