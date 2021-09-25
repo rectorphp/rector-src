534fe4a45a422d04f8635fdc03a7966140ad5042
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-09-25 17:19:35 +0700

    Arg check on StrposMatchAndRefactor

--- rules/Php80/MatchAndRefactor/StrStartsWithMatchAndRefactor/StrposMatchAndRefactor.php
+++ rules/Php80/MatchAndRefactor/StrStartsWithMatchAndRefactor/StrposMatchAndRefactor.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Rector\Php80\MatchAndRefactor\StrStartsWithMatchAndRefactor;
 
 use PhpParser\Node;
+use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\BinaryOp;
 use PhpParser\Node\Expr\BinaryOp\Identical;
 use PhpParser\Node\Expr\BinaryOp\NotIdentical;
 use PhpParser\Node\Expr\BooleanNot;
 use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Name;
+use Rector\Core\NodeAnalyzer\ArgsAnalyzer;
 use Rector\Core\PhpParser\Node\Value\ValueResolver;
 use Rector\NodeNameResolver\NodeNameResolver;
 use Rector\Php80\Contract\StrStartWithMatchAndRefactorInterface;
@@ -22,7 +24,8 @@ final class StrposMatchAndRefactor implements StrStartWithMatchAndRefactorInterf
     public function __construct(
         private NodeNameResolver $nodeNameResolver,
         private ValueResolver $valueResolver,
-        private StrStartsWithFuncCallFactory $strStartsWithFuncCallFactory
+        private StrStartsWithFuncCallFactory $strStartsWithFuncCallFactory,
+        private ArgsAnalyzer $argsAnalyzer
     ) {
     }
 
@@ -34,32 +37,60 @@ final class StrposMatchAndRefactor implements StrStartWithMatchAndRefactorInterf
         $isPositive = $binaryOp instanceof Identical;
 
         if ($binaryOp->left instanceof FuncCall && $this->nodeNameResolver->isName($binaryOp->left, 'strpos')) {
-            if (! $this->valueResolver->isValue($binaryOp->right, 0)) {
-                return null;
-            }
-
-            /** @var FuncCall $funcCall */
-            $funcCall = $binaryOp->left;
-            $haystack = $funcCall->args[0]->value;
-            $needle = $funcCall->args[1]->value;
-
-            return new StrStartsWith($funcCall, $haystack, $needle, $isPositive);
+            return $this->processBinaryOpLeft($binaryOp, $isPositive);
         }
 
         if ($binaryOp->right instanceof FuncCall && $this->nodeNameResolver->isName($binaryOp->right, 'strpos')) {
-            if (! $this->valueResolver->isValue($binaryOp->left, 0)) {
-                return null;
-            }
-
-            /** @var FuncCall $funcCall */
-            $funcCall = $binaryOp->right;
-            $haystack = $funcCall->args[0]->value;
-            $needle = $funcCall->args[1]->value;
-
-            return new StrStartsWith($funcCall, $haystack, $needle, $isPositive);
+            return $this->processBinaryOpRight($binaryOp, $isPositive);
         }
 
         return null;
+    }
+
+    private function processBinaryOpLeft(BinaryOp $binaryOp, bool $isPositive): ?StrStartsWith
+    {
+        if (! $this->valueResolver->isValue($binaryOp->right, 0)) {
+            return null;
+        }
+
+        /** @var FuncCall $funcCall */
+        $funcCall = $binaryOp->left;
+        /** @var FuncCall $funcCall */
+        $funcCall = $binaryOp->left;
+        if (! $this->argsAnalyzer->isArgsInstanceInArgsPositions($funcCall->args, [0, 1])) {
+            return null;
+        }
+
+        /** @var Arg $firstArg */
+        $firstArg = $funcCall->args[0];
+        $haystack = $firstArg->value;
+        /** @var Arg $secondArg */
+        $secondArg = $funcCall->args[1];
+        $needle = $secondArg->value;
+
+        return new StrStartsWith($funcCall, $haystack, $needle, $isPositive);
+    }
+
+    private function processBinaryOpRight(BinaryOp $binaryOp, bool $isPositive): ?StrStartsWith
+    {
+        if (! $this->valueResolver->isValue($binaryOp->left, 0)) {
+            return null;
+        }
+
+        /** @var FuncCall $funcCall */
+        $funcCall = $binaryOp->right;
+        if (! $this->argsAnalyzer->isArgsInstanceInArgsPositions($funcCall->args, [0, 1])) {
+            return null;
+        }
+
+        /** @var Arg $firstArg */
+        $firstArg = $funcCall->args[0];
+        $haystack = $firstArg->value;
+        /** @var Arg $secondArg */
+        $secondArg = $funcCall->args[1];
+        $needle = $secondArg->value;
+
+        return new StrStartsWith($funcCall, $haystack, $needle, $isPositive);
     }
 
     /**
