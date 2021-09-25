13ce5cc597d33c12a963662b602ee2b27b668afa
GitHub Action <action@github.com>
2021-09-25 10:23:11 +0000

    [ci-review] Rector Rectify

--- rules/Php80/MatchAndRefactor/StrStartsWithMatchAndRefactor/StrposMatchAndRefactor.php
+++ rules/Php80/MatchAndRefactor/StrStartsWithMatchAndRefactor/StrposMatchAndRefactor.php
@@ -39,12 +39,24 @@ final class StrposMatchAndRefactor implements StrStartWithMatchAndRefactorInterf
         if ($binaryOp->left instanceof FuncCall && $this->nodeNameResolver->isName($binaryOp->left, 'strpos')) {
             return $this->processBinaryOpLeft($binaryOp, $isPositive);
         }
-
-        if ($binaryOp->right instanceof FuncCall && $this->nodeNameResolver->isName($binaryOp->right, 'strpos')) {
-            return $this->processBinaryOpRight($binaryOp, $isPositive);
+        if (! $binaryOp->right instanceof FuncCall) {
+            return null;
         }
+        if (! $this->nodeNameResolver->isName($binaryOp->right, 'strpos')) {
+            return null;
+        }
+        return $this->processBinaryOpRight($binaryOp, $isPositive);
+    }
 
-        return null;
+    /**
+     * @return FuncCall|BooleanNot
+     */
+    public function refactorStrStartsWith(StrStartsWith $strStartsWith): Node
+    {
+        $strposFuncCall = $strStartsWith->getFuncCall();
+        $strposFuncCall->name = new Name('str_starts_with');
+
+        return $this->strStartsWithFuncCallFactory->createStrStartsWith($strStartsWith);
     }
 
     private function processBinaryOpLeft(BinaryOp $binaryOp, bool $isPositive): ?StrStartsWith
@@ -55,7 +67,6 @@ final class StrposMatchAndRefactor implements StrStartWithMatchAndRefactorInterf
 
         /** @var FuncCall $funcCall */
         $funcCall = $binaryOp->left;
-        /** @var FuncCall $funcCall */
         $funcCall = $binaryOp->left;
         if (! $this->argsAnalyzer->isArgsInstanceInArgsPositions($funcCall->args, [0, 1])) {
             return null;
@@ -91,16 +102,5 @@ final class StrposMatchAndRefactor implements StrStartWithMatchAndRefactorInterf
         $needle = $secondArg->value;
 
         return new StrStartsWith($funcCall, $haystack, $needle, $isPositive);
-    }
-
-    /**
-     * @return FuncCall|BooleanNot
-     */
-    public function refactorStrStartsWith(StrStartsWith $strStartsWith): Node
-    {
-        $strposFuncCall = $strStartsWith->getFuncCall();
-        $strposFuncCall->name = new Name('str_starts_with');
-
-        return $this->strStartsWithFuncCallFactory->createStrStartsWith($strStartsWith);
     }
 }
