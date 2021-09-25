ab315f826697b8af3b7c7af8f9e6a2e1fb19f92c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-09-25 17:06:09 +0700

    Arg check on ChangeDiffForHumansArgsRector

--- rules/Carbon/Rector/MethodCall/ChangeDiffForHumansArgsRector.php
+++ rules/Carbon/Rector/MethodCall/ChangeDiffForHumansArgsRector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Rector\Carbon\Rector\MethodCall;
 
 use PhpParser\Node;
+use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\MethodCall;
 use PHPStan\Type\ObjectType;
 use Rector\Core\Rector\AbstractRector;
@@ -78,7 +79,7 @@ CODE_SAMPLE
             return null;
         }
 
-        if (! isset($node->args[1])) {
+        if (! isset($node->args[1]) || ! $node->args[1] instanceof Arg) {
             return null;
         }
 
