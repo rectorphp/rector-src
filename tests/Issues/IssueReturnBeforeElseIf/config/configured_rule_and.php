7fb183b4f06b988adf43e43cb1a347c8439ba255
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-10-06 08:50:38 +0700

    revert

--- tests/Issues/IssueReturnBeforeElseIf/config/configured_rule_and.php
+++ tests/Issues/IssueReturnBeforeElseIf/config/configured_rule_and.php
@@ -8,6 +8,6 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
-    //$services->set(ChangeAndIfToEarlyReturnRector::class);
+    $services->set(ChangeAndIfToEarlyReturnRector::class);
     $services->set(RemoveAlwaysElseRector::class);
 };
