048bbb149a37b1336de352ba27eb31c0e0b757a6
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-10-06 16:39:10 +0700

    phpstan

--- src/Validation/EmptyConfigurableRectorChecker.php
+++ src/Validation/EmptyConfigurableRectorChecker.php
@@ -25,41 +25,7 @@ final class EmptyConfigurableRectorChecker
      */
     public function check(array $rectors): void
     {
-        $emptyConfigurableRectors = [];
-        foreach ($rectors as $rector) {
-            if (! $rector instanceof ConfigurableRectorInterface) {
-                continue;
-            }
-
-            // it seems always loaded
-            if ($rector instanceof RenameClassNonPhpRector) {
-                continue;
-            }
-
-            $ruleDefinition = $rector->getRuleDefinition();
-
-            /** @var CodeSampleInterface[] $codeSamples */
-            $codeSamples    = $ruleDefinition->getCodeSamples();
-            foreach ($codeSamples as $codeSample) {
-                $configuration = $codeSample->getConfiguration();
-                foreach ($configuration as $key => $config) {
-                    if (is_array($config)) {
-                        try {
-                            $key   = $this->propertyNaming->underscoreToName($key);
-                            $value = $this->privatesAccessor->getPrivateProperty($rector, $key);
-
-                            if (is_array($value) && $value === []) {
-                                $emptyConfigurableRectors[] = $rector;
-                                continue 3;
-                            }
-                        } catch (Throwable) {
-                            continue;
-                        }
-                    }
-                }
-            }
-        }
-
+        $emptyConfigurableRectors = $this->resolveEmptyConfigurable($rectors);
 
         if ($emptyConfigurableRectors === []) {
             return;
@@ -69,6 +35,64 @@ final class EmptyConfigurableRectorChecker
         $this->reportEmptyConfigurableMessage($emptyConfigurableRectors);
     }
 
+    /**
+     * @param RectorInterface[] $rectors
+     * @return RectorInterface[]
+     */
+    private function resolveEmptyConfigurable(array $rectors): array
+    {
+        $emptyConfigurableRectors = [];
+        foreach ($rectors as $rector) {
+            if ($this->shouldSkip($rector)) {
+                continue;
+            }
+
+            $ruleDefinition = $rector->getRuleDefinition();
+
+            /** @var CodeSampleInterface[] $codeSamples */
+            $codeSamples    = $ruleDefinition->getCodeSamples();
+            foreach ($codeSamples as $codeSample) {
+                $configuration = $codeSample->getConfiguration();
+                if (! is_array($configuration)) {
+                    continue;
+                }
+
+                foreach ($configuration as $key => $config) {
+                    if (! is_array($config)) {
+                        continue;
+                    }
+
+                    try {
+                        $key   = $this->propertyNaming->underscoreToName($key);
+                        $value = $this->privatesAccessor->getPrivateProperty($rector, $key);
+
+                        if (is_array($value) && $value === []) {
+                            $emptyConfigurableRectors[] = $rector;
+                            continue 3;
+                        }
+                    } catch (Throwable) {
+                        continue;
+                    }
+                }
+            }
+        }
+
+        return $emptyConfigurableRectors;
+    }
+
+    private function shouldSkip(RectorInterface $rector): bool
+    {
+        if (! $rector instanceof ConfigurableRectorInterface) {
+            return true;
+        }
+
+        // it seems always loaded
+        return $rector instanceof RenameClassNonPhpRector;
+    }
+
+    /**
+     * @param RectorInterface[] $emptyConfigurableRectors
+     */
     private function reportWarningMessage(array $emptyConfigurableRectors): void
     {
         $warningMessage = sprintf(
@@ -79,6 +103,9 @@ final class EmptyConfigurableRectorChecker
         $this->symfonyStyle->warning($warningMessage);
     }
 
+    /**
+     * @param RectorInterface[] $emptyConfigurableRectors
+     */
     private function reportEmptyConfigurableMessage(array $emptyConfigurableRectors): void
     {
         if (! $this->symfonyStyle->isVerbose()) {
@@ -88,7 +115,7 @@ final class EmptyConfigurableRectorChecker
         foreach ($emptyConfigurableRectors as $emptyConfigurableRector) {
             $shortRectorClass = Strings::after($emptyConfigurableRector::class, '\\', -1);
 
-            $rectorMessage = sprintf(' * %s', $shortRectorClass);
+            $rectorMessage = ' * ' . $shortRectorClass;
             $this->symfonyStyle->writeln($rectorMessage);
         }
     }
