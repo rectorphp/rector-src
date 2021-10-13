9ae376e186fd346ed882e121f76f82f423bf1238
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-10-13 15:19:45 +0700

    add for Index

--- rules-tests/Php80/Rector/Class_/AnnotationToAttributeRector/config/configured_rule.php
+++ rules-tests/Php80/Rector/Class_/AnnotationToAttributeRector/config/configured_rule.php
@@ -23,6 +23,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
                 new AnnotationToAttribute(Response::class),
                 new AnnotationToAttribute('Symfony\Component\Routing\Annotation\Route'),
                 new AnnotationToAttribute('Doctrine\ORM\Mapping\Entity'),
+                new AnnotationToAttribute('Doctrine\ORM\Mapping\Index'),
                 new AnnotationToAttribute('Doctrine\ORM\Mapping\UniqueConstraint'),
             ]),
         ]]);
