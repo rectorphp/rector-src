191f3ee8eaa65a52c5764b8dfea86406caa761a7
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-10-26 17:28:17 +0700

    [Php80] Add Php8ResourceToObjectRector

---
+++ rules/Php80/Rector/FuncCall/Php8ResourceToObjectRector.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Php80\Rector\FuncCall;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\FuncCall;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Core\ValueObject\PhpVersionFeature;
+use Rector\VersionBonding\Contract\MinPhpVersionInterface;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @changelog https://www.php.net/manual/en/migration80.incompatible.php#migration80.incompatible.resource2object
+ *
+ * @see \Rector\Tests\Php80\Rector\FuncCall\Php8ResourceToObjectRector\Php8ResourceToObjectRector
+ */
+final class Php8ResourceToObjectRector extends AbstractRector implements MinPhpVersionInterface
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Change is_resource() to instanceof Object',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run()
+    {
+        $ch = curl_init();
+        is_resource($ch);
+    }
+}
+CODE_SAMPLE
+,
+                    <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run()
+    {
+        $ch = curl_init();
+        $ch instanceof \CurlHandle;
+    }
+}
+CODE_SAMPLE
+                ),
+            ]
+        );
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [FuncCall::class];
+    }
+
+    /**
+     * @param FuncCall $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+
+    }
+
+    public function provideMinPhpVersion(): int
+    {
+        return PhpVersionFeature::PHP8_RESOURCE_TO_OBJECT;
+    }
+}
