64ac93d123881b9e29520042fd50407eff863f9b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-10-26 18:05:28 +0700

    apply with check is_resource

--- rules/Php80/Rector/FuncCall/Php8ResourceToObjectRector.php
+++ rules/Php80/Rector/FuncCall/Php8ResourceToObjectRector.php
@@ -5,9 +5,14 @@ declare(strict_types=1);
 namespace Rector\Php80\Rector\FuncCall;
 
 use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Expr\Instanceof_;
+use PhpParser\Node\Name\FullyQualified;
 use Rector\Core\Rector\AbstractRector;
 use Rector\Core\ValueObject\PhpVersionFeature;
+use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\VersionBonding\Contract\MinPhpVersionInterface;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
@@ -19,6 +24,13 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
  */
 final class Php8ResourceToObjectRector extends AbstractRector implements MinPhpVersionInterface
 {
+    /**
+     * @var array
+     */
+    private const COLLECTION_FUNCTION_TO_RETURN_OBJECT = [
+        'curl_init' => 'CurlHandle',
+    ];
+
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition(
@@ -62,8 +74,45 @@ CODE_SAMPLE
     /**
      * @param FuncCall $node
      */
-    public function refactor(Node $node): void
+    public function refactor(Node $node): ?Node
     {
+        if (! $this->nodeNameResolver->isName($node, 'is_resource')) {
+            return null;
+        }
+
+        if (! isset($node->args[0])) {
+            return null;
+        }
+
+        if (! $node->args[0] instanceof Arg) {
+            return null;
+        }
+
+        $objectInstanceCheck = null;
+        $assign = $this->betterNodeFinder->findFirstPreviousOfNode($node, function (Node $subNode) use (&$objectInstanceCheck): bool {
+            if (! $subNode instanceof Assign) {
+                return false;
+            }
+
+            if (! $subNode->expr instanceof FuncCall) {
+                return false;
+            }
+
+            foreach (self::COLLECTION_FUNCTION_TO_RETURN_OBJECT as $key => $value) {
+                if ($this->nodeNameResolver->isName($subNode->expr, $key)) {
+                    $objectInstanceCheck = $value;
+                    return true;
+                }
+            }
+
+            return false;
+        });
+
+        if (! $assign instanceof Assign) {
+            return null;
+        }
+
+        return new Instanceof_($node->args[0], new FullyQualified($objectInstanceCheck));
     }
 
     public function provideMinPhpVersion(): int
