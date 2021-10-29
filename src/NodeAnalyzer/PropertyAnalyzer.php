0a3ad78c8ba6e05406b9015ff63899b2718e8e91
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-10-29 22:13:32 +0700

    final touch: clean up

--- src/NodeAnalyzer/PropertyAnalyzer.php
+++ src/NodeAnalyzer/PropertyAnalyzer.php
@@ -7,7 +7,6 @@ namespace Rector\Core\NodeAnalyzer;
 use PhpParser\Node\Stmt\Property;
 use PHPStan\Type\CallableType;
 use PHPStan\Type\NullType;
-use PHPStan\Type\Type;
 use PHPStan\Type\UnionType;
 use Rector\NodeTypeResolver\NodeTypeResolver;
 
@@ -29,20 +28,15 @@ final class PropertyAnalyzer
         foreach ($types as $type) {
             // when types === 2 and nullable, it already handled in Nullable type check
             // to convert with ?TheType
-            if ($totalTypes > 2 && $type instanceof NullType) {
+            if ($totalTypes !== 2 && $type instanceof NullType) {
                 return true;
             }
 
-            if ($this->isCallableType($type)) {
+            if ($type instanceof CallableType) {
                 return true;
             }
         }
 
         return false;
-    }
-
-    private function isCallableType(Type $type): bool
-    {
-        return $type instanceof CallableType;
     }
 }
