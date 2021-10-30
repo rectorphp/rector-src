9ec21d7ba0af8754a2a43b13d72ead5b611f5691
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-10-30 20:20:05 +0700

    [Php 81] Add Php81ResourceReturnToObjectRector

---
+++ rules/Php81/Rector/FuncCall/Php8ResourceReturnToObjectRector.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Php81\Rector\FuncCall;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\BinaryOp\BooleanOr;
+use PhpParser\Node\Expr\FuncCall;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Core\ValueObject\PhpVersionFeature;
+use Rector\VersionBonding\Contract\MinPhpVersionInterface;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @changelog https://www.php.net/manual/en/migration81.incompatible.php#migration81.incompatible.resource2object
+ *
+ * @see \Rector\Tests\Php80\Rector\FuncCall\Php81ResourceReturnToObjectRector\Php81ResourceReturnToObjectRectorTest
+ */
+final class Php81ResourceReturnToObjectRector extends AbstractRector implements MinPhpVersionInterface
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Change is_resource() to instanceof Object',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run()
+    {
+        $f = finfo_open();
+        is_resource($f);
+    }
+}
+CODE_SAMPLE
+,
+                    <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run()
+    {
+        $f = finfo_open();
+        $f instanceof \finfo;
+    }
+}
+CODE_SAMPLE
+                ),
+            ]
+        );
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [FuncCall::class, BooleanOr::class];
+    }
+
+    /**
+     * @param FuncCall|BooleanOr $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        return $node;
+    }
+
+    public function provideMinPhpVersion(): int
+    {
+        return PhpVersionFeature::PHP81_RESOURCE_TO_OBJECT;
+    }
+}
