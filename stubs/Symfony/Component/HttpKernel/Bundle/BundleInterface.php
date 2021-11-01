9d304586bee3455bdd606fd3d240ce7578ba2412
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-11-01 22:56:11 +0700

    more clean up

--- stubs/Symfony/Component/HttpKernel/Bundle/BundleInterface.php
+++ stubs/Symfony/Component/HttpKernel/Bundle/BundleInterface.php
@@ -8,17 +8,7 @@ if (interface_exists('Symfony\Component\HttpKernel\Bundle\BundleInterface')) {
     return;
 }
 
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-
-interface BundleInterface extends ContainerAwareInterface
+interface BundleInterface
 {
-    public function boot();
-    public function shutdown();
-    public function build(ContainerBuilder $container);
-    public function getContainerExtension();
-    public function getName();
-    public function getNamespace();
-    public function getPath();
 }
 
