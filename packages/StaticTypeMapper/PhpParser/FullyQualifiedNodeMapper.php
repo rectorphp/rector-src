f3bb9ad8756092733a1bb0d08fd327f5767a58d7
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-11-02 23:49:39 +0700

    Revert "rectify"
    This reverts commit 92156c506fd9de911e3d599efd761ff2f391a00d.

--- packages/StaticTypeMapper/PhpParser/FullyQualifiedNodeMapper.php
+++ packages/StaticTypeMapper/PhpParser/FullyQualifiedNodeMapper.php
@@ -6,7 +6,11 @@ namespace Rector\StaticTypeMapper\PhpParser;
 
 use PhpParser\Node;
 use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Param;
 use PHPStan\Type\Type;
+use Rector\CodingStyle\ClassNameImport\UsedImportsResolver;
+use Rector\Core\Provider\CurrentFileProvider;
+use Rector\Core\ValueObject\Application\File;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\NodeTypeResolver\NodeTypeResolver;
 use Rector\StaticTypeMapper\Contract\PhpParser\PhpParserNodeMapperInterface;
@@ -17,6 +21,12 @@ use Symfony\Contracts\Service\Attribute\Required;
 final class FullyQualifiedNodeMapper implements PhpParserNodeMapperInterface
 {
     private NodeTypeResolver $nodeTypeResolver;
+
+    public function __construct(
+        private CurrentFileProvider $currentFileProvider,
+        private UsedImportsResolver $usedImportsResolver
+    ) {
+    }
 
     #[Required]
     public function autowireFullyQualifiedNodeMapper(NodeTypeResolver $nodeTypeResolver): void
@@ -52,6 +62,41 @@ final class FullyQualifiedNodeMapper implements PhpParserNodeMapperInterface
         }
 
         return new FullyQualifiedObjectType($fullyQualifiedName);
+    }
+
+    private function resolvePossibleAliasedObjectType(FullyQualified $fullyQualified): ?AliasedObjectType
+    {
+        $file = $this->currentFileProvider->getFile();
+        if (! $file instanceof File) {
+            return null;
+        }
+
+        $oldTokens = $file->getOldTokens();
+        $startTokenPos = $fullyQualified->getStartTokenPos();
+
+        if (! isset($oldTokens[$startTokenPos][1])) {
+            return null;
+        }
+
+        $type = $oldTokens[$startTokenPos][1];
+        if (str_contains($type, '\\')) {
+            return null;
+        }
+
+        $objectTypes = $this->usedImportsResolver->resolveForNode($fullyQualified);
+        foreach ($objectTypes as $objectType) {
+            if (! $objectType instanceof AliasedObjectType) {
+                continue;
+            }
+
+            if ($objectType->getClassName() !== $type) {
+                continue;
+            }
+
+            return $objectType;
+        }
+
+        return null;
     }
 
     private function isAliasedName(string $originalName, string $fullyQualifiedName): bool
