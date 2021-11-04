059069e4dc8c2e90bb205e1bdcf65e1cd9c4093d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-11-04 14:02:08 +0700

    define src path

--- build/target-repository/e2e/define-constant/rector.php
+++ build/target-repository/e2e/define-constant/rector.php
@@ -2,7 +2,10 @@
 
 declare(strict_types=1);
 
+use Rector\Core\Configuration\Option;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::SOURCE, [__DIR__ . '/src']);
 };
