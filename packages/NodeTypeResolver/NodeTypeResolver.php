ce25511d3f78403100175204c1029b787591d9a3
GitHub Action <action@github.com>
2021-11-12 12:52:03 +0000

    [ci-review] Rector Rectify

--- packages/NodeTypeResolver/NodeTypeResolver.php
+++ packages/NodeTypeResolver/NodeTypeResolver.php
@@ -49,7 +49,6 @@ use Rector\NodeTypeResolver\NodeTypeCorrector\GenericClassStringTypeCorrector;
 use Rector\NodeTypeResolver\NodeTypeCorrector\HasOffsetTypeCorrector;
 use Rector\NodeTypeResolver\NodeTypeResolver\IdentifierTypeResolver;
 use Rector\NodeTypeResolver\TypeAnalyzer\ArrayTypeAnalyzer;
-use Rector\StaticTypeMapper\ValueObject\Type\FullyQualifiedObjectType;
 use Rector\StaticTypeMapper\ValueObject\Type\ShortenedObjectType;
 use Rector\TypeDeclaration\PHPStan\Type\ObjectTypeSpecifier;
 use Symfony\Contracts\Service\Attribute\Required;
