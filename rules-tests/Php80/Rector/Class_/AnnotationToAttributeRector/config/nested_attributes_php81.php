f249d4b1b1d89ac37e706f7772232c41736de0bd
Tomas Votruba <tomas.vot@gmail.com>
2021-11-18 15:41:18 +0300

    improve AnnotationToAttributeRector

--- rules-tests/Php80/Rector/Class_/AnnotationToAttributeRector/config/nested_attributes_php81.php
+++ rules-tests/Php80/Rector/Class_/AnnotationToAttributeRector/config/nested_attributes_php81.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersionFeature;
+use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
 use Rector\Php80\ValueObject\AnnotationToAttribute;
+use Rector\Tests\Php80\Rector\Class_\AnnotationToAttributeRector\SourcePhp81\All;
+use Rector\Tests\Php80\Rector\Class_\AnnotationToAttributeRector\SourcePhp81\Length;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\SymfonyPhpConfig\ValueObjectInliner;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     // covers https://wiki.php.net/rfc/new_in_initializers#nested_attributes
@@ -13,14 +17,11 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersionFeature::NEW_INITIALIZERS);
 
     $services = $containerConfigurator->services();
-    $services->set(\Rector\Php80\Rector\Class_\AnnotationToAttributeRector::class)
+    $services->set(AnnotationToAttributeRector::class)
         ->call('configure', [[
-            \Rector\Php80\Rector\Class_\AnnotationToAttributeRector::ANNOTATION_TO_ATTRIBUTE => \Symplify\SymfonyPhpConfig\ValueObjectInliner::inline([
-                new AnnotationToAttribute(
-                    \Rector\Tests\Php80\Rector\Class_\AnnotationToAttributeRector\SourcePhp81\All::class,
-                    \Rector\Tests\Php80\Rector\Class_\AnnotationToAttributeRector\SourcePhp81\Length::class,
-                    \Rector\Tests\Php80\Rector\Class_\AnnotationToAttributeRector\SourcePhp81\NotNull::class,
-                ),
+            AnnotationToAttributeRector::ANNOTATION_TO_ATTRIBUTE => ValueObjectInliner::inline([
+                new AnnotationToAttribute(All::class),
+                new AnnotationToAttribute(Length::class),
             ]),
         ]]);
 };
