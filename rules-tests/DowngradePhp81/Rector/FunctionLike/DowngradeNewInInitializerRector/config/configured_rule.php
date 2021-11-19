23812145f1ca367e602ca4570a42d8559da6acfc
Fabien Villepinte <fabien.villepinte@gmail.com>
2021-11-19 20:09:16 +0000

    Expand for FunctionLike

--- rules-tests/DowngradePhp81/Rector/FunctionLike/DowngradeNewInInitializerRector/config/configured_rule.php
+++ rules-tests/DowngradePhp81/Rector/FunctionLike/DowngradeNewInInitializerRector/config/configured_rule.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Rector\DowngradePhp81\Rector\ClassMethod\DowngradeNewInInitializerRector;
+use Rector\DowngradePhp81\Rector\FunctionLike\DowngradeNewInInitializerRector;
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
