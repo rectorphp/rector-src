c2349be262a3f3f7bf143eace73de463dc5225c2
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-11-25 11:12:30 +0700

    skip if no required annotation or Required attribute

--- rules/DeadCode/Rector/ClassMethod/RemoveUnusedParamInRequiredAutowireRector.php
+++ rules/DeadCode/Rector/ClassMethod/RemoveUnusedParamInRequiredAutowireRector.php
@@ -7,11 +7,16 @@ namespace Rector\DeadCode\Rector\ClassMethod;
 use PhpParser\Node;
 use PhpParser\Node\Stmt\ClassMethod;
 use Rector\Core\Rector\AbstractRector;
+use Rector\Php80\NodeAnalyzer\PhpAttributeAnalyzer;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
 final class RemoveUnusedParamInRequiredAutowireRector extends AbstractRector
 {
+    public function __construct(private PhpAttributeAnalyzer $phpAttributeAnalyzer)
+    {
+    }
+
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition('Remove unused parameter in required autowire method', [
@@ -58,11 +63,19 @@ CODE_SAMPLE
      */
     public function refactor(Node $node): ?Node
     {
-        $methodName = $this->nodeNameResolver->getName($node);
-        if (! str_starts_with($methodName, 'autowire')) {
+        if ($this->shouldSkip($node)) {
             return null;
         }
 
         return $node;
+    }
+
+    private function shouldSkip(ClassMethod $classMethod): bool
+    {
+        $phpDocInfo            = $this->phpDocInfoFactory->createFromNodeOrEmpty($classMethod);
+        $hasRequiredAnnotation = $phpDocInfo->hasByName('required');
+        $hasRequiredAttribute  = $this->phpAttributeAnalyzer->hasPhpAttribute($classMethod, 'Symfony\Contracts\Service\Attribute\Required');
+
+        return ! $hasRequiredAnnotation && ! $hasRequiredAttribute;
     }
 }
