994959562c125671ebf55ee36c56c1ee6a4a0504
Markus Staab <m.staab@complex-it.de>
2021-11-29 15:42:28 +0100

    use local rector path config

--- e2e/plain-views/rector.php
+++ e2e/plain-views/rector.php
@@ -9,7 +9,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
 
     $parameters->set(Option::PATHS, [
-        __DIR__.'/src/view.php',
+        __DIR__.'/src/',
     ]);
 };
 
