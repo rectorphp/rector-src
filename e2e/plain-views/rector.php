76625d4a58ac51064f9a4663e60f41e48a8e027a
Markus Staab <m.staab@complex-it.de>
2021-11-29 10:06:53 +0100

    added e2e test

---
+++ e2e/plain-views/rector.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Rector\Core\Configuration\Option;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::PATHS, [
+        __DIR__.'/src/view.php',
+    ]);
+};
+
