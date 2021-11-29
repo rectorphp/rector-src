f96f86de2af3d9d4c26c16c06d53349f898305f0
Markus Staab <m.staab@complex-it.de>
2021-11-29 10:06:53 +0100

    added e2e test

---
+++ e2e/plain-views/src/view.php
@@ -0,0 +1,6 @@
+
+<div>
+    <?php echo check_box($auktion, 'skipnetzprovider', '1', "class=checkbox onclick=\"getAngebotHTML($('angebot').value);\""); ?>
+    <b class="caption">Provider und Netz nicht anzeigen</b> (Im gesamten Template)
+    <br><br>
+</div>
