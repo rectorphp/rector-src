31d192f2c01f462d84313791832aeca2e0f6dd80
Markus Staab <markus.staab@redaxo.de>
2021-11-29 10:10:14 +0100

    simplify repro

--- e2e/plain-views/src/view.php
+++ e2e/plain-views/src/view.php
@@ -1,6 +1,4 @@
 
-<div>
-    <?php echo check_box($auktion, 'skipnetzprovider', '1', "class=checkbox onclick=\"getAngebotHTML($('angebot').value);\""); ?>
-    <b class="caption">Provider und Netz nicht anzeigen</b> (Im gesamten Template)
-    <br><br>
-</div>
+	<div>
+	<?php echo 1 ?>
+	</div>
