986c41f425379338b34343584ee40e67f3d27a2a
Markus Staab <m.staab@complex-it.de>
2021-11-29 15:42:28 +0100

    use local rector path config

--- e2e/template-extends/rector.php
+++ e2e/template-extends/rector.php
@@ -5,5 +5,10 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    
+    $parameters->set(Option::PATHS, [
+        __DIR__.'/src/',
+    ]);
 };
 
