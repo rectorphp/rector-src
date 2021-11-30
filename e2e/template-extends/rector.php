655e297a235ef33514913f8bfb1e587207f03c05
Markus Staab <m.staab@complex-it.de>
2021-11-30 10:17:48 +0100

    fix

--- e2e/template-extends/rector.php
+++ e2e/template-extends/rector.php
@@ -3,10 +3,11 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Rector\Core\Configuration\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
-    
+
     $parameters->set(Option::PATHS, [
         __DIR__.'/src/',
     ]);
