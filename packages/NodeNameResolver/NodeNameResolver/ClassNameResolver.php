15bfb490353af9879622aa00c9fc64dda4563dcd
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-12-01 17:24:50 +0700

    remove namespacedName check

--- packages/NodeNameResolver/NodeNameResolver/ClassNameResolver.php
+++ packages/NodeNameResolver/NodeNameResolver/ClassNameResolver.php
@@ -5,25 +5,14 @@ declare(strict_types=1);
 namespace Rector\NodeNameResolver\NodeNameResolver;
 
 use PhpParser\Node;
-use PhpParser\Node\Name;
 use PhpParser\Node\Stmt\ClassLike;
 use Rector\NodeNameResolver\Contract\NodeNameResolverInterface;
-use Rector\NodeNameResolver\NodeNameResolver;
-use Symfony\Contracts\Service\Attribute\Required;
 
 /**
  * @implements NodeNameResolverInterface<ClassLike>
  */
 final class ClassNameResolver implements NodeNameResolverInterface
 {
-    private NodeNameResolver $nodeNameResolver;
-
-    #[Required]
-    public function autowire(NodeNameResolver $nodeNameResolver): void
-    {
-        $this->nodeNameResolver = $nodeNameResolver;
-    }
-
     public function getNode(): string
     {
         return ClassLike::class;
@@ -34,14 +23,6 @@ final class ClassNameResolver implements NodeNameResolverInterface
      */
     public function resolve(Node $node): ?string
     {
-        if ($node->namespacedName instanceof Name) {
-            return $node->namespacedName->toString();
-        }
-
-        if ($node->name === null) {
-            return null;
-        }
-
-        return $this->nodeNameResolver->getName($node->name);
+        return $node->namespacedName->toString();
     }
 }
