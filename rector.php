e2cec409a24ec8bdc3785188ba1bd4208f44368e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-12-03 10:06:55 +0700

    skip FinalizeClassesWithoutChildrenRector on DowngradeArraySpreadRector

--- rector.php
+++ rector.php
@@ -9,9 +9,11 @@ use Rector\CodingStyle\Rector\MethodCall\PreferThisOrSelfMethodCallRector;
 use Rector\CodingStyle\Rector\String_\SplitStringClassConstantToClassConstFetchRector;
 use Rector\CodingStyle\ValueObject\ReturnArrayClassMethodToYield;
 use Rector\Core\Configuration\Option;
+use Rector\DowngradePhp74\Rector\Array_\DowngradeArraySpreadRector;
 use Rector\Nette\Set\NetteSetList;
 use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -69,6 +71,10 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         StringClassNameToClassConstantRector::class,
         // some classes in config might not exist without dev dependencies
         SplitStringClassConstantToClassConstFetchRector::class,
+
+        FinalizeClassesWithoutChildrenRector::class => [
+            DowngradeArraySpreadRector::class,
+        ],
 
         // test paths
         '*/tests/**/Fixture/*',
