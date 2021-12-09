49a13f3f06214d24aebb5c33f2184dd3ababc403
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-12-09 12:29:25 +0700

    move parent to Source dir

---
+++ rules-tests/Php80/Rector/ClassMethod/AddParamBasedOnParentClassMethodRector/Source/ParentWithMoreParams.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Rector\Tests\Php80\Rector\ClassMethod\AddParamBasedOnParentClassMethodRector\Source;
+
+class ParentWithMoreParams
+{
+    public function execute($foo)
+    {
+    }
+}
