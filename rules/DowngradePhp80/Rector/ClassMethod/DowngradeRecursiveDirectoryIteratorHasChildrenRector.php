5bcbb7c3f5e15ed232e78c68c9432cc01336ad3b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-12-09 20:34:28 +0700

    adding DowngradeRecursiveDirectoryIteratorHasChildrenRector

---
+++ rules/DowngradePhp80/Rector/ClassMethod/DowngradeRecursiveDirectoryIteratorHasChildrenRector.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DowngradePhp80\Rector\ClassMethod;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\ClassMethod;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Type\StaticType;
+use Rector\BetterPhpDocParser\PhpDocParser\PhpDocFromTypeDeclarationDecorator;
+use Rector\Core\Rector\AbstractRector;
+use Rector\NodeTypeResolver\Node\AttributeKey;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\Tests\DowngradePhp80\Rector\ClassMethod\DowngradeRecursiveDirectoryIteratorHasChildrenRector\DowngradeRecursiveDirectoryIteratorHasChildrenRectorTest
+ */
+final class DowngradeRecursiveDirectoryIteratorHasChildrenRector extends AbstractRector
+{
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [ClassMethod::class];
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Remove bool type hint on child of RecursiveDirectoryIterator hasChildren allowLinks parameter',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+class RecursiveDirectoryIteratorChild extends \RecursiveDirectoryIterator
+{
+    public function hasChildren(bool $allowLinks = false): bool
+    {
+        return true;
+    }
+}
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+class RecursiveDirectoryIteratorChild extends \RecursiveDirectoryIterator
+{
+    public function hasChildren(bool $allowLinks = false): bool
+    {
+        return true;
+    }
+}
+CODE_SAMPLE
+                ),
+            ]
+        );
+    }
+
+    /**
+     * @param ClassMethod $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        return $node;
+    }
+}
