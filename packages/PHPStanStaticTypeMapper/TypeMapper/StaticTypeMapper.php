62b05617ef679aad181dbfb6634b2db14a46773e
Tomas Votruba <tomas.vot@gmail.com>
2021-12-09 16:24:52 +0100

    allow mixed on PHP 8+

--- packages/PHPStanStaticTypeMapper/TypeMapper/StaticTypeMapper.php
+++ packages/PHPStanStaticTypeMapper/TypeMapper/StaticTypeMapper.php
@@ -15,6 +15,7 @@ use Rector\Core\Php\PhpVersionProvider;
 use Rector\Core\ValueObject\PhpVersionFeature;
 use Rector\PHPStanStaticTypeMapper\Contract\TypeMapperInterface;
 use Rector\PHPStanStaticTypeMapper\Enum\TypeKind;
+use Rector\StaticTypeMapper\ValueObject\Type\SelfStaticType;
 use Rector\StaticTypeMapper\ValueObject\Type\SimpleStaticType;
 
 /**
@@ -50,7 +51,12 @@ final class StaticTypeMapper implements TypeMapperInterface
      */
     public function mapToPhpParserNode(Type $type, TypeKind $typeKind): ?Node
     {
+        // special case, for autocomplete of return type
         if ($type instanceof SimpleStaticType) {
+            return new Name(ObjectReference::STATIC()->getValue());
+        }
+
+        if ($type instanceof SelfStaticType) {
             return new Name(ObjectReference::SELF()->getValue());
         }
 
