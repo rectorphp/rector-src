f81e459c63b70b5c4b3fc840d8fe4e9d8f5ff3bd
Dan <dpock32509@gmail.com>
2021-12-09 12:00:52 -0500

    Update rules-tests/Transform/Rector/Class_/RemoveAllowDynamicPropertiesAttributeRector/config/configured_rule.php
    Co-authored-by: Abdul Malik Ikhsan <samsonasik@gmail.com>

--- rules-tests/Transform/Rector/Class_/RemoveAllowDynamicPropertiesAttributeRector/config/configured_rule.php
+++ rules-tests/Transform/Rector/Class_/RemoveAllowDynamicPropertiesAttributeRector/config/configured_rule.php
@@ -8,9 +8,9 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
     $services->set(RemoveAllowDynamicPropertiesAttributeRector::class)
-        ->call('configure', [[
-            RemoveAllowDynamicPropertiesAttributeRector::TRANSFORM_ON_NAMESPACES => [
+        ->configure(
+            [
                 '*\Fixture\Process\*'
             ]
-        ]]);
+        );
 };
