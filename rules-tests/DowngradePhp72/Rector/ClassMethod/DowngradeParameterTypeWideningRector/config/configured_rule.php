837b95cafee69218caaf62ee7edc930c442b0895
Tomas Votruba <tomas.vot@gmail.com>
2021-12-10 14:38:23 +0100

    remove fixed phpstan errors

--- rules-tests/DowngradePhp72/Rector/ClassMethod/DowngradeParameterTypeWideningRector/config/configured_rule.php
+++ rules-tests/DowngradePhp72/Rector/ClassMethod/DowngradeParameterTypeWideningRector/config/configured_rule.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Psr\Container\ContainerInterface;
 use Rector\DowngradePhp72\Rector\ClassMethod\DowngradeParameterTypeWideningRector;
+use Rector\Tests\DowngradePhp72\Rector\ClassMethod\DowngradeParameterTypeWideningRector\Fixture\SomeContainerInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -13,10 +14,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->configure([
             DowngradeParameterTypeWideningRector::UNSAFE_TYPES_TO_METHODS => [
                 ContainerInterface::class => ['set', 'get', 'has', 'initialized'],
-                \Rector\Tests\DowngradePhp72\Rector\ClassMethod\DowngradeParameterTypeWideningRector\Fixture\SomeContainerInterface::class => [
-                    'set',
-                    'has',
-                ],
+                SomeContainerInterface::class => ['set', 'has'],
             ],
         ]);
 };
