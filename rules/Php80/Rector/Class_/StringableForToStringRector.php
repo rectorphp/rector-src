d75ce219afb5b5eb88c781fed5e0d0356f1676d2
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-12-13 15:18:32 +0700

    final touch: ensure cast to int for array_key_last

--- rules/Php80/Rector/Class_/StringableForToStringRector.php
+++ rules/Php80/Rector/Class_/StringableForToStringRector.php
@@ -124,15 +124,13 @@ CODE_SAMPLE
         $hasReturn = $this->betterNodeFinder->hasInstancesOfInFunctionLikeScoped($toStringClassMethod, Return_::class);
 
         if (! $hasReturn) {
-            $lastKey = array_key_last((array) $toStringClassMethod->stmts);
+            $lastKey = (int) array_key_last((array) $toStringClassMethod->stmts);
             $toStringClassMethod->stmts[$lastKey] = new Return_(new String_(''));
 
             return;
         }
 
-        $this->traverseNodesWithCallable((array) $toStringClassMethod->stmts, function (Node $subNode) use (
-            $toStringClassMethod
-        ): void {
+        $this->traverseNodesWithCallable((array) $toStringClassMethod->stmts, function (Node $subNode): void {
             if (! $subNode instanceof Return_) {
                 return;
             }
