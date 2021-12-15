855a214253e95a13c54f516c5c04a3e8c7126163
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-12-15 21:19:10 +0700

    debug

--- src/PhpParser/Parser/RectorParser.php
+++ src/PhpParser/Parser/RectorParser.php
@@ -5,16 +5,28 @@ declare(strict_types=1);
 namespace Rector\Core\PhpParser\Parser;
 
 use PhpParser\Lexer;
+use PhpParser\Lexer\Emulative;
 use PhpParser\Node\Stmt;
 use PHPStan\Parser\Parser;
+use PHPStan\Parser\ParserErrorsException;
 use Rector\Core\PhpParser\ValueObject\StmtsAndTokens;
+use Rector\Core\Util\StringUtils;
+use ReflectionProperty;
 use Symplify\SmartFileSystem\SmartFileInfo;
+use Symplify\PackageBuilder\Reflection\PrivatesAccessor;
 
 final class RectorParser
 {
+    /**
+     * @var string
+     * @see https://regex101.com/r/txAwRA/1
+     */
+    private const FN_ERROR_REGEX = '#^Syntax error, unexpected T_FN, expecting T_STRING or \'\(\' on line#';
+
     public function __construct(
         private readonly Lexer $lexer,
-        private readonly Parser $parser,
+        private Parser $parser,
+        private readonly PrivatesAccessor $privatesAccessor
     ) {
     }
 
@@ -23,7 +35,22 @@ final class RectorParser
      */
     public function parseFile(SmartFileInfo $smartFileInfo): array
     {
-        return $this->parser->parseFile($smartFileInfo->getRealPath());
+        try {
+            return $this->parser->parseFile($smartFileInfo->getRealPath());
+        } catch (ParserErrorsException $parserErrorsException) {
+            $message = $parserErrorsException->getMessage();
+
+            if (! StringUtils::isMatch($message, self::FN_ERROR_REGEX)) {
+                throw $parserErrorsException;
+            }
+
+            $parser = $this->parser;
+
+            $lexer = &\Closure::bind(static fn&($parser) => $parser->lexer, null, $parser)($parser);
+            $lexer = new Emulative(['phpVersion' => Emulative::PHP_7_3]);
+
+            return $this->parser->parseFile($smartFileInfo->getRealPath());
+        }
     }
 
     public function parseFileToStmtsAndTokens(SmartFileInfo $smartFileInfo): StmtsAndTokens
