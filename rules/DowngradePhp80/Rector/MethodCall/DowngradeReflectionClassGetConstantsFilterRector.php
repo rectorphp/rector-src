61fbfc844c1d98bcae1b08906517733c3590da44
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-12-19 21:34:21 +0700

    [DowngradePhp80] Add DowngradeReflectionClassGetConstantsFilterRector

---
+++ rules/DowngradePhp80/Rector/MethodCall/DowngradeReflectionClassGetConstantsFilterRector.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DowngradePhp80\Rector\MethodCall;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Type\ObjectType;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\Tests\DowngradePhp80\Rector\MethodCall\DowngradeReflectionClassGetConstantsFilterRector\DowngradeReflectionClassGetConstantsFilterRectorTest
+ */
+final class DowngradeReflectionClassGetConstantsFilterRector extends AbstractRector
+{
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [MethodCall::class];
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Remove named argument',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+$reflectionClass = new ReflectionClass('SomeClass');
+$constants = $reflectionClass->getConstants(ReflectionClassConstant::IS_PUBLIC));
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+$reflectionClass = new ReflectionClass('SomeClass');
+$reflectionClassConstants = $reflectionClass->getReflectionConstants();
+$result = [];
+array_walk($reflectionClassConstants, function ($value) use (&$result) {
+    if ($value->isPublic()) {
+       $result[$value->getName()] = $value->getValue();
+    }
+});
+$constants = $result;
+CODE_SAMPLE
+                ),
+            ]
+        );
+    }
+
+    /**
+     * @param MethodCall$node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if ($this->shouldSkip($node)) {
+            return null;
+        }
+
+        return $node;
+    }
+
+    private function shouldSkip(MethodCall $methodCall): bool
+    {
+        if (! $this->nodeNameResolver->isName($methodCall->name, 'getConstants')) {
+            return true;
+        }
+
+        $varType = $this->nodeTypeResolver->getType($methodCall->var);
+        if (! $varType instanceof ObjectType) {
+            return true;
+        }
+
+        if ($varType->getName() !== 'ReflectionClass') {
+            return true;
+        }
+
+        return $methodCall->getArgs() === [];
+    }
+}
