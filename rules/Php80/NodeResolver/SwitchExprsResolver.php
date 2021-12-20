edfdccd4b8222018f7a107c8bf3eb8cb8efa4fe8
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-12-21 02:24:02 +0700

    phpstan

--- rules/Php80/NodeResolver/SwitchExprsResolver.php
+++ rules/Php80/NodeResolver/SwitchExprsResolver.php
@@ -26,7 +26,7 @@ final class SwitchExprsResolver
         $condAndExpr = [];
         $collectionEmptyCasesCond = [];
 
-        $switch = $this->moveDefaultLast($switch);
+        $this->moveDefaultLast($switch);
 
         foreach ($switch->cases as $key => $case) {
             if (! $this->isValidCase($case)) {
@@ -88,35 +88,39 @@ final class SwitchExprsResolver
         return $condAndExpr;
     }
 
-    private function moveDefaultLast(Switch_ $switch): Switch_
+    private function moveDefaultLast(Switch_ $switch): void
     {
         $keyMoved = null;
 
         foreach ($switch->cases as $key => $case) {
-            if ($case->cond === null) {
-                // check next
-                $next = $case->getAttribute(AttributeKey::NEXT_NODE);
-                if ($next instanceof Case_) {
-                    for ($loop = $key - 1; $loop >= 0; --$loop) {
-                        if ($switch->cases[$loop]->stmts !== []) {
-                            break;
-                        }
-
-                        unset($switch->cases[$loop]);
-                    }
-
-                    $keyMoved = $key;
-                }
+            if ($case->cond instanceof Expr) {
+                continue;
             }
+
+            // check next
+            $next = $case->getAttribute(AttributeKey::NEXT_NODE);
+            if (! $next instanceof Case_) {
+                return;
+            }
+
+            for ($loop = $key - 1; $loop >= 0; --$loop) {
+                if ($switch->cases[$loop]->stmts !== []) {
+                    break;
+                }
+
+                unset($switch->cases[$loop]);
+            }
+
+            $keyMoved = $key;
         }
 
-        if (is_int($keyMoved)) {
-            $caseToMove = $switch->cases[$keyMoved];
-            unset($switch->cases[$keyMoved]);
-            $switch->cases[] = $caseToMove;
+        if (! is_int($keyMoved)) {
+            return;
         }
 
-        return $switch;
+        $caseToMove = $switch->cases[$keyMoved];
+        unset($switch->cases[$keyMoved]);
+        $switch->cases[] = $caseToMove;
     }
 
     private function isValidCase(Case_ $case): bool
