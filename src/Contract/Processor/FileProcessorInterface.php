d909fd5fd44b420d8341d922094484997566e513
Tomas Votruba <tomas.vot@gmail.com>
2021-12-27 21:15:14 +0100

    return type to FileProcessor

--- src/Contract/Processor/FileProcessorInterface.php
+++ src/Contract/Processor/FileProcessorInterface.php
@@ -5,14 +5,16 @@ declare(strict_types=1);
 namespace Rector\Core\Contract\Processor;
 
 use Rector\Core\ValueObject\Application\File;
+use Rector\Core\ValueObject\Application\SystemError;
 use Rector\Core\ValueObject\Configuration;
+use Rector\Core\ValueObject\Reporting\FileDiff;
 
 interface FileProcessorInterface
 {
     public function supports(File $file, Configuration $configuration): bool;
 
     /**
-     * @return mixed[]|void
+     * @return array{system_errors: SystemError[], file_diffs: FileDiff[]}
      */
     public function process(File $file, Configuration $configuration);
 
