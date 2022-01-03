7c60037f89a5b8362688105db93b1da464a4c8c5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-01-04 01:50:45 +0700

    [Core] Failing test undefined array key 0 on FormatPerservingPrinter

---
+++ tests/Issues/PrintStmtsIndex/config/configured_rule.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Renaming\Rector\Name\RenameClassRector;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->set(RenameClassRector::class)
+        ->configure([
+            'Request' => 'Illuminate\Http\Request',
+        ]);
+};
