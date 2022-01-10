feaafc6b58cda649e6847d13428917982cfaea81
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-01-10 15:54:58 +0700

    check named arg

--- rules/Php81/Rector/FuncCall/NullToStrictStringFuncCallArgRector.php
+++ rules/Php81/Rector/FuncCall/NullToStrictStringFuncCallArgRector.php
@@ -6,6 +6,8 @@ namespace Rector\Php81\Rector\FuncCall;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Identifier;
+use Rector\Core\NodeAnalyzer\ArgsAnalyzer;
 use Rector\Core\Rector\AbstractRector;
 use Rector\Core\ValueObject\PhpVersionFeature;
 use Rector\VersionBonding\Contract\MinPhpVersionInterface;
@@ -26,6 +28,10 @@ final class NullToStrictStringFuncCallArgRector extends AbstractRector implement
         'preg_match_all' => [1, 'subject'],
         'explode' => [1, 'string'],
     ];
+
+    public function __construct(private readonly ArgsAnalyzer $argsAnalyzer)
+    {
+    }
 
     public function getRuleDefinition(): RuleDefinition
     {
@@ -74,14 +80,37 @@ CODE_SAMPLE
             return null;
         }
 
+        $args = $node->getArgs();
+        if ($this->hasNamedArgDefined($node, $args)) {
+            return $this->processNamedArgDefined($node, $args);
+        }
+
         return $node;
+    }
+
+    private function hasNamedArgDefined(FuncCall $funcCall, array $args): bool
+    {
+        $functionName = $this->nodeNameResolver->getName($funcCall);
+        [, $ArgName] = self::ARG_POSITION_NAME_NULL_TO_STRICT_STRING[$functionName];
+
+        foreach ($args as $arg) {
+            if ($arg->name instanceof Identifier && $this->nodeNameResolver->isName($arg->name, $ArgName)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function processNamedArgDefined(FuncCall $funcCall, array $args): ?FuncCall
+    {
+        return $funcCall;
     }
 
     private function shouldSkip(FuncCall $funcCall): bool
     {
         $functionNames = array_keys(self::ARG_POSITION_NAME_NULL_TO_STRICT_STRING);
-
-        return $this->nodeNameResolver->isNames($funcCall, $functionNames);
+        return ! $this->nodeNameResolver->isNames($funcCall, $functionNames);
     }
 
     public function provideMinPhpVersion(): int
