bd19274b8b110fae9788c59ea63affa98a73cccd
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-01-19 22:52:26 +0700

    add more fixture

---
+++ e2e/typed-property-namespaced-string/src/SomeClassWithArrayPropertyValue2.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace E2e\TypedPropertyNamespacedString;
+
+final class SomeClassWithArrayPropertyValue
+{
+    private $config = 'E2e\TypedPropertyNamespacedString\View\single';
+}
