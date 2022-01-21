cd915a27e1f1ba438e508603b123f6fe1a420847
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-01-22 00:06:43 +0700

    ->configure is no need to be called

--- tests/Issues/ConstructorPromoAnnotationToAttribute/config/configured_rule.php
+++ tests/Issues/ConstructorPromoAnnotationToAttribute/config/configured_rule.php
@@ -4,13 +4,11 @@ declare(strict_types=1);
 
 use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
-use Rector\Php80\ValueObject\AnnotationToAttribute;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
     $services->set(ClassPropertyAssignToConstructorPromotionRector::class);
-    $services->set(AnnotationToAttributeRector::class)
-        ->configure([new AnnotationToAttribute('Doctrine\ORM\Mapping\Table')]);
+    $services->set(AnnotationToAttributeRector::class);
 };
