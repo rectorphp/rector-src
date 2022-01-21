0e160851c76af6753299632a3dcc0ec55b3500ff
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-01-21 21:10:29 +0700

    [CodeQuality][EarlyReturn] Handle SimplifyIfElseToTernaryRector + ChangeIfElseValueAssignToEarlyReturnRector

---
+++ tests/Issues/IfElseAssignReturnUsed/config/configured_rule.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\If_\SimplifyIfElseToTernaryRector;
+use Rector\EarlyReturn\Rector\If_\ChangeIfElseValueAssignToEarlyReturnRector;
+use Rector\Set\ValueObject\SetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(SetList::EARLY_RETURN);
+
+    $services = $containerConfigurator->services();
+    $services->set(ChangeIfElseValueAssignToEarlyReturnRector::class);
+    $services->set(SimplifyIfElseToTernaryRector::class);
+};
