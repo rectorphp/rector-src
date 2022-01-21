e1577e3725dbc72eff38f58f6d02ddaa0ce909d7
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-01-22 00:02:11 +0700

    [Php80] Handle AnnotationToAttributeRector + ClassPropertyAssignToConstructorPromotionRector

---
+++ tests/Issues/ConstructorPromoAnnotationToAttribute/config/configured_rule.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Php80\ValueObject\AnnotationToAttribute;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->set(ClassPropertyAssignToConstructorPromotionRector::class);
+    $services->set(AnnotationToAttributeRector::class)
+        ->configure([
+            new AnnotationToAttribute('Doctrine\ORM\Mapping\Table'),
+        ]);
+};
