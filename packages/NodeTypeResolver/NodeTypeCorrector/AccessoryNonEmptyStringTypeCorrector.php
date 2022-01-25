3fe8161775549c24b97dc50fad61e6f41657457e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-01-25 20:53:43 +0700

    Closes #1719

--- packages/NodeTypeResolver/NodeTypeCorrector/AccessoryNonEmptyStringTypeCorrector.php
+++ packages/NodeTypeResolver/NodeTypeCorrector/AccessoryNonEmptyStringTypeCorrector.php
@@ -6,31 +6,43 @@ namespace Rector\NodeTypeResolver\NodeTypeCorrector;
 
 use PHPStan\Type\Accessory\AccessoryNonEmptyStringType;
 use PHPStan\Type\IntersectionType;
+use PHPStan\Type\ObjectWithoutClassType;
 use PHPStan\Type\Type;
 
 final class AccessoryNonEmptyStringTypeCorrector
 {
+    /**
+     * @var array<class-string<Type>>
+     */
+    private const INTERSECTION_DISALLOWED_TYPES = [
+        AccessoryNonEmptyStringType::class,
+        ObjectWithoutClassType::class,
+    ];
+
     public function correct(Type $mainType): Type | IntersectionType
     {
         if (! $mainType instanceof IntersectionType) {
             return $mainType;
         }
 
-        if (! $mainType->isSubTypeOf(new AccessoryNonEmptyStringType())->yes()) {
-            return $mainType;
-        }
-
         $clearIntersectionedTypes = [];
         foreach ($mainType->getTypes() as $intersectionedType) {
-            if ($intersectionedType instanceof AccessoryNonEmptyStringType) {
+            if (in_array($intersectionedType::class, self::INTERSECTION_DISALLOWED_TYPES, true)) {
                 continue;
             }
 
             $clearIntersectionedTypes[] = $intersectionedType;
         }
 
+        $countIntersectionTypes = count($mainType->getTypes());
+        $countClearIntersectionedTypes = count($clearIntersectionedTypes);
+
         if (count($clearIntersectionedTypes) === 1) {
             return $clearIntersectionedTypes[0];
+        }
+
+        if ($countIntersectionTypes === $countClearIntersectionedTypes) {
+            return $mainType;
         }
 
         return new IntersectionType($clearIntersectionedTypes);
