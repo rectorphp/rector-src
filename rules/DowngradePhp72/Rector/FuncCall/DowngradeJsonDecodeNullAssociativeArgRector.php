47baa88daf84a523672f0823c1228aad9805be36
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-01-25 16:30:55 +0700

    implemented for exactly null

--- rules/DowngradePhp72/Rector/FuncCall/DowngradeJsonDecodeNullAssociativeArgRector.php
+++ rules/DowngradePhp72/Rector/FuncCall/DowngradeJsonDecodeNullAssociativeArgRector.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Rector\DowngradePhp72\Rector\FuncCall;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\ConstFetch;
 use PhpParser\Node\Expr\FuncCall;
+use Rector\Core\NodeAnalyzer\ArgsAnalyzer;
 use Rector\Core\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
@@ -15,6 +17,10 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
  */
 final class DowngradeJsonDecodeNullAssociativeArgRector extends AbstractRector
 {
+    public function __construct(private readonly ArgsAnalyzer $argsAnalyzer)
+    {
+    }
+
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition('Downgrade json_decode() with null associative argument function', [
@@ -50,6 +56,22 @@ CODE_SAMPLE
     {
         if (! $this->isName($node, 'json_decode')) {
             return null;
+        }
+
+        $args = $node->getArgs();
+        if ($this->argsAnalyzer->hasNamedArg($args)) {
+            return null;
+        }
+
+        if (! isset($args[1])) {
+            return null;
+        }
+
+        $associativeValue = $args[1]->value;
+
+        if ($associativeValue instanceof ConstFetch && $this->valueResolver->isNull($associativeValue)) {
+            $node->args[1]->value = $this->nodeFactory->createFalse();
+            return $node;
         }
 
         return $node;
