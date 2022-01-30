a19caf06bea7be16f28a288ed342acc26180b120
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-01-31 05:54:08 +0700

    [Php74][TypeDeclaration] Add InlineVarDocToTypedPropertyRector + InlineVarDocToTypedInPrivatePropertyRector, deprecate TypedPropertyRector

---
+++ rules/TypeDeclaration/Rector/Property/InlineVarDocToTypedInPrivatePropertyRector.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\TypeDeclaration\Rector\Property;
+
+use Rector\TypeDeclaration\Rector\AbstractInlineVarDocToTypedPropertyRector;
+
+final class InlineVarDocToTypedInPrivatePropertyRector extends AbstractInlineVarDocToTypedPropertyRector
+{
+}
---
+++ rules/TypeDeclaration/Rector/Property/InlineVarDocToTypedPropertyRector.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\TypeDeclaration\Rector\Property;
+
+use PhpParser\Node\Stmt\Property;
+use Rector\TypeDeclaration\Rector\AbstractInlineVarDocToTypedPropertyRector;
+use PHPStan\Analyser\Scope;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+final class InlineVarDocToTypedPropertyRector extends AbstractInlineVarDocToTypedPropertyRector
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Changes property `@var` annotations from annotation to type for all modifiers.',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+final class SomeClass
+{
+    /**
+     * @var int
+     */
+    private $count;
+
+    /**
+     * @var int
+     */
+    protected $count2;
+
+    /**
+     * @var int
+     */
+    public $count3;
+}
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+final class SomeClass
+{
+    private int $count;
+
+    protected int $count2;
+
+    public int $count2;
+}
+CODE_SAMPLE
+                ),
+            ]
+        );
+    }
+
+    protected function shouldSkipProperty(Property $property, Scope $scope): bool
+    {
+        return $this->shouldSkip($property, $scope);
+    }
+}
