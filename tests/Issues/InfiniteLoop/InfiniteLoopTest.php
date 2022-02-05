4fa658156c1ae506864bcf2c676ea0ea58101440
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-02-06 06:17:58 +0700

    remove InfiniteLoopTraversingException

--- tests/Issues/InfiniteLoop/InfiniteLoopTest.php
+++ tests/Issues/InfiniteLoop/InfiniteLoopTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Rector\Core\Tests\Issues\InfiniteLoop;
 
-use Rector\Core\Exception\NodeTraverser\InfiniteLoopTraversingException;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
 use Symplify\SmartFileSystem\SmartFileInfo;
 
