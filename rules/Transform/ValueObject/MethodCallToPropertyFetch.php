55ec951b23865ecdde9ae897926c82a0b5a6cd51
Oskar Stark <oskarstark@googlemail.com>
2022-02-11 20:16:56 +0100

    Enhancement: Add MethodCallToPropertyFetch value object

---
+++ rules/Transform/ValueObject/MethodCallToPropertyFetch.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Transform\ValueObject;
+
+use PHPStan\Type\ObjectType;
+use Rector\Core\Validation\RectorAssert;
+
+final class MethodCallToPropertyFetch
+{
+    /**
+     * @param mixed[] $newGetArguments
+     */
+    public function __construct(
+        private readonly string $oldType,
+        private readonly string $oldMethod,
+        private readonly string $newProperty,
+    ) {
+        RectorAssert::className($oldType);
+    }
+
+    public function getOldObjectType(): ObjectType
+    {
+        return new ObjectType($this->oldType);
+    }
+
+    public function getNewProperty(): string
+    {
+        return $this->newProperty;
+    }
+
+    public function getOldMethod(): string
+    {
+        return $this->oldMethod;
+    }
+}
