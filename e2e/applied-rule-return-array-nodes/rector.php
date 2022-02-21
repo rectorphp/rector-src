3ed3fca3de9727e4e20e79223738200b5eef8ae0
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-02-21 13:50:28 +0700

    config

--- e2e/applied-rule-return-array-nodes/rector.php
+++ e2e/applied-rule-return-array-nodes/rector.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Rector\EarlyReturn\Rector\If_\RemoveAlwaysElseRector;
+use Rector\Core\Configuration\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/src',
+    ]);
+
     $services = $containerConfigurator->services();
     $services->set(RemoveAlwaysElseRector::class);
 };
