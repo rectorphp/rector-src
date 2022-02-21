74363a939918d4d5a2459e1bb49a9b25e0e527f9
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-02-21 14:05:09 +0700

    more fixture for multi rules

--- e2e/applied-rule-return-array-nodes/rector.php
+++ e2e/applied-rule-return-array-nodes/rector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Rector\EarlyReturn\Rector\If_\RemoveAlwaysElseRector;
 use Rector\Core\Configuration\Option;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodRector;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
@@ -14,5 +15,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services = $containerConfigurator->services();
     $services->set(RemoveAlwaysElseRector::class);
+    $services->set(RemoveUnusedPrivateMethodRector::class);
 };
 
