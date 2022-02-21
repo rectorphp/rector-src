9deffbda6733520e47a386f0ec7520fb1572b8e6
GitHub Action <action@github.com>
2022-02-21 04:18:36 +0000

    [ci-review] Rector Rectify

--- rules/Php81/NodeAnalyzer/ComplexNewAnalyzer.php
+++ rules/Php81/NodeAnalyzer/ComplexNewAnalyzer.php
@@ -10,7 +10,6 @@ use PhpParser\Node\Expr\ArrayItem;
 use PhpParser\Node\Expr\ClassConstFetch;
 use PhpParser\Node\Expr\ConstFetch;
 use PhpParser\Node\Expr\New_;
-use PhpParser\Node\Identifier;
 use PhpParser\Node\Name;
 use PhpParser\Node\Name\FullyQualified;
 use PhpParser\Node\Scalar;
