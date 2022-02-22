222af5158b4a945802102e54171e80788ac010b6
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-02-22 21:26:44 +0700

    Fixed :tada:

--- rules/DeadCode/Rector/TryCatch/RemoveDeadTryCatchRector.php
+++ rules/DeadCode/Rector/TryCatch/RemoveDeadTryCatchRector.php
@@ -6,7 +6,6 @@ namespace Rector\DeadCode\Rector\TryCatch;
 
 use PhpParser\Node;
 use PhpParser\Node\Stmt;
-use PhpParser\Node\Stmt\Catch_;
 use PhpParser\Node\Stmt\Finally_;
 use PhpParser\Node\Stmt\Nop;
 use PhpParser\Node\Stmt\Throw_;
@@ -65,34 +64,21 @@ CODE_SAMPLE
      */
     public function refactor(Node $node): null|TryCatch|array
     {
-        foreach ($node->catches as $catch) {
-            if ($catch->stmts === []) {
-                continue;
-            }
-
-            if (! isset($catch->stmts[0])) {
-                return null;
-            }
-
-            if (count($catch->stmts) !== 1) {
-                return null;
-            }
-
-            if ($catch->stmts[0] instanceof Nop) {
-                continue;
-            }
-
-            if (! $catch->stmts[0] instanceof Throw_) {
-                return null;
-            }
+        if ($this->shouldSkip($node)) {
+            return null;
         }
 
-        if ($node->finally instanceof Finally_ && count($node->finally->stmts) === 1 && isset($node->finally->stmts[0]) && $node->finally->stmts[0] instanceof Nop) {
-            $this->removeNode($node);
-            return $node;
-        }
+        if ($node->finally instanceof Finally_) {
+            if ($node->finally->stmts === []) {
+                $this->removeNode($node);
+                return $node;
+            }
 
-        if ($node->finally instanceof Finally_ && $node->finally->stmts !== []) {
+            if ($this->hasOnlyNopStmt($node->finally->stmts)) {
+                $this->removeNode($node);
+                return $node;
+            }
+
             return null;
         }
 
@@ -101,11 +87,46 @@ CODE_SAMPLE
             return $node;
         }
 
-        if (count($node->stmts) === 1 && isset($node->stmts[0]) && $node->stmts[0] instanceof Nop) {
+        if ($this->hasOnlyNopStmt($node->stmts)) {
             $this->removeNode($node);
             return $node;
         }
 
         return $node->stmts;
+    }
+
+    /**
+     * @param Stmt[] $stmts
+     */
+    private function hasOnlyNopStmt(array $stmts): bool
+    {
+        return count($stmts) === 1 && isset($stmts[0]) && $stmts[0] instanceof Nop;
+    }
+
+    private function shouldSkip(TryCatch $tryCatch): bool
+    {
+        foreach ($tryCatch->catches as $catch) {
+            if ($catch->stmts === []) {
+                continue;
+            }
+
+            if (! isset($catch->stmts[0])) {
+                return true;
+            }
+
+            if (count($catch->stmts) !== 1) {
+                return true;
+            }
+
+            if ($catch->stmts[0] instanceof Nop) {
+                continue;
+            }
+
+            if (! $catch->stmts[0] instanceof Throw_) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
