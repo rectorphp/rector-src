6f83cd2f3bb0b9b12201d2bc7749e0efaff7bbff
Grzegorz Korba <grzegorz.korba@codito.pl>
2022-03-02 11:06:40 +0100

    Introduce `RectorContainerConfigurator`
    - Stable public API for configuring services
    - Rector rule for migrating Symfony configurator to Rector configurator

---
+++ src/DependencyInjection/Loader/Configurator/RectorContainerConfigurator.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Core\DependencyInjection\Loader\Configurator;
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+/**
+ * @api
+ * Same as Symfony container configurator, with patched return type for "set()" method for easier DX.
+ * It is an alias for internal class that is prefixed during build, so it's basically for keeping stable public API.
+ */
+final class RectorContainerConfigurator extends ContainerConfigurator
+{
+}
