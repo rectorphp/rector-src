e34fd8f9be64c7d76cbb88b3a8eb5f73a18b0790
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-03-05 16:15:45 +0700

    check end equals to length identifier and silent value

--- packages/BetterPhpDocParser/Attributes/AttributeMirrorer.php
+++ packages/BetterPhpDocParser/Attributes/AttributeMirrorer.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Rector\BetterPhpDocParser\Attributes;
 
 use PHPStan\PhpDocParser\Ast\Node;
+use Rector\BetterPhpDocParser\PhpDoc\DoctrineAnnotationTagValueNode;
 use Rector\BetterPhpDocParser\ValueObject\PhpDocAttributeKey;
+use Rector\BetterPhpDocParser\ValueObject\StartAndEnd;
 
 final class AttributeMirrorer
 {
@@ -20,6 +22,18 @@ final class AttributeMirrorer
 
     public function mirror(Node $oldNode, Node $newNode): void
     {
+        if ($newNode->value instanceof DoctrineAnnotationTagValueNode) {
+            $startAndAnd = $oldNode->getAttribute(PhpDocAttributeKey::START_AND_END);
+            if ($startAndAnd instanceof StartAndEnd) {
+                $end = $startAndAnd->getEnd();
+                $lengthIdentifier = strlen((string) $newNode->value->identifierTypeNode);
+
+                if ($end === $lengthIdentifier && $newNode->value->getSilentValue() !== null) {
+                    return;
+                }
+            }
+        }
+
         foreach (self::ATTRIBUTES_TO_MIRROR as $attributeToMirror) {
             if (! $oldNode->hasAttribute($attributeToMirror)) {
                 continue;
