7d493e6ad2c94d161755288d6519fe7860bb89c7
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-03-29 04:50:34 +0700

    phpstan

--- rules/CodeQuality/Rector/ClassMethod/OptionalParametersAfterRequiredRector.php
+++ rules/CodeQuality/Rector/ClassMethod/OptionalParametersAfterRequiredRector.php
@@ -125,7 +125,7 @@ CODE_SAMPLE
             return null;
         }
 
-        $new->args = $this->argumentSorter->sortArgsByExpectedParamOrder($new->args, $expectedArgOrParamOrder);
+        $new->args = $this->argumentSorter->sortArgsByExpectedParamOrder($new->getArgs(), $expectedArgOrParamOrder);
 
         return $new;
     }
