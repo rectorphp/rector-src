c41a2aaf18ea0229a305f9539db8f09e5ad148d3
GitHub Action <action@github.com>
2022-04-03 18:38:28 +0000

    [ci-review] Rector Rectify

--- rules/DeadCode/ConditionEvaluator.php
+++ rules/DeadCode/ConditionEvaluator.php
@@ -21,7 +21,6 @@ final class ConditionEvaluator
     ) {
     }
 
-
     public function evaluate(ConditionInterface $condition): bool|int|null
     {
         if ($condition instanceof VersionCompareCondition) {
@@ -34,7 +33,6 @@ final class ConditionEvaluator
 
         return null;
     }
-
 
     private function evaluateVersionCompareCondition(
         VersionCompareCondition $versionCompareCondition
