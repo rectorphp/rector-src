678f05b1725c2e7da432a27a749230c0d3b9b3cc
GitHub Action <action@github.com>
2022-04-08 08:37:04 +0000

    [ci-review] Rector Rectify

--- tests/Issues/Issue7099/config/configured_rule.php
+++ tests/Issues/Issue7099/config/configured_rule.php
@@ -2,20 +2,24 @@
 
 declare(strict_types=1);
 
+use Rector\DeadCode\Rector\If_\RemoveDeadInstanceOfRector;
+use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
+use Rector\Strict\Rector\Ternary\BooleanInTernaryOperatorRuleFixerRector;
+
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
-    $services->set(\Rector\DeadCode\Rector\If_\RemoveDeadInstanceOfRector::class);
+    $services->set(RemoveDeadInstanceOfRector::class);
 
-    $services->set(\Rector\Strict\Rector\Ternary\BooleanInTernaryOperatorRuleFixerRector::class)
+    $services->set(BooleanInTernaryOperatorRuleFixerRector::class)
         ->configure([
-            \Rector\Strict\Rector\Ternary\BooleanInTernaryOperatorRuleFixerRector::TREAT_AS_NON_EMPTY => false,
+            BooleanInTernaryOperatorRuleFixerRector::TREAT_AS_NON_EMPTY => false,
         ]);
 
-    $services->set(\Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector::class)
+    $services->set(DisallowedEmptyRuleFixerRector::class)
         ->configure([
-            \Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector::TREAT_AS_NON_EMPTY => false,
+            DisallowedEmptyRuleFixerRector::TREAT_AS_NON_EMPTY => false,
         ]);
 };
