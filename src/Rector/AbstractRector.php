0ec2c0e35fc99f0fa35ace6c9e7561ab6db1cd2e
Tomas Votruba <tomas.vot@gmail.com>
2022-04-10 01:01:05 +0200

    Move ParameterProvider from AbstractRector to rules that actually use it

--- src/Rector/AbstractRector.php
+++ src/Rector/AbstractRector.php
@@ -43,7 +43,6 @@ use Rector\PostRector\Collector\NodesToRemoveCollector;
 use Rector\StaticTypeMapper\StaticTypeMapper;
 use Symfony\Contracts\Service\Attribute\Required;
 use Symplify\Astral\NodeTraverser\SimpleCallableNodeTraverser;
-use Symplify\PackageBuilder\Parameter\ParameterProvider;
 use Symplify\Skipper\Skipper\Skipper;
 
 /**
@@ -84,8 +83,6 @@ CODE_SAMPLE;
     protected NodeNameResolver $nodeNameResolver;
 
     protected NodeTypeResolver $nodeTypeResolver;
-
-    protected ParameterProvider $parameterProvider;
 
     protected PhpVersionProvider $phpVersionProvider;
 
@@ -143,7 +140,6 @@ CODE_SAMPLE;
         PhpVersionProvider $phpVersionProvider,
         ExclusionManager $exclusionManager,
         StaticTypeMapper $staticTypeMapper,
-        ParameterProvider $parameterProvider,
         CurrentRectorProvider $currentRectorProvider,
         CurrentNodeProvider $currentNodeProvider,
         Skipper $skipper,
@@ -165,7 +161,6 @@ CODE_SAMPLE;
         $this->phpVersionProvider = $phpVersionProvider;
         $this->exclusionManager = $exclusionManager;
         $this->staticTypeMapper = $staticTypeMapper;
-        $this->parameterProvider = $parameterProvider;
         $this->currentRectorProvider = $currentRectorProvider;
         $this->currentNodeProvider = $currentNodeProvider;
         $this->skipper = $skipper;
