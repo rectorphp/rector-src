e5086d9929ce2898602651bb78bc81c390c47028
Jiří Bok <jiri.bok@protonmail.com>
2022-04-13 22:05:32 +0200

    [Feature] Add ParamAnnotationIncorrectNullableRector for fixing incorrect null type in @param

---
+++ rules/TypeDeclaration/Rector/ClassMethod/ParamAnnotationIncorrectNullableRector.php
@@ -0,0 +1,197 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\TypeDeclaration\Rector\ClassMethod;
+
+use PhpParser\Node;
+use PhpParser\Node\Param;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Function_;
+use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocNode;
+use PHPStan\Type\Type;
+use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfo;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Core\ValueObject\PhpVersionFeature;
+use Rector\NodeTypeResolver\TypeComparator\TypeComparator;
+use Rector\PHPStanStaticTypeMapper\Enum\TypeKind;
+use Rector\TypeDeclaration\Guard\PhpDocNestedAnnotationGuard;
+use Rector\TypeDeclaration\Helper\PhpDocNullableTypeHelper;
+use Rector\TypeDeclaration\PhpDocParser\ParamPhpDocNodeFactory;
+use Rector\VendorLocker\NodeVendorLocker\ClassMethodReturnTypeOverrideGuard;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\Tests\TypeDeclaration\Rector\ClassMethod\ParamAnnotationIncorrectNullableRector\ParamAnnotationIncorrectNullableRectorTest
+ */
+final class ParamAnnotationIncorrectNullableRector extends AbstractRector
+{
+    public function __construct(
+        private readonly TypeComparator $typeComparator,
+        private readonly PhpDocNullableTypeHelper $phpDocNullableTypeHelper,
+        private readonly PhpDocNestedAnnotationGuard $phpDocNestedAnnotationGuard,
+        private readonly ClassMethodReturnTypeOverrideGuard $classMethodReturnTypeOverrideGuard,
+        private readonly ParamPhpDocNodeFactory $paramPhpDocNodeFactory
+    ) {
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Add or remove null type from @param phpdoc typehint based on php parameter type declaration',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+final class SomeClass
+{
+    /**
+     * @param \DateTime[] $dateTimes
+     */
+    public function setDateTimes(?array $dateTimes): self
+    {
+        $this->dateTimes = $dateTimes;
+
+        return $this;
+    }
+}
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+final class SomeClass
+{
+    /**
+     * @param \DateTime[]|null $dateTimes
+     */
+    public function setDateTimes(?array $dateTimes): self
+    {
+        $this->dateTimes = $dateTimes;
+
+        return $this;
+    }
+}
+CODE_SAMPLE
+                ),
+
+            ]
+        );
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [ClassMethod::class, Function_::class];
+    }
+
+    /**
+     * @param ClassMethod|Function_ $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if ($node->getParams() === []) {
+            return null;
+        }
+
+        if ($node instanceof ClassMethod && $this->classMethodReturnTypeOverrideGuard->shouldSkipClassMethod($node)) {
+            return null;
+        }
+
+        if (! $this->phpVersionProvider->isAtLeastPhpVersion(PhpVersionFeature::TYPED_PROPERTIES)) {
+            return null;
+        }
+
+        if (! $this->phpDocNestedAnnotationGuard->isPhpDocCommentCorrectlyParsed($node)) {
+            return null;
+        }
+
+        $phpDocInfo = $this->phpDocInfoFactory->createFromNodeOrEmpty($node);
+        $phpDocNode = $phpDocInfo->getPhpDocNode();
+        $wasAnyParamTagUpdated = $this->wasUpdateOfParamTagsRequired($phpDocNode, $node, $phpDocInfo);
+
+        if (! $wasAnyParamTagUpdated) {
+            return null;
+        }
+
+        return $node;
+    }
+
+    private function matchParamByName(string $desiredParamName, ClassMethod|Function_ $node): ?Param
+    {
+        foreach ($node->getParams() as $param) {
+            $paramName = $this->nodeNameResolver->getName($param);
+            if ('$' . $paramName !== $desiredParamName) {
+                continue;
+            }
+
+            return $param;
+        }
+
+        return null;
+    }
+
+    private function changeParamType(PhpDocInfo $phpDocInfo, Type $newType, Param $param, string $paramName): void
+    {
+        // better skip, could crash hard
+        if ($phpDocInfo->hasInvalidTag('@param')) {
+            return;
+        }
+
+        $typeNode = $this->staticTypeMapper->mapPHPStanTypeToPHPStanPhpDocTypeNode($newType, TypeKind::PARAM());
+        $paramTagValueNode = $phpDocInfo->getParamTagValueByName($paramName);
+        // override existing type
+        if ($paramTagValueNode !== null) {
+            // already set
+            $currentType = $this->staticTypeMapper->mapPHPStanPhpDocTypeNodeToPHPStanType(
+                $paramTagValueNode->type,
+                $param
+            );
+            if ($this->typeComparator->areTypesEqual($currentType, $newType)) {
+                return;
+            }
+
+            $paramTagValueNode->type = $typeNode;
+        } else {
+            $paramTagValueNode = $this->paramPhpDocNodeFactory->create($typeNode, $param);
+            $phpDocInfo->addTagValueNode($paramTagValueNode);
+        }
+    }
+
+    private function wasUpdateOfParamTagsRequired(
+        PhpDocNode $phpDocNode,
+        ClassMethod|Function_ $node,
+        PhpDocInfo $phpDocInfo
+    ): bool {
+        $paramTagValueNodes = $phpDocNode->getParamTagValues();
+        $paramTagWasUpdated = false;
+        foreach ($paramTagValueNodes as $paramTagValueNode) {
+            if ($paramTagValueNode->type === null) {
+                continue;
+            }
+
+            $param = $this->matchParamByName($paramTagValueNode->parameterName, $node);
+            if (! $param instanceof Param) {
+                continue;
+            }
+
+            $docType = $this->staticTypeMapper->mapPHPStanPhpDocTypeNodeToPHPStanType(
+                $paramTagValueNode->type,
+                $node
+            );
+            $updatedPhpDocType = $this->phpDocNullableTypeHelper->resolveUpdatedPhpDocTypeFromPhpDocTypeAndParamNode(
+                $docType,
+                $param
+            );
+
+            if (! $updatedPhpDocType instanceof Type) {
+                continue;
+            }
+
+            $this->changeParamType($phpDocInfo, $updatedPhpDocType, $param, $paramTagValueNode->parameterName);
+            $paramTagWasUpdated = true;
+        }
+
+        return $paramTagWasUpdated;
+    }
+}
