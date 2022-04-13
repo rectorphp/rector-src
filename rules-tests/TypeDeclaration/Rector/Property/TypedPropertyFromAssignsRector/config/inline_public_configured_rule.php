860ea0f320b5087405fc933eeecf46eced7762f6
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-04-13 20:52:02 +0700

    inline public set interesction php version

--- rules-tests/TypeDeclaration/Rector/Property/TypedPropertyFromAssignsRector/config/inline_public_configured_rule.php
+++ rules-tests/TypeDeclaration/Rector/Property/TypedPropertyFromAssignsRector/config/inline_public_configured_rule.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersionFeature;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
-
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -16,5 +15,5 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ]);
 
     $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersionFeature::TYPED_PROPERTIES);
+    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersionFeature::INTERSECTION_TYPES);
 };
