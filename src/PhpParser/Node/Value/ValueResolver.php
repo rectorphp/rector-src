7e588173cc4b2e4a2ae8191fd87d8297d0c7ee84
Jiří Bok <jiri.bok@protonmail.com>
2022-04-13 18:05:59 +0200

    [Fix] Fix UnionTypesRector

--- src/PhpParser/Node/Value/ValueResolver.php
+++ src/PhpParser/Node/Value/ValueResolver.php
@@ -62,7 +62,7 @@ final class ValueResolver
     }
 
     /**
-     * @phpstan-return mixed|null
+     * @return mixed|null
      */
     public function getValue(Expr $expr, bool $resolvedClassReference = false)
     {
@@ -215,7 +215,7 @@ final class ValueResolver
     }
 
     /**
-     * @phpstan-return mixed[]|null
+     * @return mixed[]|null
      */
     private function extractConstantArrayTypeValue(ConstantArrayType $constantArrayType): ?array
     {
