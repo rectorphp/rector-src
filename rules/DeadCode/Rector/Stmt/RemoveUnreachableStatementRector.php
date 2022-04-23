b3984843a16937bafad8a19d590cd1fce106d9f3
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-04-23 07:08:57 +0700

    Revert Simplify RemoveUnreachableStatementRector to use less connecting nodes (#2125)
    This reverts commit 5cc41ac787fcfcc7a10dbee7645207d839e75982.

--- rules/DeadCode/Rector/Stmt/RemoveUnreachableStatementRector.php
+++ rules/DeadCode/Rector/Stmt/RemoveUnreachableStatementRector.php
@@ -9,11 +9,16 @@ use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\StaticCall;
 use PhpParser\Node\FunctionLike;
 use PhpParser\Node\Stmt;
+use PhpParser\Node\Stmt\ClassLike;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Do_;
 use PhpParser\Node\Stmt\Else_;
-use PhpParser\Node\Stmt\Expression;
-use PhpParser\Node\Stmt\Foreach_;
+use PhpParser\Node\Stmt\Function_;
 use PhpParser\Node\Stmt\If_;
+use PhpParser\Node\Stmt\Namespace_;
 use PhpParser\Node\Stmt\Nop;
+use PhpParser\Node\Stmt\While_;
+use Rector\Core\NodeAnalyzer\InlineHTMLAnalyzer;
 use Rector\Core\Rector\AbstractRector;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
@@ -26,6 +31,16 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
  */
 final class RemoveUnreachableStatementRector extends AbstractRector
 {
+    /**
+     * @var array<class-string<Node>>
+     */
+    private const STMTS_WITH_IS_UNREACHABLE = [If_::class, While_::class, Do_::class];
+
+    public function __construct(
+        private readonly InlineHTMLAnalyzer $inlineHTMLAnalyzer
+    ) {
+    }
+
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition('Remove unreachable statements', [
@@ -60,59 +75,82 @@ CODE_SAMPLE
      */
     public function getNodeTypes(): array
     {
-        return [Foreach_::class, FunctionLike::class, Else_::class, If_::class];
+        return [Stmt::class];
     }
 
     /**
-     * @param Foreach_|FunctionLike|Else_|If_ $node
+     * @param Stmt $node
      */
     public function refactor(Node $node): ?Node
     {
-        $stmts = $node->stmts;
-
-        $isPassedTheUnreachable = false;
-        $hasChanged = false;
-
-        foreach ($stmts as $key => $stmt) {
-            if ($this->shouldSkipNode($stmt)) {
-                continue;
-            }
-
-            if ($this->isUnreachable($stmt)) {
-                $isPassedTheUnreachable = true;
-            }
-
-            if ($this->isAfterMarkTestSkippedMethodCall($stmt)) {
-                // keep for test case temporary ignored
-                break;
-            }
-
-            if (! $isPassedTheUnreachable) {
-                continue;
-            }
-
-            unset($stmts[$key]);
-            $hasChanged = true;
-        }
-
-        if (! $hasChanged) {
+        if ($this->shouldSkipNode($node)) {
             return null;
         }
 
-        $node->stmts = $stmts;
-        return $node;
+        // might be PHPStan false positive, better skip
+        $previousStatement = $node->getAttribute(AttributeKey::PREVIOUS_STATEMENT);
+        if ($previousStatement instanceof Stmt && in_array(
+            $previousStatement::class,
+            self::STMTS_WITH_IS_UNREACHABLE,
+            true
+        )) {
+            $node->setAttribute(
+                AttributeKey::IS_UNREACHABLE,
+                $previousStatement->getAttribute(AttributeKey::IS_UNREACHABLE)
+            );
+        }
+
+        if (! $this->isUnreachable($node)) {
+            return null;
+        }
+
+        if ($this->isAfterMarkTestSkippedMethodCall($node)) {
+            $node->setAttribute(AttributeKey::IS_UNREACHABLE, false);
+            return null;
+        }
+
+        $this->removeNode($node);
+
+        return null;
     }
 
     private function shouldSkipNode(Stmt $stmt): bool
     {
-        return $stmt instanceof Nop;
+        if ($stmt instanceof Nop) {
+            return true;
+        }
+
+        if ($stmt instanceof ClassLike) {
+            return true;
+        }
+
+        if ($stmt instanceof FunctionLike) {
+            return true;
+        }
+
+        return $this->inlineHTMLAnalyzer->hasInlineHTML($stmt);
     }
 
     private function isUnreachable(Stmt $stmt): bool
     {
         $isUnreachable = $stmt->getAttribute(AttributeKey::IS_UNREACHABLE);
+        if ($isUnreachable === true) {
+            return true;
+        }
 
-        return $isUnreachable === true;
+        // traverse up for unreachable node in the same scope
+        $previousNode = $stmt->getAttribute(AttributeKey::PREVIOUS_STATEMENT);
+
+        while ($previousNode instanceof Stmt && ! $this->isBreakingScopeNode($previousNode)) {
+            $isUnreachable = $previousNode->getAttribute(AttributeKey::IS_UNREACHABLE);
+            if ($isUnreachable === true) {
+                return true;
+            }
+
+            $previousNode = $previousNode->getAttribute(AttributeKey::PREVIOUS_STATEMENT);
+        }
+
+        return false;
     }
 
     /**
@@ -120,15 +158,44 @@ CODE_SAMPLE
      */
     private function isAfterMarkTestSkippedMethodCall(Stmt $stmt): bool
     {
-        if (! $stmt instanceof Expression) {
-            return false;
+        return (bool) $this->betterNodeFinder->findFirstPrevious($stmt, function (Node $node): bool {
+            if (! $node instanceof MethodCall && ! $node instanceof StaticCall) {
+                return false;
+            }
+
+            if ($node->name instanceof MethodCall) {
+                return false;
+            }
+
+            if ($node->name instanceof StaticCall) {
+                return false;
+            }
+
+            return $this->isNames($node->name, ['markTestSkipped', 'markTestIncomplete']);
+        });
+    }
+
+    /**
+     * Check nodes that breaks scope while traversing up
+     */
+    private function isBreakingScopeNode(Stmt $stmt): bool
+    {
+        if ($stmt instanceof ClassLike) {
+            return true;
         }
 
-        $expr = $stmt->expr;
-        if ($expr instanceof MethodCall || $expr instanceof StaticCall) {
-            return $this->isNames($expr->name, ['markTestSkipped', 'markTestIncomplete']);
+        if ($stmt instanceof ClassMethod) {
+            return true;
         }
 
-        return false;
+        if ($stmt instanceof Function_) {
+            return true;
+        }
+
+        if ($stmt instanceof Namespace_) {
+            return true;
+        }
+
+        return $stmt instanceof Else_;
     }
 }
