8f49c1e26d5982905fa7e9ff6bb089dbb392b50e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-05-04 20:37:30 +0700

    remove require constant as well

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/../src/constants.php';
-
 // make local php-parser a priority to avoid conflict
 require_once __DIR__ . '/../preload.php';
 require_once __DIR__ . '/../vendor/autoload.php';
