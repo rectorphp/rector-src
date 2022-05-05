884d2bcb62da47b9198e3238942d0efefc295c5d
Tomas Votruba <tomas.vot@gmail.com>
2022-05-05 14:35:45 +0200

    keep former scope, to include history of previously traversed path

--- src/Application/ChangedNodeScopeRefresher.php
+++ src/Application/ChangedNodeScopeRefresher.php
@@ -7,6 +7,8 @@ namespace Rector\Core\Application;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Stmt;
 use PhpParser\Node\Stmt\Expression;
+use PHPStan\Analyser\NodeScopeResolver;
+use PHPStan\Analyser\Scope;
 use Rector\NodeTypeResolver\PHPStan\Scope\PHPStanNodeScopeResolver;
 use Symplify\SmartFileSystem\SmartFileInfo;
 
@@ -16,17 +18,22 @@ use Symplify\SmartFileSystem\SmartFileInfo;
 final class ChangedNodeScopeRefresher
 {
     public function __construct(
-        private readonly PHPStanNodeScopeResolver $phpStanNodeScopeResolver
+        private readonly PHPStanNodeScopeResolver $phpStanNodeScopeResolver,
+        private readonly NodeScopeResolver $nodeScopeResolver,
     ) {
     }
 
-    public function refresh(Expr|Stmt $node, SmartFileInfo $smartFileInfo): void
+    public function refresh(Expr|Stmt $node, SmartFileInfo $smartFileInfo, Scope $currentScope): void
     {
+        // note from flight: when we traverse ClassMethod, the scope must be already in Class_, otherwise it crashes
+        // so we need to somehow get a parent scope that is already in the same place the $node is
+
         if ($node instanceof Stmt) {
-            $this->phpStanNodeScopeResolver->processNodes([$node], $smartFileInfo);
+            $stmts = [$node];
         } else {
-            $stmt = new Expression($node);
-            $this->phpStanNodeScopeResolver->processNodes([$stmt], $smartFileInfo);
+            $stmts = [new Expression($node)];
         }
+
+        $this->phpStanNodeScopeResolver->processNodes($stmts, $smartFileInfo, $currentScope);
     }
 }
