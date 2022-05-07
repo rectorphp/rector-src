2e8fbddc768aa04f5b1fd3b76c95fde67b7a0706
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-05-07 17:32:53 +0700

    add comment

--- build/target-repository/e2e/container-configurator/rector.php
+++ build/target-repository/e2e/container-configurator/rector.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
+/**
+ * This e2e test is to be removed after fully migrated to RectorConfig
+ */
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
     $services->set(TypedPropertyRector::class);
