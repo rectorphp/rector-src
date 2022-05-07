47940bf4d8224230977e98ab06d985de5816082c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-05-07 17:40:17 +0700

    move to e2e-diff

--- build/target-repository/e2e/container-configurator/rector.php
+++ build/target-repository/e2e/container-configurator/rector.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Rector\Core\Configuration\Option;
 use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
@@ -9,6 +10,9 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
  * This e2e test is to be removed after fully migrated to RectorConfig
  */
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PATHS, [__DIR__ . '/src']);
+
     $services = $containerConfigurator->services();
     $services->set(TypedPropertyRector::class);
 };
