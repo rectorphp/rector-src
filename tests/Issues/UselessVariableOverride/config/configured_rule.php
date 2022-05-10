117272a5d246286d3e540b6f02bde7324a4d52a8
GitHub Action <action@github.com>
2022-05-10 12:09:30 +0000

    [ci-review] Rector Rectify

--- tests/Issues/UselessVariableOverride/config/configured_rule.php
+++ tests/Issues/UselessVariableOverride/config/configured_rule.php
@@ -2,9 +2,12 @@
 
 declare(strict_types=1);
 
+use Rector\CodeQuality\Rector\FunctionLike\SimplifyUselessVariableRector;
 use Rector\Config\RectorConfig;
 
+use Rector\DeadCode\Rector\FunctionLike\RemoveOverriddenValuesRector;
+
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rule(\Rector\CodeQuality\Rector\FunctionLike\SimplifyUselessVariableRector::class);
-    $rectorConfig->rule(\Rector\DeadCode\Rector\FunctionLike\RemoveOverriddenValuesRector::class);
+    $rectorConfig->rule(SimplifyUselessVariableRector::class);
+    $rectorConfig->rule(RemoveOverriddenValuesRector::class);
 };
