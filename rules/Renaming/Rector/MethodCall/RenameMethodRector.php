76001ae52eca6a2300f7a3bdd49d5a2cd4ef5652
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-05-10 12:25:50 +0700

    clean up

--- rules/Renaming/Rector/MethodCall/RenameMethodRector.php
+++ rules/Renaming/Rector/MethodCall/RenameMethodRector.php
@@ -48,7 +48,7 @@ final class RenameMethodRector extends AbstractRector implements ConfigurableRec
 
     public function getRuleDefinition(): RuleDefinition
     {
-        return new RuleDefinition('Turns method names to new ones when old one no longer exists.', [
+        return new RuleDefinition('Turns method names to new ones.', [
             new ConfiguredCodeSample(
                 <<<'CODE_SAMPLE'
 $someObject = new SomeExampleClass;
