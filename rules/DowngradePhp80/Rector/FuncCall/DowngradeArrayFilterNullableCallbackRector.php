9dc0e1c7a269f65b244c896ab2171873d02e9d07
Tomas Votruba <tomas.vot@gmail.com>
2022-05-11 20:16:33 +0200

    remove requires from tests, as all tests run on PHP 8.1

--- rules/DowngradePhp80/Rector/FuncCall/DowngradeArrayFilterNullableCallbackRector.php
+++ rules/DowngradePhp80/Rector/FuncCall/DowngradeArrayFilterNullableCallbackRector.php
@@ -157,6 +157,18 @@ CODE_SAMPLE
         $identical = new Identical($args[1]->value, $this->nodeFactory->createNull());
         $constFetch = new ConstFetch(new Name('ARRAY_FILTER_USE_BOTH'));
 
+<<<<<<< HEAD
+<<<<<<< HEAD
         return new Ternary($identical, $constFetch, isset($args[2]) ? $args[2]->value : new LNumber(0));
+=======
+        return new Ternary(
+            $identical,
+            $constFetch,
+            isset($args[2]) ? $args[2]->value : new LNumber(0)
+        );
+>>>>>>> fix const to LNumber
+=======
+        return new Ternary($identical, $constFetch, isset($args[2]) ? $args[2]->value : new LNumber(0));
+>>>>>>> typo
     }
 }
