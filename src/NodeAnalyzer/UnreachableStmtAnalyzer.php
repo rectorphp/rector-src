9da03ebf140eca90303a3c172bd74ca1654395a6
GitHub Action <action@github.com>
2022-05-12 16:12:03 +0000

    [ci-review] Rector Rectify

--- src/NodeAnalyzer/UnreachableStmtAnalyzer.php
+++ src/NodeAnalyzer/UnreachableStmtAnalyzer.php
@@ -9,9 +9,9 @@ use Rector\NodeTypeResolver\Node\AttributeKey;
 
 final class UnreachableStmtAnalyzer
 {
-    public function isStmtPHPStanUnreachable(Stmt $node): bool
+    public function isStmtPHPStanUnreachable(Stmt $stmt): bool
     {
-        $isUnreachable = $node->getAttribute(AttributeKey::IS_UNREACHABLE);
+        $isUnreachable = $stmt->getAttribute(AttributeKey::IS_UNREACHABLE);
         return $isUnreachable === true;
     }
 }
