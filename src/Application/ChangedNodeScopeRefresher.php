92ce0dc9ccdd371053e998cf549dc5a812424508
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-05-14 20:47:17 +0700

    [Core] Remove UnreachableStmtAnalyzer

--- src/Application/ChangedNodeScopeRefresher.php
+++ src/Application/ChangedNodeScopeRefresher.php
@@ -14,9 +14,6 @@ use PhpParser\Node\Stmt\Property;
 use PHPStan\Analyser\MutatingScope;
 use Rector\Core\Exception\ShouldNotHappenException;
 use Rector\Core\NodeAnalyzer\ScopeAnalyzer;
-use Rector\Core\NodeAnalyzer\UnreachableStmtAnalyzer;
-use Rector\Core\PhpParser\Node\BetterNodeFinder;
-use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\NodeTypeResolver\PHPStan\Scope\PHPStanNodeScopeResolver;
 use Symplify\SmartFileSystem\SmartFileInfo;
 
@@ -27,9 +24,7 @@ final class ChangedNodeScopeRefresher
 {
     public function __construct(
         private readonly PHPStanNodeScopeResolver $phpStanNodeScopeResolver,
-        private readonly ScopeAnalyzer $scopeAnalyzer,
-        private readonly UnreachableStmtAnalyzer $unreachableStmtAnalyzer,
-        private readonly BetterNodeFinder $betterNodeFinder
+        private readonly ScopeAnalyzer $scopeAnalyzer
     ) {
     }
 
@@ -38,26 +33,6 @@ final class ChangedNodeScopeRefresher
         // nothing to refresh
         if (! $this->scopeAnalyzer->hasScope($node)) {
             return;
-        }
-
-        if (! $mutatingScope instanceof MutatingScope) {
-            /**
-             * Node does not has Scope, while:
-             *
-             * 1. Node is Scope aware
-             * 2. Its current Stmt is Reachable
-             */
-            $currentStmt = $this->betterNodeFinder->resolveCurrentStatement($node);
-            if (! $this->unreachableStmtAnalyzer->isStmtPHPStanUnreachable($currentStmt)) {
-                $parent = $node->getAttribute(AttributeKey::PARENT_NODE);
-                $errorMessage = sprintf(
-                    'Node "%s" with parent of "%s" is missing scope required for scope refresh.',
-                    $node::class,
-                    $parent instanceof Node ? $parent::class : null
-                );
-
-                throw new ShouldNotHappenException($errorMessage);
-            }
         }
 
         // note from flight: when we traverse ClassMethod, the scope must be already in Class_, otherwise it crashes
