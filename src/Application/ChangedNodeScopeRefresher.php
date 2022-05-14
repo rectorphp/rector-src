c3dda2bc235b985be104df5bf84c44388fad1cf2
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-05-14 17:44:35 +0700

    fix

--- src/Application/ChangedNodeScopeRefresher.php
+++ src/Application/ChangedNodeScopeRefresher.php
@@ -58,6 +58,8 @@ final class ChangedNodeScopeRefresher
 
                 throw new ShouldNotHappenException($errorMessage);
             }
+
+            return;
         }
 
         // note from flight: when we traverse ClassMethod, the scope must be already in Class_, otherwise it crashes
