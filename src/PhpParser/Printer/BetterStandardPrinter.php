830d09073354269d224766a7808f6b913bd74974
GitHub Action <action@github.com>
2022-05-18 10:19:25 +0000

    [ci-review] Rector Rectify

--- src/PhpParser/Printer/BetterStandardPrinter.php
+++ src/PhpParser/Printer/BetterStandardPrinter.php
@@ -447,6 +447,29 @@ final class BetterStandardPrinter extends Standard implements NodePrinterInterfa
         return $result;
     }
 
+    /**
+     * Override parent pModifiers to set position of final and abstract modifier early, so instead of
+     *
+     *      public final const MY_CONSTANT = "Hello world!";
+     *
+     * it should be
+     *
+     *      final public const MY_CONSTANT = "Hello world!";
+     *
+     * @see https://github.com/rectorphp/rector/issues/6963
+     * @see https://github.com/nikic/PHP-Parser/pull/826
+     */
+    protected function pModifiers(int $modifiers)
+    {
+        return ($modifiers & Class_::MODIFIER_FINAL ? 'final ' : '')
+            . ($modifiers & Class_::MODIFIER_ABSTRACT ? 'abstract ' : '')
+            . ($modifiers & Class_::MODIFIER_PUBLIC ? 'public ' : '')
+            . ($modifiers & Class_::MODIFIER_PROTECTED ? 'protected ' : '')
+            . ($modifiers & Class_::MODIFIER_PRIVATE ? 'private ' : '')
+            . ($modifiers & Class_::MODIFIER_STATIC ? 'static ' : '')
+            . ($modifiers & Class_::MODIFIER_READONLY ? 'readonly ' : '');
+    }
+
     private function resolveContentOnExpr(Expr $expr, string $content): string
     {
         $parentNode = $expr->getAttribute(AttributeKey::PARENT_NODE);
@@ -522,28 +545,5 @@ final class BetterStandardPrinter extends Standard implements NodePrinterInterfa
     private function wrapValueWith(String_ $string, string $wrap): string
     {
         return $wrap . $string->value . $wrap;
-    }
-
-    /**
-     * Override parent pModifiers to set position of final and abstract modifier early, so instead of
-     *
-     *      public final const MY_CONSTANT = "Hello world!";
-     *
-     * it should be
-     *
-     *      final public const MY_CONSTANT = "Hello world!";
-     *
-     * @see https://github.com/rectorphp/rector/issues/6963
-     * @see https://github.com/nikic/PHP-Parser/pull/826
-     */
-    protected function pModifiers(int $modifiers)
-    {
-        return ($modifiers & Class_::MODIFIER_FINAL    ? 'final '     : '')
-            . ($modifiers & Class_::MODIFIER_ABSTRACT  ? 'abstract '  : '')
-            . ($modifiers & Class_::MODIFIER_PUBLIC    ? 'public '    : '')
-            . ($modifiers & Class_::MODIFIER_PROTECTED ? 'protected ' : '')
-            . ($modifiers & Class_::MODIFIER_PRIVATE   ? 'private '   : '')
-            . ($modifiers & Class_::MODIFIER_STATIC    ? 'static '    : '')
-            . ($modifiers & Class_::MODIFIER_READONLY  ? 'readonly ' : '');
     }
 }
