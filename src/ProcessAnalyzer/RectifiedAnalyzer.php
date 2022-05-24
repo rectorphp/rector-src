cba7f65950662672f7f8a7dc9ba5bbfbcad7bc28
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-05-25 01:33:56 +0700

    back

--- src/ProcessAnalyzer/RectifiedAnalyzer.php
+++ src/ProcessAnalyzer/RectifiedAnalyzer.php
@@ -66,7 +66,7 @@ final class RectifiedAnalyzer
         }
 
         if ($originalNode instanceof Node) {
-            return false;
+            return true;
         }
 
         $startTokenPos = $node->getStartTokenPos();
