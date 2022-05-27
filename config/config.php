df86c7058e4a6d2aa98129bfd1c6460d906c452a
Tomas Votruba <tomas.vot@gmail.com>
2022-05-27 14:18:19 +0200

    [DX] merge parameters to config

--- config/config.php
+++ config/config.php
@@ -4,12 +4,50 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Core\Bootstrap\ExtensionConfigResolver;
+use Rector\Core\Configuration\Option;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/services.php');
     $rectorConfig->import(__DIR__ . '/services-rules.php');
     $rectorConfig->import(__DIR__ . '/services-packages.php');
-    $rectorConfig->import(__DIR__ . '/parameters.php');
+
+    // paths and extensions
+    $rectorConfig->paths([]);
+
+    $parameters = $rectorConfig->parameters();
+    $parameters->set(Option::FILE_EXTENSIONS, ['php']);
+
+    $rectorConfig->autoloadPaths([]);
+
+    // these files will be executed, useful e.g. for constant definitions
+    $rectorConfig->bootstrapFiles([]);
+
+    // parallel
+    $rectorConfig->disableParallel();
+
+    $parameters->set(Option::PARALLEL_MAX_NUMBER_OF_PROCESSES, 16);
+    $parameters->set(Option::PARALLEL_JOB_SIZE, 20);
+    $parameters->set(Option::PARALLEL_TIMEOUT_IN_SECONDS, 120);
+
+    // FQN class importing
+    $rectorConfig->disableImportNames();
+    $rectorConfig->importShortClasses();
+
+    $parameters->set(Option::NESTED_CHAIN_METHOD_CALL_LIMIT, 60);
+
+    $rectorConfig->skip([]);
+
+    $parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, null);
+
+    // cache
+    $parameters->set(Option::CACHE_DIR, sys_get_temp_dir() . '/rector_cached_files');
+
+    // use faster in-memory cache in CI.
+    // CI always starts from scratch, therefore IO intensive caching is not worth it
+    $ciDetector = new OndraM\CiDetector\CiDetector();
+    if ($ciDetector->isCiDetected()) {
+        $parameters->set(Option::CACHE_CLASS, \Rector\Caching\ValueObject\Storage\MemoryCacheStorage::class);
+    }
 
     $extensionConfigResolver = new ExtensionConfigResolver();
     $extensionConfigFiles = $extensionConfigResolver->provide();
