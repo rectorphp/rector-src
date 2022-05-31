e3b5f777f3d69be635a2618033c655a08fbb54b4
Tomas Votruba <tomas.vot@gmail.com>
2022-05-31 16:06:08 +0200

    add generic test data provider

--- packages-tests/PhpAttribute/UseAliasNameMatcherTest.php
+++ packages-tests/PhpAttribute/UseAliasNameMatcherTest.php
@@ -16,16 +16,6 @@ use Rector\Tests\Php80\Rector\Class_\AnnotationToAttributeRector\Source\Attribut
 
 final class UseAliasNameMatcherTest extends AbstractTestCase
 {
-    /**
-     * @var string
-     */
-    private const USE_IMPORT_NAME = 'Rector\Tests\Php80\Rector\Class_\AnnotationToAttributeRector\Source\Annotation\OpenApi';
-
-    /**
-     * @var string
-     */
-    private const USE_ALIAS = 'OA';
-
     private UseAliasNameMatcher $useAliasNameMatcher;
 
     protected function setUp(): void
@@ -34,24 +24,49 @@ final class UseAliasNameMatcherTest extends AbstractTestCase
         $this->useAliasNameMatcher = $this->getService(UseAliasNameMatcher::class);
     }
 
-    public function test(): void
-    {
-        $annotationToAttribute = new AnnotationToAttribute(PastAnnotation::class, FutureAttribute::class);
-
-        $uses = [new Use_([new UseUse(new Name(self::USE_IMPORT_NAME), self::USE_ALIAS), self::USE_ALIAS])];
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(
+        AnnotationToAttribute $annotationToAttribute,
+        string $useImportName,
+        string $useAlias,
+        string $shortAnnotationName,
+        // attribute
+        string $expectedAttributeUseImportName,
+        string $expectedShortAttributeName,
+    ): void {
+        $uses = [new Use_([new UseUse(new Name($useImportName), $useAlias), $useAlias])];
 
         // uses
-        $useAliasMetadata = $this->useAliasNameMatcher->match($uses, '@OA\PastAnnotation', $annotationToAttribute);
+        $useAliasMetadata = $this->useAliasNameMatcher->match($uses, $shortAnnotationName, $annotationToAttribute);
 
         $this->assertInstanceOf(UseAliasMetadata::class, $useAliasMetadata);
 
         // test new use import
-        $this->assertSame('OA\FutureAttribute', $useAliasMetadata->getShortAttributeName());
+        $this->assertSame($expectedShortAttributeName, $useAliasMetadata->getShortAttributeName());
 
         // test new short attribute name
-        $this->assertSame(
+        $this->assertSame($expectedAttributeUseImportName, $useAliasMetadata->getUseImportName());
+    }
+
+    public function provideData(): \Iterator
+    {
+        yield [
+            // configuration
+            new AnnotationToAttribute(PastAnnotation::class, FutureAttribute::class),
+
+            // use import
+            'Rector\Tests\Php80\Rector\Class_\AnnotationToAttributeRector\Source\Annotation\OpenApi',
+            // use import alias
+            'OA',
+            // short attribute name
+            '@OA\PastAnnotation',
+
+            // expected attribute import
             'Rector\Tests\Php80\Rector\Class_\AnnotationToAttributeRector\Source\Attribute\OpenApi',
-            $useAliasMetadata->getUseImportName()
-        );
+            // expected attribute short name
+            'OA\FutureAttribute',
+        ];
     }
 }
