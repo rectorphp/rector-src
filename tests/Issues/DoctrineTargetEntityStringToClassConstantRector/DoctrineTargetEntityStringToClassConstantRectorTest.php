a5f3c60a210974619c17af8a04703db9d514c2e8
Dominik Ritter <dritter03@googlemail.com>
2022-05-31 23:54:21 +0200

    Add failing integration Test for DoctrineTargetEntityStringToClassConstantRector

---
+++ tests/Issues/DoctrineTargetEntityStringToClassConstantRector/DoctrineTargetEntityStringToClassConstantRectorTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Rector\Core\Tests\Issues\DoctrineTargetEntityStringToClassConstantRector;
+
+use Iterator;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+class DoctrineTargetEntityStringToClassConstantRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    /**
+     * @return Iterator<SmartFileInfo>
+     */
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/configured_rule.php';
+    }
+}
