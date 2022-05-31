01e1bdea768acc3b6193ee31da528f0559abce53
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-05-31 09:17:27 +0700

    comment note

--- e2e/parent-property-empty/src/ProfilePresenter.php
+++ e2e/parent-property-empty/src/ProfilePresenter.php
@@ -6,7 +6,9 @@ use Nette\Security\Passwords;
 use Nette;
 
 /**
- * Future note: multiple classes in single file is needed to reproduce the issue for parent of property is empty
+ * Future note: Don't extract this class
+ *
+ * multiple classes in this single file is needed to reproduce the issue for parent of property is empty
  */
 abstract class MyBasePresenter extends Nette\Application\UI\Presenter
 {
