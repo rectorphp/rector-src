c65df01bac104bbe712095a0b97ac8b955a0c8b0
Tomas Votruba <tomas.vot@gmail.com>
2022-06-04 17:09:29 +0200

    skip previous stmts, that we already iterated

--- rules/DeadCode/Rector/StmtsAwareInterface/RemoveJustPropertyFetchRector.php
+++ rules/DeadCode/Rector/StmtsAwareInterface/RemoveJustPropertyFetchRector.php
@@ -5,13 +5,16 @@ declare(strict_types=1);
 namespace Rector\DeadCode\Rector\StmtsAwareInterface;
 
 use PhpParser\Node;
+use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Expr\PropertyFetch;
 use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Stmt\Expression;
 use PHPStan\PhpDocParser\Ast\PhpDoc\VarTagValueNode;
 use Rector\Core\Contract\PhpParser\Node\StmtsAwareInterface;
 use Rector\Core\Rector\AbstractRector;
+use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\ReadWrite\NodeFinder\NodeUsageFinder;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
@@ -73,10 +76,16 @@ CODE_SAMPLE
      */
     public function refactor(Node $node): ?Node
     {
+        $stmts = (array) $node->stmts;
+        if ($stmts === []) {
+            return null;
+        }
+
         $variableUsages = [];
         $propertyFetch = null;
+        $currentStmtsKey = null;
 
-        foreach ((array) $node->stmts as $key => $stmt) {
+        foreach ($stmts as $key => $stmt) {
             if (! $stmt instanceof Expression) {
                 continue;
             }
@@ -107,19 +116,39 @@ CODE_SAMPLE
             $currentVariable = $assign->var;
             $propertyFetch = $assign->expr;
 
-            $variableUsages = $this->nodeUsageFinder->findVariableUsages([$node], $currentVariable);
+            $followingStmts = array_slice($stmts, $key + 1);
+            $variableUsages = $this->nodeUsageFinder->findVariableUsages($followingStmts, $currentVariable);
 
-            unset($node->stmts[$key]);
+            $currentStmtsKey = $key;
 
             // @todo validate the variable is not used in some place where property fetch cannot be used
             break;
         }
 
+        // filter out variable usages that are part of nested property fetch, or change variable
+        $variableUsages = $this->filterOutReferencedVariableUsages($variableUsages);
+
         if ($variableUsages === [] || $propertyFetch === null) {
             return null;
         }
 
-        $this->traverseNodesWithCallable($node, function (Node $node) use ($variableUsages, $propertyFetch) {
+        /** @var int $currentStmtsKey */
+        return $this->replaceVariablesWithPropertyFetch($node, $currentStmtsKey, $variableUsages, $propertyFetch);
+    }
+
+    /**
+     * @param Variable[] $variableUsages
+     */
+    private function replaceVariablesWithPropertyFetch(
+        StmtsAwareInterface $stmtsAware,
+        int $currentStmtsKey,
+        array $variableUsages,
+        PropertyFetch $propertyFetch
+    ): StmtsAwareInterface {
+        // remove assign node
+        unset($stmtsAware->stmts[$currentStmtsKey]);
+
+        $this->traverseNodesWithCallable($stmtsAware, function (Node $node) use ($variableUsages, $propertyFetch) {
             if (! in_array($node, $variableUsages, true)) {
                 return null;
             }
@@ -127,6 +156,33 @@ CODE_SAMPLE
             return $propertyFetch;
         });
 
-        return $node;
+        return $stmtsAware;
+    }
+
+    /**
+     * @param Variable[] $variableUsages
+     * @return Variable[]
+     */
+    private function filterOutReferencedVariableUsages(array $variableUsages): array
+    {
+        return array_filter($variableUsages, function (Variable $variable) {
+            $variableUsageParent = $variable->getAttribute(AttributeKey::PARENT_NODE);
+            if ($variableUsageParent instanceof Arg) {
+                $variableUsageParent = $variableUsageParent->getAttribute(AttributeKey::PARENT_NODE);
+            }
+
+            // skip nested property fetch, the assign is for purpose of named variable
+            if ($variableUsageParent instanceof PropertyFetch) {
+                return false;
+            }
+
+            if ($variableUsageParent instanceof FuncCall) {
+                if ($this->isName($variableUsageParent, 'array_pop')) {
+                    return false;
+                }
+            }
+
+            return true;
+        });
     }
 }
