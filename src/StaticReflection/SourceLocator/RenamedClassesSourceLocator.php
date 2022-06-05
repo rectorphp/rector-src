5ba7728177abbf68199b2c59d16cdb5078516cdc
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-06-05 07:17:28 +0700

    use equal to PHPStan\BetterReflection\Reflection\ReflectionFunction

--- src/StaticReflection/SourceLocator/RenamedClassesSourceLocator.php
+++ src/StaticReflection/SourceLocator/RenamedClassesSourceLocator.php
@@ -27,6 +27,13 @@ final class RenamedClassesSourceLocator implements SourceLocator
 
     public function locateIdentifier(Reflector $reflector, Identifier $identifier): ?Reflection
     {
+        $identifierType = $identifier->getType();
+        $typeName = $identifierType->getName();
+
+        if ($typeName === 'PHPStan\BetterReflection\Reflection\ReflectionFunction') {
+            return null;
+        }
+
         $identifierName = $identifier->getName();
         foreach ($this->renamedClassesDataCollector->getOldClasses() as $oldClass) {
             if ($identifierName !== $oldClass) {
@@ -55,13 +62,6 @@ final class RenamedClassesSourceLocator implements SourceLocator
         $fakeLocatedSource = new LocatedSource('virtual', null);
 
         $classReflector = new ClassReflector($this);
-        $reflectionClass = ReflectionClass::createFromNode($classReflector, $class, $fakeLocatedSource);
-
-        $locatedSource = $reflectionClass->getLocatedSource();
-        if ($locatedSource->getName() === null) {
-            return null;
-        }
-
-        return $reflectionClass;
+        return ReflectionClass::createFromNode($classReflector, $class, $fakeLocatedSource);
     }
 }
