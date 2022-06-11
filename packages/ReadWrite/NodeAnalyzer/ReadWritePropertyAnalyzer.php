3811c405c5743d971943639694777b8eae15d568
GitHub Action <action@github.com>
2022-06-11 03:03:43 +0000

    [ci-review] Rector Rectify

--- packages/ReadWrite/NodeAnalyzer/ReadWritePropertyAnalyzer.php
+++ packages/ReadWrite/NodeAnalyzer/ReadWritePropertyAnalyzer.php
@@ -55,7 +55,7 @@ final class ReadWritePropertyAnalyzer
         $currentStmt = $this->betterNodeFinder->resolveCurrentStatement($node);
         if ($currentStmt instanceof Stmt) {
             $hasAssignOp = $this->betterNodeFinder->findFirstInstanceOf($currentStmt, AssignOp::class);
-            if ($hasAssignOp) {
+            if ($hasAssignOp !== null) {
                 return true;
             }
         }
