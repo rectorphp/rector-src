224cb2a21f4b7f81836e9b777d67ba33e65eea46
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-06-15 23:02:21 +0700

    Scope already not exists only on DowngradeArrayIsListRector, which use FuncCall

--- e2e/parent-arg-from-func-call/rector.php
+++ e2e/parent-arg-from-func-call/rector.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\DowngradePhp81\Rector\FuncCall\DowngradeArrayIsListRector;
-use Rector\DowngradePhp81\Rector\FuncCall\DowngradeFirstClassCallableSyntaxRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -12,7 +11,6 @@ return static function (RectorConfig $rectorConfig): void {
     ]);
 
     $rectorConfig->rules([
-        DowngradeFirstClassCallableSyntaxRector::class,
         DowngradeArrayIsListRector::class,
     ]);
 };
