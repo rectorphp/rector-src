7580379116afbefd3a3795cb83a918cacf481c72
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-06-16 11:09:50 +0700

    scope check

--- src/NodeManipulator/PropertyManipulator.php
+++ src/NodeManipulator/PropertyManipulator.php
@@ -25,6 +25,7 @@ use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Property;
 use PhpParser\Node\Stmt\Trait_;
 use PhpParser\Node\Stmt\Unset_;
+use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\ClassReflection;
 use PHPStan\Reflection\ParametersAcceptorSelector;
 use PHPStan\Type\Type;
@@ -306,8 +307,13 @@ final class PropertyManipulator
             return false;
         }
 
+        $scope = $node->getAttribute(AttributeKey::SCOPE);
+        if (! $scope instanceof Scope) {
+            return false;
+        }
+
         $parametersAcceptor = ParametersAcceptorSelector::selectFromArgs(
-            $node->getAttribute(AttributeKey::SCOPE),
+            $scope,
             $node->getArgs(),
             $functionLikeReflection->getVariants()
         );
