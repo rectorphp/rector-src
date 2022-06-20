8656432099eee57959c2c111a270dd0dcad78c32
Tomas Votruba <tomas.vot@gmail.com>
2022-06-20 16:53:18 +0200

    skip privatization, as unknown side effect

--- rules/CodeQuality/Rector/Array_/CallableThisArrayToAnonymousFunctionRector.php
+++ rules/CodeQuality/Rector/Array_/CallableThisArrayToAnonymousFunctionRector.php
@@ -6,11 +6,8 @@ namespace Rector\CodeQuality\Rector\Array_;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\Array_;
-use PhpParser\Node\Stmt\Class_;
-use PhpParser\Node\Stmt\ClassMethod;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\Php\PhpMethodReflection;
-use PHPStan\Type\ThisType;
 use Rector\Core\Rector\AbstractScopeAwareRector;
 use Rector\Core\Reflection\ReflectionResolver;
 use Rector\NodeCollector\NodeAnalyzer\ArrayCallableMethodMatcher;
@@ -103,8 +100,6 @@ CODE_SAMPLE
             return null;
         }
 
-        $this->privatizeLocalClassMethod($arrayCallable, $node);
-
         $phpMethodReflection = $this->reflectionResolver->resolveMethodReflection(
             $arrayCallable->getClass(),
             $arrayCallable->getMethod(),
@@ -119,28 +114,5 @@ CODE_SAMPLE
             $phpMethodReflection,
             $arrayCallable->getCallerExpr()
         );
-    }
-
-    private function privatizeLocalClassMethod(ArrayCallable $arrayCallable, Array_ $array): void
-    {
-        $callerExpr = $arrayCallable->getCallerExpr();
-        $callerType = $this->getType($callerExpr);
-
-        // local method, lets make it private
-        if (! $callerType instanceof ThisType) {
-            return;
-        }
-
-        $methodName = $arrayCallable->getMethod();
-
-        $class = $this->betterNodeFinder->findParentType($array, Class_::class);
-        if (! $class instanceof Class_) {
-            return;
-        }
-
-        $currentClassMethod = $class->getMethod($methodName);
-        if ($currentClassMethod instanceof ClassMethod) {
-            $this->visibilityManipulator->makePrivate($currentClassMethod);
-        }
     }
 }
