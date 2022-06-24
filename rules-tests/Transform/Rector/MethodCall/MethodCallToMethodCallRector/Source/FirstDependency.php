5433f420ce9775597996f07e304255f67f399d4f
SelrahcD <c.desneuf@gmail.com>
2022-06-24 11:15:10 +0200

    Restore final

--- rules-tests/Transform/Rector/MethodCall/MethodCallToMethodCallRector/Source/FirstDependency.php
+++ rules-tests/Transform/Rector/MethodCall/MethodCallToMethodCallRector/Source/FirstDependency.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Rector\Tests\Transform\Rector\MethodCall\MethodCallToMethodCallRector\Source;
 
-class FirstDependency
+final class FirstDependency
 {
     public function go()
     {
