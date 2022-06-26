9cb3d45d9dec4fc5b0c60cde327e77acd3378b69
Tomas Votruba <tomas.vot@gmail.com>
2022-06-26 11:01:55 +0200

    rename ReturnTypeFromStrictReturnExprRector to ReturnTypeFromStrictBoolReturnExprRector

--- config/set/type-declaration-strict.php
+++ config/set/type-declaration-strict.php
@@ -7,7 +7,7 @@ use Rector\TypeDeclaration\Rector\ClassMethod\AddMethodCallBasedStrictParamTypeR
 use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ArrayShapeFromConstantArrayReturnRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromReturnNewRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictReturnExprRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictBoolReturnExprRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictTypedCallRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictTypedPropertyRector;
 use Rector\TypeDeclaration\Rector\Closure\AddClosureReturnTypeRector;
@@ -27,7 +27,7 @@ return static function (RectorConfig $rectorConfig): void {
         TypedPropertyFromStrictGetterMethodReturnTypeRector::class,
         AddMethodCallBasedStrictParamTypeRector::class,
         ArrayShapeFromConstantArrayReturnRector::class,
-        ReturnTypeFromStrictReturnExprRector::class,
+        ReturnTypeFromStrictBoolReturnExprRector::class,
     ]);
     $rectorConfig->rule(\Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictNativeFuncCallRector::class);
 };
