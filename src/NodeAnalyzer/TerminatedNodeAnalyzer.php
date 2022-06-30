945fcc81ba3030f276e0a2d0cc8dd554e5e3171e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-06-30 14:57:15 +0700

    [DeadCode] Handle always terminated Switch_ on RemoveUnreachableStatementRector (#2591)
    * [DeadCode] Handle always terminated Switch_ on RemoveUnreachableStatementRector
    
    * implemented :tada:
    
    * failing test case for no default
    
    * skip not has default
    
    * final touch: clean up

--- src/NodeAnalyzer/TerminatedNodeAnalyzer.php
+++ src/NodeAnalyzer/TerminatedNodeAnalyzer.php
@@ -5,13 +5,21 @@ declare(strict_types=1);
 namespace Rector\Core\NodeAnalyzer;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\Exit_;
 use PhpParser\Node\Stmt;
+use PhpParser\Node\Stmt\Break_;
+use PhpParser\Node\Stmt\Continue_;
 use PhpParser\Node\Stmt\Else_;
 use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\Finally_;
+use PhpParser\Node\Stmt\Goto_;
 use PhpParser\Node\Stmt\If_;
+use PhpParser\Node\Stmt\InlineHTML;
+use PhpParser\Node\Stmt\Label;
+use PhpParser\Node\Stmt\Nop;
 use PhpParser\Node\Stmt\Return_;
+use PhpParser\Node\Stmt\Switch_;
 use PhpParser\Node\Stmt\Throw_;
 use PhpParser\Node\Stmt\TryCatch;
 
@@ -22,26 +30,99 @@ final class TerminatedNodeAnalyzer
      */
     private const TERMINATED_NODES = [Return_::class, Throw_::class];
 
-    public function isAlwaysTerminated(TryCatch|If_ $node): bool
+    /**
+     * @var array<class-string<Node>>
+     */
+    private const TERMINABLE_NODES = [Throw_::class, Return_::class, Break_::class, Continue_::class];
+
+    /**
+     * @var array<class-string<Node>>
+     */
+    private const TERMINABLE_NODES_BY_ITS_STMTS = [TryCatch::class, If_::class, Switch_::class];
+
+    /**
+     * @var array<class-string<Node>>
+     */
+    private const ALLOWED_CONTINUE_CURRENT_STMTS = [InlineHTML::class, Nop::class];
+
+    public function isAlwaysTerminated(TryCatch|If_|Switch_|Node $node, Node $currentStmt): bool
     {
-        if ($node instanceof TryCatch) {
-            if ($node->finally instanceof Finally_ && $this->isTerminated($node->finally->stmts)) {
-                return true;
-            }
-
-            foreach ($node->catches as $catch) {
-                if (! $this->isTerminated($catch->stmts)) {
-                    return false;
-                }
-            }
-
-            return $this->isTerminated($node->stmts);
+        if (in_array($currentStmt::class, self::ALLOWED_CONTINUE_CURRENT_STMTS, true)) {
+            return false;
         }
 
-        return $this->isTerminatedIf($node);
+        if (! in_array($node::class, self::TERMINABLE_NODES_BY_ITS_STMTS, true)) {
+            return $this->isTerminatedNode($node, $currentStmt);
+        }
+
+        if ($node instanceof TryCatch) {
+            return $this->isTerminatedInLastStmtsTryCatch($node, $currentStmt);
+        }
+
+        if ($node instanceof If_) {
+            return $this->isTerminatedInLastStmtsIf($node, $currentStmt);
+        }
+
+        /** @var Switch_ $node */
+        return $this->isTerminatedInLastStmtsSwitch($node, $currentStmt);
     }
 
-    private function isTerminatedIf(If_ $if): bool
+    private function isTerminatedNode(Node $previousNode, Node $currentStmt): bool
+    {
+        if (in_array($previousNode::class, self::TERMINABLE_NODES, true)) {
+            return true;
+        }
+
+        if ($previousNode instanceof Expression && $previousNode->expr instanceof Exit_) {
+            return true;
+        }
+
+        if ($previousNode instanceof Goto_) {
+            return ! $currentStmt instanceof Label;
+        }
+
+        return false;
+    }
+
+    private function isTerminatedInLastStmtsSwitch(Switch_ $switch, Node $node): bool
+    {
+        if ($switch->cases === []) {
+            return false;
+        }
+
+        $hasDefault = false;
+        foreach ($switch->cases as $case) {
+            if (! $case->cond instanceof Expr) {
+                $hasDefault = true;
+            }
+
+            if (! $this->isTerminatedInLastStmts($case->stmts, $node)) {
+                return false;
+            }
+        }
+
+        return $hasDefault;
+    }
+
+    private function isTerminatedInLastStmtsTryCatch(TryCatch $tryCatch, Node $node): bool
+    {
+        if ($tryCatch->finally instanceof Finally_ && $this->isTerminatedInLastStmts(
+            $tryCatch->finally->stmts,
+            $node
+        )) {
+            return true;
+        }
+
+        foreach ($tryCatch->catches as $catch) {
+            if (! $this->isTerminatedInLastStmts($catch->stmts, $node)) {
+                return false;
+            }
+        }
+
+        return $this->isTerminatedInLastStmts($tryCatch->stmts, $node);
+    }
+
+    private function isTerminatedInLastStmtsIf(If_ $if, Node $node): bool
     {
         // Without ElseIf_[] and Else_, after If_ is possibly executable
         if ($if->elseifs === [] && ! $if->else instanceof Else_) {
@@ -49,12 +130,12 @@ final class TerminatedNodeAnalyzer
         }
 
         foreach ($if->elseifs as $elseIf) {
-            if (! $this->isTerminated($elseIf->stmts)) {
+            if (! $this->isTerminatedInLastStmts($elseIf->stmts, $node)) {
                 return false;
             }
         }
 
-        if (! $this->isTerminated($if->stmts)) {
+        if (! $this->isTerminatedInLastStmts($if->stmts, $node)) {
             return false;
         }
 
@@ -62,13 +143,13 @@ final class TerminatedNodeAnalyzer
             return false;
         }
 
-        return $this->isTerminated($if->else->stmts);
+        return $this->isTerminatedInLastStmts($if->else->stmts, $node);
     }
 
     /**
      * @param Stmt[] $stmts
      */
-    private function isTerminated(array $stmts): bool
+    private function isTerminatedInLastStmts(array $stmts, Node $node): bool
     {
         if ($stmts === []) {
             return false;
@@ -76,6 +157,10 @@ final class TerminatedNodeAnalyzer
 
         $lastKey = array_key_last($stmts);
         $lastNode = $stmts[$lastKey];
+
+        if (isset($stmts[$lastKey - 1]) && $this->isTerminatedNode($stmts[$lastKey - 1], $node)) {
+            return false;
+        }
 
         if ($lastNode instanceof Expression) {
             return $lastNode->expr instanceof Exit_;
