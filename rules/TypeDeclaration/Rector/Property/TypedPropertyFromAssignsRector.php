390300697c276a5128f8ea3edcc7cb39f9647acd
Tomas Votruba <tomas.vot@gmail.com>
2022-06-30 15:36:09 +0200

    more API

--- rules/TypeDeclaration/Rector/Property/TypedPropertyFromAssignsRector.php
+++ rules/TypeDeclaration/Rector/Property/TypedPropertyFromAssignsRector.php
@@ -30,6 +30,7 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 final class TypedPropertyFromAssignsRector extends AbstractRector implements AllowEmptyConfigurableRectorInterface
 {
     /**
+     * @api
      * @var string
      */
     public const INLINE_PUBLIC = 'inline_public';
