59abd57464fa933061d12f43b2348ea5b228e175
Tomas Votruba <tomas.vot@gmail.com>
2022-06-30 15:45:27 +0200

    cleanup

--- rules/DeadCode/Rector/StmtsAwareInterface/RemoveJustPropertyFetchRector.php
+++ rules/DeadCode/Rector/StmtsAwareInterface/RemoveJustPropertyFetchRector.php
@@ -144,10 +144,13 @@ CODE_SAMPLE
     /**
      * @param Stmt[] $followingStmts
      */
-    public function isFollowingStatementStaticClosure(array $followingStmts): bool
+    private function isFollowingStatementStaticClosure(array $followingStmts): bool
     {
-        return count($followingStmts) > 0
-            && $followingStmts[0] instanceof Expression
+        if ($followingStmts === []) {
+            return false;
+        }
+
+        return $followingStmts[0] instanceof Expression
             && $followingStmts[0]->expr instanceof Closure
             && $followingStmts[0]->expr->static;
     }
