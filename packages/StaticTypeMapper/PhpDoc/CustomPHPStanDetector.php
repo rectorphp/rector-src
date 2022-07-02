a4790af971c6895f8d0852f39a08b00ae64c29ae
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-07-03 02:45:40 +0700

    final touch: handle direct phpstan-type definition

--- packages/StaticTypeMapper/PhpDoc/CustomPHPStanDetector.php
+++ packages/StaticTypeMapper/PhpDoc/CustomPHPStanDetector.php
@@ -6,7 +6,9 @@ namespace Rector\StaticTypeMapper\PhpDoc;
 
 use PhpParser\Node;
 use PHPStan\PhpDocParser\Ast\PhpDoc\TypeAliasImportTagValueNode;
+use PHPStan\PhpDocParser\Ast\PhpDoc\TypeAliasTagValueNode;
 use PHPStan\Type\Type;
+use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfo;
 use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfoFactory;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\StaticTypeMapper\ValueObject\Type\NonExistingObjectType;
@@ -26,22 +28,54 @@ final class CustomPHPStanDetector
 
         // start from current Node to lookup parent
         $parentNode = $node;
+        $className = $definedType->getClassName();
 
         while ($parentNode instanceof Node) {
             $phpDocInfo = $this->phpDocInfoFactory->createFromNodeOrEmpty($parentNode);
-            $tagsByName = $phpDocInfo->getTagsByName('phpstan-import-type');
 
-            foreach ($tagsByName as $tags) {
-                if (! $tags->value instanceof TypeAliasImportTagValueNode) {
-                    continue;
-                }
+            if ($this->isUsePHPStanImportType($phpDocInfo, $className)) {
+                return true;
+            }
 
-                if ($tags->value->importedAlias === $definedType->getClassName()) {
-                    return true;
-                }
+            if ($this->isUsePHPStanType($phpDocInfo, $className)) {
+                return true;
             }
 
             $parentNode = $parentNode->getAttribute(AttributeKey::PARENT_NODE);
+        }
+
+        return false;
+    }
+
+    private function isUsePHPStanImportType(PhpDocInfo $phpDocInfo, string $className): bool
+    {
+        $tagsByName = $phpDocInfo->getTagsByName('phpstan-import-type');
+
+        foreach ($tagsByName as $tags) {
+            if (! $tags->value instanceof TypeAliasImportTagValueNode) {
+                continue;
+            }
+
+            if ($tags->value->importedAlias === $className) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function isUsePHPStanType(PhpDocInfo $phpDocInfo, string $className): bool
+    {
+        $tagsByName = $phpDocInfo->getTagsByName('phpstan-type');
+
+        foreach ($tagsByName as $tags) {
+            if (! $tags->value instanceof TypeAliasTagValueNode) {
+                continue;
+            }
+
+            if ($tags->value->alias === $className) {
+                return true;
+            }
         }
 
         return false;
