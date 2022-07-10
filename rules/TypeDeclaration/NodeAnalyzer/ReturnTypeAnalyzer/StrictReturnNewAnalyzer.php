7e1c2bb2b9a0b8d5342499e0dbb8ebee921769d0
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-07-10 22:41:07 +0700

    [TypeDeclaration] Skip Type modified between Assign and Return_ on ReturnTypeFromReturnNewRector (#2650)

--- rules/TypeDeclaration/NodeAnalyzer/ReturnTypeAnalyzer/StrictReturnNewAnalyzer.php
+++ rules/TypeDeclaration/NodeAnalyzer/ReturnTypeAnalyzer/StrictReturnNewAnalyzer.php
@@ -15,8 +15,10 @@ use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\Function_;
 use PhpParser\Node\Stmt\Return_;
+use PHPStan\Type\ObjectType;
 use Rector\Core\PhpParser\Node\BetterNodeFinder;
 use Rector\NodeNameResolver\NodeNameResolver;
+use Rector\NodeTypeResolver\NodeTypeResolver;
 use Rector\TypeDeclaration\ValueObject\AssignToVariable;
 
 final class StrictReturnNewAnalyzer
@@ -24,6 +26,7 @@ final class StrictReturnNewAnalyzer
     public function __construct(
         private readonly BetterNodeFinder $betterNodeFinder,
         private readonly NodeNameResolver $nodeNameResolver,
+        private readonly NodeTypeResolver $nodeTypeResolver
     ) {
     }
 
@@ -63,11 +66,26 @@ final class StrictReturnNewAnalyzer
             return null;
         }
 
+        $returnType = $this->nodeTypeResolver->getType($onlyReturn->expr);
+
+        if (! $returnType instanceof ObjectType) {
+            return null;
+        }
+
         $createdVariablesToTypes = $this->resolveCreatedVariablesToTypes($functionLike);
 
         $returnedVariableName = $this->nodeNameResolver->getName($onlyReturn->expr);
 
-        return $createdVariablesToTypes[$returnedVariableName] ?? null;
+        $className = $createdVariablesToTypes[$returnedVariableName] ?? null;
+        if (! is_string($className)) {
+            return $className;
+        }
+
+        if ($returnType->getClassName() === $className) {
+            return $className;
+        }
+
+        return null;
     }
 
     /**
