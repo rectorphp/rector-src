2dcbad1d31f2737d111ab9f9ea0a7c15da1d5bc0
Tomas Votruba <tomas.vot@gmail.com>
2022-07-16 11:03:16 +0200

    add property name validator, to ensure the name is vlaid

--- src/Validation/RectorAssert.php
+++ src/Validation/RectorAssert.php
@@ -20,15 +20,51 @@ final class RectorAssert
     private const CLASS_NAME_REGEX = '#^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*(\\\\[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)*$#';
 
     /**
+     * @see https://www.php.net/manual/en/language.variables.basics.php
+     * @see https://regex101.com/r/hFw17T/1
+     *
+     * @var string
+     */
+    private const PROPERTY_NAME_REGEX = '#^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$#';
+
+    /**
+     * @see https://regex101.com/r/uh5B0S/1
+     * @see https://www.php.net/manual/en/functions.user-defined.php
+     *
+     * @var string
+     */
+    private const METHOD_NAME_REGEX = '#^[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*$#';
+
+    /**
      * Assert value is valid class name
      */
-    public static function className(string $className): void
+    public static function className(string $name): void
     {
-        if (StringUtils::isMatch($className, self::CLASS_NAME_REGEX)) {
+        if (StringUtils::isMatch($name, self::CLASS_NAME_REGEX)) {
             return;
         }
 
-        $errorMessage = $className . ' is not a valid class name';
+        $errorMessage = sprintf('"%s" is not a valid class name', $name);
+        throw new InvalidArgumentException($errorMessage);
+    }
+
+    public static function propertyName(string $name): void
+    {
+        if (StringUtils::isMatch($name, self::PROPERTY_NAME_REGEX)) {
+            return;
+        }
+
+        $errorMessage = sprintf('"%s" is not a valid property name', $name);
+        throw new InvalidArgumentException($errorMessage);
+    }
+
+    public static function methodName(string $name): void
+    {
+        if (StringUtils::isMatch($name, self::METHOD_NAME_REGEX)) {
+            return;
+        }
+
+        $errorMessage = sprintf('"%s" is not a valid method name', $name);
         throw new InvalidArgumentException($errorMessage);
     }
 }
