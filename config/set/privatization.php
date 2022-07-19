56961a504f5165bb16f0eab6312d690cd8d88acd
Tomas Votruba <tomas.vot@gmail.com>
2022-07-19 22:40:06 +0200

    [DX] Remove RepeatedLiteralToClassConstantRector, as very narrow use case; use rather phpstan

--- config/set/privatization.php
+++ config/set/privatization.php
@@ -7,7 +7,6 @@ use Rector\Privatization\Rector\Class_\ChangeGlobalVariablesToPropertiesRector;
 use Rector\Privatization\Rector\Class_\ChangeLocalPropertyToVariableRector;
 use Rector\Privatization\Rector\Class_\ChangeReadOnlyVariableWithDefaultValueToConstantRector;
 use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
-use Rector\Privatization\Rector\Class_\RepeatedLiteralToClassConstantRector;
 use Rector\Privatization\Rector\ClassMethod\PrivatizeFinalClassMethodRector;
 use Rector\Privatization\Rector\MethodCall\PrivatizeLocalGetterToPropertyRector;
 use Rector\Privatization\Rector\Property\ChangeReadOnlyPropertyWithDefaultValueToConstantRector;
@@ -18,7 +17,6 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(ChangeGlobalVariablesToPropertiesRector::class);
     $rectorConfig->rule(ChangeReadOnlyPropertyWithDefaultValueToConstantRector::class);
     $rectorConfig->rule(ChangeReadOnlyVariableWithDefaultValueToConstantRector::class);
-    $rectorConfig->rule(RepeatedLiteralToClassConstantRector::class);
     $rectorConfig->rule(PrivatizeLocalGetterToPropertyRector::class);
     $rectorConfig->rule(PrivatizeFinalClassPropertyRector::class);
     $rectorConfig->rule(PrivatizeFinalClassMethodRector::class);
