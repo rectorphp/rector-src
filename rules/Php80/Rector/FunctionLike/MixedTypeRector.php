8a57e37ffc9ae4bf55de700c5820fc490c80af25
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-07-22 14:14:47 +0700

    apply type param

--- rules/Php80/Rector/FunctionLike/MixedTypeRector.php
+++ rules/Php80/Rector/FunctionLike/MixedTypeRector.php
@@ -4,13 +4,18 @@ declare(strict_types=1);
 
 namespace Rector\Php80\Rector\FunctionLike;
 
+use PHPStan\Type\MixedType;
 use PhpParser\Node;
 use PhpParser\Node\Expr\ArrowFunction;
 use PhpParser\Node\Expr\Closure;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Name;
 use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Function_;
+use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfo;
 use Rector\Core\Rector\AbstractRector;
 use Rector\Core\ValueObject\PhpVersionFeature;
+use Rector\DeadCode\PhpDoc\TagRemover\ParamTagRemover;
 use Rector\VersionBonding\Contract\MinPhpVersionInterface;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
@@ -20,6 +25,12 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
  */
 final class MixedTypeRector extends AbstractRector implements MinPhpVersionInterface
 {
+    private bool $hasChanged = false;
+
+    public function __construct(private readonly ParamTagRemover $paramTagRemover)
+    {
+    }
+
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition(
@@ -64,7 +75,37 @@ CODE_SAMPLE
      */
     public function refactor(Node $node): ?Node
     {
+        $phpDocInfo = $this->phpDocInfoFactory->createFromNodeOrEmpty($node);
+
+        $this->refactorParamTypes($node, $phpDocInfo);
+
+        if (! $this->hasChanged) {
+            return null;
+        }
+
+        $this->paramTagRemover->removeParamTagsIfUseless($phpDocInfo, $node);
         return $node;
+    }
+
+    private function refactorParamTypes(
+        ClassMethod | Function_ | Closure | ArrowFunction $functionLike,
+        PhpDocInfo $phpDocInfo
+    ): void {
+        foreach ($functionLike->params as $param) {
+            if ($param->type instanceof Node) {
+                continue;
+            }
+
+            $paramName = $this->getName($param->var);
+            $paramType = $phpDocInfo->getParamType($paramName);
+
+            if (! $paramType instanceof MixedType) {
+                continue;
+            }
+
+            $this->hasChanged = true;
+            $param->type = new Name('mixed');
+        }
     }
 
     public function provideMinPhpVersion(): int
