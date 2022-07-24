86d64ff344363d263837063788689ab2fb6c29cf
GitHub Action <action@github.com>
2022-07-24 05:17:40 +0000

    [ci-review] Rector Rectify

--- tests/Issues/Issue7306/config/configured_rule.php
+++ tests/Issues/Issue7306/config/configured_rule.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-use Rector\Config\RectorConfig;
 use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
 use Rector\CodeQuality\Rector\If_\SimplifyIfNotNullReturnRector;
 use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
+use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rules([
