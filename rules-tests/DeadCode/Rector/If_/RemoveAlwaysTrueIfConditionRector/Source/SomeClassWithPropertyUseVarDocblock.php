98e0347898c9eaf376a89f3a71baaa623cf577f4
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-07-28 00:48:55 +0700

    add failing test case for call outside object

---
+++ rules-tests/DeadCode/Rector/If_/RemoveAlwaysTrueIfConditionRector/Source/SomeClassWithPropertyUseVarDocblock.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Rector\Tests\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector\Source;
+
+class SomeClassWithPropertyUseVarDocblock
+{
+    /** @var \DateTime */
+    public $property;
+}
