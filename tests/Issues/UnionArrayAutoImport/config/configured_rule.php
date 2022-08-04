be70ce6f9aa38568d743d7b35e13446e5fd85290
GitHub Action <action@github.com>
2022-08-04 08:17:39 +0000

    [ci-review] Rector Rectify

--- tests/Issues/UnionArrayAutoImport/config/configured_rule.php
+++ tests/Issues/UnionArrayAutoImport/config/configured_rule.php
@@ -7,9 +7,6 @@ use Rector\Php80\Rector\FunctionLike\UnionTypesRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddArrayReturnDocTypeRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rules([
-        UnionTypesRector::class,
-        AddArrayReturnDocTypeRector::class,
-    ]);
+    $rectorConfig->rules([UnionTypesRector::class, AddArrayReturnDocTypeRector::class]);
     $rectorConfig->importNames();
 };
