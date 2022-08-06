c0554a881998cda8e87b6822e533803266eace23
GitHub Action <action@github.com>
2022-08-06 14:54:27 +0000

    [ci-review] Rector Rectify

--- packages/NodeTypeResolver/PHPStan/ObjectWithoutClassTypeWithParentTypes.php
+++ packages/NodeTypeResolver/PHPStan/ObjectWithoutClassTypeWithParentTypes.php
@@ -5,22 +5,19 @@ declare(strict_types=1);
 namespace Rector\NodeTypeResolver\PHPStan;
 
 use PHPStan\Type\ObjectWithoutClassType;
+use PHPStan\Type\Type;
 use PHPStan\Type\TypeWithClassName;
 
 final class ObjectWithoutClassTypeWithParentTypes extends ObjectWithoutClassType
 {
     /**
-     * @var TypeWithClassName[]
-     */
-    private array $parentTypes;
-
-    /**
      * @param TypeWithClassName[] $parentTypes
      */
-    public function __construct(array $parentTypes, ?\PHPStan\Type\Type $subtractedType = null)
-    {
+    public function __construct(
+        private readonly array $parentTypes,
+        ?Type $subtractedType = null
+    ) {
         parent::__construct($subtractedType);
-        $this->parentTypes = $parentTypes;
     }
 
     /**
