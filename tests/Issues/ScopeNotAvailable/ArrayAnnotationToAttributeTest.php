75a94efc6d157a5dbb2bd857030350d431407fc1
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-08-09 23:10:33 +0700

    [Php54][Php80] Handle no scope on LongArrayToShortArrayRector+AnnotationToAttributeRector

---
+++ tests/Issues/ScopeNotAvailable/ArrayAnnotationToAttributeTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Core\Tests\Issues\ArrayAnnotationToAttributeTest;
+
+use Iterator;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class ArrayAnnotationToAttributeTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    /**
+     * @return Iterator<SmartFileInfo>
+     */
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/FixtureArrayAnnotationToAttribute');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/array_annotation_to_attribute.php';
+    }
+}
---
+++ tests/Issues/ScopeNotAvailable/config/array_annotation_to_attribute.php
@@ -0,0 +1,16 @@
+<?php
+
+declare (strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php54\Rector\Array_\LongArrayToShortArrayRector;
+use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
+use Rector\Php80\ValueObject\AnnotationToAttribute;
+
+return static function (RectorConfig $rectorConfig) : void {
+    $rectorConfig->rule(LongArrayToShortArrayRector::class);
+
+    $rectorConfig->ruleWithConfiguration(AnnotationToAttributeRector::class, [
+        new AnnotationToAttribute('Doctrine\ORM\Mapping\ManyToOne'),
+    ]);
+};
