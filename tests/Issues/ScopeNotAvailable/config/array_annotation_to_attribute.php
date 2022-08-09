2edec5defd8947b68b46e0177868e02179ef6b70
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-08-10 03:38:03 +0700

    cs fix

--- tests/Issues/ScopeNotAvailable/config/array_annotation_to_attribute.php
+++ tests/Issues/ScopeNotAvailable/config/array_annotation_to_attribute.php
@@ -1,13 +1,13 @@
 <?php
 
-declare (strict_types=1);
+declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Php54\Rector\Array_\LongArrayToShortArrayRector;
 use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
 use Rector\Php80\ValueObject\AnnotationToAttribute;
 
-return static function (RectorConfig $rectorConfig) : void {
+return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(LongArrayToShortArrayRector::class);
 
     $rectorConfig->ruleWithConfiguration(AnnotationToAttributeRector::class, [
