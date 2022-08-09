4b7522ad5d325c04e6fc1300f6d8c9c361a5f175
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-08-09 23:12:10 +0700

    Fixed :tada:

--- src/Application/ChangedNodeScopeRefresher.php
+++ src/Application/ChangedNodeScopeRefresher.php
@@ -70,7 +70,9 @@ final class ChangedNodeScopeRefresher
 
         $parentNode = $node->getAttribute(AttributeKey::PARENT_NODE);
         if (! $mutatingScope instanceof MutatingScope && $node instanceof Expr && $parentNode instanceof Node) {
-            $mutatingScope = $parentNode->getAttribute(AttributeKey::SCOPE);
+            $mutatingScope = $this->scopeAnalyzer->hasScope($parentNode)
+                ? $parentNode->getAttribute(AttributeKey::SCOPE)
+                : $this->scopeFactory->createFromFile($smartFileInfo);
         }
 
         if (! $mutatingScope instanceof MutatingScope) {
