f298af1f0dfd4cc3ca0632a7632d295f3569f897
Ethan Brigham <ebrigham1@gmail.com>
2022-08-10 18:04:05 -0400

    Correct right hand regex and update tests

--- rules/Php73/Rector/FuncCall/RegexDashEscapeRector.php
+++ rules/Php73/Rector/FuncCall/RegexDashEscapeRector.php
@@ -42,7 +42,7 @@ final class RegexDashEscapeRector extends AbstractRector implements MinPhpVersio
      * @var string
      * @see https://regex101.com/r/TBVme9/8
      */
-    private const RIGHT_HAND_UNESCAPED_DASH_REGEX = '#(?<!\[)-(\\\\(w|s|d)[.*]?)\]#i';
+    private const RIGHT_HAND_UNESCAPED_DASH_REGEX = '#(?<!\[)(?<!\\\\)-(\\\\(w|s|d)[.*]?)\]#i';
 
     private bool $hasChanged = false;
 
