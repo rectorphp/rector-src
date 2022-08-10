c0070b1d30a0040e3e58724ac1b3b339e168dcb5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-08-10 18:07:57 +0700

    [CodeQuality] Skip not ArrayType on ForeachItemsAssignToEmptyArrayToAssignRector (#2752)
    Co-authored-by: Gocha Ossinkine <ossinkine@ya.ru>

--- rules/CodeQuality/Rector/Foreach_/ForeachItemsAssignToEmptyArrayToAssignRector.php
+++ rules/CodeQuality/Rector/Foreach_/ForeachItemsAssignToEmptyArrayToAssignRector.php
@@ -9,8 +9,7 @@ use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Stmt\Foreach_;
 use PHPStan\Analyser\Scope;
-use PHPStan\Type\ObjectType;
-use PHPStan\Type\ThisType;
+use PHPStan\Type\ArrayType;
 use Rector\CodeQuality\NodeAnalyzer\ForeachAnalyzer;
 use Rector\Core\Rector\AbstractScopeAwareRector;
 use Rector\NodeNestingScope\ValueObject\ControlStructure;
@@ -117,18 +116,7 @@ CODE_SAMPLE
         }
 
         $type = $scope->getType($foreach->expr);
-
-        if ($type instanceof ObjectType) {
-            return $type->isIterable()
-                ->yes();
-        }
-
-        if ($type instanceof ThisType) {
-            return $type->isIterable()
-                ->yes();
-        }
-
-        return false;
+        return ! $type instanceof ArrayType;
     }
 
     private function shouldSkipAsPartOfOtherLoop(Foreach_ $foreach): bool
