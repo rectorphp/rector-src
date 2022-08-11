2667f35cc72a6f7da454635a24bdc3ede61082ae
Tomas Votruba <tomas.vot@gmail.com>
2022-08-11 16:58:37 +0200

    Improve ReturnTypeWillChangeRector to handle any method of defined type; move PhpDocFromTypeDeclarationDecorator to Downgrade rules (#2754)

--- tests/Issues/IssueDowngradeThrowableType/config/configured_rule.php
+++ tests/Issues/IssueDowngradeThrowableType/config/configured_rule.php
@@ -3,10 +3,13 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Core\ValueObject\PhpVersion;
 use Rector\DowngradePhp70\Rector\FunctionLike\DowngradeThrowableTypeDeclarationRector;
 use Rector\DowngradePhp74\Rector\ArrowFunction\ArrowFunctionToAnonymousFunctionRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(ArrowFunctionToAnonymousFunctionRector::class);
     $rectorConfig->rule(DowngradeThrowableTypeDeclarationRector::class);
+
+    $rectorConfig->phpVersion(PhpVersion::PHP_70);
 };
