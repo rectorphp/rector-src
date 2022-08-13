b05501d199883b3d46dc189f01190cb3597411ad
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-08-13 17:11:49 +0700

    reduce to RenameFucntionRector, ForRepeatedCountToOwnVariableRector, then LevelSetList::UP_TO_PHP_81

--- tests/Issues/IndentationCrash/config/configured_rule.php
+++ tests/Issues/IndentationCrash/config/configured_rule.php
@@ -2,10 +2,17 @@
 
 declare(strict_types=1);
 
+use Rector\CodeQuality\Rector\For_\ForRepeatedCountToOwnVariableRector;
 use Rector\Config\RectorConfig;
+use Rector\Renaming\Rector\FuncCall\RenameFunctionRector;
 use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->sets([LevelSetList::UP_TO_PHP_81, SetList::CODE_QUALITY]);
+    $rectorConfig
+        ->ruleWithConfiguration(RenameFunctionRector::class, [
+                'sizeof' => 'count',
+            ]);
+    $rectorConfig->rule(ForRepeatedCountToOwnVariableRector::class);
+
+    $rectorConfig->sets([LevelSetList::UP_TO_PHP_81]);
 };
