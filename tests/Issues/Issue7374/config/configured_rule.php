bd041ee3152d720c20560d65d0cae17491c8da66
Alejo Moreira <alejo.moreira@729solutions.com>
2022-08-15 16:49:50 -0300

    Add files via upload

---
+++ tests/Issues/Issue7374/config/configured_rule.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodParameterRector;
+use Rector\Naming\Rector\ClassMethod\RenameVariableToMatchNewTypeRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rules([
+        RemoveUnusedPrivateMethodParameterRector::class,
+        RenameVariableToMatchNewTypeRector::class
+    ]);
+};
