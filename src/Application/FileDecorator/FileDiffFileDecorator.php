a334916705654ec707fa0ccd202fa425f7619b15
Tomas Votruba <tomas.vot@gmail.com>
2022-08-24 13:48:20 +0200

    remove self-serving FileDiffFileDecorator interface

--- src/Application/FileDecorator/FileDiffFileDecorator.php
+++ src/Application/FileDecorator/FileDiffFileDecorator.php
@@ -5,10 +5,9 @@ declare(strict_types=1);
 namespace Rector\Core\Application\FileDecorator;
 
 use Rector\ChangesReporting\ValueObjectFactory\FileDiffFactory;
-use Rector\Core\Contract\Application\FileDecoratorInterface;
 use Rector\Core\ValueObject\Application\File;
 
-final class FileDiffFileDecorator implements FileDecoratorInterface
+final class FileDiffFileDecorator
 {
     public function __construct(
         private readonly FileDiffFactory $fileDiffFactory
