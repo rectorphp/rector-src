7e4141b30175401addbd2196b02449493436cf97
Tomas Votruba <tomas.vot@gmail.com>
2022-08-24 10:30:16 +0200

    [DeadCode] Merge RemoveDeadConstructorRector, to RemoveEmptyClassMethodRector with same behavior

--- e2e/applied-rule-removed-node/rector.php
+++ e2e/applied-rule-removed-node/rector.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\DeadCode\Rector\ClassMethod\RemoveDeadConstructorRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveEmptyClassMethodRector;
 use Rector\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -11,6 +11,6 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__ . '/src',
     ]);
 
-    $rectorConfig->rule(RemoveDeadConstructorRector::class);
+    $rectorConfig->rule(RemoveEmptyClassMethodRector::class);
     $rectorConfig->rule(RemoveAlwaysTrueIfConditionRector::class);
 };
--- tests/Issues/AlwaysTrueIfInDeadConstructor/config/configured_rule.php
+++ tests/Issues/AlwaysTrueIfInDeadConstructor/config/configured_rule.php
@@ -3,10 +3,10 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\DeadCode\Rector\ClassMethod\RemoveDeadConstructorRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveEmptyClassMethodRector;
 use Rector\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(RemoveAlwaysTrueIfConditionRector::class);
-    $rectorConfig->rule(RemoveDeadConstructorRector::class);
+    $rectorConfig->rule(RemoveEmptyClassMethodRector::class);
 };
