5b4fe34915ffc4b66ccbaec41cdb267f107ba4ee
Tomas Votruba <tomas.vot@gmail.com>
2022-08-29 19:52:26 +0200

    [DX] Make use of file path helper

--- packages/ChangesReporting/ValueObjectFactory/FileDiffFactory.php
+++ packages/ChangesReporting/ValueObjectFactory/FileDiffFactory.php
@@ -5,27 +5,23 @@ declare(strict_types=1);
 namespace Rector\ChangesReporting\ValueObjectFactory;
 
 use Rector\Core\Differ\DefaultDiffer;
+use Rector\Core\FileSystem\FilePathHelper;
 use Rector\Core\ValueObject\Application\File;
 use Rector\Core\ValueObject\Reporting\FileDiff;
 use Symplify\PackageBuilder\Console\Output\ConsoleDiffer;
-use Symplify\SmartFileSystem\SmartFileSystem;
 
 final class FileDiffFactory
 {
     public function __construct(
         private readonly DefaultDiffer $defaultDiffer,
         private readonly ConsoleDiffer $consoleDiffer,
-        private readonly SmartFileSystem $smartFileSystem,
+        private readonly FilePathHelper $filePathHelper,
     ) {
     }
 
     public function createFileDiff(File $file, string $oldContent, string $newContent): FileDiff
     {
-        $relativeFilePath = $this->smartFileSystem->makePathRelative(
-            $file->getFilePath(),
-            (string) realpath(getcwd())
-        );
-        $relativeFilePath = \rtrim($relativeFilePath, '/');
+        $relativeFilePath = $this->filePathHelper->relativePath($file->getFilePath());
 
         // always keep the most recent diff
         return new FileDiff(
