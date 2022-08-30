201bf5a0a4f893e962e68995e3855bad572e00a5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-08-30 17:37:42 +0700

    phpstan

--- rules/Php74/Tokenizer/ParenthesizedNestedTernaryAnalyzer.php
+++ rules/Php74/Tokenizer/ParenthesizedNestedTernaryAnalyzer.php
@@ -4,16 +4,15 @@ declare(strict_types=1);
 
 namespace Rector\Php74\Tokenizer;
 
-use PhpParser\Node;
 use PhpParser\Node\Expr\Ternary;
 use Rector\Core\ValueObject\Application\File;
 
 final class ParenthesizedNestedTernaryAnalyzer
 {
-    public function isParenthesized(File $file, Node $node): bool
+    public function isParenthesized(File $file, Ternary $ternary): bool
     {
         $oldTokens = $file->getOldTokens();
-        $startTokenPos = $node->getStartTokenPos();
+        $startTokenPos = $ternary->getStartTokenPos();
 
         return isset($oldTokens[$startTokenPos]) && $oldTokens[$startTokenPos] === '(';
     }
