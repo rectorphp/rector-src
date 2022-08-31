3addb8f8783cb5efbbbc80ef2a7c143487d19398
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-08-31 17:08:55 +0700

    [EarlyReturn] Skip else has inline html on RemoveAlwaysElseRector (#2870)
    * Add failing test fixture for RemoveAlwaysElseRector
    
    # Failing Test for RemoveAlwaysElseRector
    
    Based on https://getrector.org/demo/eb0f5c8f-86e3-40ed-bb38-86675496f0c8
    
    * rename fixture
    
    * Closes #2867 Fixes https://github.com/rectorphp/rector/issues/7441
    
    * rename fixture
    
    * clean up
    
    Co-authored-by: kkmuffme <11071985+kkmuffme@users.noreply.github.com>

--- rules/EarlyReturn/Rector/If_/RemoveAlwaysElseRector.php
+++ rules/EarlyReturn/Rector/If_/RemoveAlwaysElseRector.php
@@ -13,6 +13,7 @@ use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\If_;
 use PhpParser\Node\Stmt\Return_;
 use PhpParser\Node\Stmt\Throw_;
+use Rector\Core\NodeAnalyzer\InlineHTMLAnalyzer;
 use Rector\Core\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
@@ -22,6 +23,10 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
  */
 final class RemoveAlwaysElseRector extends AbstractRector
 {
+    public function __construct(private readonly InlineHTMLAnalyzer $inlineHTMLAnalyzer)
+    {
+    }
+
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition(
@@ -92,7 +97,9 @@ CODE_SAMPLE
             $this->mirrorComments($node, $firstElseIf);
 
             $nodesToReturnAfterNode = $this->getStatementsElseIfs($node);
-            if ($originalNode->else instanceof Else_) {
+            if ($originalNode->else instanceof Else_ && ! $this->inlineHTMLAnalyzer->hasInlineHTML(
+                $originalNode->else
+            )) {
                 $node->else = null;
                 $nodesToReturnAfterNode = array_merge($nodesToReturnAfterNode, [$originalNode->else]);
             }
@@ -100,7 +107,7 @@ CODE_SAMPLE
             return [$if, $node, ...$nodesToReturnAfterNode];
         }
 
-        if ($node->else !== null) {
+        if ($node->else instanceof Else_ && ! $this->inlineHTMLAnalyzer->hasInlineHTML($node->else)) {
             $stmts = $node->else->stmts;
             $node->else = null;
 
