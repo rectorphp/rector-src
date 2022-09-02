a911c0898e3bbee10a08bc9f62c36edb99d8310b
Tomas Votruba <tomas.vot@gmail.com>
2022-09-02 22:58:58 +0200

    [Transform] Skip transitional interface in AddInterfaceByTraitRector (#2898)
    Co-authored-by: buffcode <buffcode@users.noreply.github.com>

--- rules-tests/Transform/Rector/Class_/AddInterfaceByTraitRector/config/configured_rule.php
+++ rules-tests/Transform/Rector/Class_/AddInterfaceByTraitRector/config/configured_rule.php
@@ -3,13 +3,16 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Tests\Transform\Rector\Class_\AddInterfaceByTraitRector\Source\AnotherTrait;
 use Rector\Tests\Transform\Rector\Class_\AddInterfaceByTraitRector\Source\SomeInterface;
 use Rector\Tests\Transform\Rector\Class_\AddInterfaceByTraitRector\Source\SomeTrait;
+use Rector\Tests\Transform\Rector\Class_\AddInterfaceByTraitRector\Source\TopMostInterface;
 use Rector\Transform\Rector\Class_\AddInterfaceByTraitRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig
         ->ruleWithConfiguration(AddInterfaceByTraitRector::class, [
             SomeTrait::class => SomeInterface::class,
+            AnotherTrait::class => TopMostInterface::class,
         ]);
 };
