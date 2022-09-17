4aff915e3ab1ac35d2c83c7e8bb2d4eec082ba0b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-09-17 22:19:41 +0700

    [TypeDeclaration] Add AddArrowFunctionReturnTypeRector

---
+++ rules-tests/TypeDeclaration/Rector/ArrowFunction/AddArrowFunctionReturnTypeRector/AddArrowFunctionReturnTypeRectorTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\TypeDeclaration\Rector\ArrowFunction\AddArrowFunctionReturnTypeRector;
+
+use Iterator;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+final class AddArrowFunctionReturnTypeRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(string $filePath): void
+    {
+        $this->doTestFile($filePath);
+    }
+
+    /**
+     * @return Iterator<array<string>>
+     */
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/before_union_types.php';
+    }
+}
---
+++ rules/TypeDeclaration/Rector/ArrowFunction/AddArrowFunctionReturnTypeRector.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\TypeDeclaration\Rector\ArrowFunction;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\ArrowFunction;
+use PHPStan\Analyser\Scope;
+use Rector\Core\Rector\AbstractScopeAwareRector;
+use Rector\Core\ValueObject\PhpVersionFeature;
+use Rector\PHPStanStaticTypeMapper\Enum\TypeKind;
+use Rector\TypeDeclaration\TypeInferer\ReturnTypeInferer;
+use Rector\VersionBonding\Contract\MinPhpVersionInterface;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\Tests\TypeDeclaration\Rector\ArrowFunction\AddArrowFunctionReturnTypeRector\AddArrowFunctionReturnTypeRectorTest
+ */
+final class AddArrowFunctionReturnTypeRector extends AbstractScopeAwareRector implements MinPhpVersionInterface
+{
+    public function __construct(
+        private readonly ReturnTypeInferer $returnTypeInferer
+    ) {
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Add known return type to arrow function', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+fn () => [];
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+fn (): array => [];
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [ArrowFunction::class];
+    }
+
+    /**
+     * @param ArrowFunction $node
+     */
+    public function refactorWithScope(Node $node, Scope $scope): ?Node
+    {
+        if ($node->returnType !== null) {
+            return null;
+        }
+
+        $type = $this->getType($node->expr);
+        $returnTypeNode = $this->staticTypeMapper->mapPHPStanTypeToPhpParserNode(
+            $type,
+            TypeKind::RETURN
+        );
+
+        if ($returnTypeNode === null) {
+            return null;
+        }
+
+        $node->returnType = $returnTypeNode;
+
+        return $node;
+    }
+
+    public function provideMinPhpVersion(): int
+    {
+        return PhpVersionFeature::SCALAR_TYPES;
+    }
+}
