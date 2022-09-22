96a0f89058026c6cb1ada108da4ef94f28837226
Thomas Schiet <thomas.schiet@gmail.com>
2022-09-22 20:29:27 +0200

    Do not match non-scalar return type of function

--- rules/TypeDeclaration/TypeAnalyzer/AlwaysStrictScalarExprAnalyzer.php
+++ rules/TypeDeclaration/TypeAnalyzer/AlwaysStrictScalarExprAnalyzer.php
@@ -18,6 +18,7 @@ use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\Native\NativeFunctionReflection;
 use PHPStan\Reflection\ReflectionProvider;
 use PHPStan\Type\BooleanType;
+use PHPStan\Type\Constant\ConstantStringType;
 use PHPStan\Type\FloatType;
 use PHPStan\Type\IntegerType;
 use PHPStan\Type\NullType;
@@ -53,10 +54,41 @@ final class AlwaysStrictScalarExprAnalyzer
         }
 
         if ($expr instanceof FuncCall) {
-            return $this->resolveFuncCallType($expr);
+            $returnType = $this->resolveFuncCallType($expr);
+
+            if ($returnType === null) {
+                return null;
+            }
+
+            if (! $this->isScalarType($returnType)) {
+                return null;
+            }
+
+            return $returnType;
         }
 
         return null;
+    }
+
+    private function isScalarType(Type $type): bool
+    {
+        if ($type instanceof StringType && ! $type instanceof ConstantStringType) {
+            return true;
+        }
+
+        if ($type instanceof FloatType) {
+            return true;
+        }
+
+        if ($type instanceof IntegerType) {
+            return true;
+        }
+
+        if ($type instanceof BooleanType) {
+            return true;
+        }
+
+        return false;
     }
 
     private function resolveTypeFromScalar(Scalar $scalar): Type|null
