0b1f86d7ace986c1d4b0c0fa0cf45c9ffb5a7ef2
GitHub Action <action@github.com>
2022-09-29 15:20:05 +0000

    [ci-review] Rector Rectify

--- tests/Issues/Issue7503/config/configured_rule.php
+++ tests/Issues/Issue7503/config/configured_rule.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-use Rector\Privatization\Rector\Property\ChangeReadOnlyPropertyWithDefaultValueToConstantRector;
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\Plus\RemoveDeadZeroAndOneOperationRector;
+use Rector\Privatization\Rector\Property\ChangeReadOnlyPropertyWithDefaultValueToConstantRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(RemoveDeadZeroAndOneOperationRector::class);
