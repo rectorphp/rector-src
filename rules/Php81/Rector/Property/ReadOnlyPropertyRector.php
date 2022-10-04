f6b9b6a058b644b12f34e287c883b06ee2664b39
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-10-05 02:10:36 +0700

    Closes #2974

--- rules/Php81/Rector/Property/ReadOnlyPropertyRector.php
+++ rules/Php81/Rector/Property/ReadOnlyPropertyRector.php
@@ -6,12 +6,20 @@ namespace Rector\Php81\Rector\Property;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Expr\PropertyFetch;
+use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Param;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\ClassLike;
+use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Property;
+use PhpParser\NodeTraverser;
 use Rector\Core\NodeAnalyzer\ParamAnalyzer;
 use Rector\Core\NodeManipulator\PropertyFetchAssignManipulator;
 use Rector\Core\NodeManipulator\PropertyManipulator;
 use Rector\Core\Rector\AbstractRector;
+use Rector\Core\ValueObject\MethodName;
 use Rector\Core\ValueObject\PhpVersionFeature;
 use Rector\Core\ValueObject\Visibility;
 use Rector\NodeTypeResolver\Node\AttributeKey;
@@ -31,7 +39,7 @@ final class ReadOnlyPropertyRector extends AbstractRector implements MinPhpVersi
         private readonly PropertyManipulator $propertyManipulator,
         private readonly PropertyFetchAssignManipulator $propertyFetchAssignManipulator,
         private readonly ParamAnalyzer $paramAnalyzer,
-        private readonly VisibilityManipulator $visibilityManipulator,
+        private readonly VisibilityManipulator $visibilityManipulator
     ) {
     }
 
@@ -162,7 +170,43 @@ CODE_SAMPLE
             return null;
         }
 
+        if ($this->isPromotedPropertyAssigned($param)) {
+            return null;
+        }
+
         $this->visibilityManipulator->makeReadonly($param);
         return $param;
+    }
+
+    private function isPromotedPropertyAssigned(Param $param): bool
+    {
+        $classLike = $this->betterNodeFinder->findParentType($param, ClassLike::class);
+        if (! $classLike instanceof Class_) {
+            return false;
+        }
+
+        $constructClassMethod = $classLike->getMethod(MethodName::CONSTRUCT);
+        if (! $constructClassMethod instanceof ClassMethod) {
+            return false;
+        }
+
+        if ($param->flags === 0) {
+            return false;
+        }
+
+        $propertyFetch = new PropertyFetch(new Variable('this'), $this->getName($param));
+
+        $stmts = $classLike->stmts;
+        $isAssigned = false;
+        $this->traverseNodesWithCallable($stmts, function (Node $node) use ($propertyFetch, &$isAssigned): ?int {
+            if ($node instanceof Assign && $this->nodeComparator->areNodesEqual($propertyFetch, $node->var)) {
+                $isAssigned = true;
+                return NodeTraverser::STOP_TRAVERSAL;
+            }
+
+            return null;
+        });
+
+        return $isAssigned;
     }
 }
