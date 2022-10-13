2ba0aa6297b0ccc405564cb76c9f9fb939ac8348
Tomas Votruba <tomas.vot@gmail.com>
2022-10-13 03:58:22 +0200

    syntax

--- scoper.php
+++ scoper.php
@@ -80,7 +80,7 @@ return [
             }
 
             // comment out
-            return str_replace('\trigger_error', '\\ \trigger_error', $content);
+            return str_replace('@trigger_error', '// @trigger_error', $content);
         },
 
         static function (string $filePath, string $prefix, string $content): string {
