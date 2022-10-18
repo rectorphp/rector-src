3ad19c631b169af827611deec721f0983526260d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-10-18 15:59:04 +0700

    rollback

--- src/Rector/AbstractRector.php
+++ src/Rector/AbstractRector.php
@@ -245,6 +245,14 @@ CODE_SAMPLE;
             return $originalNode;
         }
 
+        $this->updateAndconnectParentNodes($refactoredNode, $parentNode);
+        $this->refreshScopeNodes($refactoredNode, $filePath, $currentScope);
+
+        // is equals node type? return node early
+        if ($originalNode::class === $refactoredNode::class) {
+            return $refactoredNode;
+        }
+
         // search "infinite recursion" in https://github.com/nikic/PHP-Parser/blob/master/doc/component/Walking_the_AST.markdown
         $originalNodeHash = spl_object_hash($originalNode);
 
@@ -253,12 +261,6 @@ CODE_SAMPLE;
             : $refactoredNode;
 
         $this->nodesToReturn[$originalNodeHash] = $refactoredNode;
-
-        // Node result is different? re-connect parent node and scope refresh
-        if ($originalNode::class !== $refactoredNode::class) {
-            $this->updateAndconnectParentNodes($refactoredNode, $parentNode);
-            $this->refreshScopeNodes($refactoredNode, $filePath, $currentScope);
-        }
 
         return $refactoredNode;
     }
