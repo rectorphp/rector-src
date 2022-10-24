55297fd30cff72242675e8104f3213903d361fe8
GitHub Action <action@github.com>
2022-10-24 11:50:13 +0000

    [ci-review] Rector Rectify

--- tests/Issues/IssueRemoveAlwaysElseReturnEarlyIfVariable/config/configured_rule.php
+++ tests/Issues/IssueRemoveAlwaysElseReturnEarlyIfVariable/config/configured_rule.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
 use Rector\EarlyReturn\Rector\If_\RemoveAlwaysElseRector;
 use Rector\EarlyReturn\Rector\StmtsAwareInterface\ReturnEarlyIfVariableRector;
-use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
-	$rectorConfig->rule(RemoveAlwaysElseRector::class );
-	$rectorConfig->rule(ReturnEarlyIfVariableRector::class );
+    $rectorConfig->rule(RemoveAlwaysElseRector::class);
+    $rectorConfig->rule(ReturnEarlyIfVariableRector::class);
 };
