b46d5f50092e61888943960a5320e77822e047c3
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-10-26 15:55:40 +0700

    [DeadCode] Handle crash on has first class callable on RemoveUnusedConstructorParamRector (#3015)
    * [DeadCode] Handle has first class callable on RemoveUnusedConstructorParamRector
    
    * Fixed :tada:
    
    * final touch: more fixture

--- src/NodeAnalyzer/ParamAnalyzer.php
+++ src/NodeAnalyzer/ParamAnalyzer.php
@@ -147,6 +147,10 @@ final class ParamAnalyzer
     private function isUsedAsArg(Node $node, Param $param): bool
     {
         if ($node instanceof New_ || $node instanceof CallLike) {
+            if ($node->isFirstClassCallable()) {
+                return false;
+            }
+
             foreach ($node->getArgs() as $arg) {
                 if ($this->nodeComparator->areNodesEqual($param->var, $arg->value)) {
                     return true;
