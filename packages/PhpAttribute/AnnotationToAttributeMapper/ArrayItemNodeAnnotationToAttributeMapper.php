4bf158721cf9fb872cda789f446709e6967a983f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-10-27 23:22:33 +0700

    [Php80] Handle nested annotation not end with () on AnnotationToAttributeRector (#3017)
    Co-authored-by: GitHub Action <action@github.com>

--- packages/PhpAttribute/AnnotationToAttributeMapper/ArrayItemNodeAnnotationToAttributeMapper.php
+++ packages/PhpAttribute/AnnotationToAttributeMapper/ArrayItemNodeAnnotationToAttributeMapper.php
@@ -7,11 +7,15 @@ namespace Rector\PhpAttribute\AnnotationToAttributeMapper;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\ArrayItem;
 use PhpParser\Node\Scalar\String_;
+use PHPStan\PhpDocParser\Ast\Type\IdentifierTypeNode;
 use Rector\BetterPhpDocParser\PhpDoc\ArrayItemNode;
+use Rector\BetterPhpDocParser\PhpDoc\DoctrineAnnotationTagValueNode;
+use Rector\Core\Validation\RectorAssert;
 use Rector\PhpAttribute\AnnotationToAttributeMapper;
 use Rector\PhpAttribute\Contract\AnnotationToAttributeMapperInterface;
 use Rector\PhpAttribute\Enum\DocTagNodeState;
 use Symfony\Contracts\Service\Attribute\Required;
+use Webmozart\Assert\InvalidArgumentException;
 
 /**
  * @implements AnnotationToAttributeMapperInterface<ArrayItemNode>
@@ -55,11 +59,40 @@ final class ArrayItemNodeAnnotationToAttributeMapper implements AnnotationToAttr
             /** @var Expr $keyExpr */
             $keyExpr = $this->annotationToAttributeMapper->map($keyValue);
         } else {
+            if ($this->hasNoParenthesesAnnotation($arrayItemNode)) {
+                try {
+                    RectorAssert::className(ltrim((string) $arrayItemNode->value, '@'));
+
+                    $identifierTypeNode = new IdentifierTypeNode($arrayItemNode->value);
+                    $arrayItemNode->value = new DoctrineAnnotationTagValueNode($identifierTypeNode);
+
+                    return $this->map($arrayItemNode);
+                } catch (InvalidArgumentException) {
+                }
+            }
+
             $keyExpr = null;
         }
 
         // @todo how to skip natural integer keys?
 
         return new ArrayItem($valueExpr, $keyExpr);
+    }
+
+    private function hasNoParenthesesAnnotation(ArrayItemNode $arrayItemNode): bool
+    {
+        if (is_int($arrayItemNode->kindValueQuoted)) {
+            return false;
+        }
+
+        if (! is_string($arrayItemNode->value)) {
+            return false;
+        }
+
+        if (! str_starts_with($arrayItemNode->value, '@')) {
+            return false;
+        }
+
+        return ! str_ends_with($arrayItemNode->value, ')');
     }
 }
