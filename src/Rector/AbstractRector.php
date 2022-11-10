e7a13ef5fb853a7b6cd96085169683961e0b877f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-11-10 23:00:51 +0700

    [Core] Improve performance: only update and connect parent Node when refactored Node is different

--- src/Rector/AbstractRector.php
+++ src/Rector/AbstractRector.php
@@ -250,13 +250,14 @@ CODE_SAMPLE;
             return $originalNode;
         }
 
-        $this->updateAndconnectParentNodes($refactoredNode, $parentNode);
         $this->refreshScopeNodes($refactoredNode, $filePath, $currentScope);
 
         // is equals node type? return node early
         if ($originalNode::class === $refactoredNode::class) {
             return $refactoredNode;
         }
+
+        $this->updateAndconnectParentNodes($refactoredNode, $parentNode);
 
         // search "infinite recursion" in https://github.com/nikic/PHP-Parser/blob/master/doc/component/Walking_the_AST.markdown
         $originalNodeHash = spl_object_hash($originalNode);
