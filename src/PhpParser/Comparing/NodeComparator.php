e3ff080a96e2efbcd4a62c07b44ef4f950bd023d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-11-11 20:38:29 +0700

    [Core] Improve performance on NodeComparator  (#3049)
    * [Core] Improve performance on NodeComparator
    
    * phpstan ignore cognitive complexity for abstract rector
    
    * add array and null comparison
    
    * verify all are Node when array

--- src/PhpParser/Comparing/NodeComparator.php
+++ src/PhpParser/Comparing/NodeComparator.php
@@ -7,6 +7,7 @@ namespace Rector\Core\PhpParser\Comparing;
 use PhpParser\Node;
 use Rector\Comments\CommentRemover;
 use Rector\Core\Contract\PhpParser\NodePrinterInterface;
+use Webmozart\Assert\Assert;
 
 final class NodeComparator
 {
@@ -34,6 +35,30 @@ final class NodeComparator
      */
     public function areNodesEqual(Node | array | null $firstNode, Node | array | null $secondNode): bool
     {
+        if ($firstNode instanceof Node && $secondNode === null) {
+            return false;
+        }
+
+        if ($secondNode instanceof Node && $firstNode === null) {
+            return false;
+        }
+
+        if (is_array($firstNode)) {
+            Assert::allIsAOf($firstNode, Node::class);
+
+            if ($secondNode === null) {
+                return false;
+            }
+        }
+
+        if (is_array($secondNode)) {
+            Assert::allIsAOf($secondNode, Node::class);
+
+            if ($firstNode === null) {
+                return false;
+            }
+        }
+
         return $this->printWithoutComments($firstNode) === $this->printWithoutComments($secondNode);
     }
 
@@ -61,17 +86,17 @@ final class NodeComparator
             return true;
         }
 
+        $firstClass = $firstNode::class;
+        $secondClass = $secondNode::class;
+
+        if ($firstClass !== $secondClass) {
+            return false;
+        }
+
         if ($firstNode->getStartTokenPos() !== $secondNode->getStartTokenPos()) {
             return false;
         }
 
-        if ($firstNode->getEndTokenPos() !== $secondNode->getEndTokenPos()) {
-            return false;
-        }
-
-        $firstClass = $firstNode::class;
-        $secondClass = $secondNode::class;
-
-        return $firstClass === $secondClass;
+        return $firstNode->getEndTokenPos() === $secondNode->getEndTokenPos();
     }
 }
