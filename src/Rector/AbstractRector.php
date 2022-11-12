64f4a55aa43b1197247637192b04bc48c7a77877
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-11-12 10:21:50 +0700

    [Core] Use parent attribute check instead of origNode for check update and connect parent node after refactor (#3054)
    * [Core] Use parent attribute check instead of origNode for check update and connect parent node after refactor
    
    * [ci-review] Rector Rectify
    
    Co-authored-by: GitHub Action <action@github.com>

--- src/Rector/AbstractRector.php
+++ src/Rector/AbstractRector.php
@@ -262,13 +262,13 @@ CODE_SAMPLE;
              *  - Expression on Assign with ArrowFunction changed to Closure
              *  - Return_ on ArrowFunction usage, Return_ is created dynamically on getStmts()
              *
-             * 2. When returned refactored Node doesn't has origNode yet,
+             * 2. When returned refactored Node doesn't has parent yet,
              *    it means returned with New Node instead of re-use existing Node
              */
             if (
                 $refactoredNode instanceof Expression
                 || $refactoredNode instanceof Return_
-                || ! $refactoredNode->hasAttribute(AttributeKey::ORIGINAL_NODE)
+                || ! $refactoredNode->hasAttribute(AttributeKey::PARENT_NODE)
             ) {
                 $this->updateAndconnectParentNodes($refactoredNode, $parentNode);
             }
