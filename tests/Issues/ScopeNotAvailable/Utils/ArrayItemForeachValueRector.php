5b8364b771f72b647aa1f3d8010609f32ea8dd2b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-11-16 17:42:36 +0700

    [Core] Handle no scope crash on foreach value with array item variable

---
+++ tests/Issues/ScopeNotAvailable/Utils/ArrayItemForeachValueRector.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Core\Tests\Issues\ScopeNotAvailable\Utils;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\Variable;
+use PHPStan\Analyser\Scope;
+use Rector\Core\Rector\AbstractScopeAwareRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+class ArrayItemForeachValueRector extends AbstractScopeAwareRector
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Hello!', [
+            new CodeSample('',''),
+        ]);
+    }
+
+    public function getNodeTypes(): array
+    {
+        return [Variable::class];
+    }
+
+    public function refactorWithScope(Node $node, Scope $scope)
+    {
+        return $node;
+    }
+}
