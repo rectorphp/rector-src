501209ec5cba55d212f3320d989deab475a92132
GitHub Action <action@github.com>
2022-11-16 10:49:44 +0000

    [ci-review] Rector Rectify

--- tests/Issues/ScopeNotAvailable/Utils/ArrayItemForeachValueRector.php
+++ tests/Issues/ScopeNotAvailable/Utils/ArrayItemForeachValueRector.php
@@ -5,25 +5,29 @@ declare(strict_types=1);
 namespace Rector\Core\Tests\Issues\ScopeNotAvailable\Utils;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\Variable;
 use PHPStan\Analyser\Scope;
 use Rector\Core\Rector\AbstractScopeAwareRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
-class ArrayItemForeachValueRector extends AbstractScopeAwareRector
+final class ArrayItemForeachValueRector extends AbstractScopeAwareRector
 {
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition('Hello!', [new CodeSample('', '')]);
     }
 
+    /**
+     * @return array<class-string<Expr>>
+     */
     public function getNodeTypes(): array
     {
         return [Variable::class];
     }
 
-    public function refactorWithScope(Node $node, Scope $scope)
+    public function refactorWithScope(Node $node, Scope $scope): Node
     {
         return $node;
     }
