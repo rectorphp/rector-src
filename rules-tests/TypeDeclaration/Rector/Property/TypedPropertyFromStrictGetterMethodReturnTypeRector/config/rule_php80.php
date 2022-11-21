c0bf8ad1539bc231425a4e953e52d5416f72a2bc
Matthias Schmidt <matthias@mttsch.dev>
2022-11-21 20:25:26 +0100

    Add failing test fixture for TypedPropertyFromStrictGetterMethodReturnTypeRector
    # Failing Test for TypedPropertyFromStrictGetterMethodReturnTypeRector
    
    Based on https://getrector.org/demo/ee1243f2-fa5c-4460-a27d-31fb67439bbf

---
+++ rules-tests/TypeDeclaration/Rector/Property/TypedPropertyFromStrictGetterMethodReturnTypeRector/config/rule_php80.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Core\ValueObject\PhpVersionFeature;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictGetterMethodReturnTypeRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rule(TypedPropertyFromStrictGetterMethodReturnTypeRector::class);
+
+    $rectorConfig->phpVersion(PhpVersionFeature::STATIC_RETURN_TYPE);
+};
