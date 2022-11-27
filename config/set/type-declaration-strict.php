17703f3cc6177b06fd9e3db3a94d1e46cc10a24a
Tomas Votruba <tomas.vot@gmail.com>
2022-11-27 17:32:15 +0100

    note

--- config/set/type-declaration-strict.php
+++ config/set/type-declaration-strict.php
@@ -6,4 +6,6 @@ use Rector\Config\RectorConfig;
 
 // deprecated, use type-declaration config instead
 return static function (RectorConfig $rectorConfig): void {
+    trigger_error('The TYPE_DECLARATION_STRICT is deprecated, use TYPE_DECLARATION instead that include this rules.');
+    sleep(3);
 };
