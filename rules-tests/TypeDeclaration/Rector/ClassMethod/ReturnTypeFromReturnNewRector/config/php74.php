cb021e806be5dc5adeeb0e03b709d8a0fcab5127
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-11-29 02:17:23 +0700

    fix config

--- rules-tests/TypeDeclaration/Rector/ClassMethod/ReturnTypeFromReturnNewRector/config/php74.php
+++ rules-tests/TypeDeclaration/Rector/ClassMethod/ReturnTypeFromReturnNewRector/config/php74.php
@@ -3,10 +3,10 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Core\ValueObject\PhpVersion;
+use Rector\Core\ValueObject\PhpVersionFeature;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromReturnNewRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(ReturnTypeFromReturnNewRector::class);
-    $rectorConfig->phpVersion(PhpVersion::PHP_74);
+    $rectorConfig->phpVersion(PhpVersionFeature::STATIC_RETURN_TYPE - 1);
 };
