431220094b314f08d9a1b8dc7f59ee9b8d1003de
Tomas Votruba <tomas.vot@gmail.com>
2022-11-28 01:30:16 +0100

    remove SetterNodeReturnTypeInfererTypeInferer

--- src/NodeManipulator/FunctionLikeManipulator.php
+++ src/NodeManipulator/FunctionLikeManipulator.php
@@ -4,58 +4,14 @@ declare(strict_types=1);
 
 namespace Rector\Core\NodeManipulator;
 
-use PhpParser\Node;
 use PhpParser\Node\FunctionLike;
-use PhpParser\Node\Stmt\Function_;
-use PhpParser\Node\Stmt\Return_;
-use Rector\Core\NodeAnalyzer\PropertyFetchAnalyzer;
 use Rector\NodeNameResolver\NodeNameResolver;
-use Rector\PhpDocParser\NodeTraverser\SimpleCallableNodeTraverser;
 
 final class FunctionLikeManipulator
 {
     public function __construct(
-        private readonly SimpleCallableNodeTraverser $simpleCallableNodeTraverser,
         private readonly NodeNameResolver $nodeNameResolver,
-        private readonly PropertyFetchAnalyzer $propertyFetchAnalyzer
     ) {
-    }
-
-    /**
-     * @return string[]
-     */
-    public function getReturnedLocalPropertyNames(FunctionLike $functionLike): array
-    {
-        // process only class methods
-        if ($functionLike instanceof Function_) {
-            return [];
-        }
-
-        $returnedLocalPropertyNames = [];
-        $this->simpleCallableNodeTraverser->traverseNodesWithCallable($functionLike, function (Node $node) use (
-            &$returnedLocalPropertyNames
-        ) {
-            if (! $node instanceof Return_) {
-                return null;
-            }
-
-            if ($node->expr === null) {
-                return null;
-            }
-
-            if (! $this->propertyFetchAnalyzer->isLocalPropertyFetch($node->expr)) {
-                return null;
-            }
-
-            $propertyName = $this->nodeNameResolver->getName($node->expr);
-            if ($propertyName === null) {
-                return null;
-            }
-
-            $returnedLocalPropertyNames[] = $propertyName;
-        });
-
-        return $returnedLocalPropertyNames;
     }
 
     /**
