a62512caa8aa8de726f6cec4967f088b5deef374
Tomas Votruba <tomas.vot@gmail.com>
2022-11-28 13:02:02 +0100

    remove prioarity aware sorter etc

--- rules/TypeDeclaration/TypeInferer/ReturnTypeInferer.php
+++ rules/TypeDeclaration/TypeInferer/ReturnTypeInferer.php
@@ -46,7 +46,11 @@ final class ReturnTypeInferer
 {
     public function __construct(
         private readonly TypeNormalizer $typeNormalizer,
+<<<<<<< HEAD
         private readonly ReturnedNodesReturnTypeInfererTypeInferer $returnedNodesReturnTypeInfererTypeInferer,
+=======
+        private ReturnedNodesReturnTypeInfererTypeInferer $returnedNodesReturnTypeInfererTypeInferer,
+>>>>>>> 241cae5181 (remove prioarity aware sorter etc)
         private readonly GenericClassStringTypeNormalizer $genericClassStringTypeNormalizer,
         private readonly PhpVersionProvider $phpVersionProvider,
         private readonly ParameterProvider $parameterProvider,
