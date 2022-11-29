e8e3bd5ed90bab56caef0c0fc5829e42efb9f9ba
Tomas Votruba <tomas.vot@gmail.com>
2022-11-29 20:20:51 +0100

    [TypeDeclaration] Skip property names in TypedPropertyFromStrictConstructorRector (#3128)
    * add trait failing fixture in TypedPropertyFromStrictConstructorRector
    
    * [TypeDeclaration] Skip property names in TypedPropertyFromStrictConstructorRector

---
+++ rules-tests/TypeDeclaration/Rector/Property/TypedPropertyFromStrictConstructorRector/Source/SomeSkipMeTrait.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace Rector\Tests\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector\Source;
+
+trait SomeSkipMeTrait
+{
+    private $skipMe;
+}
