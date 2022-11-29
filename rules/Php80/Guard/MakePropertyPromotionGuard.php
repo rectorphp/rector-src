757b5ecf66cb488ba497f2678bd65afeca93a595
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-11-29 17:02:43 +0700

    clean up

--- rules/Php80/Guard/MakePropertyPromotionGuard.php
+++ rules/Php80/Guard/MakePropertyPromotionGuard.php
@@ -18,14 +18,10 @@ final class MakePropertyPromotionGuard
 
     public function isLegal(Property $property, Param $param, bool $inlinePublic = true): bool
     {
-        if ($this->propertyTypeChangeGuard->isLegal($property, $inlinePublic)) {
-            return true;
+        if ($param->type instanceof Node && ! $property->type instanceof Node) {
+            return $this->propertyTypeChangeGuard->isLegal($property, $inlinePublic);
         }
 
-        if ($property->type instanceof Node) {
-            return true;
-        }
-
-        return ! $param->type instanceof Node;
+        return true;
     }
 }
