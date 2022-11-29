d9edcd83d6011d85cb0affb3ce925facef48bbd9
GitHub Action <action@github.com>
2022-11-29 10:54:18 +0000

    [ci-review] Rector Rectify

--- rules/Php80/Guard/MakePropertyPromotionGuard.php
+++ rules/Php80/Guard/MakePropertyPromotionGuard.php
@@ -27,10 +27,15 @@ final class MakePropertyPromotionGuard
             return true;
         }
 
-        if (! $inlinePublic && ! $property->isPrivate() && $param->type instanceof Node && ! $property->type instanceof Node) {
-            return false;
+        if ($inlinePublic) {
+            return true;
         }
-
-        return true;
+        if ($property->isPrivate()) {
+            return true;
+        }
+        if (! $param->type instanceof Node) {
+            return true;
+        }
+        return $property->type instanceof Node;
     }
 }
