b74f5ff4dc5a00e9781131533763edac710c338f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-11-29 16:41:14 +0700

    clean up

--- rules/Php80/Guard/MakePropertyPromotionGuard.php
+++ rules/Php80/Guard/MakePropertyPromotionGuard.php
@@ -7,18 +7,18 @@ namespace Rector\Php80\Guard;
 use PhpParser\Node;
 use PhpParser\Node\Param;
 use PhpParser\Node\Stmt\Property;
-use Rector\Php74\Guard\MakePropertyTypedGuard;
+use Rector\Php74\Guard\PropertyTypeChangeGuard;
 
 final class MakePropertyPromotionGuard
 {
     public function __construct(
-        private readonly MakePropertyTypedGuard $makePropertyTypedGuard
+        private readonly PropertyTypeChangeGuard $propertyTypeChangeGuard
     ) {
     }
 
     public function isLegal(Property $property, Param $param, bool $inlinePublic = true): bool
     {
-        if ($this->makePropertyTypedGuard->isLegal($property, $inlinePublic)) {
+        if ($this->propertyTypeChangeGuard->isLegal($property, $inlinePublic)) {
             return true;
         }
 
