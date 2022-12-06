643f7af2a54e139b069e4ede9d1bef1b7c064c6f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-12-06 22:38:47 +0700

    tweak

--- packages/NodeNameResolver/NodeNameResolver.php
+++ packages/NodeNameResolver/NodeNameResolver.php
@@ -211,7 +211,7 @@ final class NodeNameResolver
         return strtolower($resolvedName) === strtolower($desiredName);
     }
 
-    private function resolveExprOrNodewithName(Expr|Namespace_|Identifier|Name|string $name): ?string
+    private function resolveExprOrNodewithName(Expr|Namespace_|Identifier|Name|string|null $name): ?string
     {
         // unable to resolve
         if ($name instanceof Expr) {
