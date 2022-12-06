d3abaa1d05318488f8c1f401fc46a9e48b056d4f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-12-06 20:07:43 +0700

    [TypeDeclaration] Handle Anonymous class extends existing class in union (#3161)
    Co-authored-by: GitHub Action <action@github.com>

---
+++ rules-tests/TypeDeclaration/Rector/Property/TypedPropertyFromAssignsRector/FixtureComplexTypes/anonymous_extends_existing_class_remove_docblock.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Tests\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector\FixtureComplexTypes;
+
+final class AnonymousExtendsExistingClassInUnionRemoveDocblock
+{
+    /**
+     * @var \DateTime|null
+     */
+    private $x;
+
+    public function __construct()
+    {
+        if (rand(0, 1)) {
+            $this->x = new \DateTime('now');
+        } else {
+            $this->x = new class() extends \DateTime {
+            };
+        }
+    }
+}
+
+?>
+-----
+<?php
+
+namespace Rector\Tests\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector\FixtureComplexTypes;
+
+final class AnonymousExtendsExistingClassInUnion
+{
+    private \DateTime|null $x = null;
+
+    public function __construct()
+    {
+        if (rand(0, 1)) {
+            $this->x = new \DateTime('now');
+        } else {
+            $this->x = new class() extends \DateTime {
+            };
+        }
+    }
+}
+
+?>
