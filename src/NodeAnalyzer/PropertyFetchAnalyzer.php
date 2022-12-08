87543a32a080932454843437f3b62a4f811687fb
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-12-08 13:08:33 +0700

    [Core] Use FullyQualifiedObjectType and ThisType detection for local property fetch on PropertyFetchAnalyzer

--- src/NodeAnalyzer/PropertyFetchAnalyzer.php
+++ src/NodeAnalyzer/PropertyFetchAnalyzer.php
@@ -12,7 +12,6 @@ use PhpParser\Node\Expr\PropertyFetch;
 use PhpParser\Node\Expr\StaticCall;
 use PhpParser\Node\Expr\StaticPropertyFetch;
 use PhpParser\Node\Expr\Variable;
-use PhpParser\Node\Name;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassLike;
 use PhpParser\Node\Stmt\ClassMethod;
@@ -20,12 +19,14 @@ use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\Property;
 use PhpParser\Node\Stmt\Trait_;
 use PHPStan\Type\ObjectType;
-use Rector\Core\Enum\ObjectReference;
+use PHPStan\Type\ThisType;
 use Rector\Core\PhpParser\AstResolver;
 use Rector\Core\PhpParser\Node\BetterNodeFinder;
 use Rector\Core\ValueObject\MethodName;
 use Rector\NodeNameResolver\NodeNameResolver;
+use Rector\NodeTypeResolver\NodeTypeResolver;
 use Rector\PhpDocParser\NodeTraverser\SimpleCallableNodeTraverser;
+use Rector\StaticTypeMapper\ValueObject\Type\FullyQualifiedObjectType;
 
 final class PropertyFetchAnalyzer
 {
@@ -38,32 +39,29 @@ final class PropertyFetchAnalyzer
         private readonly NodeNameResolver $nodeNameResolver,
         private readonly BetterNodeFinder $betterNodeFinder,
         private readonly AstResolver $astResolver,
-        private readonly SimpleCallableNodeTraverser $simpleCallableNodeTraverser
+        private readonly SimpleCallableNodeTraverser $simpleCallableNodeTraverser,
+        private readonly NodeTypeResolver $nodeTypeResolver
     ) {
     }
 
     public function isLocalPropertyFetch(Node $node): bool
     {
-        if ($node instanceof PropertyFetch) {
-            if (! $node->var instanceof Variable) {
-                return false;
-            }
-
-            return $this->nodeNameResolver->isName($node->var, self::THIS);
+        if (! $node instanceof PropertyFetch && ! $node instanceof StaticPropertyFetch) {
+            return false;
         }
 
-        if ($node instanceof StaticPropertyFetch) {
-            if (! $node->class instanceof Name) {
-                return false;
-            }
+        $variableType = $node instanceof PropertyFetch
+            ? $this->nodeTypeResolver->getType($node->var)
+            : $this->nodeTypeResolver->getType($node->class);
 
-            return $this->nodeNameResolver->isNames($node->class, [
-                ObjectReference::SELF,
-                ObjectReference::STATIC,
-            ]);
+        if ($variableType instanceof FullyQualifiedObjectType) {
+            $currentClassLike = $this->betterNodeFinder->findParentType($node, ClassLike::class);
+            if ($currentClassLike instanceof ClassLike) {
+                return $this->nodeNameResolver->isName($currentClassLike, $variableType->getClassName());
+            }
         }
 
-        return false;
+        return $variableType instanceof ThisType;
     }
 
     public function isLocalPropertyFetchName(Node $node, string $desiredPropertyName): bool
