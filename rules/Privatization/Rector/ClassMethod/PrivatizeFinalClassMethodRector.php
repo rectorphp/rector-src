bea2e12a6d6e7a7b1eb1f7b6f82a0ec1e7c21f2c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-12-12 14:58:28 +0700

    [Privatization][TypeDeclaration] Skip inside class with @final docblock on PrivatizeFinalClassMethodRector and AddVoidReturnTypeWhereNoReturnRector (#3190)
    Co-authored-by: GitHub Action <action@github.com>

--- rules/Privatization/Rector/ClassMethod/PrivatizeFinalClassMethodRector.php
+++ rules/Privatization/Rector/ClassMethod/PrivatizeFinalClassMethodRector.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Rector\Privatization\Rector\ClassMethod;
 
 use PhpParser\Node;
+use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassMethod;
 use PHPStan\Reflection\ClassReflection;
 use Rector\Core\Rector\AbstractRector;
 use Rector\Core\Reflection\ReflectionResolver;
+use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\Privatization\NodeManipulator\VisibilityManipulator;
 use Rector\Privatization\VisibilityGuard\ClassMethodVisibilityGuard;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
@@ -72,11 +74,12 @@ CODE_SAMPLE
             return null;
         }
 
-        if (! $classReflection->isClass()) {
+        $class = $node->getAttribute(AttributeKey::PARENT_NODE);
+        if (! $class instanceof Class_) {
             return null;
         }
 
-        if (! $classReflection->isFinal()) {
+        if (! $class->isFinal()) {
             return null;
         }
 
