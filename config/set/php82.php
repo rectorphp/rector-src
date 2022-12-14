b255b56a56ab58bfbb588cd36fa785dd4f53d8ef
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-12-14 13:04:10 +0700

    rename

--- config/set/php82.php
+++ config/set/php82.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Php82\Rector\Class_\ReadOnlyClassRector;
-use Rector\Php82\Rector\FuncCall\Utf8DecodeToMbConvertEncodingRector;
+use Rector\Php82\Rector\FuncCall\Utf8DecodeEncodeToMbConvertEncodingRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rules([ReadOnlyClassRector::class, Utf8DecodeToMbConvertEncodingRector::class]);
+    $rectorConfig->rules([ReadOnlyClassRector::class, Utf8DecodeEncodeToMbConvertEncodingRector::class]);
 };
