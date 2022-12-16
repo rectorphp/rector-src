8529de959d3193e7d1b5a563ca7e72b547ed5dfc
GitHub Action <action@github.com>
2022-12-16 11:19:28 +0000

    [ci-review] Rector Rectify

--- src/Rector/AbstractRector.php
+++ src/Rector/AbstractRector.php
@@ -319,9 +319,11 @@ CODE_SAMPLE;
         if ($this->nodeComparator->areSameNode($newNode, $oldNode)) {
             return;
         }
-
         // comments or php_doc_info attribute already exists in refactored Node
-        if ($newNode->hasAttribute(AttributeKey::COMMENTS) || $newNode->hasAttribute(AttributeKey::PHP_DOC_INFO)) {
+        if ($newNode->hasAttribute(AttributeKey::COMMENTS)) {
+            return;
+        }
+        if ($newNode->hasAttribute(AttributeKey::PHP_DOC_INFO)) {
             return;
         }
 
