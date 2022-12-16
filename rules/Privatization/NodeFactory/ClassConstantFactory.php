dec1dddede5713915f90b41f932ebbaaf0996a5a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-12-16 07:37:34 +0700

    [Privatization] Do not remove comment on ChangeReadOnlyPropertyWithDefaultValueToConstantRector (#3204)
    * [Privatization] Do not remove comment on ChangeReadOnlyPropertyWithDefaultValueToConstantRector
    
    * Fixed :tada:
    
    * [ci-review] Rector Rectify
    
    * pin to phpstan/phpdoc-parser 1.15.0
    
    * require pin phpstan/phpdoc-parser:1.15.0 as 1.15.1 got issue on array shape
    
    * require pin phpstan/phpdoc-parser:1.15.0 as 1.15.1 got issue on array shape
    
    Co-authored-by: GitHub Action <action@github.com>

--- rules/Privatization/NodeFactory/ClassConstantFactory.php
+++ rules/Privatization/NodeFactory/ClassConstantFactory.php
@@ -9,15 +9,13 @@ use PhpParser\Node\Expr;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassConst;
 use PhpParser\Node\Stmt\Property;
-use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfoFactory;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\Privatization\Naming\ConstantNaming;
 
 final class ClassConstantFactory
 {
     public function __construct(
-        private readonly ConstantNaming $constantNaming,
-        private readonly PhpDocInfoFactory $phpDocInfoFactory
+        private readonly ConstantNaming $constantNaming
     ) {
     }
 
@@ -36,10 +34,8 @@ final class ClassConstantFactory
 
         $const->setAttribute(AttributeKey::PARENT_NODE, $classConst);
 
-        $phpDocInfo = $this->phpDocInfoFactory->createFromNodeOrEmpty($property);
-        $phpDocInfo->markAsChanged();
-
-        $classConst->setAttribute(AttributeKey::PHP_DOC_INFO, $phpDocInfo);
+        $classConst->setAttribute(AttributeKey::PHP_DOC_INFO, $property->getAttribute(AttributeKey::PHP_DOC_INFO));
+        $classConst->setAttribute(AttributeKey::COMMENTS, $property->getAttribute(AttributeKey::COMMENTS));
         $classConst->setAttribute(AttributeKey::PARENT_NODE, $property->getAttribute(AttributeKey::PARENT_NODE));
 
         return $classConst;
