89fd283ea27eb70fd402c9197d86ec2252a45527
GitHub Action <action@github.com>
2022-12-20 16:10:40 +0000

    [ci-review] Rector Rectify

--- tests/Issues/InlineTypedFromConstructorDefaultValue/config/configured_rule.php
+++ tests/Issues/InlineTypedFromConstructorDefaultValue/config/configured_rule.php
@@ -1,7 +1,9 @@
 <?php
 
-use Rector\Config\RectorConfig;
+declare(strict_types=1);
+
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Config\RectorConfig;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
 
 return static function (RectorConfig $rectorConfig): void {
