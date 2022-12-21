4f3c6ebf093f7907e96eb5c1d997f3bfb912bbec
GitHub Action <action@github.com>
2022-12-21 09:37:32 +0000

    [ci-review] Rector Rectify

--- rules-tests/CodingStyle/Rector/Property/NullifyUnionNullableRector/config/config.php
+++ rules-tests/CodingStyle/Rector/Property/NullifyUnionNullableRector/config/config.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Utils\Rector\Tests\Rector\VarAnnotationMissingNullableRectorTest;
 
-use Rector\Config\RectorConfig;
 use Rector\CodingStyle\Rector\Property\NullifyUnionNullableRector;
+use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(NullifyUnionNullableRector::class);
