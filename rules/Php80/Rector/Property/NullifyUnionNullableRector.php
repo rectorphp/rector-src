9d76e695cbef68286f9b269883001937862b22dd
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-12-21 13:31:23 +0700

    phpstan

--- rules/Php80/Rector/Property/NullifyUnionNullableRector.php
+++ rules/Php80/Rector/Property/NullifyUnionNullableRector.php
@@ -9,6 +9,7 @@ use PhpParser\Node\Expr\ArrowFunction;
 use PhpParser\Node\Expr\Closure;
 use PhpParser\Node\FunctionLike;
 use PhpParser\Node\Identifier;
+use PhpParser\Node\IntersectionType;
 use PhpParser\Node\Name;
 use PhpParser\Node\NullableType;
 use PhpParser\Node\Param;
@@ -129,15 +130,7 @@ CODE_SAMPLE
 
     private function areBothValidNullableType(Node $firstType, Node $secondType): bool
     {
-        if ($firstType instanceof Identifier || $firstType instanceof Name) {
-            return $secondType instanceof Identifier || $secondType instanceof Name;
-        }
-
-        if ($secondType instanceof Identifier || $secondType instanceof Name) {
-            return $firstType instanceof Identifier || $firstType instanceof Name;
-        }
-
-        return false;
+        return ! $firstType instanceof IntersectionType && ! $secondType instanceof IntersectionType;
     }
 
     private function processNullablePropertyParamType(Property|Param $node): null|Property|Param
