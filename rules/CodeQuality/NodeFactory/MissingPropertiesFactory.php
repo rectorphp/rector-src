bb770065dd1545c7770bcf812a9ca6a299d65717
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-12-24 13:59:10 +0700

    Really Really Final touch: clean up

--- rules/CodeQuality/NodeFactory/MissingPropertiesFactory.php
+++ rules/CodeQuality/NodeFactory/MissingPropertiesFactory.php
@@ -8,16 +8,11 @@ use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\Property;
 use PhpParser\Node\Stmt\PropertyProperty;
 use PHPStan\Type\Type;
-use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfoFactory;
-use Rector\BetterPhpDocParser\PhpDocManipulator\PhpDocTypeChanger;
-use Rector\Privatization\TypeManipulator\TypeNormalizer;
 
 final class MissingPropertiesFactory
 {
     public function __construct(
-        private readonly PhpDocInfoFactory $phpDocInfoFactory,
-        private readonly TypeNormalizer $typeNormalizer,
-        private readonly PhpDocTypeChanger $phpDocTypeChanger
+        private readonly PropertyTypeDecorator $propertyTypeDecorator
     ) {
     }
 
@@ -35,14 +30,7 @@ final class MissingPropertiesFactory
             }
 
             $property = new Property(Class_::MODIFIER_PUBLIC, [new PropertyProperty($propertyName)]);
-
-            $phpDocInfo = $this->phpDocInfoFactory->createFromNodeOrEmpty($property);
-            $phpDocInfo->makeMultiLined();
-
-            // generalize false/true type to bool, as mostly default value but accepts both
-            $propertyType = $this->typeNormalizer->generalizeConstantBoolTypes($propertyType);
-
-            $this->phpDocTypeChanger->changeVarType($phpDocInfo, $propertyType);
+            $this->propertyTypeDecorator->decorateProperty($property, $propertyType);
 
             $newProperties[] = $property;
         }
--- rules/CodeQuality/NodeFactory/PropertyTypeDecorator.php
+++ rules/CodeQuality/NodeFactory/PropertyTypeDecorator.php
@@ -8,22 +8,36 @@ use PhpParser\Node\ComplexType;
 use PhpParser\Node\Identifier;
 use PhpParser\Node\Name;
 use PhpParser\Node\Stmt\Property;
+use PHPStan\Type\Type;
 use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfoFactory;
 use Rector\BetterPhpDocParser\PhpDocManipulator\PhpDocTypeChanger;
+use Rector\Privatization\TypeManipulator\TypeNormalizer;
 use Rector\StaticTypeMapper\StaticTypeMapper;
 
-/**
- * @api downgrade
- */
 final class PropertyTypeDecorator
 {
     public function __construct(
         private readonly StaticTypeMapper $staticTypeMapper,
         private readonly PhpDocTypeChanger $phpDocTypeChanger,
         private readonly PhpDocInfoFactory $phpDocInfoFactory,
+        private readonly TypeNormalizer $typeNormalizer
     ) {
     }
 
+    public function decorateProperty(Property $property, Type $propertyType): void
+    {
+        // generalize false/true type to bool, as mostly default value but accepts both
+        $propertyType = $this->typeNormalizer->generalizeConstantBoolTypes($propertyType);
+
+        $phpDocInfo = $this->phpDocInfoFactory->createFromNodeOrEmpty($property);
+        $phpDocInfo->makeMultiLined();
+
+        $this->phpDocTypeChanger->changeVarType($phpDocInfo, $propertyType);
+    }
+
+    /**
+     * @api downgrade
+     */
     public function decoratePropertyWithDocBlock(Property $property, ComplexType|Identifier|Name $typeNode): void
     {
         $phpDocInfo = $this->phpDocInfoFactory->createFromNodeOrEmpty($property);
