11a07dc85bca356c642a10d029b71b222ce6a416
Tomas Votruba <tomas.vot@gmail.com>
2023-01-13 14:56:11 +0000

    fix date time

--- rules/Naming/ExpectedNameResolver/MatchParamTypeExpectedNameResolver.php
+++ rules/Naming/ExpectedNameResolver/MatchParamTypeExpectedNameResolver.php
@@ -5,15 +5,18 @@ declare(strict_types=1);
 namespace Rector\Naming\ExpectedNameResolver;
 
 use PhpParser\Node\Param;
+use PHPStan\Type\ObjectType;
 use Rector\Naming\Naming\PropertyNaming;
 use Rector\Naming\ValueObject\ExpectedName;
+use Rector\NodeNameResolver\NodeNameResolver;
 use Rector\StaticTypeMapper\StaticTypeMapper;
 
 final class MatchParamTypeExpectedNameResolver
 {
     public function __construct(
         private readonly StaticTypeMapper $staticTypeMapper,
-        private readonly PropertyNaming $propertyNaming
+        private readonly PropertyNaming $propertyNaming,
+        private readonly NodeNameResolver $nodeNameResolver,
     ) {
     }
 
@@ -25,6 +28,14 @@ final class MatchParamTypeExpectedNameResolver
         }
 
         $staticType = $this->staticTypeMapper->mapPhpParserNodePHPStanType($param->type);
+
+        // skip date time + date time interface, as should be kept
+        if ($staticType instanceof ObjectType && $staticType->isInstanceOf('DateTimeInterface')->yes()) {
+            if ($this->nodeNameResolver->isName($param, '*At')) {
+                return null;
+            }
+        }
+
         $expectedName = $this->propertyNaming->getExpectedNameFromType($staticType);
         if (! $expectedName instanceof ExpectedName) {
             return null;
