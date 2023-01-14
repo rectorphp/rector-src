a836f624240a459dbf25815b638211e9f1f34bcb
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-01-14 10:30:35 +0700

    better reprint

--- src/Rector/AbstractRector.php
+++ src/Rector/AbstractRector.php
@@ -424,7 +424,7 @@ CODE_SAMPLE;
             $previousNode = $node->getAttribute(AttributeKey::PREVIOUS_NODE);
 
             if ($previousNode instanceof InlineHTML && ! $firstNode instanceof InlineHTML) {
-                $previousNode->setAttribute(AttributeKey::ORIGINAL_NODE, null);
+                $node->setAttribute(AttributeKey::ORIGINAL_NODE, null);
             }
 
             $nodes = [$previousNode, ...$nodes];
