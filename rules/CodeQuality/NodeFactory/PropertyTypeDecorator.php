8bf0045044d1357c6d10bc61e84fd962f69ccc20
Tomas Votruba <tomas.vot@gmail.com>
2023-01-15 00:44:29 +0000

    remove downgrade only method

--- rules/CodeQuality/NodeFactory/PropertyTypeDecorator.php
+++ rules/CodeQuality/NodeFactory/PropertyTypeDecorator.php
@@ -4,20 +4,15 @@ declare(strict_types=1);
 
 namespace Rector\CodeQuality\NodeFactory;
 
-use PhpParser\Node\ComplexType;
-use PhpParser\Node\Identifier;
-use PhpParser\Node\Name;
 use PhpParser\Node\Stmt\Property;
 use PHPStan\Type\Type;
 use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfoFactory;
 use Rector\BetterPhpDocParser\PhpDocManipulator\PhpDocTypeChanger;
 use Rector\Privatization\TypeManipulator\TypeNormalizer;
-use Rector\StaticTypeMapper\StaticTypeMapper;
 
 final class PropertyTypeDecorator
 {
     public function __construct(
-        private readonly StaticTypeMapper $staticTypeMapper,
         private readonly PhpDocTypeChanger $phpDocTypeChanger,
         private readonly PhpDocInfoFactory $phpDocInfoFactory,
         private readonly TypeNormalizer $typeNormalizer
@@ -33,19 +28,5 @@ final class PropertyTypeDecorator
         $phpDocInfo->makeMultiLined();
 
         $this->phpDocTypeChanger->changeVarType($phpDocInfo, $propertyType);
-    }
-
-    /**
-     * @api downgrade
-     */
-    public function decoratePropertyWithDocBlock(Property $property, ComplexType|Identifier|Name $typeNode): void
-    {
-        $phpDocInfo = $this->phpDocInfoFactory->createFromNodeOrEmpty($property);
-        if ($phpDocInfo->getVarTagValueNode() !== null) {
-            return;
-        }
-
-        $newType = $this->staticTypeMapper->mapPhpParserNodePHPStanType($typeNode);
-        $this->phpDocTypeChanger->changeVarType($phpDocInfo, $newType);
     }
 }
