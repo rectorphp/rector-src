41bff4d6e416b9462d4c2ea3449afa5cb7297dd2
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-01-15 16:36:33 +0700

    Fixed :tada:

--- packages/PostRector/Rector/NameImportingPostRector.php
+++ packages/PostRector/Rector/NameImportingPostRector.php
@@ -10,6 +10,7 @@ use PhpParser\Node\Name;
 use PhpParser\Node\Name\FullyQualified;
 use PhpParser\Node\Stmt;
 use PhpParser\Node\Stmt\GroupUse;
+use PhpParser\Node\Stmt\InlineHTML;
 use PhpParser\Node\Stmt\Namespace_;
 use PhpParser\Node\Stmt\Use_;
 use PHPStan\Reflection\ReflectionProvider;
@@ -50,6 +51,11 @@ final class NameImportingPostRector extends AbstractPostRector
 
         $file = $this->currentFileProvider->getFile();
         if (! $file instanceof File) {
+            return null;
+        }
+
+        $currentStmt = current($file->getOldStmts());
+        if ($currentStmt instanceof InlineHTML) {
             return null;
         }
 
