5623fee4f7e65067d10da777ae77d442d4a8c431
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-01-23 22:39:49 +0700

    [Printer] Handle Add Node after Nop statement InnerHTML

---
+++ tests/Issues/AddNodeAfterNodeStmt/Source/AddNextNopRector.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Core\Tests\Issues\AddNodeAfterNodeStmt\Source;
+
+use PhpParser\Node;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\Echo_;
+use PhpParser\Node\Stmt\If_;
+use PhpParser\Node\Stmt\Nop;
+use Rector\Core\Rector\AbstractRector;
+use Rector\PostRector\Collector\NodesToAddCollector;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+class AddNextNopRector extends AbstractRector
+{
+    private bool $justAdded = false;
+
+    public function __construct(private readonly NodesToAddCollector $nodesToAddCollector)
+    {
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('uff', []);
+    }
+
+    public function getNodeTypes(): array
+    {
+        return [
+            Nop::class,
+        ];
+    }
+
+    public function refactor(Node $node)
+    {
+        if ($this->justAdded) {
+            return null;
+        }
+
+        $this->nodesToAddCollector->addNodeAfterNode(
+            new Echo_([new String_("this is new stmt after Nop")]),
+            $node
+        );
+        $this->justAdded = true;
+
+        return $node;
+    }
+}
