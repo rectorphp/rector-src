76cdc1d9585096ce30e191dfb76c7ade42c6e85d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-01-27 14:10:10 +0700

    test with addnodebeforenode

--- tests/Issues/AddNodeBeforeNodeStmt/Source/AddBeforeInlineHTMLRector.php
+++ tests/Issues/AddNodeBeforeNodeStmt/Source/AddBeforeInlineHTMLRector.php
@@ -9,6 +9,8 @@ use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Scalar\String_;
 use PhpParser\Node\Stmt\Expression;
+use PHPStan\PhpDocParser\Ast\PhpDoc\VarTagValueNode;
+use PHPStan\PhpDocParser\Ast\Type\IdentifierTypeNode;
 use Rector\Core\PhpParser\Node\CustomNode\FileWithoutNamespace;
 use Rector\Core\Rector\AbstractRector;
 use Rector\PostRector\Collector\NodesToAddCollector;
@@ -45,20 +47,43 @@ class AddBeforeInlineHTMLRector extends AbstractRector
 
         $firstStmt = current($node->stmts);
 
+        $expression1 = new Expression(
+            new Assign(
+                new Variable('test'), new String_('test')
+            )
+        );
+
+        $phpDocInfo = $this->phpDocInfoFactory->createEmpty($expression1);
+        $phpDocInfo->addTagValueNode(
+            new VarTagValueNode(
+                new IdentifierTypeNode('string'),
+                '$container',
+                ''
+            )
+        );
+
         $this->nodesToAddCollector->addNodeBeforeNode(
-            new Expression(
-                new Assign(
-                    new Variable('test'), new String_('test')
-                )
-            ),
+            $expression1,
             $firstStmt
         );
+
+        $expression2 = new Expression(
+            new Assign(
+                new Variable('test2'), new String_('test2')
+            )
+        );
+
+        $phpDocInfo = $this->phpDocInfoFactory->createEmpty($expression2);
+        $phpDocInfo->addTagValueNode(
+            new VarTagValueNode(
+                new IdentifierTypeNode('string'),
+                '$container',
+                ''
+            )
+        );
+
         $this->nodesToAddCollector->addNodeBeforeNode(
-            new Expression(
-                new Assign(
-                    new Variable('test2'), new String_('test2')
-                )
-            ),
+            $expression2,
             $firstStmt
         );
 
