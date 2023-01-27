36070a0b8de378e6941fa75e32617ac997fec207
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-01-27 13:58:10 +0700

    add test for array after inline HTML

---
+++ tests/Issues/ReturnArrayNodeAfterInlineHTMLStmt/Source/ArrayAddAfterInlineHTMLRector.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Core\Tests\Issues\ReturnArrayNodeAfterInlineHTMLStmt\Source;
+
+use PhpParser\Node;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\Echo_;
+use PhpParser\Node\Stmt\InlineHTML;
+use PHPStan\PhpDocParser\Ast\PhpDoc\VarTagValueNode;
+use PHPStan\PhpDocParser\Ast\Type\IdentifierTypeNode;
+use Rector\Core\Rector\AbstractRector;
+use Rector\PostRector\Collector\NodesToAddCollector;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+class ArrayAddAfterInlineHTMLRector extends AbstractRector
+{
+    private array $justAdded = [];
+
+    public function __construct(private readonly NodesToAddCollector $nodesToAddCollector)
+    {
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('uff', []);
+    }
+
+    public function getNodeTypes(): array
+    {
+        return [
+            InlineHTML::class,
+        ];
+    }
+
+    public function refactor(Node $node)
+    {
+        if (isset($this->justAdded[$this->file->getFilePath()])) {
+            return null;
+        }
+
+        $echo = new Echo_([new String_("this is new stmt after InlineHTML")]);
+
+        $phpDocInfo = $this->phpDocInfoFactory->createEmpty($echo);
+        $phpDocInfo->addTagValueNode(
+            new VarTagValueNode(
+                new IdentifierTypeNode('string'),
+                '$container',
+                ''
+            )
+        );
+
+        $this->justAdded[$this->file->getFilePath()] = true;
+
+        return [
+            $node,
+            $echo,
+        ];
+    }
+}
