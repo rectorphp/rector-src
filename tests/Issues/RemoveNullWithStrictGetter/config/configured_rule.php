43dd2efc5d663bc16327911fe31c6f6ecf60e753
Tomas Votruba <tomas.vot@gmail.com>
2023-02-06 19:19:06 +0000

    Remove deprecated ReturnTypeDeclarationRector, TypedPropertyRector, ParamTypeDeclarationRector rules (#3350)

--- tests/Issues/RemoveNullWithStrictGetter/config/configured_rule.php
+++ tests/Issues/RemoveNullWithStrictGetter/config/configured_rule.php
@@ -5,12 +5,9 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\DeadCode\Rector\PropertyProperty\RemoveNullPropertyInitializationRector;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictGetterMethodReturnTypeRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rule(TypedPropertyRector::class);
-
     $rectorConfig->rule(TypedPropertyFromStrictGetterMethodReturnTypeRector::class);
 
     // should be ignored if typed property is used
