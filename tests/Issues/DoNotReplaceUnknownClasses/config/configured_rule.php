586ca7f1995a85b36c91bb68a4daa5c57f7c4636
Tomas Votruba <tomas.vot@gmail.com>
2023-02-06 18:49:36 +0000

    remove deprecated ReturnTypeDeclarationRector rule

--- tests/Issues/DoNotReplaceUnknownClasses/config/configured_rule.php
+++ tests/Issues/DoNotReplaceUnknownClasses/config/configured_rule.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Doctrine\Rector\Property\DoctrineTargetEntityStringToClassConstantRector;
-use Rector\TypeDeclaration\Rector\FunctionLike\ReturnTypeDeclarationRector;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(DoctrineTargetEntityStringToClassConstantRector::class);
     $rectorConfig->rule(TypedPropertyFromAssignsRector::class);
-    $rectorConfig->rule(ReturnTypeDeclarationRector::class);
 };
