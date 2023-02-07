51fcdc2639267a875c3d5c1a985800104e984ead
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-02-07 23:50:52 +0700

    [Cache] Replace Strings::webalize() with sha1_file() from resolved path on ChangedFilesDetector (#3356)
    Co-authored-by: GitHub Action <action@github.com>

--- packages/Caching/Detector/ChangedFilesDetector.php
+++ packages/Caching/Detector/ChangedFilesDetector.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Rector\Caching\Detector;
 
-use Nette\Utils\Strings;
 use Rector\Caching\Cache;
 use Rector\Caching\Config\FileHashComputer;
 use Rector\Caching\Enum\CacheKey;
@@ -113,7 +112,7 @@ final class ChangedFilesDetector
 
     private function storeConfigurationDataHash(string $filePath, string $configurationHash): void
     {
-        $key = CacheKey::CONFIGURATION_HASH_KEY . '_' . Strings::webalize($filePath);
+        $key = CacheKey::CONFIGURATION_HASH_KEY . '_' . $this->hashFile($filePath);
         $this->invalidateCacheIfConfigurationChanged($key, $configurationHash);
 
         $this->cache->save($key, CacheKey::CONFIGURATION_HASH_KEY, $configurationHash);
