5e387212a6dc37bc87fba6e4026827058ec0cd84
Tomas Votruba <tomas.vot@gmail.com>
2023-02-07 10:47:40 +0000

    Add type crash test fixture

---
+++ tests/Issues/Issue7759/TypeContradictionsTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Core\Tests\Issues\Issue7759;
+
+use Iterator;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+/**
+ * @see https://github.com/rectorphp/rector/issues/7759
+ */
+final class TypeContradictionsTest extends AbstractRectorTestCase
+{
+    #[DataProvider('provideData')]
+    public function test(string $filePath): void
+    {
+        $this->doTestFile($filePath);
+    }
+
+    /**
+     * @return Iterator<array<string>>
+     */
+    public static function provideData(): Iterator
+    {
+        return self::yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/configured_rule.php';
+    }
+}
