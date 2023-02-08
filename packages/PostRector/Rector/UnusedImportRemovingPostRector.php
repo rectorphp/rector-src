72f4a7f75feb41b940beb89cdac0b1dae818f86f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-02-08 23:59:29 +0700

    Final touch: add used import

--- packages/PostRector/Rector/UnusedImportRemovingPostRector.php
+++ packages/PostRector/Rector/UnusedImportRemovingPostRector.php
@@ -7,6 +7,7 @@ namespace Rector\PostRector\Rector;
 use Nette\Utils\Strings;
 use PhpParser\Node;
 use PhpParser\Node\Name;
+use PhpParser\Node\Name\FullyQualified;
 use PhpParser\Node\Stmt\Namespace_;
 use PhpParser\Node\Stmt\Use_;
 use PhpParser\NodeTraverser;
