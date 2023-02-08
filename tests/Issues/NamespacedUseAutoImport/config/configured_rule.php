cb380b916a89b47c19e5732b57c26fda0397a462
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-02-08 23:46:31 +0700

    add use namespaced use auto import

---
+++ tests/Issues/NamespacedUseAutoImport/config/configured_rule.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->removeUnusedImports();
+    $rectorConfig->importNames();
+};
