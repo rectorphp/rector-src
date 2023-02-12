65c1a8a58fc8cad7e0944e9dbb7fa1a2a4b99310
GitHub Action <action@github.com>
2023-02-12 15:12:52 +0000

    [ci-review] Rector Rectify

--- rules/DeadCode/PhpDoc/DeadVarTagValueNodeAnalyzer.php
+++ rules/DeadCode/PhpDoc/DeadVarTagValueNodeAnalyzer.php
@@ -6,6 +6,7 @@ namespace Rector\DeadCode\PhpDoc;
 
 use PhpParser\Node\Stmt\Property;
 use PHPStan\PhpDocParser\Ast\PhpDoc\VarTagValueNode;
+use PHPStan\Type\IntersectionType;
 use PHPStan\Type\UnionType;
 use Rector\NodeTypeResolver\TypeComparator\TypeComparator;
 use Rector\StaticTypeMapper\StaticTypeMapper;
@@ -28,7 +29,7 @@ final class DeadVarTagValueNodeAnalyzer
         $propertyType = $this->staticTypeMapper->mapPhpParserNodePHPStanType($property->type);
         $docType = $this->staticTypeMapper->mapPHPStanPhpDocTypeNodeToPHPStanType($varTagValueNode->type, $property);
 
-        if ($propertyType instanceof UnionType && ! $docType instanceof UnionType && ! $docType instanceof \PHPStan\Type\IntersectionType) {
+        if ($propertyType instanceof UnionType && ! $docType instanceof UnionType && ! $docType instanceof IntersectionType) {
             return true;
         }
 
