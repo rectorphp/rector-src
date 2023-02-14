3fd80611a28af2742015a1f76924fdb86afd69fa
Grzegorz Korba <greg@codito.dev>
2023-02-14 14:21:52 +0100

    Fix grammar in user-facing exception

--- rules/Arguments/Rector/ClassMethod/ArgumentAdderRector.php
+++ rules/Arguments/Rector/ClassMethod/ArgumentAdderRector.php
@@ -212,7 +212,7 @@ CODE_SAMPLE
         for ($index = $lastPosition + 1; $index < $position; ++$index) {
             $param = $classMethod->params[$index];
             if (! $param->default instanceof Expr) {
-                throw new ShouldNotHappenException('Previous position does not has default value');
+                throw new ShouldNotHappenException('Previous position does not have default value');
             }
 
             $default = $this->nodePrinter->print($param->default);
