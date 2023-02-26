d09429333bdb9c199d952fbb942d1f8ee5f9678a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-02-27 01:27:32 +0700

    clean up

--- src/Application/FileProcessor/PhpFileProcessor.php
+++ src/Application/FileProcessor/PhpFileProcessor.php
@@ -173,12 +173,7 @@ final class PhpFileProcessor implements FileProcessorInterface
              * Handle space before <?php wiped on print format preserving
              * On inside content level
              */
-            $cleanedOriginalFileContent = Strings::replace(
-                $ltrimOriginalFileContent,
-                self::OPEN_TAG_SPACED_REGEX,
-                '<?php'
-            );
-            if ($cleanedOriginalFileContent === $newContent) {
+            if (Strings::replace($ltrimOriginalFileContent, self::OPEN_TAG_SPACED_REGEX, '<?php') === $newContent) {
                 return;
             }
         }
