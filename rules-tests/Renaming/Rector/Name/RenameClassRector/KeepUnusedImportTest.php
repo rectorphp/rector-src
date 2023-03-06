215fb3465eaec404a6d51eba5ddd5ee9bc5aea33
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-03-06 15:15:39 +0700

    [Renaming] Keep unused import unless configured $rectorConfig->removeUnusedImports() on RenameClassRector on no namespace

---
+++ rules-tests/Renaming/Rector/Name/RenameClassRector/KeepUnusedImportTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\Renaming\Rector\Name\RenameClassRector;
+
+use Iterator;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+final class KeepUnusedImportTest extends AbstractRectorTestCase
+{
+    #[DataProvider('provideData')]
+    public function test(string $filePath): void
+    {
+        $this->doTestFile($filePath);
+    }
+
+    public static function provideData(): Iterator
+    {
+        return self::yieldFilesFromDirectory(__DIR__ . '/FixtureKeepUnusedImport');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/keep_unused_import.php';
+    }
+}
