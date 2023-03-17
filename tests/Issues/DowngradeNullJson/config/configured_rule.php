e1dd98dc51c214a743fe1785fd264d2ea27efd72
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-03-17 18:48:42 +0700

    [DowngradePhp73][DowngradePhp74] Fix infinite loop on DowngradeNullCoalescingOperatorRector+DowngradePhp73JsonConstRector (#3484)
    * [DowngradePhp73][DowngradePhp74] Fix infinite loop on DowngradeNullCoalescingOperatorRector+DowngradePhp73JsonConstRector
    
    * fix

---
+++ tests/Issues/DowngradeNullJson/config/configured_rule.php
@@ -0,0 +1,12 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\DowngradePhp73\Rector\ConstFetch\DowngradePhp73JsonConstRector;
+use Rector\DowngradePhp74\Rector\Coalesce\DowngradeNullCoalescingOperatorRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rules([
+        DowngradeNullCoalescingOperatorRector::class,
+        DowngradePhp73JsonConstRector::class,
+    ]);
+};
