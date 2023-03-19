198c206cfd90bc0496397f67bb919b7683fb326e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-03-19 20:26:46 +0700

    final touch: clean up

--- packages/Config/RectorConfig.php
+++ packages/Config/RectorConfig.php
@@ -212,11 +212,7 @@ final class RectorConfig extends ContainerConfigurator
     public function cacheDirectory(string $directoryPath): void
     {
         // cache directory path is created via mkdir in CacheFactory
-        // when not exists, eg: default sys_get_temp_dir() . '/rector_cached_files' value
-        // on config/config.php, so:
-        //
-        //       no need to validate $directoryPath is a directory
-
+        // when not exists on config/config.php, so no need to validate $directoryPath is a directory
         $parameters = $this->parameters();
         $parameters->set(Option::CACHE_DIR, $directoryPath);
     }
