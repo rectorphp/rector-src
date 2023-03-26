b167b7109dd698b3f473bd3927a9677e46c2b887
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-03-26 16:49:35 +0700

    [core] Remove ParentConnectingVisitor from SimpleCallableNodeTraverser (#3520)

--- packages/PhpDocParser/NodeTraverser/SimpleCallableNodeTraverser.php
+++ packages/PhpDocParser/NodeTraverser/SimpleCallableNodeTraverser.php
@@ -6,8 +6,6 @@ namespace Rector\PhpDocParser\NodeTraverser;
 
 use PhpParser\Node;
 use PhpParser\NodeTraverser;
-use PhpParser\NodeVisitor\ParentConnectingVisitor;
-use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\PhpDocParser\NodeVisitor\CallableNodeVisitor;
 
 /**
@@ -33,25 +31,7 @@ final class SimpleCallableNodeTraverser
         $callableNodeVisitor = new CallableNodeVisitor($callable);
         $nodeTraverser->addVisitor($callableNodeVisitor);
 
-        if ($this->shouldConnectParent($node)) {
-            $nodeTraverser->addVisitor(new ParentConnectingVisitor());
-        }
-
         $nodes = $node instanceof Node ? [$node] : $node;
         $nodeTraverser->traverse($nodes);
-    }
-
-    /**
-     * @param Node|Node[] $node
-     */
-    private function shouldConnectParent(Node | array $node): bool
-    {
-        if ($node instanceof Node) {
-            $parentNode = $node->getAttribute(AttributeKey::PARENT_NODE);
-            return $parentNode instanceof Node;
-        }
-
-        // usage mostly by pass $node->stmts which parent node is the node
-        return true;
     }
 }
