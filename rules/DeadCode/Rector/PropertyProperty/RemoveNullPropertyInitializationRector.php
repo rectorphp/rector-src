58d862b6f54529f25ecb994ea5e7d2c3eb69f210
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-03-31 03:39:54 +0700

    [DeadCode] Remove previous and parent attribute usage on RemoveNullPropertyInitializationRector (#3541)

--- rules/DeadCode/Rector/PropertyProperty/RemoveNullPropertyInitializationRector.php
+++ rules/DeadCode/Rector/PropertyProperty/RemoveNullPropertyInitializationRector.php
@@ -7,11 +7,8 @@ namespace Rector\DeadCode\Rector\PropertyProperty;
 use PhpParser\Node;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\ConstFetch;
-use PhpParser\Node\NullableType;
 use PhpParser\Node\Stmt\Property;
-use PhpParser\Node\Stmt\PropertyProperty;
 use Rector\Core\Rector\AbstractRector;
-use Rector\NodeTypeResolver\Node\AttributeKey;
 use function strtolower;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
@@ -50,42 +47,41 @@ CODE_SAMPLE
      */
     public function getNodeTypes(): array
     {
-        return [PropertyProperty::class];
+        return [Property::class];
     }
 
     /**
-     * @param PropertyProperty $node
+     * @param Property $node
      */
     public function refactor(Node $node): ?Node
     {
-        $parentNode = $node->getAttribute(AttributeKey::PARENT_NODE);
-
-        // skip typed properties
-        if ($parentNode instanceof Property && $parentNode->type !== null) {
+        if ($node->type instanceof Node) {
             return null;
         }
 
-        $defaultValueNode = $node->default;
-        if (! $defaultValueNode instanceof Expr) {
-            return null;
+        $hasChanged = false;
+        foreach ($node->props as $prop) {
+            $defaultValueNode = $prop->default;
+            if (! $defaultValueNode instanceof Expr) {
+                continue;
+            }
+
+            if (! $defaultValueNode instanceof ConstFetch) {
+                continue;
+            }
+
+            if (strtolower((string) $defaultValueNode->name) !== 'null') {
+                continue;
+            }
+
+            $prop->default = null;
+            $hasChanged = true;
         }
 
-        if (! $defaultValueNode instanceof ConstFetch) {
-            return null;
+        if ($hasChanged) {
+            return $node;
         }
 
-        if (strtolower((string) $defaultValueNode->name) !== 'null') {
-            return null;
-        }
-
-        $nodeNode = $node->getAttribute(AttributeKey::PREVIOUS_NODE);
-
-        if ($nodeNode instanceof NullableType) {
-            return null;
-        }
-
-        $node->default = null;
-
-        return $node;
+        return null;
     }
 }
