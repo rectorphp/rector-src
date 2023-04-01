911c8ec015af3c86c770c30d4d0336e707b6886f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-04-02 02:25:01 +0700

    create dedicated service

--- src/PhpParser/NodeTraverser/NodeNopConnectingTraverser.php
+++ src/PhpParser/NodeTraverser/NodeNopConnectingTraverser.php
@@ -4,13 +4,17 @@ declare(strict_types=1);
 
 namespace Rector\Core\PhpParser\NodeTraverser;
 
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitor\NodeConnectingVisitor;
 use Rector\Core\PhpParser\NodeVisitor\NopVisitor;
 
-final class NodeNopConnectingTraverser extends NodeConnectingTraverser
+final class NodeNopConnectingTraverser extends NodeTraverser
 {
     public function __construct(NopVisitor $nopVisitor)
     {
         parent::__construct();
+
+        $this->addVisitor(new NodeConnectingVisitor());
         $this->addVisitor($nopVisitor);
     }
 }
