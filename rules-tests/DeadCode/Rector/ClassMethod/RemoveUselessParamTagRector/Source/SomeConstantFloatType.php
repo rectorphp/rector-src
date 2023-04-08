6e7b49547bcc38886d81711ada17408b1ef2771c
Tomas Votruba <tomas.vot@gmail.com>
2023-04-08 13:44:42 +0200

    fix type regressions

---
+++ rules-tests/DeadCode/Rector/ClassMethod/RemoveUselessParamTagRector/Source/SomeConstantFloatType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Rector\Tests\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector\Source;
+
+final class SomeConstantFloatType
+{
+    public const ONE_HALF = 1.5;
+
+    public const TWO_HALF = 2.5;
+}
