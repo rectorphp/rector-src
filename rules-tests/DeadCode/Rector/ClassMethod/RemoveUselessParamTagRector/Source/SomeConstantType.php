77a3c02e661dfd842550aa0597f871bd76e57a13
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-04-08 20:28:10 +0700

    add more fixtures for RemoveUselessParamTagRector (#3585)

---
+++ rules-tests/DeadCode/Rector/ClassMethod/RemoveUselessParamTagRector/Source/SomeConstantType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Rector\Tests\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector\Source;
+
+final class SomeConstantType
+{
+    public const YES = 'yes';
+
+    public const NO = 'no';
+}
