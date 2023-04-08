e1e865f3cb1a17ecccb1892df04198e0904cbdf0
Yassine Guedidi <yassine@guedidi.com>
2023-04-08 19:37:13 +0200

    Add similar tests to RemoveUselessReturnTagRector from RemoveUselessParamTagRector

---
+++ rules-tests/DeadCode/Rector/ClassMethod/RemoveUselessReturnTagRector/Source/SomeConstantFloatType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Rector\Tests\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector\Source;
+
+final class SomeConstantFloatType
+{
+    public const ONE_HALF = 1.5;
+
+    public const TWO_HALF = 2.5;
+}
---
+++ rules-tests/DeadCode/Rector/ClassMethod/RemoveUselessReturnTagRector/Source/SomeConstantType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Rector\Tests\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector\Source;
+
+final class SomeConstantType
+{
+    public const YES = 'yes';
+
+    public const NO = 'no';
+}
