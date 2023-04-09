cc30d2db2dcb4c5adacb7cf34edf25913be2bd01
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-04-09 08:23:06 +0700

    [DeadCode] Add fixture for keep constant type on @var usage on RemoveUselessVarTagRector (#3596)

---
+++ rules-tests/DeadCode/Rector/Property/RemoveUselessVarTagRector/Source/Source/SomeConstantFloatType.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\DeadCode\Rector\Property\RemoveUselessVarTagRector\Source;
+
+final class SomeConstantFloatType
+{
+    public const ONE_HALF = 1.5;
+
+    public const TWO_HALF = 2.5;
+}
