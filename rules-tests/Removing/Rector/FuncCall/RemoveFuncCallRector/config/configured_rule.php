ebc2e7efa17d9871cc5d9b90a81c41d504a214db
Geni Jaho <jahogeni@gmail.com>
2023-04-10 10:15:13 +0200

    Add new rule to remove function calls (#3524)
    * Add new rule to remove function calls
    
    * Removed RemoveFuncCall value object
    
    * Remove parent node if it is an expression or statement
    
    * Handle Expression node types
    
    * Small refactoring
    
    * Skip removing function calls in other expressions
    
    * Renamed fixture.
    
    * Generated docs.

---
+++ rules-tests/Removing/Rector/FuncCall/RemoveFuncCallRector/config/configured_rule.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Removing\Rector\FuncCall\RemoveFuncCallRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig
+        ->ruleWithConfiguration(RemoveFuncCallRector::class, [
+            'var_dump'
+        ]);
+};
