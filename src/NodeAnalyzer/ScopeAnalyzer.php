707ac86064f839712d50db1f36313efc9ea7a923
GitHub Action <actions@github.com>
2023-04-12 12:58:34 +0000

    [ci-review] Rector Rectify

--- src/NodeAnalyzer/ScopeAnalyzer.php
+++ src/NodeAnalyzer/ScopeAnalyzer.php
@@ -76,12 +76,14 @@ final class ScopeAnalyzer
         if ($node instanceof FileWithoutNamespace || $node instanceof Namespace_) {
             return $this->scopeFactory->createFromFile($filePath);
         }
-
         /**
          * Node and parent Node doesn't has Scope, and node and/or parent Node Start token pos is < 0,
          * it means the node and parent node just re-printed, the Scope need to be resolved from file
          */
-        if ($parentNode->getStartTokenPos() < 0 || $node->getStartTokenPos() < 0) {
+        if ($parentNode->getStartTokenPos() < 0) {
+            return $this->scopeFactory->createFromFile($filePath);
+        }
+        if ($node->getStartTokenPos() < 0) {
             return $this->scopeFactory->createFromFile($filePath);
         }
 
