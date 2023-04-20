3814ff810923f56a4b71d973f3ef104cb449d00f
Markus Staab <markus.staab@redaxo.de>
2023-04-20 15:03:02 +0200

    remove no longer necessary constructor

--- packages/NodeTypeResolver/NodeTypeCorrector/GenericClassStringTypeCorrector.php
+++ packages/NodeTypeResolver/NodeTypeCorrector/GenericClassStringTypeCorrector.php
@@ -4,21 +4,13 @@ declare(strict_types=1);
 
 namespace Rector\NodeTypeResolver\NodeTypeCorrector;
 
-use PHPStan\Reflection\ClassReflection;
-use PHPStan\Reflection\ReflectionProvider;
 use PHPStan\Type\Constant\ConstantStringType;
 use PHPStan\Type\Generic\GenericClassStringType;
-use PHPStan\Type\ObjectType;
 use PHPStan\Type\Type;
 use PHPStan\Type\TypeTraverser;
 
 final class GenericClassStringTypeCorrector
 {
-    public function __construct(
-        private readonly ReflectionProvider $reflectionProvider
-    ) {
-    }
-
     public function correct(Type $mainType): Type
     {
         // inspired from https://github.com/phpstan/phpstan-src/blob/94e3443b2d21404a821e05b901dd4b57fcbd4e7f/src/Type/Generic/TemplateTypeHelper.php#L18
