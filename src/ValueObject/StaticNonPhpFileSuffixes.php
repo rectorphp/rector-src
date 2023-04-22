1ed8835a87c2c3e8c89263531401395d2e476fad
Markus Staab <markus.staab@redaxo.de>
2023-04-22 10:29:39 +0200

    fix

--- src/ValueObject/StaticNonPhpFileSuffixes.php
+++ src/ValueObject/StaticNonPhpFileSuffixes.php
@@ -11,13 +11,4 @@ final class StaticNonPhpFileSuffixes
      */
     public const SUFFIXES = ['neon', 'yaml', 'xml', 'yml', 'twig', 'latte', 'blade.php', 'tpl'];
 
-    public static function getSuffixRegexPattern(): string
-    {
-        $quotedSuffixes = [];
-        foreach (self::SUFFIXES as $suffix) {
-            $quotedSuffixes[] = preg_quote($suffix, '#');
-        }
-
-        return '#\.(' . implode('|', $quotedSuffixes) . ')$#i';
-    }
 }
