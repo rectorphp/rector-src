c80983b4ac6b5bbef3ff68364f6f0e91706ab337
Yassine Guedidi <yassine@guedidi.com>
2023-04-23 19:29:11 +0200

    Add the failing test

---
+++ e2e/full-processing/rector.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
+use Rector\Config\RectorConfig;
+use Rector\EarlyReturn\Rector\If_\ChangeAndIfToEarlyReturnRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+    ]);
+
+    $rectorConfig->rule(ChangeAndIfToEarlyReturnRector::class);
+    $rectorConfig->rule(NewlineAfterStatementRector::class);
+};
