84238315f8c2171e803832b14d9bc7e004bfb01a
Markus Staab <markus.staab@redaxo.de>
2023-04-23 20:08:02 +0200

    Minor ClassMethodAssignManipulator improvements (#3662)

--- src/NodeManipulator/ClassMethodAssignManipulator.php
+++ src/NodeManipulator/ClassMethodAssignManipulator.php
@@ -216,17 +216,13 @@ final class ClassMethodAssignManipulator
         $variables = $this->betterNodeFinder->findInstanceOf($classMethod, Variable::class);
 
         foreach ($variables as $variable) {
-            if ($this->nodeNameResolver->isName($variable, 'this')) {
+            $variableName = $this->nodeNameResolver->getName($variable);
+            if ($variableName === 'this' || $variableName === null) {
                 continue;
             }
 
             $parentNode = $variable->getAttribute(AttributeKey::PARENT_NODE);
             if ($parentNode instanceof Node && $this->isExplicitlyReferenced($parentNode)) {
-                $variableName = $this->nodeNameResolver->getName($variable);
-                if ($variableName === null) {
-                    continue;
-                }
-
                 $referencedVariables[] = $variableName;
                 continue;
             }
@@ -242,11 +238,6 @@ final class ClassMethodAssignManipulator
             }
 
             if ($argumentPosition === null) {
-                continue;
-            }
-
-            $variableName = $this->nodeNameResolver->getName($variable);
-            if ($variableName === null) {
                 continue;
             }
 
@@ -332,10 +323,6 @@ final class ClassMethodAssignManipulator
             return false;
         }
 
-        if (! $this->nodeNameResolver->isNames($node, ['array_shift', '*sort'])) {
-            return false;
-        }
-
         if (! isset($node->args[0])) {
             return false;
         }
@@ -344,7 +331,11 @@ final class ClassMethodAssignManipulator
             return false;
         }
 
-        // is 1t argument
+        if (! $this->nodeNameResolver->isNames($node, ['array_shift', '*sort'])) {
+            return false;
+        }
+
+        // is 1st argument
         return $node->args[0]->value !== $variable;
     }
 
