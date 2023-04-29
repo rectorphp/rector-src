294cf41dd9e6c9988f99c0c0f44787949a16af7c
Markus Staab <maggus.staab@googlemail.com>
2023-04-29 13:50:11 +0200

    fix missing line numbers in ConsoleDiffer

--- src/Console/Formatter/ConsoleDiffer.php
+++ src/Console/Formatter/ConsoleDiffer.php
@@ -17,7 +17,7 @@ final class ConsoleDiffer
     ) {
         // @see https://github.com/sebastianbergmann/diff#strictunifieddiffoutputbuilder
         // @see https://github.com/sebastianbergmann/diff/compare/4.0.4...5.0.0#diff-251edf56a6344c03fa264a4926b06c2cee43c25f66192d5f39ebee912b7442dc for upgrade
-        $unifiedDiffOutputBuilder = new UnifiedDiffOutputBuilder();
+        $unifiedDiffOutputBuilder = new UnifiedDiffOutputBuilder( "--- Original\n+++ New\n", true);
         $this->differ = new Differ($unifiedDiffOutputBuilder);
     }
 
