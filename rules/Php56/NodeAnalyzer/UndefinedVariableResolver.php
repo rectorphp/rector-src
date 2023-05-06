07e9ccf00a1318a1083bda3aaa0188cf14b3c39f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-05-06 13:03:39 +0700

    [CodeQuality][Php56] Do not add unnecessary init value on ThrowWithPreviousExceptionRector+OptionalParametersAfterRequiredRector+AddDefaultValueForUndefinedVariableRector (#3737)
    * [CodeQuality][Php56] Do not add unnecessary init value on ThrowWithPreviousExceptionRector+OptionalParametersAfterRequiredRector+AddDefaultValueForUndefinedVariableRector
    
    * Fixed :tada:
    
    * Revert Fixed :tada:
    
    This reverts commit fe63c2186bb6ecccb5f89b38a3b4242c0e22ef91.
    
    * Fix with check original node parent is a Catch_ node
    
    * [ci-review] Rector Rectify
    
    * Revert [ci-review] Rector Rectify
    
    This reverts commit becc3fda272da484dd56169a4517f27fc0ecf0a1.
    
    * Revert Fix with check original node parent is a Catch_ node
    
    This reverts commit 77d0c381f1cb0f773c14825241459eb4d161a128.
    
    * better approach: verify parent node of variable is not just reprinted
    
    * final touch: future comment
    
    * final touch: future comment
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

--- rules/Php56/NodeAnalyzer/UndefinedVariableResolver.php
+++ rules/Php56/NodeAnalyzer/UndefinedVariableResolver.php
@@ -117,9 +117,15 @@ final class UndefinedVariableResolver
         return $currentStmt instanceof Stmt && $currentStmt->getAttribute(AttributeKey::IS_UNREACHABLE) === true;
     }
 
-    private function issetOrUnsetOrEmptyParent(Node $parentNode): bool
+    private function shouldSkipWithParent(Node $parentNode): bool
     {
-        return in_array($parentNode::class, [Unset_::class, UnsetCast::class, Isset_::class, Empty_::class], true);
+        if (in_array($parentNode::class, [Unset_::class, UnsetCast::class, Isset_::class, Empty_::class], true)) {
+            return true;
+        }
+
+        // when parent Node origNode is null, it means parent Node just reprinted, so it can't be verified
+        // so skip it
+        return ! $parentNode->getAttribute(AttributeKey::ORIGINAL_NODE) instanceof Node;
     }
 
     private function isAsCoalesceLeftOrAssignOpCoalesceVar(Node $parentNode, Variable $variable): bool
@@ -150,7 +156,7 @@ final class UndefinedVariableResolver
             return true;
         }
 
-        if ($this->issetOrUnsetOrEmptyParent($parentNode)) {
+        if ($this->shouldSkipWithParent($parentNode)) {
             return true;
         }
 
