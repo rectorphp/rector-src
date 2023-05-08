6e3b55b85620eff0b3856d8a2fa6ee6a826c6e39
Tomas Votruba <tomas.vot@gmail.com>
2023-05-08 13:42:47 +0100

    [cleanup] Make use of getArgs(), remove ignored from phpstan

--- rules/Php80/Rector/Ternary/GetDebugTypeRector.php
+++ rules/Php80/Rector/Ternary/GetDebugTypeRector.php
@@ -83,7 +83,12 @@ CODE_SAMPLE
         $getClassFuncCallOrClassConstFetchClass = $node->if;
 
         $firstExpr = $getClassFuncCallOrClassConstFetchClass instanceof FuncCall
+<<<<<<< HEAD
             ? $getClassFuncCallOrClassConstFetchClass->getArgs()[0]->value
+=======
+            ? $getClassFuncCallOrClassConstFetchClass->getArgs()[0]
+->value
+>>>>>>> cad5fa0e7c ([cleanup] Make use of getArgs(), remove ignored from phpstan)
             : $getClassFuncCallOrClassConstFetchClass->class;
 
         return $this->nodeFactory->createFuncCall('get_debug_type', [$firstExpr]);
