6655c2c4e3d72d513c38162da5087bbc2df25b83
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-05-09 14:38:07 +0700

    [PHPStanStaticTypeMapper] Reduce bool check on UnionTypeMapper (#3778)

--- packages/PHPStanStaticTypeMapper/TypeAnalyzer/BoolUnionTypeAnalyzer.php
+++ packages/PHPStanStaticTypeMapper/TypeAnalyzer/BoolUnionTypeAnalyzer.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Rector\PHPStanStaticTypeMapper\TypeAnalyzer;
 
-use PHPStan\Type\NullType;
 use PHPStan\Type\UnionType;
 
 final class BoolUnionTypeAnalyzer
@@ -18,24 +17,5 @@ final class BoolUnionTypeAnalyzer
         }
 
         return true;
-    }
-
-    public function isNullableBoolUnionType(UnionType $unionType): bool
-    {
-        $hasNullable = false;
-        foreach ($unionType->getTypes() as $unionedType) {
-            if ($unionedType instanceof NullType) {
-                $hasNullable = true;
-                continue;
-            }
-
-            if ($unionedType->isBoolean()->yes()) {
-                continue;
-            }
-
-            return false;
-        }
-
-        return $hasNullable;
     }
 }
