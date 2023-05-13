a74c640bc1043f19006c7403461108f61d8bf2a5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-05-14 00:33:42 +0700

    [Traverser] Add ByRefVariableNodeVisitor (#3821)
    * [Traverser] Add ByRefVariableNodeVisitor
    
    * rectify
    
    * stmts
    
    * [ci-review] Rector Rectify
    
    * fix phpstan
    
    * [ci-review] Rector Rectify
    
    * fix phpstan
    
    * [ci-review] Rector Rectify
    
    * fix
    
    * fix
    
    * check AssignRef early
    
    * Fix
    
    * Fix phpstan
    
    * [ci-review] Rector Rectify
    
    * clean up
    
    * move private method below
    
    * clean up: param var name is always string, early return on null stmts
    
    * Final touch: clean up
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

--- src/NodeAnalyzer/VariableAnalyzer.php
+++ src/NodeAnalyzer/VariableAnalyzer.php
@@ -4,23 +4,11 @@ declare(strict_types=1);
 
 namespace Rector\Core\NodeAnalyzer;
 
-use PhpParser\Node;
-use PhpParser\Node\Expr\AssignRef;
-use PhpParser\Node\Expr\ClosureUse;
 use PhpParser\Node\Expr\Variable;
-use PhpParser\Node\Param;
-use Rector\Core\PhpParser\Comparing\NodeComparator;
-use Rector\Core\PhpParser\Node\BetterNodeFinder;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 
 final class VariableAnalyzer
 {
-    public function __construct(
-        private readonly BetterNodeFinder $betterNodeFinder,
-        private readonly NodeComparator $nodeComparator
-    ) {
-    }
-
     public function isStaticOrGlobal(Variable $variable): bool
     {
         if ($variable->getAttribute(AttributeKey::IS_GLOBAL_VAR) === true) {
@@ -32,40 +20,6 @@ final class VariableAnalyzer
 
     public function isUsedByReference(Variable $variable): bool
     {
-        return (bool) $this->betterNodeFinder->findFirstPrevious($variable, function (Node $subNode) use (
-            $variable
-        ): bool {
-            if ($this->isParamReferenced($subNode, $variable)) {
-                return true;
-            }
-
-            if (! $subNode instanceof Variable) {
-                return false;
-            }
-
-            if (! $this->nodeComparator->areNodesEqual($subNode, $variable)) {
-                return false;
-            }
-
-            $parentNode = $subNode->getAttribute(AttributeKey::PARENT_NODE);
-            if ($parentNode instanceof ClosureUse) {
-                return $parentNode->byRef;
-            }
-
-            return $parentNode instanceof AssignRef;
-        });
-    }
-
-    private function isParamReferenced(Node $node, Variable $variable): bool
-    {
-        if (! $node instanceof Param) {
-            return false;
-        }
-
-        if (! $this->nodeComparator->areNodesEqual($node->var, $variable)) {
-            return false;
-        }
-
-        return $node->byRef;
+        return $variable->getAttribute(AttributeKey::IS_BYREF_VAR) === true;
     }
 }
