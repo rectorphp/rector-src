b23743780b1e2a209d19e463ce595f3e12e280e2
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-05-14 00:52:47 +0700

    [Traverser] Add ByRefReturnNodeVisitor

---
+++ packages/NodeTypeResolver/PHPStan/Scope/NodeVisitor/ByRefReturnNodeVisitor.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\NodeTypeResolver\PHPStan\Scope\NodeVisitor;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\CallLike;
+use PhpParser\Node\FunctionLike;
+use PhpParser\Node\Stmt\Return_;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitorAbstract;
+use Rector\NodeTypeResolver\Node\AttributeKey;
+use Rector\PhpDocParser\NodeTraverser\SimpleCallableNodeTraverser;
+
+final class ByRefReturnNodeVisitor extends NodeVisitorAbstract
+{
+    public function __construct(private readonly SimpleCallableNodeTraverser $simpleCallableNodeTraverser)
+    {
+    }
+
+    public function enterNode(Node $node): ?Node
+    {
+        if (! $node instanceof FunctionLike) {
+            return null;
+        }
+
+        if (! $node->returnsByRef()) {
+            return null;
+        }
+
+        $stmts = $node->getStmts();
+        if ($stmts === null) {
+            return null;
+        }
+
+        $this->simpleCallableNodeTraverser->traverseNodesWithCallable(
+            $stmts,
+            function (Node $subNode) {
+                if ($subNode instanceof CallLike || $subNode instanceof FunctionLike) {
+                    return NodeTraverser::DONT_TRAVERSE_CURRENT_AND_CHILDREN;
+                }
+
+                if (! $subNode instanceof Return_) {
+                    return null;
+                }
+
+                $subNode->setAttribute(AttributeKey::IS_BYREF_RETURN, true);
+                return $subNode;
+            }
+        );
+
+        return null;
+    }
+}
