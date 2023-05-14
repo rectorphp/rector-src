c538985a35955ea70b01ab379bd3ea8a6b98bd70
Markus Staab <maggus.staab@googlemail.com>
2023-05-14 20:07:28 +0200

    Discard changes to packages/Caching/Detector/ChangedFilesDetector.php

--- packages/Caching/Detector/ChangedFilesDetector.php
+++ packages/Caching/Detector/ChangedFilesDetector.php
@@ -7,7 +7,6 @@ namespace Rector\Caching\Detector;
 use Rector\Caching\Cache;
 use Rector\Caching\Config\FileHashComputer;
 use Rector\Caching\Enum\CacheKey;
-use Rector\Core\Util\FileHasher;
 
 /**
  * Inspired by https://github.com/symplify/symplify/pull/90/files#diff-72041b2e1029a08930e13d79d298ef11
@@ -145,14 +144,12 @@ final class ChangedFilesDetector
 
     private function getFilePathCacheKey(string $filePath): string
     {
-        $fileHasher = new FileHasher();
-        return $fileHasher->hash($this->resolvePath($filePath));
+        return sha1($this->resolvePath($filePath));
     }
 
     private function hashFile(string $filePath): string
     {
-        $fileHasher = new FileHasher();
-        return $fileHasher->hashFiles([$this->resolvePath($filePath)]);
+        return (string) sha1_file($this->resolvePath($filePath));
     }
 
     private function storeConfigurationDataHash(string $filePath, string $configurationHash): void
