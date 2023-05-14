554c43624ed786f5f1562845f8188a201d7f1625
Markus Staab <maggus.staab@googlemail.com>
2023-05-14 20:04:33 +0200

    drop version checks

--- tests/Util/FileHasherTest.php
+++ tests/Util/FileHasherTest.php
@@ -24,20 +24,12 @@ final class FileHasherTest extends AbstractTestCase {
 
         public function testHash(): void
         {
-            if (PHP_VERSION_ID < 80100) {
-                $this->markTestSkipped('xxh128 is not available on PHP 8.0');
-            }
-
             $hash = $this->fileHasher->hash('some string');
             $this->assertSame('8df638f91bacc826bf50c04efd7df1b1', $hash);
         }
 
         public function testHashFiles(): void
         {
-            if (PHP_VERSION_ID < 80100) {
-                $this->markTestSkipped('xxh128 is not available on PHP 8.0');
-            }
-
             $dir = sys_get_temp_dir();
             $file = $dir.'/FileHasherTest-fixture.txt';
 
