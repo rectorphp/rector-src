dcd4cb37550b8a65db7c0b7aa026def5e2a49704
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-05-17 18:49:47 +0700

    Fix

--- packages/NodeTypeResolver/PHPStan/Scope/NodeVisitor/StmtKeyNodeVisitor.php
+++ packages/NodeTypeResolver/PHPStan/Scope/NodeVisitor/StmtKeyNodeVisitor.php
@@ -11,11 +11,17 @@ use PhpParser\Node\Stmt\Namespace_;
 use PhpParser\NodeVisitorAbstract;
 use Rector\Core\Contract\PhpParser\Node\StmtsAwareInterface;
 use Rector\Core\PhpParser\Node\CustomNode\FileWithoutNamespace;
+use Rector\Core\Provider\CurrentFileProvider;
+use Rector\Core\ValueObject\Application\File;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\NodeTypeResolver\PHPStan\Scope\Contract\NodeVisitor\ScopeResolverNodeVisitorInterface;
 
 final class StmtKeyNodeVisitor extends NodeVisitorAbstract implements ScopeResolverNodeVisitorInterface
 {
+    public function __construct(private readonly CurrentFileProvider $currentFileProvider)
+    {
+    }
+
     public function beforeTraverse(array $nodes): array
     {
         foreach ($nodes as $key => $node) {
@@ -42,7 +48,15 @@ final class StmtKeyNodeVisitor extends NodeVisitorAbstract implements ScopeResol
 
         $parentNode = $node->getAttribute(AttributeKey::PARENT_NODE);
         if (! $parentNode instanceof StmtsAwareInterface) {
-            // handle on beforeTraverse()
+            // on __construct(), $file not yet a File object
+            $file = $this->currentFileProvider->getFile();
+            if ($file instanceof File) {
+                $newStmts = $file->getNewStmts();
+                foreach ($newStmts as $key => $childStmt) {
+                    $childStmt->setAttribute(AttributeKey::STMT_KEY, $key);
+                }
+            }
+
             return null;
         }
 
