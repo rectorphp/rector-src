d6dd0f081bc4db30aaacbba90f2e076f5a7c7cf9
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-05-18 04:18:58 +0700

    Cherry pick key compare on BetterNodeFinder::resolvePreviousNodeFromFile()

--- src/PhpParser/Node/BetterNodeFinder.php
+++ src/PhpParser/Node/BetterNodeFinder.php
@@ -597,9 +597,15 @@ final class BetterNodeFinder
         $currentStmtKey = $node->getAttribute(AttributeKey::STMT_KEY);
         foreach ($newStmts as $key => $newStmt) {
             $stmtKey = $newStmt->getAttribute(AttributeKey::STMT_KEY);
-            if ($stmtKey === $currentStmtKey) {
-                return $newStmts[$key - 1] ?? null;
+            if ($stmtKey !== $currentStmtKey) {
+                continue;
             }
+
+            if ($key !== $currentStmtKey) {
+                continue;
+            }
+
+            return $newStmts[$key - 1] ?? null;
         }
 
         return null;
