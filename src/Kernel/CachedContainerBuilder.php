5e3afcbdafde000f950ee844f22a2b020216cd82
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-05-17 22:03:46 +0700

    Revert Cleanup CachedContainer invalidation (#3867) (#3880)
    This reverts commit f0141afbc14283e1d7b2f40b5d30e42ab0045623.

--- src/Kernel/CachedContainerBuilder.php
+++ src/Kernel/CachedContainerBuilder.php
@@ -37,11 +37,10 @@ final class CachedContainerBuilder
         if (file_exists($file)) {
             require_once $file;
             $className = '\\' . __NAMESPACE__ . '\\' . $className;
-            $cachedContainer = new $className();
-            if (! $cachedContainer instanceof ContainerInterface) {
+            $container = new $className();
+            if (! $container instanceof ContainerInterface) {
                 throw new ShouldNotHappenException();
             }
-            $container = new CacheInvalidatingContainer($cachedContainer);
         } else {
             $container = ($containerBuilderCallback)($configFiles);
 
