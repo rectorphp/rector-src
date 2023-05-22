8f3569e84677b4a29feb0af0788a0156086f921f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-05-23 01:55:14 +0700

    [DeadCode][CodeQuality] Fix infinite loop on RemoveUnusedVariableAssignRector+SimplifyIfElseToTernaryRector (#3933)
    * [DeadCode][CodeQuality] Fix infinite loop on RemoveUnusedVariableAssignRector+SimplifyIfElseToTernaryRector
    
    * [ci-review] Rector Rectify
    
    * revert
    
    * update fixture
    
    * Fixed
    
    * stmt
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

---
+++ tests/Issues/RemoveUnusedVariableElseToTernary/config/configured_rule.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\Assign\RemoveUnusedVariableAssignRector;
+use Rector\CodeQuality\Rector\If_\SimplifyIfElseToTernaryRector;
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rules([
+        RemoveUnusedVariableAssignRector::class,
+        SimplifyIfElseToTernaryRector::class
+    ]);
+};
