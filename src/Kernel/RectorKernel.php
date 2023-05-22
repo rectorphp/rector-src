bb8b8fc8e8f6a2c2049e4b2cf7b2fc9e63bcb945
Tomas Votruba <tomas.vot@gmail.com>
2023-05-22 09:43:53 +0200

    rebuild cache

--- src/Kernel/RectorKernel.php
+++ src/Kernel/RectorKernel.php
@@ -17,7 +17,7 @@ final class RectorKernel
     /**
      * @var string
      */
-    private const CACHE_KEY = 'v17';
+    private const CACHE_KEY = 'v18';
 
     private ContainerInterface|null $container = null;
 
