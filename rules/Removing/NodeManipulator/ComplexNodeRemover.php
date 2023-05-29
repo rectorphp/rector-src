45a9dd5df8411c31f89e87b8cfcc4348625ce627
Tomas Votruba <tomas.vot@gmail.com>
2023-05-29 16:17:23 +0200

    fix cs

--- rules/Removing/NodeManipulator/ComplexNodeRemover.php
+++ rules/Removing/NodeManipulator/ComplexNodeRemover.php
@@ -119,6 +119,38 @@ final class ComplexNodeRemover
         return true;
     }
 
+    /**
+     * @param int[] $paramKeysToBeRemoved
+     * @return int[]
+     */
+    public function processRemoveParamWithKeys(ClassMethod $classMethod, array $paramKeysToBeRemoved): array
+    {
+        $totalKeys = count($classMethod->params) - 1;
+        $removedParamKeys = [];
+
+        foreach ($paramKeysToBeRemoved as $paramKeyToBeRemoved) {
+            $startNextKey = $paramKeyToBeRemoved + 1;
+            for ($nextKey = $startNextKey; $nextKey <= $totalKeys; ++$nextKey) {
+                if (! isset($classMethod->params[$nextKey])) {
+                    // no next param, break the inner loop, remove the param
+                    break;
+                }
+
+                if (in_array($nextKey, $paramKeysToBeRemoved, true)) {
+                    // keep searching next key not in $paramKeysToBeRemoved
+                    continue;
+                }
+
+                return [];
+            }
+
+            unset($classMethod->params[$paramKeyToBeRemoved]);
+            $removedParamKeys[] = $paramKeyToBeRemoved;
+        }
+
+        return $removedParamKeys;
+    }
+
     private function removeConstructorDependency(Class_ $class, string $propertyName): void
     {
         $classMethod = $class->getMethod(MethodName::CONSTRUCT);
@@ -167,38 +199,6 @@ final class ComplexNodeRemover
         }
 
         $this->processRemoveParamWithKeys($classMethod, $paramKeysToBeRemoved);
-    }
-
-    /**
-     * @param int[] $paramKeysToBeRemoved
-     * @return int[]
-     */
-    public function processRemoveParamWithKeys(ClassMethod $classMethod, array $paramKeysToBeRemoved): array
-    {
-        $totalKeys = count($classMethod->params) - 1;
-        $removedParamKeys = [];
-
-        foreach ($paramKeysToBeRemoved as $paramKeyToBeRemoved) {
-            $startNextKey = $paramKeyToBeRemoved + 1;
-            for ($nextKey = $startNextKey; $nextKey <= $totalKeys; ++$nextKey) {
-                if (! isset($classMethod->params[$nextKey])) {
-                    // no next param, break the inner loop, remove the param
-                    break;
-                }
-
-                if (in_array($nextKey, $paramKeysToBeRemoved, true)) {
-                    // keep searching next key not in $paramKeysToBeRemoved
-                    continue;
-                }
-
-                return [];
-            }
-
-            unset($classMethod->params[$paramKeyToBeRemoved]);
-            $removedParamKeys[] = $paramKeyToBeRemoved;
-        }
-
-        return $removedParamKeys;
     }
 
     /**
