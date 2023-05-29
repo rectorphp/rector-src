65a34348284b8c419eb3386c9eed8c312bcfcb20
Tomas Votruba <tomas.vot@gmail.com>
2023-05-29 16:28:24 +0200

    Remove removeNode() from FuncGetArgsToVariadicParamRector

--- rules/CodingStyle/Rector/ClassMethod/FuncGetArgsToVariadicParamRector.php
+++ rules/CodingStyle/Rector/ClassMethod/FuncGetArgsToVariadicParamRector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Rector\CodingStyle\Rector\ClassMethod;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\Closure;
 use PhpParser\Node\Expr\FuncCall;
@@ -70,16 +71,27 @@ CODE_SAMPLE
             return null;
         }
 
-        /** @var Assign $assign */
-        $assign = $expression->expr;
-
-        if ($assign->var instanceof Variable) {
-            $variableName = $this->getName($assign->var);
-            if ($variableName === null) {
-                return null;
+        foreach ($node->stmts as $key => $stmt) {
+            if (! $stmt instanceof Expression) {
+                continue;
             }
 
-            $this->removeNode($assign);
+            if (! $stmt->expr instanceof Assign) {
+                continue;
+            }
+
+            $assign = $stmt->expr;
+            if (! $this->isFuncGetArgsFuncCall($assign->expr)) {
+                continue;
+            }
+
+            if (! $assign->var instanceof Variable) {
+                continue;
+            }
+
+            $variableName = $this->getName($assign->var);
+            unset($node->stmts[$key]);
+
             return $this->applyVariadicParams($node, $variableName);
         }
 
@@ -97,8 +109,8 @@ CODE_SAMPLE
         string $variableName
     ): ClassMethod | Function_ | Closure | null {
         $param = $this->createVariadicParam($variableName);
-        $variableParam = $param->var;
-        if ($variableParam instanceof Variable && $this->hasFunctionOrClosureInside($node, $variableParam)) {
+
+        if ($param->var instanceof Variable && $this->hasFunctionOrClosureInside($node, $param->var)) {
             return null;
         }
 
@@ -168,5 +180,14 @@ CODE_SAMPLE
     {
         $variable = new Variable($variableName);
         return new Param($variable, null, null, false, true);
+    }
+
+    private function isFuncGetArgsFuncCall(Expr $expr): bool
+    {
+        if (! $expr instanceof FuncCall) {
+            return false;
+        }
+
+        return $this->isName($expr, 'func_get_args');
     }
 }
