3fdf7579b394d12c5a0ec249f86cb3b35775394a
Tomas Votruba <tomas.vot@gmail.com>
2023-05-30 10:39:03 +0200

    Remove unused method addNodesBeforeNode()

--- src/Kernel/RectorKernel.php
+++ src/Kernel/RectorKernel.php
@@ -17,7 +17,7 @@ final class RectorKernel
     /**
      * @var string
      */
-    private const CACHE_KEY = 'v41';
+    private const CACHE_KEY = 'v42';
 
     private ContainerInterface|null $container = null;
 
