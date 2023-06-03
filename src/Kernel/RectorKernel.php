e8a77f5219f6da45aefb410c35daef2b7e64b4f1
Tomas Votruba <tomas.vot@gmail.com>
2023-06-03 17:45:42 +0200

    update cache

--- src/Kernel/RectorKernel.php
+++ src/Kernel/RectorKernel.php
@@ -17,7 +17,7 @@ final class RectorKernel
     /**
      * @var string
      */
-    private const CACHE_KEY = 'v47';
+    private const CACHE_KEY = 'v48';
 
     private ContainerInterface|null $container = null;
 
