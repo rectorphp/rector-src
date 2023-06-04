6c742c2b77722accec382c111d4ad7598e55dbb5
Tomas Votruba <tomas.vot@gmail.com>
2023-06-04 19:48:37 +0200

    Remove RemoveAlwaysTrueConditionSetInConstructor, as overly specific and not practical; better handle with PHPStan

--- src/Kernel/RectorKernel.php
+++ src/Kernel/RectorKernel.php
@@ -17,7 +17,7 @@ final class RectorKernel
     /**
      * @var string
      */
-    private const CACHE_KEY = 'v52';
+    private const CACHE_KEY = 'v53';
 
     private ContainerInterface|null $container = null;
 
