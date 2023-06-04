063c488b9b6a4eefe4a65fb6236f0b00b0e14890
Tomas Votruba <tomas.vot@gmail.com>
2023-06-05 00:40:12 +0200

    Remove removeNode() from RemoveEmptyMethodCallRector (#4071)

--- src/Kernel/RectorKernel.php
+++ src/Kernel/RectorKernel.php
@@ -17,7 +17,7 @@ final class RectorKernel
     /**
      * @var string
      */
-    private const CACHE_KEY = 'v53';
+    private const CACHE_KEY = 'v54';
 
     private ContainerInterface|null $container = null;
 
