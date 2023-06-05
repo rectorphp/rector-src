2dccbb6b176a836e174babbaf6611a0d4730c55c
Tomas Votruba <tomas.vot@gmail.com>
2023-06-05 15:57:36 +0200

    Remove removeNode() from RemoveDeadReturnRector, merge RemoveLastReturnRector to RemoveDeadReturnRector (#4085)

--- rules/DeadCode/Rector/FunctionLike/RemoveDeadReturnRector.php
+++ rules/DeadCode/Rector/FunctionLike/RemoveDeadReturnRector.php
@@ -8,7 +8,9 @@ use PhpParser\Node;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\Closure;
 use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Else_;
 use PhpParser\Node\Stmt\Function_;
+use PhpParser\Node\Stmt\If_;
 use PhpParser\Node\Stmt\Return_;
 use Rector\Core\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
@@ -72,16 +74,22 @@ CODE_SAMPLE
      */
     public function refactor(Node $node): ?Node
     {
-        if ($node->stmts === []) {
+        if ($node->stmts === [] || $node->stmts === null) {
             return null;
         }
 
-        if ($node->stmts === null) {
-            return null;
-        }
+        $lastStmtKey = array_key_last($node->stmts);
 
-        $stmtValues = array_values($node->stmts);
-        $lastStmt = end($stmtValues);
+        $lastStmt = $node->stmts[$lastStmtKey];
+
+        if ($lastStmt instanceof If_) {
+            if (! $this->isBareIfWithOnlyStmtEmptyReturn($lastStmt)) {
+                return null;
+            }
+
+            $lastStmt->stmts = [];
+            return $node;
+        }
 
         if (! $lastStmt instanceof Return_) {
             return null;
@@ -91,8 +99,29 @@ CODE_SAMPLE
             return null;
         }
 
-        $this->removeNode($lastStmt);
-
+        unset($node->stmts[$lastStmtKey]);
         return $node;
+    }
+
+    private function isBareIfWithOnlyStmtEmptyReturn(If_ $if): bool
+    {
+        if ($if->else instanceof Else_) {
+            return false;
+        }
+
+        if ($if->elseifs !== []) {
+            return false;
+        }
+
+        if (count($if->stmts) !== 1) {
+            return false;
+        }
+
+        $onlyStmt = $if->stmts[0];
+        if (! $onlyStmt instanceof Return_) {
+            return false;
+        }
+
+        return ! $onlyStmt->expr instanceof Expr;
     }
 }
