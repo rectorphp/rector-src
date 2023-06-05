5d1127fea05e1a1e06bf11c4cdbcdb569e67e47b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-05 16:51:26 +0700

    [FileProcessor] Show warning on has InlineHTML node if file has changed

--- src/Application/FileProcessor/PhpFileProcessor.php
+++ src/Application/FileProcessor/PhpFileProcessor.php
@@ -69,14 +69,6 @@ final class PhpFileProcessor implements FileProcessorInterface
             return $systemErrorsAndFileDiffs;
         }
 
-        // show warning on has InlineHTML node
-        if ($file->hasInlineHTMLNode()) {
-            $this->symfonyStyle->warning(sprintf(
-                'File %s has InlineHTML node, this may cause unexpected output, you may need to manually verify the changed file',
-                $file->getFilePath()
-            ));
-        }
-
         $fileHasChanged = false;
 
         // 2. change nodes with Rectors
@@ -103,6 +95,14 @@ final class PhpFileProcessor implements FileProcessorInterface
                 $fileHasChanged = true;
             }
         } while ($fileHasChangedInCurrentPass);
+
+        // show warning on has InlineHTML node if file has changed
+        if ($fileHasChanged && $file->hasInlineHTMLNode()) {
+            $this->symfonyStyle->warning(sprintf(
+                'File %s has InlineHTML node, this may cause unexpected output, you may need to manually verify the changed file',
+                $this->filePathHelper->relativePath($file->getFilePath())
+            ));
+        }
 
         // 5. add as cacheable if not changed at all
         if (! $fileHasChanged) {
