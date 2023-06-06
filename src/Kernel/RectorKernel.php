bf9224dde5ada8bc2afe996e41d031c988311a12
Tomas Votruba <tomas.vot@gmail.com>
2023-06-06 18:45:45 +0200

    flip key

--- src/Kernel/RectorKernel.php
+++ src/Kernel/RectorKernel.php
@@ -17,7 +17,7 @@ final class RectorKernel
     /**
      * @var string
      */
-    private const CACHE_KEY = 'v63';
+    private const CACHE_KEY = 'v64';
 
     private ContainerInterface|null $container = null;
 
