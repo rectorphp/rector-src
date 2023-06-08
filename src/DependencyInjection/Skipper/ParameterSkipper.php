f56119552e46954c9dfb588a00628b89204c9fc3
Tomas Votruba <tomas.vot@gmail.com>
2023-06-08 23:02:15 +0200

    ease Rector params

--- src/DependencyInjection/Skipper/ParameterSkipper.php
+++ src/DependencyInjection/Skipper/ParameterSkipper.php
@@ -14,35 +14,9 @@ use Symfony\Component\DependencyInjection\Definition;
 
 final class ParameterSkipper
 {
-    /**
-     * Classes that create circular dependencies
-     *
-     * @var class-string<LoaderInterface>[]|string[]
-     */
-    private const DEFAULT_EXCLUDED_FATAL_CLASSES = [
-        'Symfony\Component\Form\FormExtensionInterface',
-        'Symfony\Component\Asset\PackageInterface',
-        'Symfony\Component\Config\Loader\LoaderInterface',
-        'Symfony\Component\VarDumper\Dumper\ContextProvider\ContextProviderInterface',
-        'EasyCorp\Bundle\EasyAdminBundle\Form\Type\Configurator\TypeConfiguratorInterface',
-        'Sonata\CoreBundle\Model\Adapter\AdapterInterface',
-        'Sonata\Doctrine\Adapter\AdapterChain',
-        'Sonata\Twig\Extension\TemplateExtension',
-    ];
-
-    /**
-     * @var string[]
-     */
-    private array $excludedFatalClasses = [];
-
-    /**
-     * @param string[] $excludedFatalClasses
-     */
     public function __construct(
         private readonly ParameterTypeResolver $parameterTypeResolver,
-        array $excludedFatalClasses = []
     ) {
-        $this->excludedFatalClasses = array_merge(self::DEFAULT_EXCLUDED_FATAL_CLASSES, $excludedFatalClasses);
     }
 
     public function shouldSkipParameter(
@@ -69,11 +43,8 @@ final class ParameterSkipper
             return true;
         }
 
-        if (in_array($parameterType, $this->excludedFatalClasses, true)) {
-            return true;
-        }
-
-        if (! class_exists($parameterType) && ! interface_exists($parameterType)) {
+        // autowire only rector classes
+        if (! str_starts_with($parameterType, 'Rector\\')) {
             return true;
         }
 
