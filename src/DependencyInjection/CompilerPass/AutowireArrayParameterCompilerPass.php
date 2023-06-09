4b20f894c8949eada453e2cbbf7e01bc12170a23
Tomas Votruba <tomas.vot@gmail.com>
2023-06-09 16:18:29 +0200

    autowire TypeWithClassTypeSpecifierInterface explicit

--- src/DependencyInjection/CompilerPass/AutowireArrayParameterCompilerPass.php
+++ src/DependencyInjection/CompilerPass/AutowireArrayParameterCompilerPass.php
@@ -107,6 +107,8 @@ final class AutowireArrayParameterCompilerPass implements CompilerPassInterface
                 $reflectionMethod
             );
 
+            dump($parameterType);
+
             if ($parameterType === null) {
                 continue;
             }
