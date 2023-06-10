65765cf54e851c4b768a9124a4abf2790d3037f8
Tomas Votruba <tomas.vot@gmail.com>
2023-06-10 23:12:26 +0200

    Remove parent node look up from ReturnTypeFromStrictTypedCallRector (#4166)

--- rules/TypeDeclaration/Rector/ClassMethod/ReturnTypeFromStrictTypedCallRector.php
+++ rules/TypeDeclaration/Rector/ClassMethod/ReturnTypeFromStrictTypedCallRector.php
@@ -6,7 +6,7 @@ namespace Rector\TypeDeclaration\Rector\ClassMethod;
 
 use PhpParser\Node;
 use PhpParser\Node\ComplexType;
-use PhpParser\Node\Expr\ArrowFunction;
+use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\Closure;
 use PhpParser\Node\FunctionLike;
 use PhpParser\Node\Identifier;
@@ -17,6 +17,7 @@ use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Function_;
 use PhpParser\Node\Stmt\Return_;
 use PhpParser\Node\UnionType as PhpParserUnionType;
+use PhpParser\NodeTraverser;
 use PHPStan\Analyser\Scope;
 use PHPStan\Type\NullType;
 use PHPStan\Type\ObjectType;
@@ -24,7 +25,6 @@ use PHPStan\Type\UnionType;
 use Rector\Core\Php\PhpVersionProvider;
 use Rector\Core\Rector\AbstractScopeAwareRector;
 use Rector\Core\ValueObject\PhpVersionFeature;
-use Rector\PHPStanStaticTypeMapper\Enum\TypeKind;
 use Rector\TypeDeclaration\NodeAnalyzer\TypeNodeUnwrapper;
 use Rector\TypeDeclaration\TypeAnalyzer\ReturnStrictTypeAnalyzer;
 use Rector\TypeDeclaration\TypeInferer\ReturnTypeInferer;
@@ -89,7 +89,7 @@ CODE_SAMPLE
      */
     public function getNodeTypes(): array
     {
-        return [ClassMethod::class, Function_::class, Closure::class, ArrowFunction::class];
+        return [ClassMethod::class, Function_::class, Closure::class];
     }
 
     public function provideMinPhpVersion(): int
@@ -98,46 +98,27 @@ CODE_SAMPLE
     }
 
     /**
-     * @param ClassMethod|Function_|Closure|ArrowFunction $node
+     * @param ClassMethod|Function_|Closure $node
      */
     public function refactorWithScope(Node $node, Scope $scope): ?Node
     {
-        if ($this->isSkipped($node, $scope)) {
+        if ($node->stmts === null) {
             return null;
         }
 
-        if ($node instanceof ArrowFunction) {
-            return $this->processArrowFunction($node);
+        if ($this->shouldSkip($node, $scope)) {
+            return null;
         }
 
-        /** @var Return_[] $returns */
-        $returns = $this->betterNodeFinder->find((array) $node->stmts, function (Node $subNode) use ($node): bool {
-            $currentFunctionLike = $this->betterNodeFinder->findParentType($subNode, FunctionLike::class);
+        $currentScopeReturns = $this->findCurrentScopeReturns($node);
 
-            if ($currentFunctionLike === $node) {
-                return $subNode instanceof Return_;
-            }
-
-            $currentReturn = $this->betterNodeFinder->findParentType($subNode, Return_::class);
-            if (! $currentReturn instanceof Return_) {
-                return false;
-            }
-
-            $currentReturnFunctionLike = $this->betterNodeFinder->findParentType($currentReturn, FunctionLike::class);
-            if ($currentReturnFunctionLike !== $currentFunctionLike) {
-                return false;
-            }
-
-            return $subNode instanceof Return_;
-        });
-
-        $returnedStrictTypes = $this->returnStrictTypeAnalyzer->collectStrictReturnTypes($returns);
+        $returnedStrictTypes = $this->returnStrictTypeAnalyzer->collectStrictReturnTypes($currentScopeReturns);
         if ($returnedStrictTypes === []) {
             return null;
         }
 
         if (count($returnedStrictTypes) === 1) {
-            return $this->refactorSingleReturnType($returns[0], $returnedStrictTypes[0], $node);
+            return $this->refactorSingleReturnType($currentScopeReturns[0], $returnedStrictTypes[0], $node);
         }
 
         if ($this->phpVersionProvider->isAtLeastPhpVersion(PhpVersionFeature::UNION_TYPES)) {
@@ -149,25 +130,6 @@ CODE_SAMPLE
         }
 
         return null;
-    }
-
-    private function processArrowFunction(ArrowFunction $arrowFunction): ?ArrowFunction
-    {
-        $resolvedType = $this->nodeTypeResolver->getType($arrowFunction->expr);
-
-        // void type is not accepted for arrow functions - https://www.php.net/manual/en/functions.arrow.php#125673
-        if ($resolvedType->isVoid()->yes()) {
-            return null;
-        }
-
-        $returnType = $this->staticTypeMapper->mapPHPStanTypeToPhpParserNode($resolvedType, TypeKind::RETURN);
-
-        if (! $returnType instanceof Node) {
-            return null;
-        }
-
-        $arrowFunction->returnType = $returnType;
-        return $arrowFunction;
     }
 
     private function isUnionPossibleReturnsVoid(ClassMethod | Function_ | Closure $node): bool
@@ -198,29 +160,23 @@ CODE_SAMPLE
         return $node;
     }
 
-    private function isSkipped(ClassMethod | Function_ | Closure | ArrowFunction $node, Scope $scope): bool
+    private function shouldSkip(ClassMethod | Function_ | Closure $node, Scope $scope): bool
     {
-        if ($node instanceof ArrowFunction) {
-            return $node->returnType !== null;
-        }
-
         if ($node->returnType !== null) {
             return true;
         }
 
-        if (! $node instanceof ClassMethod) {
-            return $this->isUnionPossibleReturnsVoid($node);
+        if ($node instanceof ClassMethod) {
+            if ($this->classMethodReturnTypeOverrideGuard->shouldSkipClassMethod($node, $scope)) {
+                return true;
+            }
+
+            if ($node->isMagic()) {
+                return true;
+            }
         }
 
-        if ($this->classMethodReturnTypeOverrideGuard->shouldSkipClassMethod($node, $scope)) {
-            return true;
-        }
-
-        if (! $node->isMagic()) {
-            return $this->isUnionPossibleReturnsVoid($node);
-        }
-
-        return true;
+        return $this->isUnionPossibleReturnsVoid($node);
     }
 
     private function refactorSingleReturnType(
@@ -246,5 +202,36 @@ CODE_SAMPLE
         $functionLike->returnType = $returnType;
 
         return $functionLike;
+    }
+
+    /**
+     * @return Return_[]
+     */
+    private function findCurrentScopeReturns(ClassMethod|Function_|Closure $node): array
+    {
+        $currentScopeReturns = [];
+
+        if ($node->stmts === null) {
+            return [];
+        }
+
+        $this->traverseNodesWithCallable($node->stmts, static function (Node $node) use (&$currentScopeReturns): ?int {
+            // skip scope nesting
+            if ($node instanceof FunctionLike) {
+                return NodeTraverser::STOP_TRAVERSAL;
+            }
+
+            if (! $node instanceof Return_) {
+                return null;
+            }
+
+            if (! $node->expr instanceof Expr) {
+                return null;
+            }
+
+            $currentScopeReturns[] = $node;
+            return null;
+        });
+        return $currentScopeReturns;
     }
 }
