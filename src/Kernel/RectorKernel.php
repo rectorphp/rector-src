f548c580c68bd3d8a102cf2767a7383a4138d9b4
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-13 02:52:14 +0700

    increase kernel cache key

--- src/Kernel/RectorKernel.php
+++ src/Kernel/RectorKernel.php
@@ -17,7 +17,7 @@ final class RectorKernel
     /**
      * @var string
      */
-    private const CACHE_KEY = 'v114';
+    private const CACHE_KEY = 'v115';
 
     private ContainerInterface|null $container = null;
 
