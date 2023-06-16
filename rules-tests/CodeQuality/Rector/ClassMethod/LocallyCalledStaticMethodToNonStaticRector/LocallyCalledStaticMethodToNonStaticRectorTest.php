813511bda184c98d94a84e77104f8803275bc9f7
Tomas Votruba <tomas.vot@gmail.com>
2023-06-16 15:51:21 +0200

    Move LocallyCalledStaticMethodToNonStaticRector to code quality namespace, to avoid dumping removed static set

--- rules-tests/CodeQuality/Rector/ClassMethod/LocallyCalledStaticMethodToNonStaticRector/LocallyCalledStaticMethodToNonStaticRectorTest.php
+++ rules-tests/CodeQuality/Rector/ClassMethod/LocallyCalledStaticMethodToNonStaticRector/LocallyCalledStaticMethodToNonStaticRectorTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Rector\Tests\RemovingStatic\Rector\ClassMethod\LocallyCalledStaticMethodToNonStaticRector;
+namespace Rector\Tests\CodeQuality\Rector\ClassMethod\LocallyCalledStaticMethodToNonStaticRector;
 
 use Iterator;
 use PHPUnit\Framework\Attributes\DataProvider;
