a572ba67009597739ac2c66d34c0473d200dbb97
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-16 17:24:02 +0700

    [ReadWrite] Remove parent lookup on ArrayDimFetchParentNodeReadAnalyzer

--- packages/ReadWrite/ParentNodeReadAnalyzer/ArrayDimFetchParentNodeReadAnalyzer.php
+++ packages/ReadWrite/ParentNodeReadAnalyzer/ArrayDimFetchParentNodeReadAnalyzer.php
@@ -7,8 +7,8 @@ namespace Rector\ReadWrite\ParentNodeReadAnalyzer;
 use PhpParser\Node;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\ArrayDimFetch;
-use PhpParser\Node\Expr\Assign;
 use Rector\Core\PhpParser\Node\BetterNodeFinder;
+use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\ReadWrite\Contract\ParentNodeReadAnalyzerInterface;
 
 final class ArrayDimFetchParentNodeReadAnalyzer implements ParentNodeReadAnalyzerInterface
@@ -29,19 +29,6 @@ final class ArrayDimFetchParentNodeReadAnalyzer implements ParentNodeReadAnalyze
         }
 
         // is left part of assign
-        return $this->isLeftPartOfAssign($expr);
-    }
-
-    private function isLeftPartOfAssign(Expr $expr): bool
-    {
-        $parentAssign = $this->betterNodeFinder->findParentType($expr, Assign::class);
-        if (! $parentAssign instanceof Assign) {
-            return true;
-        }
-
-        return ! (bool) $this->betterNodeFinder->findFirst(
-            $parentAssign->var,
-            static fn (Node $node): bool => $node === $expr
-        );
+        return $expr->getAttribute(AttributeKey::IS_ASSIGNED_TO) === true;
     }
 }
