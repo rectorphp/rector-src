3d4a86c31511bda1ded00a70ed36c72849d19824
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-16 17:28:57 +0700

    [ReadWrite] Remove parent lookup on ArrayDimFetchParentNodeReadAnalyzer (#4239)
    * [ReadWrite] Remove parent lookup on ArrayDimFetchParentNodeReadAnalyzer
    
    * [ci-review] Rector Rectify
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

--- packages/ReadWrite/ParentNodeReadAnalyzer/ArrayDimFetchParentNodeReadAnalyzer.php
+++ packages/ReadWrite/ParentNodeReadAnalyzer/ArrayDimFetchParentNodeReadAnalyzer.php
@@ -7,17 +7,11 @@ namespace Rector\ReadWrite\ParentNodeReadAnalyzer;
 use PhpParser\Node;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\ArrayDimFetch;
-use PhpParser\Node\Expr\Assign;
-use Rector\Core\PhpParser\Node\BetterNodeFinder;
+use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\ReadWrite\Contract\ParentNodeReadAnalyzerInterface;
 
 final class ArrayDimFetchParentNodeReadAnalyzer implements ParentNodeReadAnalyzerInterface
 {
-    public function __construct(
-        private readonly BetterNodeFinder $betterNodeFinder,
-    ) {
-    }
-
     public function isRead(Expr $expr, Node $parentNode): bool
     {
         if (! $parentNode instanceof ArrayDimFetch) {
@@ -29,19 +23,6 @@ final class ArrayDimFetchParentNodeReadAnalyzer implements ParentNodeReadAnalyze
         }
 
         // is left part of assign
-        return $this->isLeftPartOfAssign($expr);
-    }
-
-    private function isLeftPartOfAssign(Expr $expr): bool
-    {
-        $parentAssign = $this->betterNodeFinder->findParentType($expr, Assign::class);
-        if (! $parentAssign instanceof Assign) {
-            return true;
-        }
-
-        return ! (bool) $this->betterNodeFinder->findFirst(
-            $parentAssign->var,
-            static fn (Node $node): bool => $node === $expr
-        );
+        return $expr->getAttribute(AttributeKey::IS_ASSIGNED_TO) === true;
     }
 }
