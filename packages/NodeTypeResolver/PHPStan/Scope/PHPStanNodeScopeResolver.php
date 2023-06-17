db5914857e63e1f25cb890b10ba8ddedd9ae24ab
GitHub Action <actions@github.com>
2023-06-17 06:11:18 +0000

    [ci-review] Rector Rectify

--- packages/NodeTypeResolver/PHPStan/Scope/PHPStanNodeScopeResolver.php
+++ packages/NodeTypeResolver/PHPStan/Scope/PHPStanNodeScopeResolver.php
@@ -31,7 +31,6 @@ use PhpParser\Node\Name;
 use PhpParser\Node\NullableType;
 use PhpParser\Node\Stmt;
 use PhpParser\Node\Stmt\Class_;
-use PhpParser\Node\Stmt\ClassConst;
 use PhpParser\Node\Stmt\ClassLike;
 use PhpParser\Node\Stmt\Declare_;
 use PhpParser\Node\Stmt\Enum_;
