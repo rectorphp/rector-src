aa30c89dc29f604d44729ab0a0aa625b04f098e7
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-18 02:11:55 +0700

    [NodeTypeResolver] Remove parent lookup on ArrayTypeAnalyzer

--- packages/NodeTypeResolver/TypeAnalyzer/ArrayTypeAnalyzer.php
+++ packages/NodeTypeResolver/TypeAnalyzer/ArrayTypeAnalyzer.php
@@ -11,6 +11,7 @@ use PhpParser\Node\Expr\StaticPropertyFetch;
 use PhpParser\Node\Stmt\ClassLike;
 use PhpParser\Node\Stmt\Property;
 use PHPStan\PhpDocParser\Ast\Type\ArrayShapeNode;
+use PHPStan\Reflection\ClassReflection;
 use PHPStan\Reflection\Php\PhpPropertyReflection;
 use PHPStan\Type\Accessory\HasOffsetType;
 use PHPStan\Type\Accessory\NonEmptyArrayType;
@@ -21,6 +22,7 @@ use PHPStan\Type\MixedType;
 use PHPStan\Type\Type;
 use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfo;
 use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfoFactory;
+use Rector\Core\PhpParser\ClassLikeAstResolver;
 use Rector\Core\PhpParser\Node\BetterNodeFinder;
 use Rector\Core\Reflection\ReflectionResolver;
 use Rector\NodeNameResolver\NodeNameResolver;
@@ -34,6 +36,7 @@ final class ArrayTypeAnalyzer
         private readonly BetterNodeFinder $betterNodeFinder,
         private readonly PhpDocInfoFactory $phpDocInfoFactory,
         private readonly ReflectionResolver $reflectionResolver,
+        private readonly ClassLikeAstResolver $classLikeAstResolver
     ) {
     }
 
@@ -100,8 +103,8 @@ final class ArrayTypeAnalyzer
             return false;
         }
 
-        $classLike = $this->betterNodeFinder->findParentType($expr, ClassLike::class);
-        if (! $classLike instanceof ClassLike) {
+        $classReflection = $this->reflectionResolver->resolveClassReflection($expr);
+        if (! $classReflection instanceof ClassReflection) {
             return false;
         }
 
@@ -110,7 +113,10 @@ final class ArrayTypeAnalyzer
             return false;
         }
 
+        /** @var ClassLike $classLike */
+        $classLike = $this->classLikeAstResolver->resolveClassFromClassReflection($classReflection);
         $property = $classLike->getProperty($propertyName);
+
         if (! $property instanceof Property) {
             return false;
         }
