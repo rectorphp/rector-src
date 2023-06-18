c3333d83c9df918d68159bb18e87cfff5ecb6aa2
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-18 13:15:58 +0700

    [Performance] Do not traverse current and children on context check Break_ and If_

--- packages/NodeTypeResolver/PHPStan/Scope/NodeVisitor/ContextNodeVisitor.php
+++ packages/NodeTypeResolver/PHPStan/Scope/NodeVisitor/ContextNodeVisitor.php
@@ -5,12 +5,16 @@ declare(strict_types=1);
 namespace Rector\NodeTypeResolver\PHPStan\Scope\NodeVisitor;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\ArrowFunction;
+use PhpParser\Node\FunctionLike;
 use PhpParser\Node\Stmt\Break_;
+use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\Do_;
 use PhpParser\Node\Stmt\For_;
 use PhpParser\Node\Stmt\Foreach_;
 use PhpParser\Node\Stmt\If_;
 use PhpParser\Node\Stmt\While_;
+use PhpParser\NodeTraverser;
 use PhpParser\NodeVisitorAbstract;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\NodeTypeResolver\PHPStan\Scope\Contract\NodeVisitor\ScopeResolverNodeVisitorInterface;
@@ -27,7 +31,11 @@ final class ContextNodeVisitor extends NodeVisitorAbstract implements ScopeResol
         if ($node instanceof For_ || $node instanceof Foreach_ || $node instanceof While_ || $node instanceof Do_) {
             $this->simpleCallableNodeTraverser->traverseNodesWithCallable(
                 $node->stmts,
-                static function (Node $subNode) {
+                static function (Node $subNode): ?int {
+                    if ($subNode instanceof Class_ || ($subNode instanceof FunctionLike && ! $subNode instanceof ArrowFunction)) {
+                        return NodeTraverser::DONT_TRAVERSE_CURRENT_AND_CHILDREN;
+                    }
+
                     if ($subNode instanceof If_ || $subNode instanceof Break_) {
                         $subNode->setAttribute(AttributeKey::IS_IN_LOOP, true);
                     }
