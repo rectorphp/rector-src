290caa3f9a89dcf067ee268c09e49b465a92344a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-19 20:23:05 +0700

    clean up

--- src/Rector/AbstractScopeAwareRector.php
+++ src/Rector/AbstractScopeAwareRector.php
@@ -59,8 +59,6 @@ abstract class AbstractScopeAwareRector extends AbstractRector implements ScopeA
                 static::class,
             );
 
-            print_node($node);
-
             throw new ShouldNotHappenException($errorMessage);
         }
 
