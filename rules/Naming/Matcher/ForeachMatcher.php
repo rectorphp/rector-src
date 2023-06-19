231b87935eef55dc8ec4d54dbb6646edf84cb97d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-20 02:16:13 +0700

    fix phpstan

--- rules/Naming/Matcher/ForeachMatcher.php
+++ rules/Naming/Matcher/ForeachMatcher.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Rector\Naming\Matcher;
 
+use PhpParser\Node\Expr\Closure;
 use PhpParser\Node\Expr\Variable;
-use PhpParser\Node\FunctionLike;
+use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Foreach_;
+use PhpParser\Node\Stmt\Function_;
 use Rector\Naming\ValueObject\VariableAndCallForeach;
 use Rector\NodeNameResolver\NodeNameResolver;
 
@@ -18,7 +20,7 @@ final class ForeachMatcher
     ) {
     }
 
-    public function match(Foreach_ $foreach, FunctionLike $functionLike): ?VariableAndCallForeach
+    public function match(Foreach_ $foreach, ClassMethod|Closure|Function_ $functionLike): ?VariableAndCallForeach
     {
         $call = $this->callMatcher->matchCall($foreach);
         if ($call === null) {
