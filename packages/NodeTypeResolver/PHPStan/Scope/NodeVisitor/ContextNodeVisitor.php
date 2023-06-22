94d9278e3957bccced6eeea3873929a7229a3b3d
GitHub Action <actions@github.com>
2023-06-22 11:43:39 +0000

    [ci-review] Rector Rectify

--- packages/NodeTypeResolver/PHPStan/Scope/NodeVisitor/ContextNodeVisitor.php
+++ packages/NodeTypeResolver/PHPStan/Scope/NodeVisitor/ContextNodeVisitor.php
@@ -9,7 +9,6 @@ use PhpParser\Node\Attribute;
 use PhpParser\Node\Expr\Array_;
 use PhpParser\Node\Expr\Closure;
 use PhpParser\Node\Expr\Isset_;
-use PhpParser\Node\FunctionLike;
 use PhpParser\Node\Stmt\Break_;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\Do_;
