a83130e25c515f2b52c753dda415c2e831b4b842
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-22 23:29:12 +0700

    [NodeTypeResolver] Remove callable traverse on ContextNodeVisitor

--- packages/NodeTypeResolver/PHPStan/Scope/NodeVisitor/ContextNodeVisitor.php
+++ packages/NodeTypeResolver/PHPStan/Scope/NodeVisitor/ContextNodeVisitor.php
@@ -7,32 +7,22 @@ namespace Rector\NodeTypeResolver\PHPStan\Scope\NodeVisitor;
 use PhpParser\Node;
 use PhpParser\Node\Attribute;
 use PhpParser\Node\Expr\Array_;
-use PhpParser\Node\Expr\Closure;
 use PhpParser\Node\Expr\Isset_;
 use PhpParser\Node\Stmt\Break_;
-use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\Do_;
 use PhpParser\Node\Stmt\Else_;
 use PhpParser\Node\Stmt\ElseIf_;
 use PhpParser\Node\Stmt\For_;
 use PhpParser\Node\Stmt\Foreach_;
-use PhpParser\Node\Stmt\Function_;
 use PhpParser\Node\Stmt\If_;
 use PhpParser\Node\Stmt\Unset_;
 use PhpParser\Node\Stmt\While_;
-use PhpParser\NodeTraverser;
 use PhpParser\NodeVisitorAbstract;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\NodeTypeResolver\PHPStan\Scope\Contract\NodeVisitor\ScopeResolverNodeVisitorInterface;
-use Rector\PhpDocParser\NodeTraverser\SimpleCallableNodeTraverser;
 
 final class ContextNodeVisitor extends NodeVisitorAbstract implements ScopeResolverNodeVisitorInterface
 {
-    public function __construct(
-        private readonly SimpleCallableNodeTraverser $simpleCallableNodeTraverser
-    ) {
-    }
-
     public function enterNode(Node $node): ?Node
     {
         if ($node instanceof For_ || $node instanceof Foreach_ || $node instanceof While_ || $node instanceof Do_) {
@@ -46,16 +36,13 @@ final class ContextNodeVisitor extends NodeVisitorAbstract implements ScopeResol
         }
 
         if ($node instanceof Attribute) {
-            $this->simpleCallableNodeTraverser->traverseNodesWithCallable(
-                $node->args,
-                static function (Node $subNode) {
-                    if ($subNode instanceof Array_) {
-                        $subNode->setAttribute(AttributeKey::IS_ARRAY_IN_ATTRIBUTE, true);
-                    }
-
-                    return null;
+            foreach ($node->args as $arg) {
+                if ($arg->value instanceof Array_) {
+                    $arg->value->setAttribute(AttributeKey::IS_ARRAY_IN_ATTRIBUTE, true);
                 }
-            );
+            }
+
+            return null;
         }
 
         if ($node instanceof If_ || $node instanceof Else_ || $node instanceof ElseIf_) {
@@ -83,37 +70,19 @@ final class ContextNodeVisitor extends NodeVisitorAbstract implements ScopeResol
 
     private function processContextInIf(If_|Else_|ElseIf_ $node): void
     {
-        $this->simpleCallableNodeTraverser->traverseNodesWithCallable(
-            $node->stmts,
-            static function (Node $subNode): ?int {
-                if ($subNode instanceof Class_ || $subNode instanceof Function_ || $subNode instanceof Closure) {
-                    return NodeTraverser::DONT_TRAVERSE_CURRENT_AND_CHILDREN;
-                }
-
-                if ($subNode instanceof Break_) {
-                    $subNode->setAttribute(AttributeKey::IS_IN_IF, true);
-                }
-
-                return null;
+        foreach ($node->stmts as $stmt) {
+            if ($stmt instanceof Break_) {
+                $stmt->setAttribute(AttributeKey::IS_IN_IF, true);
             }
-        );
+        }
     }
 
     private function processContextInLoop(For_|Foreach_|While_|Do_ $node): void
     {
-        $this->simpleCallableNodeTraverser->traverseNodesWithCallable(
-            $node->stmts,
-            static function (Node $subNode): ?int {
-                if ($subNode instanceof Class_ || $subNode instanceof Function_ || $subNode instanceof Closure) {
-                    return NodeTraverser::DONT_TRAVERSE_CURRENT_AND_CHILDREN;
-                }
-
-                if ($subNode instanceof If_ || $subNode instanceof Break_) {
-                    $subNode->setAttribute(AttributeKey::IS_IN_LOOP, true);
-                }
-
-                return null;
+        foreach ($node->stmts as $stmt) {
+            if ($stmt instanceof If_ || $stmt instanceof Break_) {
+                $stmt->setAttribute(AttributeKey::IS_IN_LOOP, true);
             }
-        );
+        }
     }
 }
