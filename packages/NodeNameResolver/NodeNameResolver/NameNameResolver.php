cb1e27396e0f7327372b72b88a976131f7d03e66
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-23 21:42:32 +0700

    [NodeNameResolver] Remove parent attribute on NameNameResolver

--- packages/NodeNameResolver/NodeNameResolver/NameNameResolver.php
+++ packages/NodeNameResolver/NodeNameResolver/NameNameResolver.php
@@ -5,22 +5,15 @@ declare(strict_types=1);
 namespace Rector\NodeNameResolver\NodeNameResolver;
 
 use PhpParser\Node;
-use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Name;
 use PHPStan\Analyser\Scope;
 use Rector\NodeNameResolver\Contract\NodeNameResolverInterface;
-use Rector\NodeTypeResolver\Node\AttributeKey;
 
 /**
  * @implements NodeNameResolverInterface<Name>
  */
 final class NameNameResolver implements NodeNameResolverInterface
 {
-    public function __construct(
-        private readonly FuncCallNameResolver $funcCallNameResolver
-    ) {
-    }
-
     public function getNode(): string
     {
         return Name::class;
@@ -31,12 +24,6 @@ final class NameNameResolver implements NodeNameResolverInterface
      */
     public function resolve(Node $node, ?Scope $scope): ?string
     {
-        // possible function parent
-        $parentNode = $node->getAttribute(AttributeKey::PARENT_NODE);
-        if ($parentNode instanceof FuncCall) {
-            return $this->funcCallNameResolver->resolve($parentNode, $scope);
-        }
-
         return $node->toString();
     }
 }
