c703f4106c2ca07af3c308de758af77c106300cc
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-25 16:15:38 +0700

    [PhpParser] Remove parent lookup on ValueResolver take 5  (#4339)
    * Revert Revert [PhpParser] Remove parent lookup on ValueResolver
    
    This reverts commit e5bd9995aef09cf778177e76110e27fc1c389108.
    
    * remove ArrayDimFetch
    
    * [PhpParser] Remove parent lookup on ValueResolver take 5

--- src/PhpParser/Node/Value/ValueResolver.php
+++ src/PhpParser/Node/Value/ValueResolver.php
@@ -11,11 +11,10 @@ use PhpParser\Node\Expr\BinaryOp\Concat;
 use PhpParser\Node\Expr\ClassConstFetch;
 use PhpParser\Node\Expr\ConstFetch;
 use PhpParser\Node\Name;
-use PhpParser\Node\Name\FullyQualified;
 use PhpParser\Node\Scalar\MagicConst\Dir;
 use PhpParser\Node\Scalar\MagicConst\File;
-use PhpParser\Node\Stmt\Class_;
-use PhpParser\Node\Stmt\ClassLike;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\ClassReflection;
 use PHPStan\Reflection\ReflectionProvider;
 use PHPStan\Type\Constant\ConstantArrayType;
 use PHPStan\Type\ConstantScalarType;
@@ -23,9 +22,11 @@ use PHPStan\Type\TypeWithClassName;
 use Rector\Core\Enum\ObjectReference;
 use Rector\Core\Exception\ShouldNotHappenException;
 use Rector\Core\NodeAnalyzer\ConstFetchAnalyzer;
-use Rector\Core\PhpParser\Node\BetterNodeFinder;
 use Rector\Core\Provider\CurrentFileProvider;
+use Rector\Core\Reflection\ReflectionResolver;
+use Rector\Core\Util\Reflection\PrivatesAccessor;
 use Rector\NodeNameResolver\NodeNameResolver;
+use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\NodeTypeResolver\NodeTypeResolver;
 
 /**
@@ -42,7 +43,8 @@ final class ValueResolver
         private readonly ConstFetchAnalyzer $constFetchAnalyzer,
         private readonly ReflectionProvider $reflectionProvider,
         private readonly CurrentFileProvider $currentFileProvider,
-        private readonly BetterNodeFinder $betterNodeFinder
+        private readonly ReflectionResolver $reflectionResolver,
+        private readonly PrivatesAccessor $privatesAccessor
     ) {
     }
 
@@ -61,10 +63,9 @@ final class ValueResolver
             $class = $this->nodeNameResolver->getName($expr->class);
 
             if (in_array($class, [ObjectReference::SELF, ObjectReference::STATIC], true)) {
-                // @todo scope is needed
-                $classLike = $this->betterNodeFinder->findParentType($expr, ClassLike::class);
-                if ($classLike instanceof ClassLike) {
-                    return (string) $this->nodeNameResolver->getName($classLike);
+                $classReflection = $this->reflectionResolver->resolveClassReflection($expr);
+                if ($classReflection instanceof ClassReflection) {
+                    return $classReflection->getName();
                 }
             }
 
@@ -307,27 +308,42 @@ final class ValueResolver
 
     private function resolveClassFromSelfStaticParent(ClassConstFetch $classConstFetch, string $class): string
     {
-        $classLike = $this->betterNodeFinder->findParentType($classConstFetch, ClassLike::class);
-        if (! $classLike instanceof ClassLike) {
+        // Scope may be loaded too late, so return empty string early
+        // it will be resolved on next traverse
+        $scope = $classConstFetch->getAttribute(AttributeKey::SCOPE);
+        if (! $scope instanceof Scope) {
+            return '';
+        }
+
+        $classReflection = $this->reflectionResolver->resolveClassReflection($classConstFetch);
+        if (! $classReflection instanceof ClassReflection) {
             throw new ShouldNotHappenException(
                 'Complete class parent node for to class const fetch, so "self" or "static" references is resolvable to a class name'
             );
         }
 
-        if ($class === ObjectReference::PARENT) {
-            if (! $classLike instanceof Class_) {
-                throw new ShouldNotHappenException(
-                    'Complete class parent node for to class const fetch, so "parent" references is resolvable to lookup parent class'
-                );
-            }
-
-            if (! $classLike->extends instanceof FullyQualified) {
-                throw new ShouldNotHappenException();
-            }
-
-            return $classLike->extends->toString();
+        if ($class !== ObjectReference::PARENT) {
+            return $classReflection->getName();
         }
 
-        return (string) $this->nodeNameResolver->getName($classLike);
+        if (! $classReflection->isClass()) {
+            throw new ShouldNotHappenException(
+                'Complete class parent node for to class const fetch, so "parent" references is resolvable to lookup parent class'
+            );
+        }
+
+        // ensure parent class name still resolved even not autoloaded
+        $nativeReflection = $classReflection->getNativeReflection();
+        $betterReflectionClass = $this->privatesAccessor->getPrivateProperty(
+            $nativeReflection,
+            'betterReflectionClass'
+        );
+        $parentClassName = $this->privatesAccessor->getPrivateProperty($betterReflectionClass, 'parentClassName');
+
+        if ($parentClassName === null) {
+            throw new ShouldNotHappenException();
+        }
+
+        return $parentClassName;
     }
 }
