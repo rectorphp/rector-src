cabeeffce6d655807fef99f05c6933d0bcebd142
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-25 14:32:28 +0700

    Update configure_rule.php

--- tests/Issues/DisableAutoImportDocBlock/config/configure_rule.php
+++ tests/Issues/DisableAutoImportDocBlock/config/configure_rule.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Symfony\Rector\ClassMethod\ReplaceSensioRouteAnnotationWithSymfonyRector;
+use Rector\Symfony34\Rector\ClassMethod\ReplaceSensioRouteAnnotationWithSymfonyRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->importNames(true, false);
