f8bc487500b703f33bc66806f151a7757f4be3dc
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-26 20:26:44 +0700

    [ReadWrite] Reduce parent attribut usage on JustReadExprAnalyzer (#4350)
    * [ReadWrite] Reduce parent attribut usage on JustReadExprAnalyzer
    
    * [ReadWrite] Reduce parent attribut usage on JustReadExprAnalyzer
    
    * [ReadWrite] Reduce parent attribut usage on JustReadExprAnalyzer

--- packages/NodeTypeResolver/PHPStan/Scope/NodeVisitor/ContextNodeVisitor.php
+++ packages/NodeTypeResolver/PHPStan/Scope/NodeVisitor/ContextNodeVisitor.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Rector\NodeTypeResolver\PHPStan\Scope\NodeVisitor;
 
 use PhpParser\Node;
+use PhpParser\Node\Arg;
 use PhpParser\Node\Attribute;
+use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\Array_;
 use PhpParser\Node\Expr\Closure;
 use PhpParser\Node\Expr\Isset_;
@@ -18,6 +20,7 @@ use PhpParser\Node\Stmt\For_;
 use PhpParser\Node\Stmt\Foreach_;
 use PhpParser\Node\Stmt\Function_;
 use PhpParser\Node\Stmt\If_;
+use PhpParser\Node\Stmt\Return_;
 use PhpParser\Node\Stmt\Unset_;
 use PhpParser\Node\Stmt\While_;
 use PhpParser\NodeTraverser;
@@ -61,6 +64,14 @@ final class ContextNodeVisitor extends NodeVisitorAbstract implements ScopeResol
         if ($node instanceof If_ || $node instanceof Else_ || $node instanceof ElseIf_) {
             $this->processContextInIf($node);
             return null;
+        }
+
+        if ($node instanceof Return_ && $node->expr instanceof Expr) {
+            $node->expr->setAttribute(AttributeKey::IS_RETURN_EXPR, true);
+        }
+
+        if ($node instanceof Arg) {
+            $node->value->setAttribute(AttributeKey::IS_ARG_VALUE, true);
         }
 
         return null;
