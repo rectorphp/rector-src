a653146486eea313a569bd84a19e6ebaec7015d9
Tomas Votruba <tomas.vot@gmail.com>
2023-06-27 11:00:04 +0200

    avoid including copiler extension stub

--- build/config/config-downgrade.php
+++ build/config/config-downgrade.php
@@ -8,7 +8,6 @@ use Rector\Set\ValueObject\DowngradeLevelSetList;
 require_once  __DIR__ . '/../target-repository/stubs-rector/PHPUnit/Framework/TestCase.php';
 require_once  __DIR__ . '/../../stubs/Composer/EventDispatcher/EventSubscriberInterface.php';
 require_once  __DIR__ . '/../../stubs/Composer/Plugin/PluginInterface.php';
-require_once  __DIR__ . '/../../stubs/Nette/DI/CompilerExtension.php';
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->parallel();
