879144482883ee61cbea4a90267b9b517bbba2a9
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-29 01:07:54 +0700

    [PhpParser] Remove NodeFinder usage on FileWithoutNamespaceNodeTraverser

--- src/PhpParser/NodeTraverser/FileWithoutNamespaceNodeTraverser.php
+++ src/PhpParser/NodeTraverser/FileWithoutNamespaceNodeTraverser.php
@@ -6,16 +6,14 @@ namespace Rector\Core\PhpParser\NodeTraverser;
 
 use PhpParser\Node;
 use PhpParser\Node\Stmt\Namespace_;
-use PhpParser\NodeFinder;
 use PhpParser\NodeTraverser;
 use Rector\Core\PhpParser\Node\CustomNode\FileWithoutNamespace;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 
 final class FileWithoutNamespaceNodeTraverser extends NodeTraverser
 {
-    public function __construct(
-        private readonly NodeFinder $nodeFinder,
-    ) {
+    public function __construct()
+    {
         parent::__construct();
     }
 
@@ -26,16 +24,17 @@ final class FileWithoutNamespaceNodeTraverser extends NodeTraverser
      */
     public function traverse(array $nodes): array
     {
-        $hasNamespace = (bool) $this->nodeFinder->findFirstInstanceOf($nodes, Namespace_::class);
-        if (! $hasNamespace && $nodes !== []) {
-            $fileWithoutNamespace = new FileWithoutNamespace($nodes);
-            foreach ($nodes as $node) {
-                $node->setAttribute(AttributeKey::PARENT_NODE, $fileWithoutNamespace);
+        foreach ($nodes as $node) {
+            if ($node instanceof Namespace_) {
+                return $nodes;
             }
-
-            return [$fileWithoutNamespace];
         }
 
-        return $nodes;
+        $fileWithoutNamespace = new FileWithoutNamespace($nodes);
+        foreach ($nodes as $node) {
+            $node->setAttribute(AttributeKey::PARENT_NODE, $fileWithoutNamespace);
+        }
+
+        return [$fileWithoutNamespace];
     }
 }
