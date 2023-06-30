53b9984969873803b3a35bd9521b4103302678e0
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-30 08:24:41 +0700

    fix

--- packages/NodeTypeResolver/PHPStan/Scope/PHPStanNodeScopeResolver.php
+++ packages/NodeTypeResolver/PHPStan/Scope/PHPStanNodeScopeResolver.php
@@ -521,7 +521,7 @@ final class PHPStanNodeScopeResolver
         }
 
         // on refresh, remove entered class avoid entering the class again
-        if ($mutatingScope->isInClass() && ! $isAnonymous) {
+        if ($isScopeRefreshing && $mutatingScope->isInClass() && ! $isAnonymous) {
             $context = $this->privatesAccessor->getPrivateProperty($mutatingScope, 'context');
             $this->privatesAccessor->setPrivateProperty($context, 'classReflection', null);
         }
