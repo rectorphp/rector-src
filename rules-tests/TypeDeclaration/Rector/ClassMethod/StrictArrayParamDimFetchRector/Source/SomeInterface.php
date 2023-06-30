8040759d5fff1d9f9e5ca933dedad256d16bc571
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-07-01 01:30:18 +0700

    [TypeDeclaration] Skip guarded by parent on StrictArrayParamDimFetchRector (#4386)
    * [TypeDeclaration] Skip guarded by parent on StrictArrayParamDimFetchRector
    
    * Fixed :tada:

---
+++ rules-tests/TypeDeclaration/Rector/ClassMethod/StrictArrayParamDimFetchRector/Source/SomeInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\TypeDeclaration\Rector\ClassMethod\StrictArrayParamDimFetchRector\Source;
+
+interface SomeInterface
+{
+    public function go($item);
+}
