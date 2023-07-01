9353719b74f4107231156f819c624a83541b630e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-07-01 18:30:20 +0700

    check has variable type

--- packages/ReadWrite/ReadNodeAnalyzer/VariableReadNodeAnalyzer.php
+++ packages/ReadWrite/ReadNodeAnalyzer/VariableReadNodeAnalyzer.php
@@ -6,6 +6,7 @@ namespace Rector\ReadWrite\ReadNodeAnalyzer;
 
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\Variable;
+use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\ReadWrite\Contract\ReadNodeAnalyzerInterface;
 
 /**
@@ -28,6 +29,19 @@ final class VariableReadNodeAnalyzer implements ReadNodeAnalyzerInterface
      */
     public function isRead(Expr $expr): bool
     {
-        return $this->justReadExprAnalyzer->isReadContext($expr);
+        if ($this->justReadExprAnalyzer->isReadContext($expr)) {
+            return true;
+        }
+
+        $scope = $expr->getAttribute(AttributeKey::SCOPE);
+        if (! $scope instanceof \PHPStan\Analyser\Scope) {
+            return true;
+        }
+
+        if ($expr->name instanceof Expr) {
+            return true;
+        }
+
+        return $scope->hasVariableType($expr->name)->yes();
     }
 }
