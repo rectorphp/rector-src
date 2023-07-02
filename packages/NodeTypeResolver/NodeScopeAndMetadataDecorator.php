5e586e890274952f83e41431c0a3200f0eb34e66
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-07-02 21:35:44 +0700

    Remove ParentConnectingVisitor on NodeScopeAndMetadataDecorator
    It already in phpstan config
    
    
    https://github.com/rectorphp/rector-src/blob/4a0008452981d918b6590b82cf8f2577b3aa8171/config/phpstan/static-reflection.neon#L7
    
    Reconnecting in file level should not be needed.

--- packages/NodeTypeResolver/NodeScopeAndMetadataDecorator.php
+++ packages/NodeTypeResolver/NodeScopeAndMetadataDecorator.php
@@ -20,7 +20,6 @@ final class NodeScopeAndMetadataDecorator
     public function __construct(
         CloningVisitor $cloningVisitor,
         private readonly PHPStanNodeScopeResolver $phpStanNodeScopeResolver,
-        ParentConnectingVisitor $parentConnectingVisitor,
         FunctionLikeParamArgPositionNodeVisitor $functionLikeParamArgPositionNodeVisitor,
         private readonly FileWithoutNamespaceNodeTraverser $fileWithoutNamespaceNodeTraverser
     ) {
@@ -28,9 +27,6 @@ final class NodeScopeAndMetadataDecorator
 
         // needed also for format preserving printing
         $this->nodeTraverser->addVisitor($cloningVisitor);
-
-        // this one has to be run again to re-connect parent nodes with new attributes
-        $this->nodeTraverser->addVisitor($parentConnectingVisitor);
 
         $this->nodeTraverser->addVisitor($functionLikeParamArgPositionNodeVisitor);
     }
