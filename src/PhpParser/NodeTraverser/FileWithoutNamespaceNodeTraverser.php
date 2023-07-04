9a4f52b7d00ec17f4e1ef608480881fb551464e6
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-07-04 14:40:19 +0700

    fix phpstan

--- src/PhpParser/NodeTraverser/FileWithoutNamespaceNodeTraverser.php
+++ src/PhpParser/NodeTraverser/FileWithoutNamespaceNodeTraverser.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Rector\Core\PhpParser\NodeTraverser;
 
+use PhpParser\Node;
 use PhpParser\Node\Stmt\Namespace_;
 use PhpParser\NodeTraverser;
 use Rector\Core\PhpParser\Node\CustomNode\FileWithoutNamespace;
