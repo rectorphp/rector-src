f01d5d0032043597f37780747351b7980681d91a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-07-05 19:25:46 +0700

    [NodeTypeResolver] Remove ArrayItem scope filling from Array_ on PHPStanNodeScopeResolver

--- packages/NodeTypeResolver/PHPStan/Scope/PHPStanNodeScopeResolver.php
+++ packages/NodeTypeResolver/PHPStan/Scope/PHPStanNodeScopeResolver.php
@@ -316,7 +316,6 @@ final class PHPStanNodeScopeResolver
         foreach ($array->items as $arrayItem) {
             if ($arrayItem instanceof ArrayItem) {
                 $arrayItem->setAttribute(AttributeKey::SCOPE, $mutatingScope);
-                $this->processArrayItem($arrayItem, $mutatingScope);
             }
         }
     }
