ac63f2a6261963e8a7d353474b7c20311a72ded3
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-07-09 04:45:43 +0700

    [NodeTypeResolver] Remove type detection from parent FunctionLike docblock on ParamTypeResolver (#4441)
    * [NodeTypeResolver] Remove type detection from parent FunctionLike docblock on ParamTypeResolver
    
    * Fix phpstan

--- packages-tests/NodeTypeResolver/PerNodeTypeResolver/ParamTypeResolver/ParamTypeResolverTest.php
+++ packages-tests/NodeTypeResolver/PerNodeTypeResolver/ParamTypeResolver/ParamTypeResolverTest.php
@@ -6,11 +6,10 @@ namespace Rector\Tests\NodeTypeResolver\PerNodeTypeResolver\ParamTypeResolver;
 
 use Iterator;
 use PhpParser\Node\Param;
-use PHPStan\Type\ObjectType;
-use PHPStan\Type\TypeWithClassName;
+use PHPStan\Type\MixedType;
 use PHPUnit\Framework\Attributes\DataProvider;
+use Rector\StaticTypeMapper\ValueObject\Type\FullyQualifiedObjectType;
 use Rector\Tests\NodeTypeResolver\PerNodeTypeResolver\AbstractNodeTypeResolverTestCase;
-use Rector\Tests\NodeTypeResolver\PerNodeTypeResolver\ParamTypeResolver\Source\Html;
 
 /**
  * @see \Rector\NodeTypeResolver\NodeTypeResolver\ParamTypeResolver
@@ -18,23 +17,17 @@ use Rector\Tests\NodeTypeResolver\PerNodeTypeResolver\ParamTypeResolver\Source\H
 final class ParamTypeResolverTest extends AbstractNodeTypeResolverTestCase
 {
     #[DataProvider('provideData')]
-    public function test(string $file, int $nodePosition, TypeWithClassName $expectedTypeWithClassName): void
+    public function test(string $file, int $nodePosition, string $expectedType): void
     {
         $variableNodes = $this->getNodesForFileOfType($file, Param::class);
 
         $resolvedType = $this->nodeTypeResolver->getType($variableNodes[$nodePosition]);
-
-        $this->assertInstanceOf(TypeWithClassName::class, $resolvedType);
-
-        /** @var TypeWithClassName $resolvedType */
-        $this->assertSame($expectedTypeWithClassName->getClassName(), $resolvedType->getClassName());
+        $this->assertSame($resolvedType::class, $expectedType);
     }
 
     public static function provideData(): Iterator
     {
-        $objectType = new ObjectType(Html::class);
-
-        yield [__DIR__ . '/Source/MethodParamTypeHint.php', 0, $objectType];
-        yield [__DIR__ . '/Source/MethodParamDocBlock.php', 0, $objectType];
+        yield [__DIR__ . '/Source/MethodParamTypeHint.php', 0, FullyQualifiedObjectType::class];
+        yield [__DIR__ . '/Source/MethodParamDocBlock.php', 0, MixedType::class];
     }
 }
