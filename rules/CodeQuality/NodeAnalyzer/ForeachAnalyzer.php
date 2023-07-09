ccca07bd502fdb90f74d96255594100f8ea75c1e
Tomas Votruba <tomas.vot@gmail.com>
2023-07-09 13:18:09 +0200

    Remove deprecated findFirstNext()

--- rules/CodeQuality/NodeAnalyzer/ForeachAnalyzer.php
+++ rules/CodeQuality/NodeAnalyzer/ForeachAnalyzer.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace Rector\CodeQuality\NodeAnalyzer;
 
-use PhpParser\Node;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\ArrayDimFetch;
 use PhpParser\Node\Expr\Assign;
-use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\Foreach_;
 use Rector\Core\PhpParser\Comparing\NodeComparator;
@@ -55,30 +53,4 @@ final class ForeachAnalyzer
 
         return $onlyStatement->var->var;
     }
-
-    //    public function isValueVarUsed(Foreach_ $foreach, string $singularValueVarName): bool
-    //    {
-    //        $isUsedInStmts = (bool) $this->betterNodeFinder->findFirst($foreach->stmts, function (Node $node) use (
-    //            $singularValueVarName
-    //        ): bool {
-    //            if (! $node instanceof Variable) {
-    //                return false;
-    //            }
-    //
-    //            return $this->nodeNameResolver->isName($node, $singularValueVarName);
-    //        });
-    //
-    //        if ($isUsedInStmts) {
-    //            return true;
-    //        }
-    //
-    //        if ($this->variableNameUsedNextAnalyzer->isValueVarUsedNext($foreach, $singularValueVarName)) {
-    //            return true;
-    //        }
-    //
-    //        return $this->variableNameUsedNextAnalyzer->isValueVarUsedNext(
-    //            $foreach,
-    //            (string) $this->nodeNameResolver->getName($foreach->valueVar)
-    //        );
-    //    }
 }
