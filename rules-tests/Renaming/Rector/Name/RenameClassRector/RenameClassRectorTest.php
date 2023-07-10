bd63539b168a9ff21ba1d17893aff63798a646dd
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-07-10 18:12:49 +0700

    [Renaming] Use RunTestsInSeparateProcesses on RenameClassRector tests

--- rules-tests/Renaming/Rector/Name/RenameClassRector/RenameClassRectorTest.php
+++ rules-tests/Renaming/Rector/Name/RenameClassRector/RenameClassRectorTest.php
@@ -6,8 +6,10 @@ namespace Rector\Tests\Renaming\Rector\Name\RenameClassRector;
 
 use Iterator;
 use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
 
+#[RunTestsInSeparateProcesses]
 final class RenameClassRectorTest extends AbstractRectorTestCase
 {
     #[DataProvider('provideData')]
