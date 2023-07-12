0b15715894f7da1e88121ed19c713b1a0ce51092
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-07-13 05:06:23 +0700

    Fix test

--- tests/Issues/UndefinedStmtIndex/config/configured_rule.php
+++ tests/Issues/UndefinedStmtIndex/config/configured_rule.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\StmtsAwareInterface\RemoveJustPropertyFetchForAssignRector;
-use Rector\PHPUnit\Rector\Foreach_\SimplifyForeachInstanceOfRector;
+use Rector\PHPUnit\CodeQuality\Rector\Foreach_\SimplifyForeachInstanceOfRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(RemoveJustPropertyFetchForAssignRector::class);
