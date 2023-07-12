b75c08d8f0d31d3039b10ca706f403ef34db8c12
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-07-13 05:07:20 +0700

    Fix test

--- rules-tests/Arguments/Rector/RectorOrder/config/configured_rule.php
+++ rules-tests/Arguments/Rector/RectorOrder/config/configured_rule.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\PHPUnit\Rector\MethodCall\AssertComparisonToSpecificMethodRector;
+use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertComparisonToSpecificMethodRector;
 use Rector\PHPUnit\Rector\MethodCall\AssertFalseStrposToContainsRector;
 use Rector\PHPUnit\Rector\MethodCall\AssertSameBoolNullToSpecificMethodRector;
 
