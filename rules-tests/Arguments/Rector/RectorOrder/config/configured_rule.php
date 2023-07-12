9e9a314b23fa9f217c236412591c4cb9ad94edf5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-07-13 05:11:36 +0700

    [NodeTypeResolver] Remove unused AttributeKey::INSIDE_ARRAY_ITEM on ContextNodeVisitor (#4496)
    * [NodeTypeResolver] Remove unused AttributeKey::INSIDE_ARRAY_ITEM on ContextNodeVisitor
    
    * [ci-review] Rector Rectify
    
    * Fix test
    
    * Fix test
    
    * Fix test
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

--- rules-tests/Arguments/Rector/RectorOrder/config/configured_rule.php
+++ rules-tests/Arguments/Rector/RectorOrder/config/configured_rule.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\PHPUnit\Rector\MethodCall\AssertComparisonToSpecificMethodRector;
-use Rector\PHPUnit\Rector\MethodCall\AssertFalseStrposToContainsRector;
-use Rector\PHPUnit\Rector\MethodCall\AssertSameBoolNullToSpecificMethodRector;
+use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertComparisonToSpecificMethodRector;
+use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertFalseStrposToContainsRector;
+use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertSameBoolNullToSpecificMethodRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(AssertComparisonToSpecificMethodRector::class);
