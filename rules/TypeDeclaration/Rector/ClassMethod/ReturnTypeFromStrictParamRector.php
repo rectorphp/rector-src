341ff56e6b29c9ecd6dc4ad1cf907f178795d914
Markus Staab <markus.staab@redaxo.de>
2023-07-13 12:46:30 +0200

    Implement ReturnTypeFromStrictParamRector

---
+++ rules/TypeDeclaration/Rector/ClassMethod/ReturnTypeFromStrictParamRector.php
@@ -0,0 +1,197 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\TypeDeclaration\Rector\ClassMethod;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\ArrayDimFetch;
+use PhpParser\Node\Expr\Closure;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\FunctionLike;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Name;
+use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Param;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Function_;
+use PhpParser\Node\Stmt\Return_;
+use PhpParser\NodeTraverser;
+use PHPStan\Analyser\Scope;
+use PHPStan\Type\ObjectType;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Core\Rector\AbstractScopeAwareRector;
+use Rector\Core\ValueObject\PhpVersionFeature;
+use Rector\VendorLocker\ParentClassMethodTypeOverrideGuard;
+use Rector\VersionBonding\Contract\MinPhpVersionInterface;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\Tests\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictParamRector\ReturnTypeFromStrictParamRectorTest
+ */
+final class ReturnTypeFromStrictParamRector extends AbstractScopeAwareRector implements MinPhpVersionInterface
+{
+    public function __construct(
+        private readonly ParentClassMethodTypeOverrideGuard $parentClassMethodTypeOverrideGuard
+    ) {
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Add return type based on strict parameter type', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function resolve(ParamType $item)
+    {
+        return $item;
+    }
+}
+CODE_SAMPLE
+
+                ,
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function resolve(ParamType $item): ParamType
+    {
+        return $item;
+    }
+}
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [ClassMethod::class, Function_::class];
+    }
+
+    public function provideMinPhpVersion(): int
+    {
+        return PhpVersionFeature::NULLABLE_TYPE;
+    }
+
+    /**
+     * @param ClassMethod|Function_ $node
+     */
+    public function refactorWithScope(Node $node, Scope $scope): ?Node
+    {
+        if ($node->returnType !== null) {
+            return null;
+        }
+
+        if ($node instanceof ClassMethod && $this->parentClassMethodTypeOverrideGuard->hasParentClassMethod($node)) {
+            return null;
+        }
+
+        $return = $this->findCurrentScopeReturn($node);
+        if ($return === null) {
+            return null;
+        }
+
+        $returnName = $this->getName($return->expr);
+        foreach ($node->getParams() as $param) {
+            if ($this->shouldSkipParam($param, $node)) {
+                continue;
+            }
+
+            $paramName = $this->getName($param);
+            if ($returnName !== $paramName) {
+                continue;
+            }
+
+            $resolvedType = $scope->getType($param->var);
+
+            /** @var Name $returnType */
+            $returnType = $resolvedType instanceof ObjectType
+                ? new FullyQualified($resolvedType->getClassName())
+                : $param->type;
+
+            $node->returnType = $returnType;
+            return $node;
+        }
+
+        return null;
+    }
+
+    private function findCurrentScopeReturn(ClassMethod|Function_ $node): ?Return_
+    {
+        $return = null;
+
+        if ($node->stmts === null) {
+            return null;
+        }
+
+        $returnVarName = null;
+
+        $this->traverseNodesWithCallable($node->stmts, function (Node $node) use (&$return, &$returnVarName): ?int {
+            // skip scope nesting
+            if ($node instanceof FunctionLike) {
+                return NodeTraverser::DONT_TRAVERSE_CURRENT_AND_CHILDREN;
+            }
+
+            if (! $node instanceof Return_) {
+                return null;
+            }
+
+            if (! $node->expr instanceof Variable) {
+                return null;
+            }
+
+            $returnName = $this->getName($node->expr);
+            if ($returnVarName !== null && $returnName !== $returnVarName) {
+                $return = null;
+
+                return NodeTraverser::DONT_TRAVERSE_CURRENT_AND_CHILDREN;
+            }
+
+            $returnVarName = $returnName;
+            $return = $node;
+            return null;
+        });
+
+        return $return;
+    }
+
+    private function shouldSkipParam(Param $param, ClassMethod|Function_ $functionLike): bool
+    {
+        if (!$param->type instanceof Node) {
+            return true;
+        }
+
+        $paramName = $this->getName($param);
+
+        $isParamModified = false;
+
+        $this->traverseNodesWithCallable($functionLike, function (Node $node) use (
+            $paramName,
+            &$isParamModified
+        ): int|null {
+            if (! $node instanceof Expr\Assign && !$node instanceof Expr\AssignRef) {
+                return null;
+            }
+
+            if (! $node->var instanceof Variable) {
+                return null;
+            }
+
+            if (! $this->isName($node->var, $paramName)) {
+                return null;
+            }
+
+            $isParamModified = true;
+            return NodeTraverser::STOP_TRAVERSAL;
+        });
+
+        return $isParamModified;
+    }
+}
