f0121f2b4044ce4955f7cb1e4725e69f2a5a65a3
Tomas Votruba <tomas.vot@gmail.com>
2023-07-13 11:57:34 +0200

    cleanup RecastingRemovalRector test

---
+++ rules-tests/DeadCode/Rector/Cast/RecastingRemovalRector/Source/ExternalStrictTypedProperty.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\DeadCode\Rector\Cast\RecastingRemovalRector\Source;
+
+final class ExternalStrictTypedProperty
+{
+    public int $property = 1;
+}
