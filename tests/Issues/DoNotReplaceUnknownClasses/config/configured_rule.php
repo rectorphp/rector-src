8537bbd52a5b9d8aa619326b750df6dcb1b30f32
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-07-13 22:32:27 +0700

    [DeadCode] Skip append += assignment on RemoveUnusedPrivatePropertyRector (#4506)
    * [DeadCode] Skip append += assignment on RemoveUnusedPrivatePropertyRector
    
    * skip appender
    
    * add test appender on AddDefaultValueForUndefinedVariableRector to avoid regression
    
    * Fix test

--- tests/Issues/DoNotReplaceUnknownClasses/config/configured_rule.php
+++ tests/Issues/DoNotReplaceUnknownClasses/config/configured_rule.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Doctrine\Rector\Property\DoctrineTargetEntityStringToClassConstantRector;
+use Rector\Doctrine\CodeQuality\Rector\Property\DoctrineTargetEntityStringToClassConstantRector;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
 
 return static function (RectorConfig $rectorConfig): void {
