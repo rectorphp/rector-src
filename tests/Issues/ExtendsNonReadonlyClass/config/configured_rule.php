984cea323ab81a0253f02fc1feb3e86a477e9546
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-07-16 05:21:09 +0700

    [Php81][Php82][Privatization] Handle extends non-readonly class on ReadOnlyPropertyRector+ReadOnlyClassRector+FinalizeClassesWithoutChildrenRector

---
+++ tests/Issues/ExtendsNonReadonlyClass/config/configured_rule.php
@@ -0,0 +1,14 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
+use Rector\Php82\Rector\Class_\ReadOnlyClassRector;
+use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rules([
+        ReadOnlyPropertyRector::class,
+        FinalizeClassesWithoutChildrenRector::class,
+        ReadOnlyClassRector::class,
+    ]);
+};
