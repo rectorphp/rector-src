91231069ea3604383f7a2152dd0a0ebe6ff00b81
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-07-16 06:04:20 +0700

    use complete set list to reproduce in CI

--- tests/Issues/ExtendsNonReadonlyClass/config/configured_rule.php
+++ tests/Issues/ExtendsNonReadonlyClass/config/configured_rule.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
@@ -9,6 +7,12 @@ use Rector\Set\ValueObject\SetList;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_82,
+		SetList::CODE_QUALITY,
+		SetList::CODING_STYLE,
+		SetList::DEAD_CODE,
+		SetList::INSTANCEOF,
 		SetList::PRIVATIZATION,
+		SetList::STRICT_BOOLEANS,
+		SetList::TYPE_DECLARATION,
     ]);
 };
