afae350f98dbfd8d147d5d93ac293345b6372826
Markus Staab <maggus.staab@googlemail.com>
2023-07-16 21:33:23 +0200

    added failling test

---
+++ e2e/inline-html-warning/rector.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+    ]);
+
+    $rectorConfig->sets([SetList::CODING_STYLE]);
+};
---
+++ e2e/inline-html-warning/src/SomeInlineHtml.php
@@ -0,0 +1,10 @@
+<?php
+$x = array();
+?>
+<html>
+    <head></head>
+    <body>
+        <?php echo count($x) === 0 ? 1 : 0; ?>
+        <h1>SomeFixturePrinter</h1>
+    </body>
+</html>
