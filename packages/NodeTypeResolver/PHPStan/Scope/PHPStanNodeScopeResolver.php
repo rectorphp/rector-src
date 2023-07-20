084a6673ffc27903ba8e0698715bfdf5ad40978c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-07-20 07:02:56 +0700

    using try catch: fill classReflection property when possible

--- packages/NodeTypeResolver/PHPStan/Scope/PHPStanNodeScopeResolver.php
+++ packages/NodeTypeResolver/PHPStan/Scope/PHPStanNodeScopeResolver.php
@@ -458,13 +458,17 @@ final class PHPStanNodeScopeResolver
             $classReflection = $this->reflectionProvider->getClass($className);
         }
 
-        // on refresh, remove entered class avoid entering the class again
-        if ($isScopeRefreshing && $mutatingScope->isInClass() && ! $isAnonymous) {
+        try {
+            $context = $this->privatesAccessor->getPrivateProperty($mutatingScope, 'context');
+            $this->privatesAccessor->setPrivateProperty($context, 'classReflection', $classReflection);
+
+            return $mutatingScope->enterClass($classReflection);
+        } catch (\PHPStan\ShouldNotHappenException) {
             $context = $this->privatesAccessor->getPrivateProperty($mutatingScope, 'context');
             $this->privatesAccessor->setPrivateProperty($context, 'classReflection', null);
-        }
 
-        return $mutatingScope->enterClass($classReflection);
+            return $mutatingScope->enterClass($classReflection);
+        }
     }
 
     private function resolveClassName(Class_ | Interface_ | Trait_| Enum_ $classLike): string
