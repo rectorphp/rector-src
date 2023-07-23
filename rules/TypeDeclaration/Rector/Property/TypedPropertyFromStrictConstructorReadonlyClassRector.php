4b85dbded4a92972f15076ef5db2a128777994a5
GitHub Action <actions@github.com>
2023-07-23 23:22:57 +0000

    [ci-review] Rector Rectify

--- rules/TypeDeclaration/Rector/Property/TypedPropertyFromStrictConstructorReadonlyClassRector.php
+++ rules/TypeDeclaration/Rector/Property/TypedPropertyFromStrictConstructorReadonlyClassRector.php
@@ -194,11 +194,6 @@ CODE_SAMPLE
                 $isReadOnlyByPhpdoc = $propertyReflection->isReadOnlyByPhpDoc();
             }
         }
-
-        if (! $isReadOnlyByPhpdoc) {
-            return true;
-        }
-
-        return false;
+        return ! $isReadOnlyByPhpdoc;
     }
 }
