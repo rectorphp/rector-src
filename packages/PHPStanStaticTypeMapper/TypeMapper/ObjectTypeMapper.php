96c9cd71166796a87e5369b95e03bb57717170d0
GitHub Action <actions@github.com>
2023-07-23 01:40:49 +0000

    [ci-review] Rector Rectify

--- packages/PHPStanStaticTypeMapper/TypeMapper/ObjectTypeMapper.php
+++ packages/PHPStanStaticTypeMapper/TypeMapper/ObjectTypeMapper.php
@@ -16,22 +16,18 @@ use PHPStan\Type\MixedType;
 use PHPStan\Type\ObjectType;
 use PHPStan\Type\Type;
 use Rector\PHPStanStaticTypeMapper\Contract\TypeMapperInterface;
-use Rector\PHPStanStaticTypeMapper\PHPStanStaticTypeMapper;
 use Rector\StaticTypeMapper\ValueObject\Type\AliasedObjectType;
 use Rector\StaticTypeMapper\ValueObject\Type\FullyQualifiedGenericObjectType;
 use Rector\StaticTypeMapper\ValueObject\Type\FullyQualifiedObjectType;
 use Rector\StaticTypeMapper\ValueObject\Type\NonExistingObjectType;
 use Rector\StaticTypeMapper\ValueObject\Type\SelfObjectType;
 use Rector\StaticTypeMapper\ValueObject\Type\ShortenedObjectType;
-use Symfony\Contracts\Service\Attribute\Required;
 
 /**
  * @implements TypeMapperInterface<ObjectType>
  */
 final class ObjectTypeMapper implements TypeMapperInterface
 {
-    private PHPStanStaticTypeMapper $phpStanStaticTypeMapper;
-
     /**
      * @return class-string<Type>
      */
@@ -68,7 +64,7 @@ final class ObjectTypeMapper implements TypeMapperInterface
         if ($type instanceof FullyQualifiedObjectType) {
             $className = $type->getClassName();
 
-            if (str_starts_with($className, '\\')) {
+            if (str_starts_with((string) $className, '\\')) {
                 // skip leading \
                 return new FullyQualified(Strings::substring($className, 1));
             }
@@ -81,47 +77,5 @@ final class ObjectTypeMapper implements TypeMapperInterface
         }
 
         return new FullyQualified($type->getClassName());
-    }
-
-    #[Required]
-    public function autowire(PHPStanStaticTypeMapper $phpStanStaticTypeMapper): void
-    {
-        $this->phpStanStaticTypeMapper = $phpStanStaticTypeMapper;
-    }
-
-    private function mapGenericObjectType(GenericObjectType $genericObjectType): TypeNode
-    {
-        $name = $this->resolveGenericObjectTypeName($genericObjectType);
-        $identifierTypeNode = new IdentifierTypeNode($name);
-
-        $genericTypeNodes = [];
-        foreach ($genericObjectType->getTypes() as $key => $genericType) {
-            // mixed type on 1st item in iterator has no value
-            if ($name === 'Iterator' && $genericType instanceof MixedType && $key === 0) {
-                continue;
-            }
-
-            $typeNode = $this->phpStanStaticTypeMapper->mapToPHPStanPhpDocTypeNode($genericType);
-            $genericTypeNodes[] = $typeNode;
-        }
-
-        if ($genericTypeNodes === []) {
-            return $identifierTypeNode;
-        }
-
-        return new GenericTypeNode($identifierTypeNode, $genericTypeNodes);
-    }
-
-    private function resolveGenericObjectTypeName(GenericObjectType $genericObjectType): string
-    {
-        if ($genericObjectType instanceof FullyQualifiedGenericObjectType) {
-            return '\\' . $genericObjectType->getClassName();
-        }
-
-        if (\str_contains($genericObjectType->getClassName(), '\\')) {
-            return '\\' . $genericObjectType->getClassName();
-        }
-
-        return $genericObjectType->getClassName();
     }
 }
