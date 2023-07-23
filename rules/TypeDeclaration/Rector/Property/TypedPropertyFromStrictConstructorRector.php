625d9911b7093fca2c3d74386e6e3e7fbf279dce
Markus Staab <maggus.staab@googlemail.com>
2023-07-23 14:19:20 +0200

    fix

--- rules/TypeDeclaration/Rector/Property/TypedPropertyFromStrictConstructorRector.php
+++ rules/TypeDeclaration/Rector/Property/TypedPropertyFromStrictConstructorRector.php
@@ -19,9 +19,11 @@ use Rector\Core\Reflection\ReflectionResolver;
 use Rector\Core\ValueObject\MethodName;
 use Rector\Core\ValueObject\PhpVersionFeature;
 use Rector\DeadCode\PhpDoc\TagRemover\VarTagRemover;
+use Rector\PHPStanStaticTypeMapper\DoctrineTypeAnalyzer;
 use Rector\PHPStanStaticTypeMapper\Enum\TypeKind;
 use Rector\TypeDeclaration\AlreadyAssignDetector\ConstructorAssignDetector;
 use Rector\TypeDeclaration\Guard\PropertyTypeOverrideGuard;
+use Rector\TypeDeclaration\TypeAnalyzer\PropertyTypeDefaultValueAnalyzer;
 use Rector\TypeDeclaration\TypeInferer\PropertyTypeInferer\TrustedClassMethodPropertyTypeInferer;
 use Rector\VersionBonding\Contract\MinPhpVersionInterface;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
@@ -38,7 +40,9 @@ final class TypedPropertyFromStrictConstructorRector extends AbstractRector impl
         private readonly PhpDocTypeChanger $phpDocTypeChanger,
         private readonly ConstructorAssignDetector $constructorAssignDetector,
         private readonly PropertyTypeOverrideGuard $propertyTypeOverrideGuard,
-        private readonly ReflectionResolver $reflectionResolver
+        private readonly ReflectionResolver $reflectionResolver,
+        private readonly DoctrineTypeAnalyzer $doctrineTypeAnalyzer,
+        private readonly PropertyTypeDefaultValueAnalyzer $propertyTypeDefaultValueAnalyzer
     ) {
     }
 
