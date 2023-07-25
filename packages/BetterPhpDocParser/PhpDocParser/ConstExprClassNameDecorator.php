729f03b6e9abccb7c4d7a4515e4dfed61e725e71
Tomas Votruba <tomas.vot@gmail.com>
2023-07-25 17:46:02 +0200

    [DX] Testing ECS 12.0 (#4604)

--- packages/BetterPhpDocParser/PhpDocParser/ConstExprClassNameDecorator.php
+++ packages/BetterPhpDocParser/PhpDocParser/ConstExprClassNameDecorator.php
@@ -28,10 +28,10 @@ final class ConstExprClassNameDecorator implements PhpDocNodeDecoratorInterface
     public function decorate(PhpDocNode $phpDocNode, PhpNode $phpNode): void
     {
         // iterating all phpdocs has big overhead. peek into the phpdoc to exit early
-        if (str_contains($phpDocNode->__toString(), '::') === false) {
+        if (! str_contains($phpDocNode->__toString(), '::')) {
             return;
         }
-        
+
         $this->phpDocNodeTraverser->traverseWithCallable($phpDocNode, '', function (Node $node) use (
             $phpNode
         ): Node|null {
