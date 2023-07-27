6d31d3a303341e9605823f8499a9acdcf1c25ecd
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-07-27 15:50:35 +0700

    rename fixture

---
+++ rules-tests/Php81/Rector/Property/ReadOnlyPropertyRector/Source/WithByRefMethod.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\Php81\Rector\Property\ReadOnlyPropertyRector\Source;
+
+final class WithByRefMethod
+{
+    public function install(&$output = null)
+    {
+    }
+}
