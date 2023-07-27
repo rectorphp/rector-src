dcfa7111b4523e63d70bbbeed7dff206d0291014
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-07-27 22:50:41 +0700

    [CodeQuality][Strict] Handle infinite loop on SimplifyDeMorganBinaryRector+SimplifyIfReturnBoolRector+DisallowedEmptyRuleFixerRector (#4615)
    * [CodeQuality][Strict] Handle infinite loop on SimplifyDeMorganBinaryRector+SimplifyIfReturnBoolRector+DisallowedEmptyRuleFixerRector
    
    * [CodeQuality][Strict] Handle infinite loop on SimplifyDeMorganBinaryRector+SimplifyIfReturnBoolRector+DisallowedEmptyRuleFixerRector

---
+++ tests/Issues/SimplifyEmpty/config/configured_rule.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\BooleanNot\SimplifyDeMorganBinaryRector;
+use Rector\CodeQuality\Rector\If_\SimplifyIfReturnBoolRector;
+use Rector\Config\RectorConfig;
+use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
+
+return static function (RectorConfig $rectorConfig): void {
+
+    $rectorConfig->rules([
+        SimplifyIfReturnBoolRector::class,
+        SimplifyDeMorganBinaryRector::class,
+        DisallowedEmptyRuleFixerRector::class,
+    ]);
+};
