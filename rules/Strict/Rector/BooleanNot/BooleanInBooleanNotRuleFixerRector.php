8ffacac3b77f7539e5bef55a462ed0451c752469
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-08-04 00:10:05 +0700

    [Strict] Skip docblock on BooleanInBooleanNotRuleFixerRector (#4640)
    * [Strict] Skip docblock on BooleanInBooleanNotRuleFixerRector
    
    * Fixed :tada:
    
    * Fixed :tada:

--- rules/Strict/Rector/BooleanNot/BooleanInBooleanNotRuleFixerRector.php
+++ rules/Strict/Rector/BooleanNot/BooleanInBooleanNotRuleFixerRector.php
@@ -82,7 +82,7 @@ CODE_SAMPLE
      */
     public function refactorWithScope(Node $node, Scope $scope): ?Expr
     {
-        $exprType = $scope->getType($node->expr);
+        $exprType = $scope->getNativeType($node->expr);
         if ($exprType->isBoolean()->yes()) {
             return null;
         }
