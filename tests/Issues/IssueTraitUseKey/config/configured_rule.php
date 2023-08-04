5615c2203e22322efe743f8c99be825539a6cbf3
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-08-04 21:53:41 +0700

    [CodingStyle][Php80][Privatization] Handle SeparateMultiUseImportsRector+ClassPropertyAssignToConstructorPromotionRector+FinalizeClassesWithoutChildrenRector cause invalid removal

---
+++ tests/Issues/IssueTraitUseKey/config/configured_rule.php
@@ -0,0 +1,12 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\CodingStyle\Rector\Use_\SeparateMultiUseImportsRector;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rule(SeparateMultiUseImportsRector::class);
+    $rectorConfig->rule(ClassPropertyAssignToConstructorPromotionRector::class);
+    $rectorConfig->rule(FinalizeClassesWithoutChildrenRector::class);
+};
