8b4cd8e5d03265dfc65e98f63c1bc50d3068012d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-08-04 17:45:18 +0700

    skipping parts

--- rules/TypeDeclaration/Rector/ClassMethod/FollowParentReturnTypeDeclarationRector.php
+++ rules/TypeDeclaration/Rector/ClassMethod/FollowParentReturnTypeDeclarationRector.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace Rector\TypeDeclaration\Rector\ClassMethod;
 
 use PhpParser\Node;
-use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\Interface_;
 use Rector\Core\Rector\AbstractRector;
+use Rector\Core\ValueObject\MethodName;
 use Rector\Core\ValueObject\PhpVersionFeature;
 use Rector\VersionBonding\Contract\MinPhpVersionInterface;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
@@ -60,14 +63,41 @@ CODE_SAMPLE
      */
     public function getNodeTypes(): array
     {
-        return [ClassMethod::class];
+        return [Class_::class, Interface_::class];
     }
 
     /**
-     * @param ClassMethod $node
+     * @param Class_|Interface_ $node
      */
     public function refactor(Node $node): ?Node
     {
+        // a class, no extends/implements, skip
+        if ($node instanceof Class_ && (! $node->extends instanceof FullyQualified || $node->implements === [])) {
+            return null;
+        }
+
+        // an interface, no extends, skip
+        if ($node instanceof Interface_ && $node->extends === []) {
+            return null;
+        }
+
+        foreach ($node->getMethods() as $method) {
+            // private scope is only local
+            if ($method->isPrivate()) {
+                continue;
+            }
+
+            // __construct don't have return type
+            if ($this->isName($method, MethodName::CONSTRUCT)) {
+                continue;
+            }
+
+            // already return typed
+            if ($method->returnType instanceof Node) {
+                continue;
+            }
+        }
+
         return null;
     }
 
