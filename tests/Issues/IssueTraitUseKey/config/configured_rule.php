a613624231fa10fd5bae41c03e8130cc9ead7354
GitHub Action <actions@github.com>
2023-08-04 14:55:55 +0000

    [ci-review] Rector Rectify

--- tests/Issues/IssueTraitUseKey/config/configured_rule.php
+++ tests/Issues/IssueTraitUseKey/config/configured_rule.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Rector\Config\RectorConfig;
 use Rector\CodingStyle\Rector\Use_\SeparateMultiUseImportsRector;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
