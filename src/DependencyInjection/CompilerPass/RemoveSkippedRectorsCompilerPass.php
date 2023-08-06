bd5c2ed2fc8f5d0e5d3196f2dee4db1c7e81e3f7
Tomas Votruba <tomas.vot@gmail.com>
2023-08-06 12:50:25 +0200

    Remove deprecated ParameterProvider, use SimpleParameterProvider instead

--- src/DependencyInjection/CompilerPass/RemoveSkippedRectorsCompilerPass.php
+++ src/DependencyInjection/CompilerPass/RemoveSkippedRectorsCompilerPass.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Rector\Core\DependencyInjection\CompilerPass;
 
 use Rector\Core\Configuration\Option;
+use Rector\Core\Configuration\Parameter\SimpleParameterProvider;
 use Rector\Core\Contract\Rector\RectorInterface;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -17,7 +18,7 @@ final class RemoveSkippedRectorsCompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $containerBuilder): void
     {
-        $skippedRectorClasses = $this->resolveSkippedRectorClasses($containerBuilder);
+        $skippedRectorClasses = $this->resolveSkippedRectorClasses();
 
         foreach ($containerBuilder->getDefinitions() as $id => $definition) {
             if ($definition->getClass() === null) {
@@ -35,9 +36,9 @@ final class RemoveSkippedRectorsCompilerPass implements CompilerPassInterface
     /**
      * @return string[]
      */
-    private function resolveSkippedRectorClasses(ContainerBuilder $containerBuilder): array
+    private function resolveSkippedRectorClasses(): array
     {
-        $skipParameters = (array) $containerBuilder->getParameter(Option::SKIP);
+        $skipParameters = SimpleParameterProvider::provideArrayParameter(Option::SKIP);
 
         return array_filter($skipParameters, fn ($element): bool => $this->isRectorClass($element));
     }
