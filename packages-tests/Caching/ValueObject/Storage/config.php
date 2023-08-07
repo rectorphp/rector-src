48f3d5653a3beed69bd268ae9b8d0da413eb1145
Tomas Votruba <tomas.vot@gmail.com>
2023-08-07 13:43:32 +0200

    use direct config methods

--- packages-tests/Caching/ValueObject/Storage/config.php
+++ packages-tests/Caching/ValueObject/Storage/config.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 use Rector\Caching\ValueObject\Storage\MemoryCacheStorage;
 use Rector\Config\RectorConfig;
-use Rector\Core\Configuration\Option;
 
 return static function (RectorConfig $rectorConfig): void {
-    $parameters = $rectorConfig->parameters();
-    $parameters->set(Option::CACHE_DIR, sys_get_temp_dir() . '/_rector_cached_files_test');
-    $parameters->set(Option::CACHE_CLASS, MemoryCacheStorage::class);
+    $rectorConfig->cacheDirectory(sys_get_temp_dir() . '/_rector_cached_files_test');
+    $rectorConfig->cacheClass(MemoryCacheStorage::class);
 };
--- packages/VendorLocker/Exception/UnresolvableClassException.php
+++ packages/VendorLocker/Exception/UnresolvableClassException.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Rector\VendorLocker\Exception;
 
 use Exception;
+
 final class UnresolvableClassException extends Exception
 {
 }
