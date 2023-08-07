f537a8834ef54747d8808dd999811a77e4db92c6
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-08-07 10:06:23 +0700

    fix

--- packages/StaticTypeMapper/PhpParser/IntersectionTypeNodeMapper.php
+++ packages/StaticTypeMapper/PhpParser/IntersectionTypeNodeMapper.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Rector\StaticTypeMapper\PhpParser;
 
 use PhpParser\Node;
+use PhpParser\Node\Name;
 use PhpParser\Node\Name\FullyQualified;
 use PHPStan\Type\IntersectionType;
 use PHPStan\Type\Type;
@@ -17,6 +18,7 @@ final class IntersectionTypeNodeMapper implements PhpParserNodeMapperInterface
 {
     public function __construct(
         private readonly FullyQualifiedNodeMapper $fullyQualifiedNodeMapper,
+        private readonly NameNodeMapper $nameNodeMapper,
         private readonly IdentifierNodeMapper $identifierNodeMapper
     )
     {
@@ -34,9 +36,17 @@ final class IntersectionTypeNodeMapper implements PhpParserNodeMapperInterface
     {
         $types = [];
         foreach ($node->types as $intersectionedType) {
-            $types[] = $intersectionedType instanceof FullyQualified
-                ? $this->fullyQualifiedNodeMapper->mapToPHPStan($intersectionedType)
-                : $this->identifierNodeMapper->mapToPHPStan($intersectionedType);
+            if ($intersectionedType instanceof FullyQualified) {
+                $types[] = $this->fullyQualifiedNodeMapper->mapToPHPStan($intersectionedType);
+                continue;
+            }
+
+            if ($intersectionedType instanceof Name) {
+                $types[] = $this->nameNodeMapper->mapToPHPStan($intersectionedType);
+                continue;
+            }
+
+            $types[] = $this->identifierNodeMapper->mapToPHPStan($intersectionedType);
         }
 
         return new IntersectionType($types);
