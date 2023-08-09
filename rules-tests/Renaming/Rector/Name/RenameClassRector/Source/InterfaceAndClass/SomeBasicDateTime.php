9f4d232d605875d9feaea285c13fd9debafb0550
Tomas Votruba <tomas.vot@gmail.com>
2023-08-09 22:09:53 +0200

    [DX] Cleanup and simplify class naming tests (#4749)

---
+++ rules-tests/Renaming/Rector/Name/RenameClassRector/Source/InterfaceAndClass/SomeBasicDateTime.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\Renaming\Rector\Name\RenameClassRector\Source\InterfaceAndClass;
+
+class SomeBasicDateTime
+{
+    public static function createFromFormat()
+    {
+        return 'something';
+    }
+}
--- rules-tests/Renaming/Rector/Name/RenameClassRector/config/configured_rule.php
+++ rules-tests/Renaming/Rector/Name/RenameClassRector/config/configured_rule.php
@@ -3,11 +3,9 @@
 declare(strict_types=1);
 
 use Acme\Bar\DoNotUpdateExistingTargetNamespace;
-use Manual\Twig\TwigFilter;
 use Rector\Config\RectorConfig;
 use Rector\Renaming\Rector\Name\RenameClassRector;
 use Rector\Tests\Renaming\Rector\Name\RenameClassRector\Fixture\DuplicatedClass;
-use Rector\Tests\Renaming\Rector\Name\RenameClassRector\Source\AbstractManualExtension;
 use Rector\Tests\Renaming\Rector\Name\RenameClassRector\Source\Contract\FirstInterface;
 use Rector\Tests\Renaming\Rector\Name\RenameClassRector\Source\Contract\SecondInterface;
 use Rector\Tests\Renaming\Rector\Name\RenameClassRector\Source\Contract\ThirdInterface;
@@ -25,16 +23,17 @@ return static function (RectorConfig $rectorConfig): void {
         ->ruleWithConfiguration(RenameClassRector::class, [
             'FqnizeNamespaced' => 'Abc\FqnizeNamespaced',
             OldClass::class => NewClass::class,
+            // interface to class
+            \Rector\Tests\Renaming\Rector\Name\RenameClassRector\Source\InterfaceAndClass\SomeBasicDateTime::class =>
+            \Rector\Tests\Renaming\Rector\Name\RenameClassRector\Source\InterfaceAndClass\SomeBasicDateTimeInterface::class,
+
+            // test casing
             OldClassWithTypo::class => NewClassWithoutTypo::class,
-            'DateTime' => 'DateTimeInterface',
             'Countable' => 'stdClass',
-            Manual_Twig_Filter::class => TwigFilter::class,
-            'Twig_AbstractManualExtension' => AbstractManualExtension::class,
             'Twig_Extension_Sandbox' => 'Twig\Extension\SandboxExtension',
             // Renaming class itself and its namespace
             'MyNamespace\MylegacyClass' => 'MyNewNamespace\MyNewClass',
             'MyNamespace\MyTrait' => 'MyNewNamespace\MyNewTrait',
-            'MyNamespace\MyInterface' => 'MyNewNamespace\MyNewInterface',
             'MyOldClass' => 'MyNamespace\MyNewClass2',
             'AnotherMyOldClass' => 'AnotherMyNewClass',
             'MyNamespace\AnotherMyClass' => 'MyNewClassWithoutNamespace',
@@ -45,11 +44,8 @@ return static function (RectorConfig $rectorConfig): void {
             SecondInterface::class => ThirdInterface::class,
             \Acme\Foo\DoNotUpdateExistingTargetNamespace::class => DoNotUpdateExistingTargetNamespace::class,
             SomeNonFinalClass::class => SomeFinalClass::class,
-            'Foo\Bar' => 'Foo\Bar\BarInterface',
+
             'Doctrine\DBAL\DBALException' => 'Doctrine\DBAL\Exception',
-            'Bar' => 'BarInterface',
-            'App' => 'Illuminate\Support\Facades\App',
             'NotExistsClass' => 'NewClass',
-            'Exception' => 'RuntimeException',
         ]);
 };
