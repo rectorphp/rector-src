6ccf61e8efc61eb445e38c90ff4ccdb7ad9a9c1f
Tomas Votruba <tomas.vot@gmail.com>
2023-08-09 17:35:39 +0200

    [DX] Add simple parmater provider hash (#4742)
    * add simple parmater provider hash
    
    * mark with api

--- src/Configuration/Parameter/SimpleParameterProvider.php
+++ src/Configuration/Parameter/SimpleParameterProvider.php
@@ -95,6 +95,16 @@ final class SimpleParameterProvider
     }
 
     /**
+     * @api
+     * For cache invalidation
+     */
+    public static function hash(): string
+    {
+        $parameterKeys = self::$parameters;
+        return sha1(serialize($parameterKeys));
+    }
+
+    /**
      * @param Option::* $name
      */
     private static function ensureParameterIsSet(string $name): void
