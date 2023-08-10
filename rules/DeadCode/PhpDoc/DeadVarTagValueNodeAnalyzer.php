f0558bded25edcebbcddcef9d088bbb768979839
GitHub Action <actions@github.com>
2023-08-10 11:44:43 +0000

    [ci-review] Rector Rectify

--- rules/DeadCode/PhpDoc/DeadVarTagValueNodeAnalyzer.php
+++ rules/DeadCode/PhpDoc/DeadVarTagValueNodeAnalyzer.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Rector\DeadCode\PhpDoc;
 
-use PHPStan\Type\IntersectionType;
 use PhpParser\Node\Stmt\Property;
 use PHPStan\PhpDocParser\Ast\PhpDoc\VarTagValueNode;
 use PHPStan\Type\UnionType;
