f48b7cc64852e27f8ac9455e6a372b05ec03f8fb
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-08-14 10:17:24 +0700

    [PHPStanStaticTypeMapper] Allow Closure type on ClosureTypeMapper based on PHP versions

--- packages/PHPStanStaticTypeMapper/TypeMapper/ClosureTypeMapper.php
+++ packages/PHPStanStaticTypeMapper/TypeMapper/ClosureTypeMapper.php
@@ -12,6 +12,8 @@ use PHPStan\PhpDocParser\Ast\Type\TypeNode;
 use PHPStan\Type\ClosureType;
 use PHPStan\Type\Type;
 use Rector\BetterPhpDocParser\ValueObject\Type\FullyQualifiedIdentifierTypeNode;
+use Rector\Core\Php\PhpVersionProvider;
+use Rector\Core\ValueObject\PhpVersionFeature;
 use Rector\PhpDocParser\PhpDocParser\PhpDocNodeTraverser;
 use Rector\PHPStanStaticTypeMapper\Contract\TypeMapperInterface;
 use Rector\PHPStanStaticTypeMapper\Enum\TypeKind;
@@ -21,6 +23,10 @@ use Rector\PHPStanStaticTypeMapper\Enum\TypeKind;
  */
 final class ClosureTypeMapper implements TypeMapperInterface
 {
+    public function __construct(private readonly PhpVersionProvider $phpVersionProvider)
+    {
+    }
+
     /**
      * @return class-string<Type>
      */
@@ -62,10 +68,18 @@ final class ClosureTypeMapper implements TypeMapperInterface
      */
     public function mapToPhpParserNode(Type $type, string $typeKind): ?Node
     {
-        if ($typeKind === TypeKind::PROPERTY) {
-            return null;
+        if ($typeKind === TypeKind::PARAM && $this->phpVersionProvider->isAtLeastPhpVersion(PhpVersionFeature::ANONYMOUS_FUNCTION_PARAM_TYPE)) {
+            return new FullyQualified('Closure');
         }
 
-        return new FullyQualified('Closure');
+        if ($typeKind === TypeKind::PROPERTY && $this->phpVersionProvider->isAtLeastPhpVersion(PhpVersionFeature::TYPED_PROPERTIES)) {
+            return new FullyQualified('Closure');
+        }
+
+        if ($typeKind === TypeKind::RETURN && $this->phpVersionProvider->isAtLeastPhpVersion(PhpVersionFeature::ANONYMOUS_FUNCTION_RETURN_TYPE)) {
+            return new FullyQualified('Closure');
+        }
+
+        return null;
     }
 }
