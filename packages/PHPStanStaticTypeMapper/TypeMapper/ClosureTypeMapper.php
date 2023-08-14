440810baf9f10d05530251ffc8057b09374ca6f9
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-08-14 10:26:51 +0700

    add ref

--- packages/PHPStanStaticTypeMapper/TypeMapper/ClosureTypeMapper.php
+++ packages/PHPStanStaticTypeMapper/TypeMapper/ClosureTypeMapper.php
@@ -68,10 +68,12 @@ final class ClosureTypeMapper implements TypeMapperInterface
      */
     public function mapToPhpParserNode(Type $type, string $typeKind): ?Node
     {
+        // ref https://3v4l.org/iKMK6#v5.3.29
         if ($typeKind === TypeKind::PARAM && $this->phpVersionProvider->isAtLeastPhpVersion(PhpVersionFeature::ANONYMOUS_FUNCTION_PARAM_TYPE)) {
             return new FullyQualified('Closure');
         }
 
+        // ref https://3v4l.org/g8WvW#v7.4.0
         if ($typeKind === TypeKind::PROPERTY && $this->phpVersionProvider->isAtLeastPhpVersion(PhpVersionFeature::TYPED_PROPERTIES)) {
             return new FullyQualified('Closure');
         }
@@ -80,7 +82,8 @@ final class ClosureTypeMapper implements TypeMapperInterface
             return null;
         }
 
-        if (!$this->phpVersionProvider->isAtLeastPhpVersion(PhpVersionFeature::ANONYMOUS_FUNCTION_RETURN_TYPE)) {
+        // ref https://3v4l.org/nUreN#v7.0.0
+        if (! $this->phpVersionProvider->isAtLeastPhpVersion(PhpVersionFeature::ANONYMOUS_FUNCTION_RETURN_TYPE)) {
             return null;
         }
 
