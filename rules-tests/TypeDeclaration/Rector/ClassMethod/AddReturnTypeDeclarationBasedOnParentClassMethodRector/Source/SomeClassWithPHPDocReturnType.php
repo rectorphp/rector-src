44e9c7ed7a8cfec8c9cf2395e4a0308c7ccf0554
Markus Staab <markus.staab@redaxo.de>
2023-08-17 17:07:18 +0200

    Fix typo in AddReturnTypeDeclarationBasedOnParentClassMethodRector test

--- rules-tests/TypeDeclaration/Rector/ClassMethod/AddReturnTypeDeclarationBasedOnParentClassMethodRector/Source/SomeClassWithPHPDocReturnType.php
+++ rules-tests/TypeDeclaration/Rector/ClassMethod/AddReturnTypeDeclarationBasedOnParentClassMethodRector/Source/SomeClassWithPHPDocReturnType.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Rector\ClassMethod\AddReturnTypeDeclarationBasedOnParentClassMethodRector\Source;
+namespace Rector\Tests\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationBasedOnParentClassMethodRector\Source;
 
 class SomeClassWithPHPDocReturnType
 {
