cdccb5199ddc160fb7945879cf1403f1c7289770
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-08-21 22:05:00 +0700

    [AutoImport] Handle case insensitive name collide on auto import (#4829)
    * [AutoImport] Handle case insensiive name collide on auto import
    
    * eol
    
    * skip import
    
    * Revert "skip import"
    
    This reverts commit f2f25ba87495e9b9a68f88470a31991c39d4eadb.
    
    * Fixed :tada:
    
    * fix test
    
    * comment future

--- packages/PostRector/Collector/UseNodesToAddCollector.php
+++ packages/PostRector/Collector/UseNodesToAddCollector.php
@@ -116,6 +116,7 @@ final class UseNodesToAddCollector implements NodeCollectorInterface
         $fileConstantUseImportTypes = $this->constantUseImportTypesInFilePath[$filePath] ?? [];
 
         foreach ($fileConstantUseImportTypes as $fileConstantUseImportType) {
+            // don't compare strtolower for use const as insensitive is allowed, see https://3v4l.org/lteVa
             if ($fileConstantUseImportType->getShortName() === $shortName) {
                 return true;
             }
@@ -124,7 +125,7 @@ final class UseNodesToAddCollector implements NodeCollectorInterface
         $fileFunctionUseImportTypes = $this->functionUseImportTypesInFilePath[$filePath] ?? [];
 
         foreach ($fileFunctionUseImportTypes as $fileFunctionUseImportType) {
-            if ($fileFunctionUseImportType->getShortName() === $shortName) {
+            if (strtolower($fileFunctionUseImportType->getShortName()) === strtolower($shortName)) {
                 return true;
             }
         }
@@ -189,7 +190,7 @@ final class UseNodesToAddCollector implements NodeCollectorInterface
         $fileUseImports = $this->useImportTypesInFilePath[$filePath] ?? [];
 
         foreach ($fileUseImports as $fileUseImport) {
-            if ($fileUseImport->getShortName() === $shortName) {
+            if (strtolower($fileUseImport->getShortName()) === strtolower($shortName)) {
                 return true;
             }
         }
