c73bc294a61249c7e7f35581cebd29864e49385a
Tomas Votruba <tomas.vot@gmail.com>
2023-08-23 11:59:19 +0200

    [tests] Improve resetable interface call (#4834)

--- packages-tests/Skipper/Skipper/SkipperRectorRuleTest.php
+++ packages-tests/Skipper/Skipper/SkipperRectorRuleTest.php
@@ -20,7 +20,6 @@ final class SkipperRectorRuleTest extends AbstractLazyTestCase
         self::$rectorConfig = null;
     }
 
-
     protected function tearDown(): void
     {
         // cleanup configuration
