43d4bbfa6d683696348d21f0773a87045dd5613c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-08-23 12:08:49 +0700

    add e2e test for wilcards path import config

---
+++ e2e/wildcards-path-import-config/rector.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+    ]);
+
+    $rectorConfig->import(__DIR__ . '/config/*');
+};
