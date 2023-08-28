6d8f136c9466d505b49d5b918873edaea19d76b2
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-08-28 13:19:32 +0700

    clean up in ClassRenamePhpDocNodeVisitor as well

--- packages/NodeTypeResolver/PhpDocNodeVisitor/ClassRenamePhpDocNodeVisitor.php
+++ packages/NodeTypeResolver/PhpDocNodeVisitor/ClassRenamePhpDocNodeVisitor.php
@@ -131,11 +131,13 @@ final class ClassRenamePhpDocNodeVisitor extends AbstractPhpDocNodeVisitor
         }
 
         $uses = $this->useImportsResolver->resolve();
-        $originalNode = $phpParserNode->getAttribute(AttributeKey::ORIGINAL_NODE) ?? $phpParserNode;
-        $scope = $originalNode->getAttribute(AttributeKey::SCOPE);
+        $originalNode = $phpParserNode->getAttribute(AttributeKey::ORIGINAL_NODE);
+        $scope = $originalNode instanceof PhpParserNode
+            ? $originalNode->getAttribute(AttributeKey::SCOPE)
+            : $phpParserNode->getAttribute(AttributeKey::SCOPE);
 
         if (! $scope instanceof Scope) {
-            if (! $phpParserNode->hasAttribute(AttributeKey::ORIGINAL_NODE)) {
+            if (! $originalNode instanceof PhpParserNode) {
                 return $this->resolveNamefromUse($uses, $name);
             }
 
