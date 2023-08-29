8b07c8e7de96a3b304b5409f1ef3415a513fee91
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-08-30 01:06:12 +0700

    Add test for merge ruleWithConfiguration() after sets on $isBound usage (#4881)
    * Add test for merge ruleWithConfiguration() after sets on $isBound usage
    
    * Add test for merge ruleWithConfiguration() after sets on $isBound usage

---
+++ tests/Issues/RuleWithConfigurationAfterSet/config/configured_rule.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
+use Rector\Set\ValueObject\LevelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_81,
+    ]);
+
+    $rectorConfig->ruleWithConfiguration(StringClassNameToClassConstantRector::class, [
+        'Doctrine\*'
+    ]);
+};
