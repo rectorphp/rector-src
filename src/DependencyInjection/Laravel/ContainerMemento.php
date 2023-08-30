17335cbae159c4d4022034cf3d0c8f036b0af3d6
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-08-30 22:46:54 +0700

    [Performance][DI] Remove double loop on ContainerMemento

--- src/DependencyInjection/Laravel/ContainerMemento.php
+++ src/DependencyInjection/Laravel/ContainerMemento.php
@@ -5,28 +5,28 @@ declare(strict_types=1);
 namespace Rector\Core\DependencyInjection\Laravel;
 
 use Illuminate\Container\Container;
+use Rector\Core\Contract\Rector\RectorInterface;
 use Rector\Core\Util\Reflection\PrivatesAccessor;
 
 /**
- * Helper service to modify Laravel container
+ * Helper service to modify Laravel container to remove skipped Rector rule
  */
 final class ContainerMemento
 {
-    public static function forgetService(Container $container, string $typeToForget): void
+    public static function forgetService(Container $container, string $rectorClass): void
     {
         // 1. remove the service
-        $container->offsetUnset($typeToForget);
+        $container->offsetUnset($rectorClass);
 
-        // 2. remove all tagged rules
+        // 2. remove tagged rule
         $privatesAccessor = new PrivatesAccessor();
         $privatesAccessor->propertyClosure($container, 'tags', static function (array $tags) use (
-            $typeToForget
+            $rectorClass
         ): array {
-            foreach ($tags as $tagName => $taggedClasses) {
-                foreach ($taggedClasses as $key => $taggedClass) {
-                    if (is_a($taggedClass, $typeToForget, true)) {
-                        unset($tags[$tagName][$key]);
-                    }
+            foreach ($tags[RectorInterface::class] ?? [] as $key => $taggedClass) {
+                if ($taggedClass === $rectorClass) {
+                    unset($tags[RectorInterface::class][$key]);
+                    break;
                 }
             }
 
