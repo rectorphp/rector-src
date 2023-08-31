4b0a688b8117689c243adc1c8dd18e9512673428
Celebrate-Reinhard <100134454+Celebrate-Reinhard@users.noreply.github.com>
2023-08-31 12:33:27 +0200

    [TypeDeclaration] skip variadic constructor param of mixed type on AddParamTypeFromPropertyTypeRector (#4887)
    * skip variadic constructor param of mixed type
    
    * [WIP] add testcase for explicit type
    
    * remove unneeded expectation as it is the same as input fixture
    
    * Revert "[WIP] add testcase for explicit type"
    
    This reverts commit 353a75f884315ebec47e69a07e327520e2731ca5.
    
    Taking over type of doc block would be nice. As discussed in PR:
    we can't rely on docblock, so skipping is the safest.

--- rules/TypeDeclaration/Rector/ClassMethod/AddParamTypeFromPropertyTypeRector.php
+++ rules/TypeDeclaration/Rector/ClassMethod/AddParamTypeFromPropertyTypeRector.php
@@ -93,7 +93,9 @@ CODE_SAMPLE
             if ($param->type instanceof Node) {
                 continue;
             }
-
+            if ($param->variadic) {
+                continue;
+            }
             if (! $this->paramTypeAddGuard->isLegal($param, $node)) {
                 continue;
             }
