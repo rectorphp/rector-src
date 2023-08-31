7d1e9a7d501d1e89237ff3f93a13df5a92bf8db5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-08-31 22:49:34 +0700

    add test for return self on php 7.4

--- rules-tests/TypeDeclaration/Rector/ClassMethod/ReturnTypeFromStrictFluentReturnRector/config/configured_rule.php
+++ rules-tests/TypeDeclaration/Rector/ClassMethod/ReturnTypeFromStrictFluentReturnRector/config/configured_rule.php
@@ -3,8 +3,10 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Core\ValueObject\PhpVersion;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictFluentReturnRector;
 
 return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->phpVersion(PhpVersion::PHP_80);
     $rectorConfig->rule(ReturnTypeFromStrictFluentReturnRector::class);
 };
