83eb3a057ef6ea3fbe689d647a68ede62dbbae63
Tomas Votruba <tomas.vot@gmail.com>
2023-08-31 10:08:49 +0200

    Fix default value for string typed property

--- tests/Issues/RuleWithConfigurationAfterSet/config/configured_rule.php
+++ tests/Issues/RuleWithConfigurationAfterSet/config/configured_rule.php
@@ -7,11 +7,7 @@ use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
-    ]);
+    $rectorConfig->sets([LevelSetList::UP_TO_PHP_81]);
 
-    $rectorConfig->ruleWithConfiguration(StringClassNameToClassConstantRector::class, [
-        'Doctrine\*'
-    ]);
+    $rectorConfig->ruleWithConfiguration(StringClassNameToClassConstantRector::class, ['Doctrine\*']);
 };
