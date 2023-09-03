bf7cb9e33b515ab8af4f9172760d114c739b3959
GitHub Action <actions@github.com>
2023-09-03 18:23:18 +0000

    [ci-review] Rector Rectify

--- packages-tests/Caching/Config/FileHashComputer/FileHashComputerTest.php
+++ packages-tests/Caching/Config/FileHashComputer/FileHashComputerTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Rector\Tests\Caching\Config\FileHashComputer;
 
-use PHPUnit\Framework\TestCase;
 use Rector\Caching\Config\FileHashComputer;
 use Rector\Testing\PHPUnit\AbstractLazyTestCase;
 
