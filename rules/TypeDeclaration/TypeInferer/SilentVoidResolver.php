eca01b375a62cca2d79ee5a1a7fff1795f3cba11
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-09-06 19:38:01 +0700

    Trigger Rectify (#4924)
    * Trigger Rectify
    
    * [ci-review] Rector Rectify
    
    * cs fix
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

--- rules/TypeDeclaration/TypeInferer/SilentVoidResolver.php
+++ rules/TypeDeclaration/TypeInferer/SilentVoidResolver.php
@@ -22,7 +22,6 @@ use PHPStan\Reflection\ClassReflection;
 use Rector\Core\PhpParser\Node\BetterNodeFinder;
 use Rector\Core\Reflection\ReflectionResolver;
 
-
 final class SilentVoidResolver
 {
     public function __construct(
@@ -46,14 +45,11 @@ final class SilentVoidResolver
             return false;
         }
 
-        $return = $this->betterNodeFinder->findFirstInFunctionLikeScoped($functionLike, function(Node $node) {
-            if ($node instanceof Return_ && $node->expr instanceof Expr) {
-                return true;
-            }
-            return false;
-        });
-
-        return $return === null;
+        $return = $this->betterNodeFinder->findFirstInFunctionLikeScoped(
+            $functionLike,
+            static fn (Node $node): bool => $node instanceof Return_ && $node->expr instanceof Expr
+        );
+        return ! $return instanceof Return_;
     }
 
     public function hasSilentVoid(FunctionLike $functionLike): bool
