fbb98c592a76768b2fb27d7c5ed31661eb522f24
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-09-10 11:15:57 +0700

    reduce RunInSeparateProcess

--- packages-tests/Caching/Config/FileHashComputer/FileHashComputerTest.php
+++ packages-tests/Caching/Config/FileHashComputer/FileHashComputerTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Rector\Tests\Caching\Config\FileHashComputer;
 
-use PHPUnit\Framework\Attributes\RunInSeparateProcess;
 use Rector\Caching\Config\FileHashComputer;
 use Rector\Core\Configuration\Option;
 use Rector\Core\Configuration\Parameter\SimpleParameterProvider;
@@ -19,7 +18,6 @@ final class FileHashComputerTest extends AbstractLazyTestCase
         $this->fileHashComputer = $this->make(FileHashComputer::class);
     }
 
-    #[RunInSeparateProcess]
     public function testRectorPhpChanged(): void
     {
         $this->bootFromConfigFiles([__DIR__ . '/Fixture/rector.php']);
@@ -39,7 +37,6 @@ final class FileHashComputerTest extends AbstractLazyTestCase
         $this->assertNotSame($newHashedFile, $hashedFile);
     }
 
-    #[RunInSeparateProcess]
     public function testRectorPhpNotChanged(): void
     {
         $this->bootFromConfigFiles([__DIR__ . '/Fixture/rector.php']);
