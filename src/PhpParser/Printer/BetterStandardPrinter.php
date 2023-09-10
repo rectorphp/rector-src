cda957718f1eb9d1629bc27ed8b060c6f41ce033
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-09-11 01:44:58 +0700

    [Performance][Printer] Reduce regex check use( no space on BetterStandardPrinter (#4971)
    * [Printer] Reduce regex check use( no space on BetterStandardPrinter
    
    * same behav
    
    * [ci-review] Rector Rectify
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

--- src/PhpParser/Printer/BetterStandardPrinter.php
+++ src/PhpParser/Printer/BetterStandardPrinter.php
@@ -43,12 +43,6 @@ final class BetterStandardPrinter extends Standard
 {
     /**
      * @var string
-     * @see https://regex101.com/r/F5x783/1
-     */
-    private const USE_REGEX = '#( use)\(#';
-
-    /**
-     * @var string
      * @see https://regex101.com/r/DrsMY4/1
      */
     private const QUOTED_SLASH_REGEX = "#'|\\\\(?=[\\\\']|$)#";
@@ -299,7 +293,7 @@ final class BetterStandardPrinter extends Standard
             return $closureContent;
         }
 
-        return Strings::replace($closureContent, self::USE_REGEX, '$1 (');
+        return str_replace(' use(', ' use (', (string) $closureContent);
     }
 
     /**
