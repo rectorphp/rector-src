27d4c55bebe8706a74273d8c2379f59b75543c0e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-09-11 20:43:31 +0700

    [Transform] Check str_contains * before fnmatch() early on AddAllowDynamicPropertiesAttributeRector (#4991)
    * [Php55] Check str_contains * before fnmatch() early on AddAllowDynamicPropertiesAttributeRector
    
    * fix

--- rules/Transform/Rector/Class_/AddAllowDynamicPropertiesAttributeRector.php
+++ rules/Transform/Rector/Class_/AddAllowDynamicPropertiesAttributeRector.php
@@ -139,16 +139,18 @@ CODE_SAMPLE
 
     private function shouldSkip(Class_ $class): bool
     {
-        if ($this->transformOnNamespaces !== []) {
-            $className = (string) $this->nodeNameResolver->getName($class);
-            foreach ($this->transformOnNamespaces as $transformOnNamespace) {
-                if ($this->nodeNameResolver->isStringName($className, $transformOnNamespace)) {
-                    continue;
-                }
-
+        $className = (string) $this->nodeNameResolver->getName($class);
+        foreach ($this->transformOnNamespaces as $transformOnNamespace) {
+            if (str_contains($transformOnNamespace, '*')) {
                 if (! fnmatch($transformOnNamespace, $className, FNM_NOESCAPE)) {
                     return true;
                 }
+
+                continue;
+            }
+
+            if ($this->nodeNameResolver->isStringName($className, $transformOnNamespace)) {
+                continue;
             }
         }
 
