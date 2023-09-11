51554e27561865e047bf9826839e77a3cb1de4c4
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-09-12 04:25:37 +0700

    add back fixture tests

---
+++ tests/Issues/ConstructorPromoAnnotationToAttribute/config/configured_rule.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Php80\ValueObject\AnnotationToAttribute;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rule(ClassPropertyAssignToConstructorPromotionRector::class);
+    $rectorConfig->ruleWithConfiguration(AnnotationToAttributeRector::class, [
+        new AnnotationToAttribute('OldTag', 'NewAttribute'),
+    ]);
+};
