fa97736817c1dd2568d6f3a9d1fb80a2f4df85c3
Markus Staab <maggus.staab@googlemail.com>
2023-09-19 21:06:27 +0200

    fix

--- rules-tests/Php80/Rector/Class_/ClassPropertyAssignToConstructorPromotionRector/Source/ValidatedElement.php
+++ rules-tests/Php80/Rector/Class_/ClassPropertyAssignToConstructorPromotionRector/Source/ValidatedElement.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector\Source;
+namespace Rector\Tests\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector\Source;
 
 final class ValidatedElement
 {
