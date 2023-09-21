b8cdb58f3f2939936bf299bbe3341e7493603558
GitHub Action <actions@github.com>
2023-09-21 11:54:15 +0000

    [ci-review] Rector Rectify

--- tests/Issues/InfiniteIfElse/config/configured_rule.php
+++ tests/Issues/InfiniteIfElse/config/configured_rule.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Rector\Config\RectorConfig;
 use Rector\CodingStyle\Rector\Switch_\BinarySwitchToIfElseRector;
 use Rector\EarlyReturn\Rector\If_\RemoveAlwaysElseRector;
