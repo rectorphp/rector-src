51ed7b693fbfab70cf6a246bec4cc35f0ceacf7a
Tomas Votruba <tomas.vot@gmail.com>
2023-09-27 14:37:36 +0200

    [build] Add phpstan/phpstan removal on the build, as require by build package in composer.json already

---
+++ bin/add-phpstan-self-replace.php
@@ -0,0 +1,20 @@
+<?php
+
+// this is part of downgrade build
+
+declare(strict_types=1);
+
+use Nette\Utils\FileSystem;
+use Nette\Utils\Json;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$composerJsonFileContents = FileSystem::read(__DIR__ . '/../composer.json');
+
+$composerJson = Json::decode($composerJsonFileContents, Json::FORCE_ARRAY);
+$composerJson['replace']['phpstan/phpstan'] = $composerJson['require']['phpstan/phpstan'];
+
+$modifiedComposerJsonFileContents = Json::encode($composerJson, Json::PRETTY);
+FileSystem::write(__DIR__ . '/../composer.json', $modifiedComposerJsonFileContents);
+
+echo 'Done!' . PHP_EOL;
