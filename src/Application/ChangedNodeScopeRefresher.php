9793540e79b87bb08e2203b761c2c5b361ac8e27
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-09-29 00:00:37 +0700

    [DX] Remove Attribute support on ChangedNodeScopeRefresher (#5090)

--- src/Application/ChangedNodeScopeRefresher.php
+++ src/Application/ChangedNodeScopeRefresher.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Rector\Core\Application;
 
 use PhpParser\Node;
-use PhpParser\Node\Attribute;
-use PhpParser\Node\AttributeGroup;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\CallLike;
 use PhpParser\Node\Expr\Closure;
@@ -21,7 +19,6 @@ use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\Function_;
 use PhpParser\Node\Stmt\If_;
-use PhpParser\Node\Stmt\Property;
 use PhpParser\Node\Stmt\Switch_;
 use PhpParser\Node\Stmt\TryCatch;
 use PHPStan\Analyser\MutatingScope;
@@ -55,15 +52,6 @@ final class ChangedNodeScopeRefresher
             $errorMessage = sprintf('Node "%s" with is missing scope required for scope refresh', $node::class);
 
             throw new ShouldNotHappenException($errorMessage);
-        }
-
-        // note from flight: when we traverse ClassMethod, the scope must be already in Class_, otherwise it crashes
-        // so we need to somehow get a parent scope that is already in the same place the $node is
-
-        if ($node instanceof Attribute) {
-            // we'll have to fake-traverse 2 layers up, as PHPStan skips Scope for AttributeGroups and consequently Attributes
-            $attributeGroup = new AttributeGroup([$node]);
-            $node = new Property(0, [], [], null, [$attributeGroup]);
         }
 
         $stmts = $this->resolveStmts($node);
