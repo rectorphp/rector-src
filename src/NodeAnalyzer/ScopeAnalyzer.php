c2382d486c520282159c351d4843327a004f330c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-10-01 17:13:27 +0700

    clean up

--- src/NodeAnalyzer/ScopeAnalyzer.php
+++ src/NodeAnalyzer/ScopeAnalyzer.php
@@ -6,14 +6,10 @@ namespace Rector\Core\NodeAnalyzer;
 
 use PhpParser\Node;
 use PhpParser\Node\Arg;
-use PhpParser\Node\Expr\Array_;
 use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Identifier;
 use PhpParser\Node\Name;
 use PhpParser\Node\Param;
-use PhpParser\Node\Stmt;
-use PHPStan\Analyser\Scope;
-use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\NodeTypeResolver\PHPStan\Scope\ScopeFactory;
 
 final class ScopeAnalyzer
@@ -37,23 +33,5 @@ final class ScopeAnalyzer
         }
 
         return true;
-    }
-
-    public function resolveScope(Node $node, string $filePath): ?Scope
-    {
-        // on File level
-        if ($node instanceof Stmt && $node->getAttribute(AttributeKey::STATEMENT_DEPTH) === 0) {
-            return $this->scopeFactory->createFromFile($filePath);
-        }
-
-        /**
-         * Node and parent Node doesn't has Scope, and Node Start token pos is < 0,
-         * it means the node and parent node just re-printed, the Scope need to be resolved from file
-         */
-        if ($node->getStartTokenPos() < 0) {
-            return $this->scopeFactory->createFromFile($filePath);
-        }
-
-        return null;
     }
 }
