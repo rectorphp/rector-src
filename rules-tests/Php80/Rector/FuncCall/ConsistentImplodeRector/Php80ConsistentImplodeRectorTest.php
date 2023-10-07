14c08d11f68cf31be019c53ffc230cc5e745e797
Just Only Big <justonlybig@gmail.com>
2023-10-07 17:01:26 +0200

    change name to file test

--- rules-tests/Php80/Rector/FuncCall/ConsistentImplodeRector/Php80ConsistentImplodeRectorTest.php
+++ rules-tests/Php80/Rector/FuncCall/ConsistentImplodeRector/Php80ConsistentImplodeRectorTest.php
@@ -8,7 +8,7 @@ use Iterator;
 use PHPUnit\Framework\Attributes\DataProvider;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
 
-final class ConsistentImplodeRectorTest extends AbstractRectorTestCase
+final class Php80ConsistentImplodeRectorTest extends AbstractRectorTestCase
 {
     #[DataProvider('provideData')]
     public function test(string $filePath): void
