b7a5fefedc9d1a32c1f238a6a8c9b89b88e7d21c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-10-12 17:22:32 +0700

    [DeadCode] Add RemoveUselessReturnExprInConstructRector (#5158)
    * [DeadCode] Add RemoveUselessReturnExprInConstruct
    
    * [ci-review] Rector Rectify
    
    * skip inner class test
    
    * [ci-review] Rector Rectify
    
    * fix phpstan
    
    * add Rector suffix
    
    * register
    
    * add fixture for return dynamic
    
    * update doc sample
    
    * fix
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

--- src/Rector/AbstractRector.php
+++ src/Rector/AbstractRector.php
@@ -275,7 +275,7 @@ CODE_SAMPLE;
 
     /**
      * @param Node|Node[] $nodes
-     * @param callable(Node $node): (Node|null|int) $callable
+     * @param callable(Node): (int|Node|null|Node[]) $callable
      */
     protected function traverseNodesWithCallable(Node | array $nodes, callable $callable): void
     {
