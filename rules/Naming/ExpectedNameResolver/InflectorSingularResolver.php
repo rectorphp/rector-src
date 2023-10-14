de7fbf78455c4aa0e7cafdb498d3965092a60e7b
Markus Staab <maggus.staab@googlemail.com>
2023-10-14 07:29:17 +0200

    Replace doctrine/inflector with simpler mapping function

--- rules/Naming/ExpectedNameResolver/InflectorSingularResolver.php
+++ rules/Naming/ExpectedNameResolver/InflectorSingularResolver.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Rector\Naming\ExpectedNameResolver;
 
-use Doctrine\Inflector\Inflector;
 use Nette\Utils\Strings;
 use Rector\Core\Util\StringUtils;
 
@@ -36,11 +35,6 @@ final class InflectorSingularResolver
      * @var string
      */
     private const CAMELCASE = 'camelcase';
-
-    public function __construct(
-        private readonly Inflector $inflector
-    ) {
-    }
 
     public function resolve(string $currentName): string
     {
@@ -95,7 +89,7 @@ final class InflectorSingularResolver
 
         $resolvedName = '';
         foreach ($camelCases as $camelCase) {
-            $value = $this->inflector->singularize($camelCase[self::CAMELCASE]);
+            $value = $this->singularize($camelCase[self::CAMELCASE]);
 
             if (in_array($camelCase[self::CAMELCASE], ['is', 'has'], true)) {
                 $value = $camelCase[self::CAMELCASE];
@@ -105,5 +99,85 @@ final class InflectorSingularResolver
         }
 
         return $resolvedName;
+    }
+
+    // see https://gist.github.com/peter-mcconnell/9757549
+    private function singularize (string $word): string
+    {
+        $singular = array (
+            '/(quiz)zes$/i' => '\\1',
+            '/(matr)ices$/i' => '\\1ix',
+            '/(vert|ind)ices$/i' => '\\1ex',
+            '/^(ox)en/i' => '\\1',
+            '/(alias|status)es$/i' => '\\1',
+            '/([octop|vir])i$/i' => '\\1us',
+            '/(cris|ax|test)es$/i' => '\\1is',
+            '/(shoe)s$/i' => '\\1',
+            '/(o)es$/i' => '\\1',
+            '/(bus)es$/i' => '\\1',
+            '/([m|l])ice$/i' => '\\1ouse',
+            '/(x|ch|ss|sh)es$/i' => '\\1',
+            '/(m)ovies$/i' => '\\1ovie',
+            '/(s)eries$/i' => '\\1eries',
+            '/([^aeiouy]|qu)ies$/i' => '\\1y',
+            '/([lr])ves$/i' => '\\1f',
+            '/(tive)s$/i' => '\\1',
+            '/(hive)s$/i' => '\\1',
+            '/([^f])ves$/i' => '\\1fe',
+            '/(^analy)ses$/i' => '\\1sis',
+            '/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$/i' => '\\1\\2sis',
+            '/([ti])a$/i' => '\\1um',
+            '/(n)ews$/i' => '\\1ews',
+            '/s$/i' => ''
+        );
+
+        $irregular = array(
+            'person' => 'people',
+            'man' => 'men',
+            'child' => 'children',
+            'sex' => 'sexes',
+            'move' => 'moves'
+        );
+
+        $ignore = array(
+            'data',
+            'equipment',
+            'information',
+            'rice',
+            'money',
+            'species',
+            'series',
+            'fish',
+            'sheep',
+            'press',
+            'sms',
+        );
+
+        $lower_word = strtolower($word);
+        foreach ($ignore as $ignore_word)
+        {
+            if (substr($lower_word, (-1 * strlen($ignore_word))) == $ignore_word)
+            {
+                return $word;
+            }
+        }
+
+        foreach ($irregular as $singular_word => $plural_word)
+        {
+            if (preg_match('/('.$plural_word.')$/i', $word, $arr))
+            {
+                return preg_replace('/('.$plural_word.')$/i', substr($arr[0],0,1).substr($singular_word,1), $word);
+            }
+        }
+
+        foreach ($singular as $rule => $replacement)
+        {
+            if (preg_match($rule, $word))
+            {
+                return preg_replace($rule, $replacement, $word);
+            }
+        }
+
+        return $word;
     }
 }
