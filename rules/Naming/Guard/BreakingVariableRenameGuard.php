1a98723aebf1bdf9462b785d49f75d914b569779
Tomas Votruba <tomas.vot@gmail.com>
2023-10-18 11:35:59 +0200

    [Naming] Skip all kinds of generators (#5187)

--- rules/Naming/Guard/BreakingVariableRenameGuard.php
+++ rules/Naming/Guard/BreakingVariableRenameGuard.php
@@ -197,6 +197,10 @@ final class BreakingVariableRenameGuard
             return false;
         }
 
+        if (str_ends_with($paramType->getClassName(), 'Generator') || str_ends_with($paramType->getClassName(), 'Iterator')) {
+            return true;
+        }
+
         return $paramType->isInstanceOf('Symfony\Component\DependencyInjection\Argument\RewindableGenerator')
             ->yes();
     }
