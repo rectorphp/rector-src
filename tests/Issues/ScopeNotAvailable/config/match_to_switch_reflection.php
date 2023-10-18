beeba1770967a4f5e59cf21db28a5a1ccae48a2f
Tomas Votruba <tomas.vot@gmail.com>
2023-10-18 10:02:31 +0200

    [Naming] Skip DateTime from renaming, to keep convention of named dates (#5184)

--- tests/Issues/ScopeNotAvailable/config/match_to_switch_reflection.php
+++ tests/Issues/ScopeNotAvailable/config/match_to_switch_reflection.php
@@ -3,12 +3,12 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\DowngradePhp81\Rector\StmtsAwareInterface\DowngradeSetAccessibleReflectionPropertyRector;
 use Rector\DowngradePhp80\Rector\Expression\DowngradeMatchToSwitchRector;
+use Rector\DowngradePhp81\Rector\StmtsAwareInterface\DowngradeSetAccessibleReflectionPropertyRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rules([
-       DowngradeSetAccessibleReflectionPropertyRector::class,
-       DowngradeMatchToSwitchRector::class,
+        DowngradeSetAccessibleReflectionPropertyRector::class,
+        DowngradeMatchToSwitchRector::class,
     ]);
 };
