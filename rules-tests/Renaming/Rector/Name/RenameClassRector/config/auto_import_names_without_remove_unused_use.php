3e505e5d39a709a67f2400dc647a48a697b0baf1
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-10-23 21:36:58 +0700

    add fixture for only importNames() without removeUnusedIMpotrs() (#5194)

---
+++ rules-tests/Renaming/Rector/Name/RenameClassRector/config/auto_import_names_without_remove_unused_use.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Renaming\Rector\Name\RenameClassRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->importNames();
+
+    $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
+        'Interop\Container\ContainerInterface' => 'Psr\Container\ContainerInterface',
+    ]);
+};
