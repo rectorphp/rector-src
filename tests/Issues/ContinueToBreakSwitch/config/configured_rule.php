0d55d15b79dbe81e07776c72a5b99f08efa64bd5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-10-31 19:47:47 +0700

    [Php52][Php70] Handle ContinueToBreakInSwitchRector + BreakNotInLoopOrSwitchToReturnRector (#5217)
    * [Php52][Php70] Handle ContinueToBreakInSwitchRector + BreakNotInLoopOrSwitchToReturnRector
    
    * Fixed :tada:
    
    * Fixed :tada:
    
    * Fixed :tada:
    
    * Fixed :tada:
    
    * rollback RectifiedAnalyzer
    
    * Fix

---
+++ tests/Issues/ContinueToBreakSwitch/config/configured_rule.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php52\Rector\Switch_\ContinueToBreakInSwitchRector;
+use Rector\Php70\Rector\Break_\BreakNotInLoopOrSwitchToReturnRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rules([
+        BreakNotInLoopOrSwitchToReturnRector::class,
+        ContinueToBreakInSwitchRector::class,
+    ]);
+};
