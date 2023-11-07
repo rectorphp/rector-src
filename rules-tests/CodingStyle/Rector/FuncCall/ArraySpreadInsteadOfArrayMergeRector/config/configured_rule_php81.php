9adc7a04b3a312957bb305edafcd0427fa6bd257
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-11-08 01:54:33 +0700

    [CodingStyle] Move ArraySpreadInsteadOfArrayMergeRector from php74 to codingstyle sets list (#5230)
    * [CodingStyle] Move ArraySpreadInsteadOfArrayMergeRector from php74 to codingstyle sets list
    
    * regenerate docs

---
+++ rules-tests/CodingStyle/Rector/FuncCall/ArraySpreadInsteadOfArrayMergeRector/config/configured_rule_php81.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Core\ValueObject\PhpVersion;
+use Rector\CodingStyle\Rector\FuncCall\ArraySpreadInsteadOfArrayMergeRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->phpVersion(PhpVersion::PHP_81);
+    $rectorConfig->rule(ArraySpreadInsteadOfArrayMergeRector::class);
+};
