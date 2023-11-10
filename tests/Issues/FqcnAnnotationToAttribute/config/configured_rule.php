54558fe8d222a53a34c6f9139b1251998754f025
GitHub Action <actions@github.com>
2023-11-10 20:38:24 +0000

    [ci-review] Rector Rectify

--- tests/Issues/FqcnAnnotationToAttribute/config/configured_rule.php
+++ tests/Issues/FqcnAnnotationToAttribute/config/configured_rule.php
@@ -1,12 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\SetList;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
 
 return static function (RectorConfig $rectorConfig): void {
         $rectorConfig->sets([
-            \Rector\Symfony\Set\SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
-            \Rector\Doctrine\Set\DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
+            SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
+            DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
     ]);
 };
