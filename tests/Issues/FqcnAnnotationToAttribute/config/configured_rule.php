9b0e4e9a53bc98dbda4a85c09433d237af4b20b9
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-11-11 03:56:19 +0700

    update config

--- tests/Issues/FqcnAnnotationToAttribute/config/configured_rule.php
+++ tests/Issues/FqcnAnnotationToAttribute/config/configured_rule.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\SetList;
-use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
+use Rector\Symfony\Set\SymfonySetList;
+use Rector\Doctrine\Set\DoctrineSetList;
 
 return static function (RectorConfig $rectorConfig): void {
         $rectorConfig->sets([
