0ffb98ea050007a4186e8e99bb73cdb76887742b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-11-14 14:56:57 +0700

    [AutoImport][Renaming] Handle same last name no namespace just renamed auto import

--- tests/Issues/AutoImport/config/configured_rule.php
+++ tests/Issues/AutoImport/config/configured_rule.php
@@ -3,7 +3,11 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Renaming\Rector\Name\RenameClassRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->importNames();
+    $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
+        'Some\Exception' => 'Some\Target\Exception',
+    ]);
 };
