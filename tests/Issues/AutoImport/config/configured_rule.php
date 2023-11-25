cc9b4dae0aa373331a101958f576e18d212ad54a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-11-25 17:45:57 +0700

    [AutoImport] Apply @\ auto import on AnnotationToAttributeRector

--- tests/Issues/AutoImport/config/configured_rule.php
+++ tests/Issues/AutoImport/config/configured_rule.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Php70\Rector\Ternary\TernaryToNullCoalescingRector;
+use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
+use Rector\Php80\ValueObject\AnnotationToAttribute;
 use Rector\Renaming\Rector\Name\RenameClassRector;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -12,4 +14,7 @@ return static function (RectorConfig $rectorConfig): void {
         'Some\Exception' => 'Some\Target\Exception',
     ]);
     $rectorConfig->rule(TernaryToNullCoalescingRector::class);
+    $rectorConfig->ruleWithConfiguration(AnnotationToAttributeRector::class, [
+        new AnnotationToAttribute('Doctrine\ORM\Mapping\Entity')
+    ]);
 };
