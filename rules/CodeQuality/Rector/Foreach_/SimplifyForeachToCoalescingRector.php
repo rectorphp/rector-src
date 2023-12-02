5c8fa12ec90e4d4b55fd05f48509c538c243482d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-12-02 12:03:12 +0700

    [Doc] Typo fix on sample code documentation on SimplifyForeachToCoalescingRector (#5308)

--- rules/CodeQuality/Rector/Foreach_/SimplifyForeachToCoalescingRector.php
+++ rules/CodeQuality/Rector/Foreach_/SimplifyForeachToCoalescingRector.php
@@ -39,15 +39,15 @@ final class SimplifyForeachToCoalescingRector extends AbstractRector implements 
                     <<<'CODE_SAMPLE'
 foreach ($this->oldToNewFunctions as $oldFunction => $newFunction) {
     if ($currentFunction === $oldFunction) {
-        innerForeachReturn $newFunction;
+        return $newFunction;
     }
 }
 
-innerForeachReturn null;
+return null;
 CODE_SAMPLE
                     ,
                     <<<'CODE_SAMPLE'
-innerForeachReturn $this->oldToNewFunctions[$currentFunction] ?? null;
+return $this->oldToNewFunctions[$currentFunction] ?? null;
 CODE_SAMPLE
                 ),
             ]
