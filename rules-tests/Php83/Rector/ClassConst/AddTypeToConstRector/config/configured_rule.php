1de04c3c31805510ee17bf98b721f010f9b12a36
GitHub Action <actions@github.com>
2023-12-03 15:17:19 +0000

    [ci-review] Rector Rectify

--- rules-tests/Php83/Rector/ClassConst/AddTypeToConstRector/config/configured_rule.php
+++ rules-tests/Php83/Rector/ClassConst/AddTypeToConstRector/config/configured_rule.php
@@ -1,12 +1,13 @@
 <?php
 
 declare(strict_types=1);
+use Rector\Php83\Rector\ClassConst\AddTypeToConstRector;
 
 use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rule(\Rector\Php83\Rector\ClassConst\AddTypeToConstRector::class);
+    $rectorConfig->rule(AddTypeToConstRector::class);
 
     $rectorConfig->phpVersion(PhpVersion::PHP_83);
 };
