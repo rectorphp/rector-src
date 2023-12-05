962e84b028c75b9b6b498ece3d15070deaa09c35
Peter Fox <peter.fox@peterfox.me>
2023-12-05 12:06:43 +0000

    Fix const from applying incorrect types

--- rules/Php83/Rector/ClassConst/AddTypeToConstRector.php
+++ rules/Php83/Rector/ClassConst/AddTypeToConstRector.php
@@ -90,6 +90,8 @@ CODE_SAMPLE
         $changes = false;
 
         foreach ($consts as $const) {
+            $valueType = null;
+
             // If a type is set, skip
             if ($const->type !== null) {
                 continue;
