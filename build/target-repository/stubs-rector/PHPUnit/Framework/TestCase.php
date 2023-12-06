78ae75825a76cb71ab03db5cf58e6ed96b722314
Tomas Votruba <tomas.vot@gmail.com>
2023-12-06 23:56:39 +0100

    add mock object test fixture

--- build/target-repository/stubs-rector/PHPUnit/Framework/TestCase.php
+++ build/target-repository/stubs-rector/PHPUnit/Framework/TestCase.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace PHPUnit\Framework;
 
+use PHPUnit\Framework\MockObject\MockObject;
+
 if (! class_exists('PHPUnit\Framework\TestCase')) {
     abstract class TestCase
     {
+        protected function createMock(string $originalClassName): MockObject
+        {
+        }
     }
 }
