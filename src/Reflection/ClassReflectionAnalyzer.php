aa52c4ecedb83773b12c4d3b2906ba36601057ba
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-12-17 13:05:17 +0700

    [Reflection] Remove usage on PrivatesAccessor on ClassReflectionAnalyzer (#5366)

--- src/Reflection/ClassReflectionAnalyzer.php
+++ src/Reflection/ClassReflectionAnalyzer.php
@@ -4,18 +4,11 @@ declare(strict_types=1);
 
 namespace Rector\Core\Reflection;
 
-use PHPStan\BetterReflection\Reflection\ReflectionClass;
 use PHPStan\Reflection\ClassReflection;
-use Rector\Core\Util\Reflection\PrivatesAccessor;
 use ReflectionEnum;
 
 final class ClassReflectionAnalyzer
 {
-    public function __construct(
-        private readonly PrivatesAccessor $privatesAccessor
-    ) {
-    }
-
     public function resolveParentClassName(ClassReflection $classReflection): ?string
     {
         $nativeReflection = $classReflection->getNativeReflection();
@@ -23,11 +16,6 @@ final class ClassReflectionAnalyzer
             return null;
         }
 
-        $betterReflectionClass = $this->privatesAccessor->getPrivateProperty(
-            $nativeReflection,
-            'betterReflectionClass'
-        );
-        /** @var ReflectionClass $betterReflectionClass */
-        return $betterReflectionClass->getParentClassName();
+        return $nativeReflection->getParentClassName();
     }
 }
