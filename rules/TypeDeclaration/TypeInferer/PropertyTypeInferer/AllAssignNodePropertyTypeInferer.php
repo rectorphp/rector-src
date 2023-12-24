c6ca4f1f5ccdf73d9aa0f8f441267725e07aee67
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-12-24 10:09:23 +0700

    [Performance][TypeDeclaration] Avoid using AstResolver on class is on same file on AllAssignNodePropertyTypeInferer (#5387)
    * [TypeDeclaration] Avoid using AstResolver on same class is on same file on AllAssignNodePropertyTypeInferer
    
    * clean up

--- rules/TypeDeclaration/TypeInferer/PropertyTypeInferer/AllAssignNodePropertyTypeInferer.php
+++ rules/TypeDeclaration/TypeInferer/PropertyTypeInferer/AllAssignNodePropertyTypeInferer.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace Rector\TypeDeclaration\TypeInferer\PropertyTypeInferer;
 
+use PhpParser\Node;
 use PhpParser\Node\Stmt\ClassLike;
 use PhpParser\Node\Stmt\Property;
 use PHPStan\Reflection\ClassReflection;
 use PHPStan\Type\Type;
 use Rector\Core\PhpParser\AstResolver;
+use Rector\Core\PhpParser\Node\BetterNodeFinder;
+use Rector\Core\ValueObject\Application\File;
 use Rector\NodeNameResolver\NodeNameResolver;
 use Rector\TypeDeclaration\TypeInferer\AssignToPropertyTypeInferer;
 
@@ -17,14 +20,27 @@ final class AllAssignNodePropertyTypeInferer
     public function __construct(
         private readonly AssignToPropertyTypeInferer $assignToPropertyTypeInferer,
         private readonly NodeNameResolver $nodeNameResolver,
-        private readonly AstResolver $astResolver
+        private readonly AstResolver $astResolver,
+        private readonly BetterNodeFinder $betterNodeFinder
     ) {
     }
 
-    public function inferProperty(Property $property, ClassReflection $classReflection): ?Type
+    public function inferProperty(Property $property, ClassReflection $classReflection, File $file): ?Type
     {
-        /** @var ClassLike $classLike */
-        $classLike = $this->astResolver->resolveClassFromClassReflection($classReflection);
+        if ($classReflection->getFileName() === $file->getFilePath()) {
+            $className = $classReflection->getName();
+            $classLike = $this->betterNodeFinder->findFirst(
+                $file->getNewStmts(),
+                fn (Node $node): bool => $node instanceof ClassLike && $this->nodeNameResolver->isName($node, $className)
+            );
+        } else {
+            $classLike = $this->astResolver->resolveClassFromClassReflection($classReflection);
+        }
+
+        if (! $classLike instanceof ClassLike) {
+            return null;
+        }
+
         $propertyName = $this->nodeNameResolver->getName($property);
 
         return $this->assignToPropertyTypeInferer->inferPropertyInClassLike($property, $propertyName, $classLike);
