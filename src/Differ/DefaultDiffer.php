8b1b8b79b1cdf56bf28594b600722a89a65caa42
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-12-26 00:10:55 +0700

    update new line verify on DefaultDiffer

--- src/Differ/DefaultDiffer.php
+++ src/Differ/DefaultDiffer.php
@@ -10,6 +10,7 @@ use SebastianBergmann\Diff\Output\StrictUnifiedDiffOutputBuilder;
 final class DefaultDiffer
 {
     private readonly Differ $differ;
+    private readonly bool $isWindows;
 
     public function __construct()
     {
@@ -18,12 +19,18 @@ final class DefaultDiffer
             'toFile' => 'New',
         ]);
         $this->differ = new Differ($strictUnifiedDiffOutputBuilder);
+        $this->isWindows = strncasecmp(PHP_OS, 'WIN', 3) === 0;
     }
 
     public function diff(string $old, string $new): string
     {
         if ($old === $new) {
             return '';
+        }
+
+        if ($this->isWindows) {
+            $old = str_replace("\r\n", "\n", $old);
+            $new = str_replace("\r\n", "\n", $new);
         }
 
         return $this->differ->diff($old, $new);
