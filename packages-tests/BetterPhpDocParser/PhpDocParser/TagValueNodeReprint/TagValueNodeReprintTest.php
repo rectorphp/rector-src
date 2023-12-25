e0a1a6269e14f575ec5a56f50995ebbd16b69f92
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-12-25 23:38:03 +0700

    step 21: use DIRECTORY_SEPARATOR on TagValueNodeReprintTest

--- packages-tests/BetterPhpDocParser/PhpDocParser/TagValueNodeReprint/TagValueNodeReprintTest.php
+++ packages-tests/BetterPhpDocParser/PhpDocParser/TagValueNodeReprint/TagValueNodeReprintTest.php
@@ -133,6 +133,11 @@ final class TagValueNodeReprintTest extends AbstractLazyTestCase
         $hasByAnnotationClass = $phpDocInfo->hasByAnnotationClass($annotationClass);
 
         $relativeFilePath = $this->filePathHelper->relativePath($filePath);
+
+        if (strncasecmp(PHP_OS, 'WIN', 3) === 0) {
+            $relativeFilePath = str_replace('/', DIRECTORY_SEPARATOR, $relativeFilePath);
+        }
+
         $this->assertTrue($hasByAnnotationClass, $relativeFilePath);
     }
 }
