9b90100340f573bed0f9a475f8250f676fbd64f2
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-12-28 00:59:11 +0700

    register

--- config/set/php83.php
+++ config/set/php83.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Php83\Rector\ClassConst\AddTypeToConstRector;
 use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use Rector\Php83\Rector\FuncCall\CombineHostPortLdapUriRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rules([AddOverrideAttributeToOverriddenMethodsRector::class, AddTypeToConstRector::class]);
+    $rectorConfig->rules([
+        AddOverrideAttributeToOverriddenMethodsRector::class,
+        AddTypeToConstRector::class,
+        CombineHostPortLdapUriRector::class,
+    ]);
 };
