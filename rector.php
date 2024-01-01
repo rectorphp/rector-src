14dfcb52341e3a3f2293cb5e5258fa82f8c59cce
Tomas Votruba <tomas.vot@gmail.com>
2024-01-01 19:14:52 +0000

    bump min dependency to PHP 8.2

--- rector.php
+++ rector.php
@@ -18,7 +18,7 @@ use Rector\Utils\Rector\MoveAbstractRectorToChildrenRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
+        LevelSetList::UP_TO_PHP_82,
         SetList::CODE_QUALITY,
         SetList::DEAD_CODE,
         SetList::PRIVATIZATION,
