423794880f62959533de59efdac04f660f9c7d87
Tomas Votruba <tomas.vot@gmail.com>
2024-01-02 02:09:00 +0000

    add Collector stub

---
+++ stubs/PHPStan/Collectors/Collector.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPStan\Collectors;
+
+use PHPStan\Analyser\Scope;
+
+if (class_exists('PHPStan\Collectors\Collector', false)) {
+    return;
+}
+
+/**
+ * This is the interface custom collectors implement. To register it in the configuration file
+ * use the `phpstan.collector` service tag:
+ *
+ * ```
+ * services:
+ * 	-
+ *		class: App\MyCollector
+ *		tags:
+ *			- phpstan.collector
+ * ```
+ *
+ * Learn more: https://phpstan.org/developing-extensions/collectors
+ *
+ * @api
+ * @phpstan-template-covariant TNodeType of Node
+ * @phpstan-template-covariant TValue
+ */
+interface Collector
+{
+    /**
+     * @phpstan-return class-string<TNodeType>
+     */
+
+    public function getNodeType() : string;
+    /**
+     * @phpstan-param TNodeType $node
+     * @return TValue|null Collected data
+     */
+    public function processNode(Node $node, Scope $scope);
+}
