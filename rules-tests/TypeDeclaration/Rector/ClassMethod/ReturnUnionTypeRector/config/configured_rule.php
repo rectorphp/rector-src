9df65ebbacddad29b6a3e7e0259e852c5c302fc8
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-01-03 12:15:27 +0700

    fix namespace

--- rules-tests/TypeDeclaration/Rector/ClassMethod/ReturnUnionTypeRector/config/configured_rule.php
+++ rules-tests/TypeDeclaration/Rector/ClassMethod/ReturnUnionTypeRector/config/configured_rule.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Core\ValueObject\PhpVersionFeature;
+use Rector\ValueObject\PhpVersionFeature;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnUnionTypeRector;
 
 return static function (RectorConfig $rectorConfig): void {
