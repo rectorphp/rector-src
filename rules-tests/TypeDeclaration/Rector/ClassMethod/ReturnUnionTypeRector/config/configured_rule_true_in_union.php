1358c451a185e2c14678f8ac5f00e783841c778d
Tomas Votruba <tomas.vot@gmail.com>
2024-01-03 11:25:50 +0000

    cs

--- rules-tests/TypeDeclaration/Rector/ClassMethod/ReturnUnionTypeRector/config/configured_rule_true_in_union.php
+++ rules-tests/TypeDeclaration/Rector/ClassMethod/ReturnUnionTypeRector/config/configured_rule_true_in_union.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\ValueObject\PhpVersionFeature;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnUnionTypeRector;
+use Rector\ValueObject\PhpVersionFeature;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(ReturnUnionTypeRector::class);
