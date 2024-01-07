bd7d15896ad2c69f0673d805e38e6435bcd5bd2c
Tomas Votruba <tomas.vot@gmail.com>
2024-01-07 22:22:58 +0100

    [cs] Make use of new ECS config

--- ecs.php
+++ ecs.php
@@ -2,16 +2,11 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\ClassNotation\SelfAccessorFixer;
-use PhpCsFixer\Fixer\FunctionNotation\FunctionTypehintSpaceFixer;
-use PhpCsFixer\Fixer\Phpdoc\PhpdocTypesFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->sets([SetList::SYMPLIFY, SetList::COMMON, SetList::CLEAN_CODE, SetList::PSR_12]);
-
-    $ecsConfig->paths([
+return ECSConfig::configure()
+    ->withPreparedSets(symplify: true, common: true, psr12: true)
+    ->withPaths([
         __DIR__ . '/bin',
         __DIR__ . '/src',
         __DIR__ . '/rules',
@@ -19,25 +14,22 @@ return static function (ECSConfig $ecsConfig): void {
         __DIR__ . '/tests',
         __DIR__ . '/utils',
         __DIR__ . '/config',
-        __DIR__ . '/ecs.php',
-        __DIR__ . '/rector.php',
         __DIR__ . '/build/build-preload.php',
-    ]);
+    ])
+    ->withRootFiles();
 
-    $ecsConfig->rules([FunctionTypehintSpaceFixer::class]);
-
-    $ecsConfig->skip([
-        '*/Source/*',
-        '*/Fixture/*',
-        '*/Expected/*',
-
-        PhpdocTypesFixer::class => [
-            // double to Double false positive
-            __DIR__ . '/rules/Php74/Rector/Double/RealToFloatTypeCastRector.php',
-            // skip for enum types
-            __DIR__ . '/rules/Php70/Rector/MethodCall/ThisCallOnStaticMethodToStaticCallRector.php',
-        ],
-
-        SelfAccessorFixer::class => ['*/*Rector.php'],
-    ]);
-};
+//
+//    $ecsConfig->skip([
+//        '*/Source/*',
+//        '*/Fixture/*',
+//        '*/Expected/*',
+//
+//        PhpdocTypesFixer::class => [
+//            // double to Double false positive
+//            __DIR__ . '/rules/Php74/Rector/Double/RealToFloatTypeCastRector.php',
+//            // skip for enum types
+//            __DIR__ . '/rules/Php70/Rector/MethodCall/ThisCallOnStaticMethodToStaticCallRector.php',
+//        ],
+//
+//        SelfAccessorFixer::class => ['*/*Rector.php'],
+//    ]);
