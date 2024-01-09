30c1e374bd7de0704a5cc6ce3f97c555c0977087
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-01-09 14:50:35 +0700

    [Php80] Handle NestedAnnotationToAttributeRector with empty new line

---
+++ tests/Issues/EmptyLineNestedAnnotationToAttribute/config/configured_rule.php
@@ -0,0 +1,10 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\Doctrine\Set\DoctrineSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->sets([
+        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
+    ]);
+};
