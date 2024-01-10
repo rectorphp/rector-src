19c86a6d646d614ce574be01e9925408c466e502
Tomas Votruba <tomas.vot@gmail.com>
2024-01-10 22:08:19 +0100

    apply cs

--- tests/Issues/EmptyLineNestedAnnotationToAttribute/config/configured_rule.php
+++ tests/Issues/EmptyLineNestedAnnotationToAttribute/config/configured_rule.php
@@ -6,7 +6,5 @@ use Rector\Config\RectorConfig;
 use Rector\Doctrine\Set\DoctrineSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->sets([
-        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
-    ]);
+    $rectorConfig->sets([DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES]);
 };
