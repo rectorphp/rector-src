add643650a5ae9aac5152aadee2e988c56488a69
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-01-25 20:20:17 +0700

    cs fix

--- rector.php
+++ rector.php
@@ -1,8 +1,8 @@
 <?php
 
 declare(strict_types=1);
-use Rector\CodeQuality\Rector\FuncCall\SimplifyRegexPatternRector;
 
+use Rector\CodeQuality\Rector\FuncCall\SimplifyRegexPatternRector;
 use Rector\CodingStyle\Rector\String_\UseClassKeywordForClassNameResolutionRector;
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\ConstFetch\RemovePhpVersionIdCheckRector;
