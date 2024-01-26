e93c52d99d6946730e4463c43eeb08d4af5118b9
GitHub Action <actions@github.com>
2024-01-26 15:03:55 +0000

    [ci-review] Rector Rectify

--- src/Differ/DefaultDiffer.php
+++ src/Differ/DefaultDiffer.php
@@ -10,6 +10,7 @@ use SebastianBergmann\Diff\Output\StrictUnifiedDiffOutputBuilder;
 final readonly class DefaultDiffer
 {
     private Differ $differ;
+
     private readonly bool $isWindows;
 
     public function __construct()
