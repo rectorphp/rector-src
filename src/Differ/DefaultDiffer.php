835973ae7c1c3e0998c281643187543745d6e182
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-01-26 22:02:30 +0700

    normalize use \n for ending on DefaultDiffer

--- src/Differ/DefaultDiffer.php
+++ src/Differ/DefaultDiffer.php
@@ -10,6 +10,7 @@ use SebastianBergmann\Diff\Output\StrictUnifiedDiffOutputBuilder;
 final readonly class DefaultDiffer
 {
     private Differ $differ;
+    private readonly bool $isWindows;
 
     public function __construct()
     {
@@ -18,12 +19,19 @@ final readonly class DefaultDiffer
             'toFile' => 'New',
         ]);
         $this->differ = new Differ($strictUnifiedDiffOutputBuilder);
+        $this->isWindows = strncasecmp(PHP_OS, 'WIN', 3) === 0;
     }
 
     public function diff(string $old, string $new): string
     {
         if ($old === $new) {
             return '';
+        }
+
+        // avoid Strings contain different line endings warning
+        if ($this->isWindows) {
+            $old = str_replace("\r\n", "\n", $old);
+            $new = str_replace("\r\n", "\n", $new);
         }
 
         return $this->differ->diff($old, $new);
