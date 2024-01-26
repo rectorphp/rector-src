9a4add0b0eea66854c71081037d9c905034f0eb2
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-01-26 21:06:01 +0700

    normalize file paths

---
+++ src/Skipper/FileSystem/PathNormalizer.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Skipper\FileSystem;
+
+final class PathNormalizer
+{
+    public static function normalize(string $path): string
+    {
+        return str_replace('\\', '/', $path);
+    }
+}
