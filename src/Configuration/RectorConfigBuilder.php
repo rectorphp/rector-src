77b3829444f7f97f73ae60d88af6e35c4afc164c
Tomas Votruba <tomas.vot@gmail.com>
2024-01-29 21:40:24 +0100

    Fix configured rule

--- src/Configuration/RectorConfigBuilder.php
+++ src/Configuration/RectorConfigBuilder.php
@@ -6,6 +6,7 @@ namespace Rector\Configuration;
 
 use Rector\Caching\Contract\ValueObject\Storage\CacheStorageInterface;
 use Rector\Config\RectorConfig;
+use Rector\Contract\Rector\ConfigurableRectorInterface;
 use Rector\Contract\Rector\RectorInterface;
 use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\PHPUnit\Set\PHPUnitSetList;
@@ -43,9 +44,9 @@ final class RectorConfigBuilder
     private array $rules = [];
 
     /**
-     * @var array<class-string<RectorInterface>, mixed[]>
+     * @var array<class-string<ConfigurableRectorInterface>, mixed[]>
      */
-    private array $rulesWithConfiguration = [];
+    private array $rulesWithConfigurations = [];
 
     /**
      * @var string[]
@@ -115,8 +116,10 @@ final class RectorConfigBuilder
         $rectorConfig->skip($this->skip);
         $rectorConfig->rules($this->rules);
 
-        foreach ($this->rulesWithConfiguration as $ruleWithConfiguration) {
-            $rectorConfig->ruleWithConfiguration($ruleWithConfiguration[0], $ruleWithConfiguration[1]);
+        foreach ($this->rulesWithConfigurations as $rectorClass => $configurations) {
+            foreach ($configurations as $configuration) {
+                $rectorConfig->ruleWithConfiguration($rectorClass, $configuration);
+            }
         }
 
         if ($this->fileExtensions !== []) {
@@ -365,12 +368,12 @@ final class RectorConfigBuilder
     }
 
     /**
-     * @param class-string<(RectorInterface)> $rectorClass
+     * @param class-string<ConfigurableRectorInterface> $rectorClass
      * @param mixed[] $configuration
      */
     public function withConfiguredRule(string $rectorClass, array $configuration): self
     {
-        $this->rulesWithConfiguration[$rectorClass] = $configuration;
+        $this->rulesWithConfigurations[$rectorClass][] = $configuration;
 
         return $this;
     }
