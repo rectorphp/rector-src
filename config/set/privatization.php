5979c0932fd77f40f601ea37687c466833e83ed7
Tomas Votruba <tomas.vot@gmail.com>
2024-01-30 14:08:30 +0100

    Exclude finalize rule from private set, to keep safer (#5523)

--- config/set/privatization.php
+++ config/set/privatization.php
@@ -3,14 +3,12 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
 use Rector\Privatization\Rector\ClassMethod\PrivatizeFinalClassMethodRector;
 use Rector\Privatization\Rector\MethodCall\PrivatizeLocalGetterToPropertyRector;
 use Rector\Privatization\Rector\Property\PrivatizeFinalClassPropertyRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rules([
-        FinalizeClassesWithoutChildrenRector::class,
         PrivatizeLocalGetterToPropertyRector::class,
         PrivatizeFinalClassPropertyRector::class,
         PrivatizeFinalClassMethodRector::class,
