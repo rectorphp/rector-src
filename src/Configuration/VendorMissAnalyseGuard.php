5412bfebdd2df905f1513ce568ab1fb387e8f304
Tomas Votruba <tomas.vot@gmail.com>
2024-01-30 21:22:45 +0100

    [DX] Skip vendor warning in case of downgrade

---
+++ src/Configuration/VendorMissAnalyseGuard.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Rector\Configuration;
+
+use Rector\Configuration\Parameter\SimpleParameterProvider;
+use Rector\Skipper\FileSystem\PathNormalizer;
+
+final class VendorMissAnalyseGuard
+{
+    /**
+     * @param string[] $filePaths
+     */
+    public function isVendorAnalyzed(array $filePaths): bool
+    {
+        if ($this->hasDowngradeSets()) {
+            return false;
+        }
+
+        return $this->containsVendorPath($filePaths);
+    }
+
+    private function hasDowngradeSets(): bool
+    {
+        $registeredRectorSets = SimpleParameterProvider::provideArrayParameter(Option::REGISTERED_RECTOR_SETS);
+
+        foreach ($registeredRectorSets as $registeredRectorSet) {
+            if (str_contains($registeredRectorSet, 'downgrade-')) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param string[] $filePaths
+     */
+    private function containsVendorPath(array $filePaths): bool
+    {
+        foreach ($filePaths as $filePath) {
+            if (str_contains(PathNormalizer::normalize($filePath), '/vendor/')) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
