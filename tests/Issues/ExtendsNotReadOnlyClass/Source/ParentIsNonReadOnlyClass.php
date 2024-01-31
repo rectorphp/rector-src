15fdbc249a816b78492fcda529a8b616300d7f7c
Tomas Votruba <tomas.vot@gmail.com>
2024-01-31 22:35:35 +0100

    tidy up

--- tests/Issues/ExtendsNotReadOnlyClass/Source/ParentIsNonReadOnlyClass.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Rector\Tests\Issues\ExtendsNotReadOnlyClass\Source;
-
-class ParentIsNonReadOnlyClass
-{
-}
--- tests/Issues/IssueTraitUseKey/Source/TraitA.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Rector\Tests\Issues\IssueTraitUseKey\Source;
-
-trait TraitA
-{
-}
