fa75fcf0d1445f90cff9199875aba352671795f9
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-02-02 06:52:25 +0700

    [Performance][PhpVersionResolver] Cache resolve platform/require from composer.json on ProjectComposerJsonPhpVersionResolver (#5542)
    * [PhpVersionResolver] Cache resolve platform/require from composer.json on ProjectComposerJsonPhpVersionResolver
    
    * show
    
    * fix phpstan

--- tests/Php/PhpVersionResolver/ProjectComposerJsonPhpVersionResolver/ProjectComposerJsonPhpVersionResolverTest.php
+++ tests/Php/PhpVersionResolver/ProjectComposerJsonPhpVersionResolver/ProjectComposerJsonPhpVersionResolverTest.php
@@ -12,18 +12,19 @@ use Rector\Testing\PHPUnit\AbstractLazyTestCase;
 final class ProjectComposerJsonPhpVersionResolverTest extends AbstractLazyTestCase
 {
     #[DataProvider('provideData')]
-    public function test(string $composerJsonFilePath, int $expectedPhpVersion): void
+    public function test(string $composerJsonFilePath, int|null $expectedPhpVersion): void
     {
         $resolvePhpVersion = ProjectComposerJsonPhpVersionResolver::resolve($composerJsonFilePath);
         $this->assertSame($expectedPhpVersion, $resolvePhpVersion);
     }
 
     /**
-     * @return Iterator<array<string|int>>
+     * @return Iterator<array<string|int|null>>
      */
     public static function provideData(): Iterator
     {
         yield [__DIR__ . '/Fixture/some_composer.json', 70300];
         yield [__DIR__ . '/Fixture/some_composer_with_platform.json', 70400];
+        yield [__DIR__ . '/Fixture/no_php_definition_composer_json.json', null];
     }
 }
