02e743fc532a6070e38a1e61e5a4f02a707c3188
Tomas Votruba <tomas.vot@gmail.com>
2024-02-07 15:19:06 +0100

    misc

--- src/CustomRules/SimpleNodeDumper.php
+++ src/CustomRules/SimpleNodeDumper.php
@@ -6,6 +6,10 @@ namespace Rector\CustomRules;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\Include_;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Name;
+use PhpParser\Node\Scalar;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\GroupUse;
 use PhpParser\Node\Stmt\Use_;
@@ -27,88 +31,36 @@ final class SimpleNodeDumper
         }
 
         if ($node instanceof Node) {
-            $result = get_class($node);
+            return self::dumpSingleNode($node);
+        }
 
-            if ($node instanceof Node\Expr\Variable && is_string($node->name)) {
-                $result .= '( name: "' . $node->name . '" )';
-            } elseif ($node instanceof Node\Identifier) {
-                $result .= '( name: "' . $node->name . '" )';
-            } elseif ($node instanceof Node\Name) {
-                $result .= '( parts: ' . json_encode($node->parts) . ' )';
-            }
+        if (self::isStringList($node)) {
+            return json_encode($node, JSON_THROW_ON_ERROR);
+        }
 
-            // simple nodes
-            elseif ($node instanceof Node\Scalar && $node->getSubNodeNames() === ['value']) {
-                if (is_string($node->value)) {
-                    $result .= '( value: "' . $node->value . '" )';
-                } else {
-                    $result .= '( value: ' . $node->value . ' )';
-                }
+        $result = '[';
+        foreach ($node as $key => $value) {
+            $result .= "\n    " . $key . ': ';
+
+            if ($value === null) {
+                $result .= 'null';
+            } elseif ($value === false) {
+                $result .= 'false';
+            } elseif ($value === true) {
+                $result .= 'true';
+            } elseif (is_string($value)) {
+                $result .= '"' . $value . '"';
+            } elseif (is_scalar($value)) {
+                $result .= $value;
             } else {
-                $result .= '(';
-
-                foreach ($node->getSubNodeNames() as $key) {
-                    $result .= "\n    " . $key . ': ';
-
-                    $value = $node->{$key};
-                    if ($value === null) {
-                        $result .= 'null';
-                    } elseif ($value === false) {
-                        $result .= 'false';
-                    } elseif ($value === true) {
-                        $result .= 'true';
-                    } elseif (is_scalar($value)) {
-                        if ($key === 'flags' || $key === 'newModifier') {
-                            $result .= self::dumpFlags($value);
-                        } elseif ($key === 'type' && $node instanceof Include_) {
-                            $result .= self::dumpIncludeType($value);
-                        } elseif ($key === 'type'
-                            && ($node instanceof Use_ || $node instanceof UseUse || $node instanceof GroupUse)) {
-                            $result .= self::dumpUseType($value);
-                        } elseif (is_string($value)) {
-                            $result .= '"' . $value . '"';
-                        } else {
-                            $result .= $value;
-                        }
-                    } else {
-                        $result .= str_replace("\n", "\n    ", self::dump($value, false));
-                    }
-                }
-
-                $result .= "\n)";
+                $result .= str_replace("\n", "\n    ", self::dump($value, false));
             }
+        }
+
+        if (count($node) === 0) {
+            $result .= ']';
         } else {
-            if (self::isStringList($node)) {
-                $result = json_encode($node);
-            } else {
-                $result = '[';
-
-                $valuesCount = count($node);
-
-                foreach ($node as $key => $value) {
-                    $result .= "\n    " . $key . ': ';
-
-                    if ($value === null) {
-                        $result .= 'null';
-                    } elseif ($value === false) {
-                        $result .= 'false';
-                    } elseif ($value === true) {
-                        $result .= 'true';
-                    } elseif (is_string($value)) {
-                        $result .= '"' . $value . '"';
-                    } elseif (is_scalar($value)) {
-                        $result .= $value;
-                    } else {
-                        $result .= str_replace("\n", "\n    ", self::dump($value, false));
-                    }
-                }
-
-                if (count($node) === 0) {
-                    $result .= ']';
-                } else {
-                    $result .= "\n]";
-                }
-            }
+            $result .= "\n]";
         }
 
         return $result;
@@ -117,7 +69,7 @@ final class SimpleNodeDumper
     /**
      * @param mixed[] $items
      */
-    public static function isStringList(array $items): bool
+    private static function isStringList(array $items): bool
     {
         foreach ($items as $value) {
             if (! is_string($value)) {
@@ -190,5 +142,59 @@ final class SimpleNodeDumper
         }
 
         return $map[$type] . ' (' . $type . ')';
+    }
+
+    private static function dumpSingleNode(Node $node): string
+    {
+        $result = get_class($node);
+
+        // print simple nodes on same line, to make output more readable
+        if ($node instanceof Variable && is_string($node->name)) {
+            $result .= '( name: "' . $node->name . '" )';
+        } elseif ($node instanceof Identifier) {
+            $result .= '( name: "' . $node->name . '" )';
+        } elseif ($node instanceof Name) {
+            $result .= '( parts: ' . json_encode($node->getParts(), JSON_THROW_ON_ERROR) . ' )';
+        } elseif ($node instanceof Scalar && $node->getSubNodeNames() === ['value']) {
+            if (is_string($node->value)) {
+                $result .= '( value: "' . $node->value . '" )';
+            } else {
+                $result .= '( value: ' . $node->value . ' )';
+            }
+        } else {
+            $result .= '(';
+
+            foreach ($node->getSubNodeNames() as $key) {
+                $result .= "\n    " . $key . ': ';
+
+                $value = $node->{$key};
+                if ($value === null) {
+                    $result .= 'null';
+                } elseif ($value === false) {
+                    $result .= 'false';
+                } elseif ($value === true) {
+                    $result .= 'true';
+                } elseif (is_scalar($value)) {
+                    if ($key === 'flags' || $key === 'newModifier') {
+                        $result .= self::dumpFlags($value);
+                    } elseif ($key === 'type' && $node instanceof Include_) {
+                        $result .= self::dumpIncludeType($value);
+                    } elseif ($key === 'type'
+                        && ($node instanceof Use_ || $node instanceof UseUse || $node instanceof GroupUse)) {
+                        $result .= self::dumpUseType($value);
+                    } elseif (is_string($value)) {
+                        $result .= '"' . $value . '"';
+                    } else {
+                        $result .= $value;
+                    }
+                } else {
+                    $result .= str_replace("\n", "\n    ", self::dump($value, false));
+                }
+            }
+
+            $result .= "\n)";
+        }
+
+        return $result;
     }
 }
