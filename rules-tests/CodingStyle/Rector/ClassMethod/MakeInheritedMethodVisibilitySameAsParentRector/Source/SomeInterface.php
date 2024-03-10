5835ad89f3313a92625dd195d235ff91cb9117f5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-03-11 06:31:35 +0700

    [CodingStyle] Skip implements interface on MakeInheritedMethodVisibilitySameAsParentRector

---
+++ rules-tests/CodingStyle/Rector/ClassMethod/MakeInheritedMethodVisibilitySameAsParentRector/Source/SomeInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\CodingStyle\Rector\ClassMethod\MakeInheritedMethodVisibilitySameAsParentRector\Source;
+
+interface SomeInterface
+{
+    public function run();
+}
