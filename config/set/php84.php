5c3c9410906e40550d492ea02e467492efab979e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-03-15 17:17:36 +0700

    fix phpstan

--- config/set/php84.php
+++ config/set/php84.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Php83\Rector\ClassConst\ExplicitNullableParamTypeRector;
+use Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rules([
