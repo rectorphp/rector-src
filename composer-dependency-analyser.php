fd27084e1cd3fbd890e1cee6963149878073272a
Jan Nedbal <janedbal@gmail.com>
2024-03-28 09:11:23 +0100

    Revert react/promise and nette/robot-loader changes

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -8,6 +8,8 @@ $config = new Configuration();
 return $config
     ->addPathToScan(__DIR__ . '/build/config', false)
     ->ignoreErrorsOnPackage('phpunit/phpunit', [ErrorType::DEV_DEPENDENCY_IN_PROD]) // prepared test tooling
+    ->ignoreErrorsOnPackage('react/promise', [ErrorType::UNUSED_DEPENDENCY]) // pinned v2.x version
+    ->ignoreErrorsOnPackage('nette/robot-loader', [ErrorType::DEV_DEPENDENCY_IN_PROD]) // https://github.com/rectorphp/rector-src/pull/5777#discussion_r1541304301
     ->ignoreErrorsOnPaths([
         __DIR__ . '/stubs',
         __DIR__ . '/tests',
