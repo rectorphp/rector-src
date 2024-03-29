9d1cb3af91ed7b13779116a8802e29a39b40f1ab
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-03-30 00:11:14 +0700

    fix constant

--- rules-tests/Php82/Rector/Encapsed/config/configured_rule.php
+++ rules-tests/Php82/Rector/Encapsed/config/configured_rule.php
@@ -7,7 +7,7 @@ use Rector\Php82\Rector\Encapsed\VariableInStringInterpolationFixerRector;
 use Rector\ValueObject\PhpVersionFeature;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->phpVersion(PhpVersionFeature::DEPRECATE_VARIABLE_IN_EncapsedINTERPOLATION);
+    $rectorConfig->phpVersion(PhpVersionFeature::DEPRECATE_VARIABLE_IN_STRING_INTERPOLATION);
 
     $rectorConfig->rule(VariableInStringInterpolationFixerRector::class);
 };
