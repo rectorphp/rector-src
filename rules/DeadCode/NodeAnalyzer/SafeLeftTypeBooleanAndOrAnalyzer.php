7d69fc226cab739ec6c47b09bfdd784a24445fe1
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-04-12 16:17:16 +0700

    [DeadCode] Skip $this instanceof in Trait on RemoveAlwaysTrueIfConditionRector (#5817)
    * [DeadCode] Skip $this instanceof in Trait on RemoveAlwaysTrueIfConditionRector
    
    * rename trait name

--- rules/DeadCode/NodeAnalyzer/SafeLeftTypeBooleanAndOrAnalyzer.php
+++ rules/DeadCode/NodeAnalyzer/SafeLeftTypeBooleanAndOrAnalyzer.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Rector\DeadCode\NodeAnalyzer;
 
+use PHPStan\Type\ObjectType;
 use PhpParser\Node;
 use PhpParser\Node\Expr\ArrayDimFetch;
 use PhpParser\Node\Expr\BinaryOp\BooleanAnd;
@@ -11,14 +12,19 @@ use PhpParser\Node\Expr\BinaryOp\BooleanOr;
 use PhpParser\Node\Expr\PropertyFetch;
 use PhpParser\Node\Expr\StaticPropertyFetch;
 use PhpParser\Node\Expr\Variable;
+use PHPStan\Reflection\ClassReflection;
 use Rector\NodeAnalyzer\ExprAnalyzer;
+use Rector\NodeTypeResolver\NodeTypeResolver;
 use Rector\PhpParser\Node\BetterNodeFinder;
+use Rector\Reflection\ReflectionResolver;
 
 final readonly class SafeLeftTypeBooleanAndOrAnalyzer
 {
     public function __construct(
         private readonly BetterNodeFinder $betterNodeFinder,
-        private readonly ExprAnalyzer $exprAnalyzer
+        private readonly ExprAnalyzer $exprAnalyzer,
+        private readonly ReflectionResolver $reflectionResolver,
+        private readonly NodeTypeResolver $nodeTypeResolver
     ) {
     }
 
@@ -33,10 +39,25 @@ final readonly class SafeLeftTypeBooleanAndOrAnalyzer
             return false;
         }
 
-        // get type from Property and ArrayDimFetch is unreliable
-        return ! (bool) $this->betterNodeFinder->findFirst(
+        $hasPropertyFetchOrArrayDimFetch = (bool) $this->betterNodeFinder->findFirst(
             $booleanAnd->left,
             static fn (Node $node): bool => $node instanceof PropertyFetch || $node instanceof StaticPropertyFetch || $node instanceof ArrayDimFetch
         );
+
+        // get type from Property and ArrayDimFetch is unreliable
+        if ($hasPropertyFetchOrArrayDimFetch) {
+            return false;
+        }
+
+        // skip trait this
+        $classReflection = $this->reflectionResolver->resolveClassReflection($booleanAnd);
+        if ($classReflection instanceof ClassReflection && $classReflection->isTrait()) {
+            return ! (bool) $this->betterNodeFinder->findFirst(
+                $booleanAnd->left,
+                fn (Node $node): bool => $this->nodeTypeResolver->getType($node) instanceof ObjectType
+            );
+        }
+
+        return true;
     }
 }
