1374e15d742f4f36f0bef45db9f8920a589865b3
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-04-20 17:59:14 +0700

    [CodeQuality] Add Function_ and FuncCall support on OptionalParametersAfterRequiredRector (#5835)
    * [CodeQuality] Add Function_ and FuncCall support on OptionalParametersAfterRequiredRector
    
    * rename method
    
    * clean up
    
    * cs fix
    
    * [ci-review] Rector Rectify
    
    * fix phpstan
    
    * test skip after 2nd try
    
    * skip native func call
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

--- rules/CodingStyle/Reflection/VendorLocationDetector.php
+++ rules/CodingStyle/Reflection/VendorLocationDetector.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Rector\CodingStyle\Reflection;
 
+use PHPStan\Reflection\FunctionReflection;
 use PHPStan\Reflection\MethodReflection;
 use Rector\FileSystem\FilePathHelper;
 
@@ -19,6 +20,18 @@ final readonly class VendorLocationDetector
         $declaringClassReflection = $methodReflection->getDeclaringClass();
         $fileName = $declaringClassReflection->getFileName();
 
+        return $this->detect($fileName);
+    }
+
+    public function detectFunctionReflection(FunctionReflection $functionReflection): bool
+    {
+        $fileName = $functionReflection->getFileName();
+
+        return $this->detect($fileName);
+    }
+
+    private function detect(?string $fileName = null): bool
+    {
         // probably internal
         if ($fileName === null) {
             return false;
