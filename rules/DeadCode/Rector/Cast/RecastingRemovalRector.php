ac40e08f9fcd569a03eff63e367566fc83a22bd4
Markus Staab <markus.staab@redaxo.de>
2024-04-22 11:24:23 +0200

    Ignore phpdoc type in RecastingRemovalRector

--- rules/DeadCode/Rector/Cast/RecastingRemovalRector.php
+++ rules/DeadCode/Rector/Cast/RecastingRemovalRector.php
@@ -96,7 +96,7 @@ CODE_SAMPLE
             return null;
         }
 
-        $nodeType = $this->getType($node->expr);
+        $nodeType = $this->nodeTypeResolver->getNativeType($node->expr);
         if ($nodeType instanceof MixedType) {
             return null;
         }
