c7d2ca3f1f2b9153fb05f0ad974b1d4a403be9c3
Markus Staab <markus.staab@redaxo.de>
2024-05-02 09:36:31 +0200

    Early-return NullSafeMethodCall in NodeNameResolver (#5840)
    * Support NullsafeMethodCall in NodeNameResolver
    
    * Simplify with CallLike
    
    * fix
    
    * doc
    
    * simplify
    
    * simplify

--- src/NodeNameResolver/NodeNameResolver.php
+++ src/NodeNameResolver/NodeNameResolver.php
@@ -6,7 +6,10 @@ namespace Rector\NodeNameResolver;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\CallLike;
+use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\NullsafeMethodCall;
 use PhpParser\Node\Expr\StaticCall;
 use PhpParser\Node\Identifier;
 use PhpParser\Node\Name;
@@ -71,14 +74,6 @@ final class NodeNameResolver
      */
     public function isName(Node | array $node, string $name): bool
     {
-        if ($node instanceof MethodCall) {
-            return false;
-        }
-
-        if ($node instanceof StaticCall) {
-            return false;
-        }
-
         $nodes = is_array($node) ? $node : [$node];
 
         foreach ($nodes as $node) {
@@ -100,11 +95,7 @@ final class NodeNameResolver
             return false;
         }
 
-        if ($node instanceof MethodCall) {
-            return false;
-        }
-
-        if ($node instanceof StaticCall) {
+        if ($node instanceof CallLike && !$node instanceof FuncCall) {
             return false;
         }
 
@@ -143,7 +134,10 @@ final class NodeNameResolver
             return $namespacedName;
         }
 
-        if (($node instanceof MethodCall || $node instanceof StaticCall) && $this->isCallOrIdentifier($node->name)) {
+        if (
+            ($node instanceof MethodCall || $node instanceof StaticCall || $node instanceof NullsafeMethodCall)
+            && $this->isCallOrIdentifier($node->name)
+        ) {
             return null;
         }
 
@@ -242,7 +236,7 @@ final class NodeNameResolver
 
     private function isSingleName(Node $node, string $desiredName): bool
     {
-        if ($node instanceof MethodCall) {
+        if ($node instanceof CallLike && !$node instanceof FuncCall) {
             // method call cannot have a name, only the variable or method name
             return false;
         }
