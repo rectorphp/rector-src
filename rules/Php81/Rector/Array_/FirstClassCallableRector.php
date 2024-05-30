28be793d893b06ae54092bc896d16e89dad728fc
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-05-30 21:06:33 +0700

    [Php81] Skip private method from instantiated class on FirstClassCallableRector

--- rules/Php81/Rector/Array_/FirstClassCallableRector.php
+++ rules/Php81/Rector/Array_/FirstClassCallableRector.php
@@ -21,6 +21,7 @@ use PHPStan\Reflection\ReflectionProvider;
 use Rector\NodeCollector\NodeAnalyzer\ArrayCallableMethodMatcher;
 use Rector\NodeCollector\ValueObject\ArrayCallable;
 use Rector\Rector\AbstractScopeAwareRector;
+use Rector\Reflection\ReflectionResolver;
 use Rector\StaticTypeMapper\ValueObject\Type\FullyQualifiedObjectType;
 use Rector\ValueObject\PhpVersion;
 use Rector\VersionBonding\Contract\MinPhpVersionInterface;
@@ -37,6 +38,7 @@ final class FirstClassCallableRector extends AbstractScopeAwareRector implements
     public function __construct(
         private readonly ArrayCallableMethodMatcher $arrayCallableMethodMatcher,
         private readonly ReflectionProvider $reflectionProvider,
+        private readonly ReflectionResolver $reflectionResolver
     ) {
     }
 
@@ -117,7 +119,18 @@ CODE_SAMPLE
             return new StaticCall($callerExpr->class, $arrayCallable->getMethod(), $args);
         }
 
-        return new MethodCall($callerExpr, $arrayCallable->getMethod(), $args);
+        $methodName = $arrayCallable->getMethod();
+        $methodCall = new MethodCall($callerExpr, $methodName, $args);
+        $classReflection = $this->reflectionResolver->resolveClassReflectionSourceObject($methodCall);
+
+        if ($classReflection instanceof ClassReflection && $classReflection->hasNativeMethod($methodName)) {
+            $method = $classReflection->getNativeMethod($methodName);
+            if (! $method->isPublic()) {
+                return null;
+            }
+        }
+
+        return $methodCall;
     }
 
     public function provideMinPhpVersion(): int
