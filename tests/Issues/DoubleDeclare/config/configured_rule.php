04e9cec47a6f07e4c6ffbcd69fe3ac92fac7faff
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-05-30 18:42:51 +0700

    [TypeDeclaration] Handle double declare(strict_types=1) addition on DeclareStrictTypesRector + IncreaseDeclareStrictTypesRector

---
+++ tests/Issues/DoubleDeclare/config/configured_rule.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\IncreaseDeclareStrictTypesRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rules([
+        DeclareStrictTypesRector::class,
+        IncreaseDeclareStrictTypesRector::class,
+    ]);
+};
