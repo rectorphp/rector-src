6b69971dfc740a58aed918782d02710e42004e9d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-06-01 16:21:53 +0700

    [NodeTypeCorrector] Directly use StringType on Intersection of strings on AccessoryNonEmptyStringTypeCorrector (#5933)
    * [NodeTypeCorrector] Directly use StringType on Intersection of strings on AccessoryNonEmptyStringTypeCorrector
    
    * [ci-review] Rector Rectify
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

--- src/NodeTypeResolver/NodeTypeCorrector/AccessoryNonEmptyStringTypeCorrector.php
+++ src/NodeTypeResolver/NodeTypeCorrector/AccessoryNonEmptyStringTypeCorrector.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace Rector\NodeTypeResolver\NodeTypeCorrector;
 
-use PHPStan\Type\Accessory\AccessoryNonEmptyStringType;
-use PHPStan\Type\Accessory\AccessoryNonFalsyStringType;
+use PHPStan\Type\StringType;
 use PHPStan\Type\IntersectionType;
 use PHPStan\Type\Type;
 
@@ -21,23 +20,6 @@ final class AccessoryNonEmptyStringTypeCorrector
             return $mainType;
         }
 
-        $clearIntersectionedTypes = [];
-        foreach ($mainType->getTypes() as $intersectionedType) {
-            if ($intersectionedType instanceof AccessoryNonEmptyStringType) {
-                continue;
-            }
-
-            if ($intersectionedType instanceof AccessoryNonFalsyStringType) {
-                continue;
-            }
-
-            $clearIntersectionedTypes[] = $intersectionedType;
-        }
-
-        if (count($clearIntersectionedTypes) === 1) {
-            return $clearIntersectionedTypes[0];
-        }
-
-        return new IntersectionType($clearIntersectionedTypes);
+        return new StringType();
     }
 }
