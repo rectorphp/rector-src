3376578a6e4ab5ec68ff56d863da9e36a36ab857
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-06-09 20:03:44 +0700

    [VendorLocker] Move early private method check on ParentClassMethodTypeOverrideGuard to resolveParentClassMethod() for re-use (#5954)
    * [VendorLocker] Move early private method check on ParentClassMethodTypeOverrideGuard to resolveParentClassMethod() for re-use
    
    * null
    
    * fix phpstan

--- src/VendorLocker/ParentClassMethodTypeOverrideGuard.php
+++ src/VendorLocker/ParentClassMethodTypeOverrideGuard.php
@@ -28,11 +28,6 @@ final readonly class ParentClassMethodTypeOverrideGuard
 
     public function hasParentClassMethod(ClassMethod|MethodReflection $classMethod): bool
     {
-        // early got false on private method
-        if ($classMethod->isPrivate()) {
-            return false;
-        }
-
         try {
             $parentClassMethod = $this->resolveParentClassMethod($classMethod);
 
@@ -70,19 +65,24 @@ final readonly class ParentClassMethodTypeOverrideGuard
 
     private function resolveParentClassMethod(ClassMethod|MethodReflection $classMethod): ?MethodReflection
     {
-        if ($classMethod instanceof ClassMethod) {
-            $classReflection = $this->reflectionResolver->resolveClassReflection($classMethod);
-            if (! $classReflection instanceof ClassReflection) {
-                // we can't resolve the class, so we don't know.
-                throw new UnresolvableClassException();
-            }
-
-            /** @var string $methodName */
-            $methodName = $this->nodeNameResolver->getName($classMethod);
-        } else {
-            $classReflection = $classMethod->getDeclaringClass();
-            $methodName = $classMethod->getName();
+        // early got null on private method
+        if ($classMethod->isPrivate()) {
+            return null;
         }
+
+        $classReflection = $classMethod instanceof ClassMethod
+            ? $this->reflectionResolver->resolveClassReflection($classMethod)
+            : $classMethod->getDeclaringClass();
+
+        if (! $classReflection instanceof ClassReflection) {
+            // we can't resolve the class, so we don't know.
+            throw new UnresolvableClassException();
+        }
+
+        /** @var string $methodName */
+        $methodName = $classMethod instanceof ClassMethod
+            ? $this->nodeNameResolver->getName($classMethod)
+            : $classMethod->getName();
 
         $currentClassReflection = $classReflection;
         while ($this->hasClassParent($currentClassReflection)) {
