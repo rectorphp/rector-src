17f522c7eadf382e1028b101eb8a9bb280297f63
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-06-18 16:40:15 +0700

    Fix @see test

--- rules/Php74/Rector/FuncCall/HebrevcToNl2brHebrevRector.php
+++ rules/Php74/Rector/FuncCall/HebrevcToNl2brHebrevRector.php
@@ -16,7 +16,7 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
 /**
  * @changelog https://www.php.net/manual/en/migration74.deprecated.php#migration74.deprecated.core.hebrevc
- * @see \Rector\Tests\Php74\Rector\FuncCall\HebrevcToNl2brHebrevRector\HebrevcToNl2brHebrevRector
+ * @see \Rector\Tests\Php74\Rector\FuncCall\HebrevcToNl2brHebrevRector\HebrevcToNl2brHebrevRectorTest
  */
 final class HebrevcToNl2brHebrevRector extends AbstractRector implements MinPhpVersionInterface
 {
