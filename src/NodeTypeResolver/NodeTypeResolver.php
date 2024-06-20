00dadb99a0c22798c2465254a24b8f9563d2d6f2
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-06-20 22:49:21 +0700

    [NodeTypeResolver] Clean up check ConstFetch on getType() on no scope

--- src/NodeTypeResolver/NodeTypeResolver.php
+++ src/NodeTypeResolver/NodeTypeResolver.php
@@ -9,7 +9,6 @@ use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\ArrayDimFetch;
 use PhpParser\Node\Expr\BinaryOp\Coalesce;
 use PhpParser\Node\Expr\ClassConstFetch;
-use PhpParser\Node\Expr\ConstFetch;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\New_;
 use PhpParser\Node\Expr\NullsafeMethodCall;
@@ -173,13 +172,6 @@ final class NodeTypeResolver
         $scope = $node->getAttribute(AttributeKey::SCOPE);
 
         if (! $scope instanceof Scope) {
-            if ($node instanceof ConstFetch) {
-                $name = $node->name->toString();
-                if (strtolower($name) === 'null') {
-                    return new NullType();
-                }
-            }
-
             return new MixedType();
         }
 
