fb05cf0557a2d8e73386d461aa9fbadd931d1ee1
Tomas Votruba <tomas.vot@gmail.com>
2024-06-23 11:31:10 +0900

    [DX] Add withDowngradeSets() to better target downgrades (#6015)

--- build/config/config-downgrade.php
+++ build/config/config-downgrade.php
@@ -3,14 +3,13 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\DowngradeLevelSetList;
 
 require_once  __DIR__ . '/../target-repository/stubs-rector/PHPUnit/Framework/TestCase.php';
 
 return RectorConfig::configure()
     ->withSkip(DowngradeRectorConfig::DEPENDENCY_EXCLUDE_PATHS)
     ->withPHPStanConfigs([__DIR__ . '/phpstan-for-downgrade.neon'])
-    ->withSets([DowngradeLevelSetList::DOWN_TO_PHP_72]);
+    ->withDowngradeSets(php72: true);
 
 /**
  * Configuration consts for the different rector.php config files
