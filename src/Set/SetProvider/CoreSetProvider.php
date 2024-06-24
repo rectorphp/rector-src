92baed1bf6f173ebce1a0706f6a47ced0ffbeb31
Tomas Votruba <tomas.vot@gmail.com>
2024-06-25 00:36:59 +0900

    add set provider for standardized set access

---
+++ src/Set/SetProvider/CoreSetProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Set\SetProvider;
+
+use Rector\Set\Contract\SetProviderInterface;
+
+final class CoreSetProvider implements SetProviderInterface
+{
+    // TODO: Implement the method
+    public function provide(): array
+    {
+    }
+}
