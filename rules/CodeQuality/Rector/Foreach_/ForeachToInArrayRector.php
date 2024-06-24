c766d0fd8554c60460eb816452f0c955c89035f3
Aleksa Nikolic <50905985+nikolicaleksa@users.noreply.github.com>
2024-06-24 09:59:12 +0200

    fix: Skip refactoring when foreach if statement contains calls to met… (#6019)
    * fix: Skip refactoring when foreach if statement contains calls to methods / functions
    
    * Changes based on MR comment

--- rules/CodeQuality/Rector/Foreach_/ForeachToInArrayRector.php
+++ rules/CodeQuality/Rector/Foreach_/ForeachToInArrayRector.php
@@ -14,6 +14,7 @@ use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Stmt\Foreach_;
 use PhpParser\Node\Stmt\If_;
 use PhpParser\Node\Stmt\Return_;
+use PhpParser\NodeFinder;
 use PHPStan\Type\ObjectType;
 use Rector\Contract\PhpParser\Node\StmtsAwareInterface;
 use Rector\NodeManipulator\BinaryOpManipulator;
@@ -31,6 +32,7 @@ final class ForeachToInArrayRector extends AbstractRector
     public function __construct(
         private readonly BinaryOpManipulator $binaryOpManipulator,
         private readonly ValueResolver $valueResolver,
+        private readonly NodeFinder $nodeFinder
     ) {
     }
 
@@ -106,6 +108,14 @@ CODE_SAMPLE
                 return null;
             }
 
+            $variableNodes = $this->nodeFinder->findInstanceOf($twoNodeMatch->getSecondExpr(), Variable::class);
+
+            foreach ($variableNodes as $variableNode) {
+                if ($this->nodeComparator->areNodesEqual($variableNode, $foreach->valueVar)) {
+                    return null;
+                }
+            }
+
             $comparedExpr = $twoNodeMatch->getSecondExpr();
             if (! $this->isIfBodyABoolReturnNode($firstNodeInsideForeach)) {
                 return null;
@@ -160,7 +170,6 @@ CODE_SAMPLE
         if (! $foreach->stmts[0] instanceof If_) {
             return true;
         }
-
         $foreachValueStaticType = $this->getType($foreach->expr);
         return $foreachValueStaticType instanceof ObjectType;
     }
