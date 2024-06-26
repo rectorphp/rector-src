93a1b812647d7859947d4b92ea84fbf1d9e3d1ca
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-06-26 17:33:55 +0700

    Fix downgrade error: Foreach should be Foreach_

--- rules-tests/CodingStyle/Rector/Foreach_/MultiDimensionalArrayToArrayDestructRector/config/configured_rule.php
+++ rules-tests/CodingStyle/Rector/Foreach_/MultiDimensionalArrayToArrayDestructRector/config/configured_rule.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Rector\CodingStyle\Rector\Foreach\MultiDimensionalArrayToArrayDestructRector;
+use Rector\CodingStyle\Rector\Foreach_\MultiDimensionalArrayToArrayDestructRector;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
