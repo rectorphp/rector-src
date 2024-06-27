985551d97b823b2e8e3948be600f49f4cfb20054
Tomas Votruba <tomas.vot@gmail.com>
2024-06-27 09:32:16 +0200

    add version to tests to trigger the rule

--- rules-tests/DeadCode/Rector/Property/RemoveUselessReadOnlyTagRector/config/configured_rule.php
+++ rules-tests/DeadCode/Rector/Property/RemoveUselessReadOnlyTagRector/config/configured_rule.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\Property\RemoveUselessReadOnlyTagRector;
+use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
-    ->withRules([RemoveUselessReadOnlyTagRector::class]);
+    ->withRules([RemoveUselessReadOnlyTagRector::class])
+    ->withPhpVersion(PhpVersion::PHP_81);
