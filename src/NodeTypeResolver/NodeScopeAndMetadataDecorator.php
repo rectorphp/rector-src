93b8ad7bd59ffc06d714e9b2cd5fccbb45bd022d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-06-28 09:19:56 +0700

    [NodeTypeResolver] Clean up unreachableStatementNode flag flip-flop check on NodeScopeAndMetadataDecorator

--- src/NodeTypeResolver/NodeScopeAndMetadataDecorator.php
+++ src/NodeTypeResolver/NodeScopeAndMetadataDecorator.php
@@ -8,9 +8,7 @@ use PhpParser\Node\Stmt;
 use PhpParser\NodeTraverser;
 use PhpParser\NodeVisitor\CloningVisitor;
 use Rector\NodeTypeResolver\PHPStan\Scope\PHPStanNodeScopeResolver;
-use Rector\NodeTypeResolver\PHPStan\Scope\ScopeFactory;
 use Rector\PhpParser\NodeTraverser\FileWithoutNamespaceNodeTraverser;
-use Rector\PHPStan\NodeVisitor\UnreachableStatementNodeVisitor;
 
 final class NodeScopeAndMetadataDecorator
 {
@@ -19,7 +17,6 @@ final class NodeScopeAndMetadataDecorator
     public function __construct(
         CloningVisitor $cloningVisitor,
         private readonly PHPStanNodeScopeResolver $phpStanNodeScopeResolver,
-        private readonly ScopeFactory $scopeFactory,
         private readonly FileWithoutNamespaceNodeTraverser $fileWithoutNamespaceNodeTraverser
     ) {
         $this->nodeTraverser = new NodeTraverser();
@@ -36,28 +33,6 @@ final class NodeScopeAndMetadataDecorator
     {
         $stmts = $this->fileWithoutNamespaceNodeTraverser->traverse($stmts);
         $stmts = $this->phpStanNodeScopeResolver->processNodes($stmts, $filePath);
-
-        if ($this->phpStanNodeScopeResolver->hasUnreachableStatementNode()) {
-            $unreachableStatementNodeVisitor = new UnreachableStatementNodeVisitor(
-                $this->phpStanNodeScopeResolver,
-                $filePath,
-                $this->scopeFactory
-            );
-            $this->nodeTraverser->addVisitor($unreachableStatementNodeVisitor);
-
-            $stmts = $this->nodeTraverser->traverse($stmts);
-
-            /**
-             * immediate remove UnreachableStatementNodeVisitor after traverse to avoid
-             * re-used in nodeTraverser property in next file
-             */
-            $this->nodeTraverser->removeVisitor($unreachableStatementNodeVisitor);
-
-            // next file must be init hasUnreachableStatementNode to be false again
-            $this->phpStanNodeScopeResolver->resetHasUnreachableStatementNode();
-
-            return $stmts;
-        }
 
         return $this->nodeTraverser->traverse($stmts);
     }
