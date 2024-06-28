30d13c2fa96e43bd42e9fa4992bb81a643525808
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-06-29 00:12:03 +0700

    Fix

--- src/FileSystem/FilesFinder.php
+++ src/FileSystem/FilesFinder.php
@@ -14,12 +14,6 @@ use Symfony\Component\Finder\Finder;
  */
 final readonly class FilesFinder
 {
-    /**
-     * @var string
-     * @see https://regex101.com/r/3NwDLo/1
-     */
-    private const OPEN_SHORTTAG_REGEX = '#^\<\?=#';
-
     public function __construct(
         private FilesystemTweaker $filesystemTweaker,
         private UnchangedFilesFilter $unchangedFilesFilter,
@@ -40,10 +34,10 @@ final readonly class FilesFinder
         $files = $this->fileAndDirectoryFilter->filterFiles($filesAndDirectories);
 
         // exclude short "<?=" tags as lead to invalid changes
-        $files = array_filter($files, static function (string $file): bool {
-            $fileContents = FileSystem::read($file);
-            return ! str_starts_with($fileContents, '<?=');
-        });
+        $files = array_filter(
+            $files,
+            fn (string $file): bool => ! $this->isStartWithShortPHPTag(FileSystem::read($file))
+        );
 
         $filteredFilePaths = array_filter(
             $files,
@@ -65,6 +59,11 @@ final readonly class FilesFinder
         return $this->unchangedFilesFilter->filterFilePaths($filePaths);
     }
 
+    private function isStartWithShortPHPTag(string $fileContent): bool
+    {
+        return str_starts_with(ltrim($fileContent), '<?=');
+    }
+
     /**
      * @param string[] $directories
      * @param string[] $suffixes
@@ -80,8 +79,6 @@ final readonly class FilesFinder
             ->files()
             // skip empty files
             ->size('> 0')
-            // changes in PHP files with short echo tag will mostly create invalid code, "<?php" tags are required
-            ->notContains(self::OPEN_SHORTTAG_REGEX)
             ->in($directories);
 
         if ($sortByName) {
@@ -105,6 +102,10 @@ final readonly class FilesFinder
             }
 
             if ($this->pathSkipper->shouldSkip($path)) {
+                continue;
+            }
+
+            if ($this->isStartWithShortPHPTag($fileInfo->getContents())) {
                 continue;
             }
 
