6057637722e93f340875a39dc49d5dae36ddac64
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-06-28 13:36:32 +0700

    Revert "[Printer] Remove AlwaysRememberedExpr check on BetterStandardPrinter …"
    This reverts commit 0b867444475d26537e488742e437f73305f1a4c1.

--- src/PhpParser/Printer/BetterStandardPrinter.php
+++ src/PhpParser/Printer/BetterStandardPrinter.php
@@ -27,6 +27,7 @@ use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Declare_;
 use PhpParser\Node\Stmt\Nop;
 use PhpParser\PrettyPrinter\Standard;
+use PHPStan\Node\Expr\AlwaysRememberedExpr;
 use Rector\Configuration\Option;
 use Rector\Configuration\Parameter\SimpleParameterProvider;
 use Rector\NodeTypeResolver\Node\AttributeKey;
@@ -131,6 +132,10 @@ final class BetterStandardPrinter extends Standard
 
     protected function p(Node $node, $parentFormatPreserved = false): string
     {
+        while ($node instanceof AlwaysRememberedExpr) {
+            $node = $node->getExpr();
+        }
+
         $content = parent::p($node, $parentFormatPreserved);
 
         return $node->getAttribute(AttributeKey::WRAPPED_IN_PARENTHESES) === true
