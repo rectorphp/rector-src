4013377b3448c7cabaeac3921049af7d81a99958
Peter Fox <peter.fox@peterfox.me>
2024-07-11 21:02:15 +0100

    Fix ArrayDimFetchToMethodCallRector

--- rules-tests/Transform/Rector/ArrayDimFetch/ArrayDimFetchToMethodCallRector/Source/SomeExtendedClass.php
+++ rules-tests/Transform/Rector/ArrayDimFetch/ArrayDimFetchToMethodCallRector/Source/SomeExtendedClass.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Rector\Tests\Transform\Rector\ArrayDimFetch\ArrayDimFetchToMethodCallRector\Source;
 
-final class ChoiceControl
+class SomeExtendedClass
 {
-
+    protected \SomeClass $something;
 }
