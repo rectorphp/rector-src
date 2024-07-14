25b437a67243133d39aaf9579723013730b4d3f5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-07-15 03:55:14 +0700

    [CodeQuality] Allow change empty() to [] compare of other object property usage on SimplifyEmptyCheckOnEmptyArrayRector (#6147)

---
+++ rules-tests/CodeQuality/Rector/Empty_/SimplifyEmptyCheckOnEmptyArrayRector/Source/OtherClass.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\CodeQuality\Rector\Empty_\SimplifyEmptyCheckOnEmptyArrayRector\Source;
+
+class OtherClass
+{
+    public array $prop = [];
+}
