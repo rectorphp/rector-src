379c0ce4f485bd9a600c21b3cb45a11143d4d0ba
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-07-17 02:07:19 +0700

    Fix with set php version

--- rules-tests/TypeDeclaration/Rector/ClassMethod/ParamTypeByMethodCallTypeRector/config/configured_rule.php
+++ rules-tests/TypeDeclaration/Rector/ClassMethod/ParamTypeByMethodCallTypeRector/config/configured_rule.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\TypeDeclaration\Rector\ClassMethod\ParamTypeByMethodCallTypeRector;
+use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
-    ->withRules([ParamTypeByMethodCallTypeRector::class]);
+    ->withRules([ParamTypeByMethodCallTypeRector::class])
+    ->withPhpVersion(PhpVersion::PHP_80);
