28027252405256321c5f01f6f9a823d288c151ee
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-07-19 04:44:16 +0700

    [DeadCode] Remove unnecessary print variable to detect variable variable on ExprUsedInNodeAnalyzer (#6161)

--- rules/DeadCode/NodeAnalyzer/ExprUsedInNodeAnalyzer.php
+++ rules/DeadCode/NodeAnalyzer/ExprUsedInNodeAnalyzer.php
@@ -5,18 +5,17 @@ declare(strict_types=1);
 namespace Rector\DeadCode\NodeAnalyzer;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Expr\Include_;
 use PhpParser\Node\Expr\Variable;
 use Rector\NodeAnalyzer\CompactFuncCallAnalyzer;
-use Rector\PhpParser\Printer\BetterStandardPrinter;
 
 final readonly class ExprUsedInNodeAnalyzer
 {
     public function __construct(
         private UsedVariableNameAnalyzer $usedVariableNameAnalyzer,
-        private CompactFuncCallAnalyzer $compactFuncCallAnalyzer,
-        private BetterStandardPrinter $betterStandardPrinter
+        private CompactFuncCallAnalyzer $compactFuncCallAnalyzer
     ) {
     }
 
@@ -27,11 +26,8 @@ final readonly class ExprUsedInNodeAnalyzer
         }
 
         // variable as variable variable need mark as used
-        if ($node instanceof Variable) {
-            $print = $this->betterStandardPrinter->print($node);
-            if (\str_starts_with($print, '${$')) {
-                return true;
-            }
+        if ($node instanceof Variable && $node->name instanceof Expr) {
+            return true;
         }
 
         if ($node instanceof FuncCall) {
