a6bb912bcd02986008d3876b1d18412ff7c5a4ed
GitHub Action <actions@github.com>
2024-07-23 16:38:19 +0000

    [ci-review] Rector Rectify

--- rules-tests/TypeDeclaration/Rector/Class_/TypedPropertyFromCreateMockAssignRector/config/rule_config.php
+++ rules-tests/TypeDeclaration/Rector/Class_/TypedPropertyFromCreateMockAssignRector/config/rule_config.php
@@ -1,10 +1,11 @@
 <?php
 
 declare(strict_types=1);
+use Rector\ValueObject\PhpVersionFeature;
 
 use Rector\Config\RectorConfig;
 use Rector\TypeDeclaration\Rector\Class_\TypedPropertyFromCreateMockAssignRector;
 
 return RectorConfig::configure()
     ->withRules([TypedPropertyFromCreateMockAssignRector::class])
-    ->withPhpVersion(\Rector\ValueObject\PhpVersionFeature::TYPED_PROPERTIES);
+    ->withPhpVersion(PhpVersionFeature::TYPED_PROPERTIES);
