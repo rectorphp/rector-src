def51997c7a3b635586dc9029015f4370e442007
Tomas Votruba <tomas.vot@gmail.com>
2024-07-23 15:41:52 +0200

    [TypeDeclaration] Add TypedPropertyFromCreateMockAssignRector

---
+++ rules-tests/TypeDeclaration/Rector/Class_/TypedPropertyFromCreateMockAssignRector/Source/SomeFinalizedMockedClass.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\TypeDeclaration\Rector\Class_\TypedPropertyFromCreateMockAssignRector\Source;
+
+final class SomeFinalizedMockedClass
+{
+}
