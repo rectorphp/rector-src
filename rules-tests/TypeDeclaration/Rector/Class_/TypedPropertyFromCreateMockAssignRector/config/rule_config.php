6f28234a400a7d0abb4e855f0fc14927e90efa5b
TomasVotruba <924196+TomasVotruba@users.noreply.github.com>
2024-07-28 00:29:33 +0000

    [automated] Apply Coding Standard

--- rules-tests/TypeDeclaration/Rector/Class_/TypedPropertyFromCreateMockAssignRector/config/rule_config.php
+++ rules-tests/TypeDeclaration/Rector/Class_/TypedPropertyFromCreateMockAssignRector/config/rule_config.php
@@ -1,10 +1,10 @@
 <?php
 
 declare(strict_types=1);
-use Rector\ValueObject\PhpVersionFeature;
 
 use Rector\Config\RectorConfig;
 use Rector\TypeDeclaration\Rector\Class_\TypedPropertyFromCreateMockAssignRector;
+use Rector\ValueObject\PhpVersionFeature;
 
 return RectorConfig::configure()
     ->withRules([TypedPropertyFromCreateMockAssignRector::class])
