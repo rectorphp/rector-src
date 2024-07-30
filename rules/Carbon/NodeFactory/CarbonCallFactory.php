23d15cb33e39c573e5a2067eb0f5f25fffb2ccd4
Florisbosch <floris-bosch@live.nl>
2024-07-30 12:46:29 +0200

    Fix most of possible cases for carbon

--- rules/Carbon/NodeFactory/CarbonCallFactory.php
+++ rules/Carbon/NodeFactory/CarbonCallFactory.php
@@ -11,13 +11,16 @@ use PhpParser\Node\Identifier;
 use PhpParser\Node\Name\FullyQualified;
 use PhpParser\Node\Scalar\LNumber;
 use PhpParser\Node\Scalar\String_;
+
 final class CarbonCallFactory
 {
     /**
      * @var string
      * @see https://regex101.com/r/19qPHr/1
      */
-    private const PLUS_MINUS_COUNT_REGEX = '#(?<operator>\+|-)(\\s+)?(?<count>\\d+)(\s+)?(?<unit>sec|second|seconds|min|minute|minutes|hour|hours|day|days|week|weeks|month|months|year|years)#';
+    private const PLUS_MINUS_COUNT_REGEX = '#(?<operator>\+|-)(\\s+)?(?<count>\\d+)(\s+)?(?<unit>seconds|second|sec|minutes|minute|min|hours|hour|days|day|weeks|week|months|month|years|year)#';
+
+    private const SET_TIME_REGEX = '#(?<hour>.\\d{1,2}):(?<minute>\\d{2})(:(?<second>\\d{2}))?#';
 
     /**
      * @var string
@@ -30,23 +33,91 @@ final class CarbonCallFactory
      */
     public function createFromDateTimeString(FullyQualified $carbonFullyQualified, String_ $string)
     {
-        $dateTimeValue = $string->value;
-        if (in_array($dateTimeValue, ['now', 'today', 'yesterday', 'tomorrow'])) {
-            return new StaticCall($carbonFullyQualified, new Identifier($dateTimeValue));
+        $carbonCall = $this->createStaticCall($carbonFullyQualified, $string);
+        $string->value = Strings::replace($string->value, self::STATIC_DATE_REGEX);
+
+        $carbonCall = $this->createSetTimeMethodCall($carbonCall, $string);
+        $string->value = Strings::replace($string->value, self::SET_TIME_REGEX);
+
+        // Handle add/sub multiple times
+        while ($match = Strings::match($string->value, self::PLUS_MINUS_COUNT_REGEX)) {
+            $carbonCall = $this->createModifyMethodCall($carbonCall, new LNumber((int) $match['count']), $match['unit'], $match['operator']);
+            $string->value = Strings::replace($string->value, self::PLUS_MINUS_COUNT_REGEX, '', 1);
         }
 
-        $startDate = Strings::match($dateTimeValue, self::STATIC_DATE_REGEX)[0] ?? 'now';
+        // If we still have something in the string, we go back to the first method and replace this with a parse
+        if (($rest = Strings::trim($string->value)) !== '') {
+            $originialStaticCall = &$carbonCall;
+            while (!$originialStaticCall instanceof StaticCall) {
+                $originialStaticCall = &$originialStaticCall->var;
+            }
+
+            // If we fallback to a parse we want to include tomorrow/today/yesterday etc
+            if ($originialStaticCall->name != 'now') {
+                $rest .= ' ' . $originialStaticCall->name;
+            }
+
+            $originialStaticCall->name = new Identifier('parse');
+            $originialStaticCall->args = [new Arg(new String_($rest))];
+        }
+
+        return $carbonCall;
+    }
+
+    private function createStaticCall(FullyQualified $carbonFullyQualified, String_ $string): StaticCall
+    {
+        $startDate = Strings::match($string->value, self::STATIC_DATE_REGEX)[0] ?? 'now';
         $carbonCall = new StaticCall($carbonFullyQualified, new Identifier($startDate));
 
-        $match = Strings::match($dateTimeValue, self::PLUS_MINUS_COUNT_REGEX);
+        return $carbonCall;
+    }
 
-        if ($match === null) {
-            return new StaticCall($carbonFullyQualified, new Identifier('parse'), [new Arg($string)]);
+    private function createSetTimeMethodCall(StaticCall $carbonCall, String_ $string): MethodCall|StaticCall
+    {
+        $hour = null;
+        $minute = null;
+        $second = null;
+
+        $matches = Strings::match($string->value, self::SET_TIME_REGEX);
+
+        foreach($matches ?? [] as $group => $value) {
+            switch ($group) {
+                case 'hour':
+                    $hour = (int) $value;
+                    break;
+                case 'minute':
+                    $minute = (int) $value;
+                    break;
+                case 'second':
+                    $second = (int) $value;
+                    break;
+                default:
+                    break;
+            }
         }
 
-        $countLNumber = new LNumber((int) $match['count']);
+        if (!isset($hour) || !isset($minute)) {
+            return $carbonCall;
+        }
 
-        $unit = match((string) $match['unit']) {
+        // Use today when we set a time so base is always 00:00:00
+        $carbonCall->name = new Identifier('today');
+        $second = $second ?? 0;
+        if (($hour > 0) || ($minute > 0) || ($second > 0)) {
+            $args = [new Arg(new LNumber($hour)), new Arg(new LNumber($minute))];
+            if ($second > 0) {
+                $args[] = new Arg(new LNumber($second));
+            }
+
+            return new MethodCall($carbonCall, new Identifier('setTime'), $args);
+        }
+
+        return $carbonCall;
+    }
+
+    private function createModifyMethodCall(MethodCall|StaticCall $carbonCall, LNumber $countLNumber, string $unit, string $operator): MethodCall|null
+    {
+        $unit = match($unit) {
             'sec', 'second', 'seconds' => 'seconds',
             'min', 'minute', 'minutes' => 'minutes',
             'hour', 'hours' => 'hours',
@@ -57,14 +128,14 @@ final class CarbonCallFactory
             default => null,
         };
 
-        $operator = match((string) $match['operator']) {
+        $operator = match($operator) {
             '+' => 'add',
             '-' => 'sub',
             default => null,
         };
 
         if ($unit === null || $operator === null) {
-            return new StaticCall($carbonFullyQualified, new Identifier('parse'), [new Arg($string)]);
+            return null;
         }
 
         $methodName = $operator . ucfirst($unit);
