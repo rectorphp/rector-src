15d4581163d1869e711000f0a12b57f44be5f8a5
Florisbosch <floris-bosch@live.nl>
2024-07-31 10:21:56 +0200

    Configure rule config for test

--- rules-tests/Carbon/Rector/New_/DateTimeInstanceToCarbonRector/config/configured_rule.php
+++ rules-tests/Carbon/Rector/New_/DateTimeInstanceToCarbonRector/config/configured_rule.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 use Rector\Carbon\Rector\New_\DateTimeInstanceToCarbonRector;
 use Rector\Config\RectorConfig;
+use Rector\Renaming\Rector\Name\RenameClassRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(DateTimeInstanceToCarbonRector::class);
+    $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
+            'DateTime' => 'Carbon\Carbon',
+            'DateTimeImmutable' => 'Carbon\CarbonImmutable',
+        ]);
 };
