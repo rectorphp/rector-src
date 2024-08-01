1331f9792c0b14488fe66ef140145ac331956b6f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-08-01 15:10:40 +0700

    [Php54] No need reprint when array is just created on LongArrayToShortArrayRector (#6206)
    * [Php54] No need reprint when array is just created on LongArrayToShortArrayRector
    
    * [Php54] No need reprint when array is just created on LongArrayToShortArrayRector

---
+++ tests/Issues/CountArrayLongToShort/config/configured_rule.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodingStyle\Rector\FuncCall\CountArrayToEmptyArrayComparisonRector;
+use Rector\Config\RectorConfig;
+use Rector\Php54\Rector\Array_\LongArrayToShortArrayRector;
+
+return RectorConfig::configure()
+    ->withRules([
+        CountArrayToEmptyArrayComparisonRector::class,
+        LongArrayToShortArrayRector::class,
+    ]);
