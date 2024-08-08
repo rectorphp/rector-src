1ee5a1e7e9fff6a92bc5cfcfc6aebb630aea2a5e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-08-08 21:25:28 +0700

    clean up stmts check on UnreachableStatementNodeVisitor

--- src/PHPStan/NodeVisitor/UnreachableStatementNodeVisitor.php
+++ src/PHPStan/NodeVisitor/UnreachableStatementNodeVisitor.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Rector\PHPStan\NodeVisitor;
 
 use PhpParser\Node;
-use PhpParser\Node\Stmt\ClassLike;
-use PhpParser\Node\Stmt\Declare_;
 use PhpParser\NodeVisitorAbstract;
 use PHPStan\Analyser\MutatingScope;
 use PHPStan\Analyser\Scope;
@@ -25,7 +23,7 @@ final class UnreachableStatementNodeVisitor extends NodeVisitorAbstract
 
     public function enterNode(Node $node): ?Node
     {
-        if (! $node instanceof StmtsAwareInterface && ! $node instanceof ClassLike && ! $node instanceof Declare_) {
+        if (! $node instanceof StmtsAwareInterface) {
             return null;
         }
 
