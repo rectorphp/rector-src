58702ca7e37149949ba28d2bfaeeb15c82f4d266
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-08-12 19:24:35 +0700

    cover default

--- rules/TypeDeclaration/Rector/ClassMethod/StrictArrayParamDimFetchRector.php
+++ rules/TypeDeclaration/Rector/ClassMethod/StrictArrayParamDimFetchRector.php
@@ -15,6 +15,7 @@ use PhpParser\Node\Expr\Cast\Array_;
 use PhpParser\Node\Expr\Closure;
 use PhpParser\Node\Expr\Empty_;
 use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Expr\Isset_;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\FunctionLike;
@@ -125,6 +126,7 @@ CODE_SAMPLE
 
         $isParamAccessedArrayDimFetch = false;
         $this->traverseNodesWithCallable($functionLike->stmts, function (Node $node) use (
+            $param,
             $paramName,
             &$isParamAccessedArrayDimFetch,
         ): int|null {
@@ -132,7 +134,7 @@ CODE_SAMPLE
                 return NodeTraverser::DONT_TRAVERSE_CURRENT_AND_CHILDREN;
             }
 
-            if ($this->shouldStop($node, $paramName)) {
+            if ($this->shouldStop($node, $param, $paramName)) {
                 // force set to false to avoid too early replaced
                 $isParamAccessedArrayDimFetch = false;
                 return NodeTraverser::STOP_TRAVERSAL;
@@ -190,9 +192,25 @@ CODE_SAMPLE
         return false;
     }
 
-    private function shouldStop(Node $node, string $paramName): bool
+    private function shouldStop(Node $node, Param $param, string $paramName): bool
     {
         $nodeToCheck = null;
+
+        if (! $param->default instanceof Expr) {
+            if ($node instanceof Isset_) {
+                foreach ($node->vars as $var) {
+                    if ($var instanceof ArrayDimFetch && $var->var instanceof Variable && $var->var->name === $paramName) {
+                        return true;
+                    }
+                }
+            }
+
+            if ($node instanceof Empty_) {
+                if ($node->expr instanceof ArrayDimFetch && $node->expr->var instanceof Variable && $node->expr->var->name === $paramName) {
+                    return true;
+                }
+            }
+        }
 
         if ($node instanceof FuncCall
             && ! $node->isFirstClassCallable()
