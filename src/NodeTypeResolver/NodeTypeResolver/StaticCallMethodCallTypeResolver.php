54bf4397654836bd28b5419768e739f6f372df05
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-08-17 01:27:29 +0700

    [AutoImport] Do not add cast on valid Integer type on aliased Name Node on auto import enabled (#6237)
    * [AutoImport] Do not add cast on valid Integer type on aliased Name Node on auto import enabled
    
    * Fix
    
    * Fix

--- src/NodeTypeResolver/NodeTypeResolver/StaticCallMethodCallTypeResolver.php
+++ src/NodeTypeResolver/NodeTypeResolver/StaticCallMethodCallTypeResolver.php
@@ -18,6 +18,8 @@ use Rector\NodeTypeResolver\Contract\NodeTypeResolverInterface;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\NodeTypeResolver\NodeTypeResolver;
 use Rector\NodeTypeResolver\PHPStan\ParametersAcceptorSelectorVariantsWrapper;
+use Rector\StaticTypeMapper\ValueObject\Type\AliasedObjectType;
+use Rector\StaticTypeMapper\ValueObject\Type\FullyQualifiedObjectType;
 
 /**
  * @implements NodeTypeResolverInterface<StaticCall|MethodCall>
@@ -70,6 +72,10 @@ final class StaticCallMethodCallTypeResolver implements NodeTypeResolverInterfac
             $callerType = $this->nodeTypeResolver->getType($node->var);
         } else {
             $callerType = $this->nodeTypeResolver->getType($node->class);
+        }
+
+        if ($callerType instanceof AliasedObjectType) {
+            $callerType = new FullyQualifiedObjectType($callerType->getFullyQualifiedName());
         }
 
         foreach ($callerType->getObjectClassReflections() as $objectClassReflection) {
