feb5d4d9ff429217a0f467b89f695b0fc774cf09
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-08-20 20:59:22 +0700

    Use ocramius/package-versions to get output phpstan version to replace on build

--- bin/add-phpstan-self-replace.php
+++ bin/add-phpstan-self-replace.php
@@ -4,6 +4,7 @@
 
 declare(strict_types=1);
 
+use PackageVersions\Versions;
 use Nette\Utils\FileSystem;
 use Nette\Utils\Json;
 
@@ -12,7 +13,9 @@ require __DIR__ . '/../vendor/autoload.php';
 $composerJsonFileContents = FileSystem::read(__DIR__ . '/../composer.json');
 
 $composerJson = Json::decode($composerJsonFileContents, forceArrays: true);
-$composerJson['replace']['phpstan/phpstan'] = $composerJson['require']['phpstan/phpstan'];
+// result output is like: // 1.0.0@0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33
+[$phpstanVersion, ] = explode('@', Versions::getVersion('phpstan/phpstan'));
+$composerJson['replace']['phpstan/phpstan'] = $phpstanVersion;
 
 $modifiedComposerJsonFileContents = Json::encode($composerJson, pretty: true);
 FileSystem::write(__DIR__ . '/../composer.json', $modifiedComposerJsonFileContents, null);
