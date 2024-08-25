a8e7ddeaaaccbd70d06177b0c934bbe763b508fd
Peter Fox <peter.fox@peterfox.me>
2024-08-25 13:41:47 +0100

    Adds a rule to handle class strings exclusively

---
+++ rules-tests/TypeDeclaration/Rector/FunctionLike/AddClosureParamTypeFromArgClassStringRector/config/configured_rule.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\FunctionLike\AddClosureParamTypeFromArgClassStringRector;
+use Rector\TypeDeclaration\ValueObject\AddClosureParamTypeFromArg;
+use Rector\ValueObject\PhpVersionFeature;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig
+        ->ruleWithConfiguration(AddClosureParamTypeFromArgClassStringRector::class, [
+            new AddClosureParamTypeFromArg('SomeNamespace\SomeClass', 'someCall', 1, 0, 0),
+        ]);
+
+    $rectorConfig->phpVersion(PhpVersionFeature::MIXED_TYPE);
+};
