eec090e628a66834f49dbaa8daa1aab7966317e5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-08-28 18:32:12 +0700

    [CodeQuality] Handle crash on first class callable inside match on OptionalParametersAfterRequiredRector

---
+++ rules-tests/CodeQuality/Rector/ClassMethod/OptionalParametersAfterRequiredRector/Source/SomeClass.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\CodeQuality\Rector\ClassMethod\OptionalParametersAfterRequiredRector\Source;
+
+class SomeClass
+{
+    public static function fromInt(int $id) {}
+}
