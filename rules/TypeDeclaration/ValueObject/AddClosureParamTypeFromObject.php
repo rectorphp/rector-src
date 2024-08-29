13ee64b8904209f8a15f5510fa6100ef97474c58
Peter Fox <peter.fox@peterfox.me>
2024-08-29 01:40:13 +0100

    Adds AddClosureParamTypeFromObjectRector

---
+++ rules/TypeDeclaration/ValueObject/AddClosureParamTypeFromObject.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\TypeDeclaration\ValueObject;
+
+use PHPStan\Type\ObjectType;
+use Rector\Validation\RectorAssert;
+
+final readonly class AddClosureParamTypeFromObject
+{
+    /**
+     * @param int<0, max> $callLikePosition
+     * @param int<0, max> $functionLikePosition
+     */
+    public function __construct(
+        private string $className,
+        private string $methodName,
+        private int $callLikePosition,
+        private int $functionLikePosition,
+    ) {
+        RectorAssert::className($className);
+    }
+
+    public function getObjectType(): ObjectType
+    {
+        return new ObjectType($this->className);
+    }
+
+    public function getMethodName(): string
+    {
+        return $this->methodName;
+    }
+
+    /**
+     * @return int<0, max>
+     */
+    public function getCallLikePosition(): int
+    {
+        return $this->callLikePosition;
+    }
+
+    /**
+     * @return int<0, max>
+     */
+    public function getFunctionLikePosition(): int
+    {
+        return $this->functionLikePosition;
+    }
+}
