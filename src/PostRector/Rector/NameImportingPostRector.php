72994a4f7074dc8433dd46806559cd158c0439f2
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-08-31 14:38:03 +0700

    [Refactor] Move bloated logic on NameImportingPostRector into ClassNameImportSkipper and NameImporter

--- src/PostRector/Rector/NameImportingPostRector.php
+++ src/PostRector/Rector/NameImportingPostRector.php
@@ -5,17 +5,12 @@ declare(strict_types=1);
 namespace Rector\PostRector\Rector;
 
 use PhpParser\Node;
-use PhpParser\Node\Identifier;
-use PhpParser\Node\Name;
 use PhpParser\Node\Name\FullyQualified;
 use PhpParser\Node\Stmt;
 use PhpParser\Node\Stmt\GroupUse;
 use PhpParser\Node\Stmt\Use_;
-use Rector\CodingStyle\ClassNameImport\ClassNameImportSkipper;
 use Rector\CodingStyle\Node\NameImporter;
-use Rector\Naming\Naming\AliasNameResolver;
 use Rector\Naming\Naming\UseImportsResolver;
-use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\PostRector\Guard\AddUseStatementGuard;
 
 final class NameImportingPostRector extends AbstractPostRector
@@ -27,9 +22,7 @@ final class NameImportingPostRector extends AbstractPostRector
 
     public function __construct(
         private readonly NameImporter $nameImporter,
-        private readonly ClassNameImportSkipper $classNameImportSkipper,
         private readonly UseImportsResolver $useImportsResolver,
-        private readonly AliasNameResolver $aliasNameResolver,
         private readonly AddUseStatementGuard $addUseStatementGuard
     ) {
     }
@@ -46,32 +39,7 @@ final class NameImportingPostRector extends AbstractPostRector
             return null;
         }
 
-        if ($node->isSpecialClassName()) {
-            return null;
-        }
-
-        // verify long name, as short name verify may conflict
-        // see test PR: https://github.com/rectorphp/rector-src/pull/6208
-        // ref https://3v4l.org/21H5j vs https://3v4l.org/GIHSB
-        if (substr_count($node->toCodeString(), '\\') > 1) {
-            $originalName = $node->getAttribute(AttributeKey::ORIGINAL_NAME);
-            if ($originalName instanceof Name && $originalName->getLast() === $originalName->toString()) {
-                return null;
-            }
-        }
-
-        if ($this->classNameImportSkipper->shouldSkipName($node, $this->currentUses)) {
-            return null;
-        }
-
-        // make use of existing use import
-        $nameInUse = $this->resolveNameInUse($node);
-        if ($nameInUse instanceof Name) {
-            $nameInUse->setAttribute(AttributeKey::NAMESPACED_NAME, $node->toString());
-            return $nameInUse;
-        }
-
-        return $this->nameImporter->importName($node, $this->getFile());
+        return $this->nameImporter->importName($node, $this->getFile(), $this->currentUses);
     }
 
     /**
@@ -80,32 +48,5 @@ final class NameImportingPostRector extends AbstractPostRector
     public function shouldTraverse(array $stmts): bool
     {
         return $this->addUseStatementGuard->shouldTraverse($stmts, $this->getFile()->getFilePath());
-    }
-
-    private function resolveNameInUse(FullyQualified $fullyQualified): null|Name
-    {
-        $aliasName = $this->aliasNameResolver->resolveByName($fullyQualified, $this->currentUses);
-        if (is_string($aliasName)) {
-            return new Name($aliasName);
-        }
-
-        if (substr_count($fullyQualified->toCodeString(), '\\') === 1) {
-            return null;
-        }
-
-        $lastName = $fullyQualified->getLast();
-        foreach ($this->currentUses as $currentUse) {
-            foreach ($currentUse->uses as $useUse) {
-                if ($useUse->name->getLast() !== $lastName) {
-                    continue;
-                }
-
-                if ($useUse->alias instanceof Identifier && $useUse->alias->toString() !== $lastName) {
-                    return new Name($lastName);
-                }
-            }
-        }
-
-        return null;
     }
 }
