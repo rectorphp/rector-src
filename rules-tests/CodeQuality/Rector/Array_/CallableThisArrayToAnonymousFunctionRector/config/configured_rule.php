90d1926ea1bfc06874fd8c3a9052b2b7b6375278
Tomas Votruba <tomas.vot@gmail.com>
2024-09-02 01:42:06 +0100

    remove deprecated CallableThisArrayToAnonymousFunctionRector

--- rules-tests/CodeQuality/Rector/Array_/CallableThisArrayToAnonymousFunctionRector/config/configured_rule.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Rector\CodeQuality\Rector\Array_\CallableThisArrayToAnonymousFunctionRector;
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()
-    ->withRules([CallableThisArrayToAnonymousFunctionRector::class]);
