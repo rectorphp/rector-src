0dedd20027ca23248ebb4454e695da88ec381794
Tomas Votruba <tomas.vot@gmail.com>
2024-09-15 15:22:58 +0200

    Add RemoveMockObjectPropertyAnnotationRector

--- rules/TypeDeclaration/Rector/Class_/TypedPropertyFromCreateMockAssignRector.php
+++ rules/TypeDeclaration/Rector/Class_/TypedPropertyFromCreateMockAssignRector.php
@@ -9,6 +9,7 @@ use PhpParser\Node\NullableType;
 use PhpParser\Node\Stmt\Class_;
 use PHPStan\Type\ObjectType;
 use PHPStan\Type\Type;
+use Rector\Enum\ClassName;
 use Rector\PHPStanStaticTypeMapper\Enum\TypeKind;
 use Rector\Rector\AbstractRector;
 use Rector\StaticTypeMapper\StaticTypeMapper;
@@ -27,18 +28,12 @@ final class TypedPropertyFromCreateMockAssignRector extends AbstractRector imple
     /**
      * @var string
      */
-    private const TEST_CASE_CLASS = 'PHPUnit\Framework\TestCase';
-
-    /**
-     * @var string
-     */
     private const MOCK_OBJECT_CLASS = 'PHPUnit\Framework\MockObject\MockObject';
 
     public function __construct(
         private readonly AssignToPropertyTypeInferer $assignToPropertyTypeInferer,
         private readonly StaticTypeMapper $staticTypeMapper,
         private readonly ConstructorAssignDetector $constructorAssignDetector,
-        private readonly \Rector\DeadCode\PhpDoc\TagRemover\VarTagRemover $varTagRemover,
     ) {
     }
 
@@ -87,7 +82,7 @@ CODE_SAMPLE
      */
     public function refactor(Node $node): ?Node
     {
-        if (! $this->isObjectType($node, new ObjectType(self::TEST_CASE_CLASS))) {
+        if (! $this->isObjectType($node, new ObjectType(ClassName::TEST_CASE_CLASS))) {
             return null;
         }
 
@@ -128,10 +123,6 @@ CODE_SAMPLE
             }
 
             $property->type = $propertyType;
-
-            // clear var docblock
-            $this->varTagRemover->removeVarPhpTagValueNodeIfNotComment($property, $type);
-
             $hasChanged = true;
         }
 
