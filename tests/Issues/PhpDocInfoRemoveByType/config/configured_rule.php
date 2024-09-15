dd93b38091aa7f5ff5ba15791f49d86059cd1ef8
Tomas Votruba <tomas.vot@gmail.com>
2024-09-15 02:33:52 +0200

    [automated] Apply Coding Standard (#6303)
    Co-authored-by: TomasVotruba <924196+TomasVotruba@users.noreply.github.com>

--- tests/Issues/PhpDocInfoRemoveByType/config/configured_rule.php
+++ tests/Issues/PhpDocInfoRemoveByType/config/configured_rule.php
@@ -7,7 +7,4 @@ use Rector\DeadCode\Rector\Node\RemoveNonExistingVarAnnotationRector;
 use Rector\TypeDeclaration\Rector\Expression\InlineVarDocTagToAssertRector;
 
 return RectorConfig::configure()
-    ->withRules([
-        InlineVarDocTagToAssertRector::class,
-        RemoveNonExistingVarAnnotationRector::class,
-    ]);
+    ->withRules([InlineVarDocTagToAssertRector::class, RemoveNonExistingVarAnnotationRector::class]);
