6da1719604f7ad1372029a64cab4fab5f2a78b66
Peter Fox <peter.fox@peterfox.me>
2024-10-10 23:44:48 +0100

    working rule

---
+++ rules-tests/TypeDeclaration/Rector/FunctionLike/AddClosureParamTypeForArrayMapRector/config/configured_rule.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\FunctionLike\AddClosureParamTypeForArrayMapRector;
+use Rector\ValueObject\PhpVersionFeature;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig
+        ->rules([AddClosureParamTypeForArrayMapRector::class]);
+
+    $rectorConfig->phpVersion(PhpVersionFeature::INTERSECTION_TYPES);
+};
