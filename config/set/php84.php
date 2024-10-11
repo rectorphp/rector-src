c11f57265f01f5048a4a7d56a5af17bef8d7872f
Jorg Sowa <jorg.sowa@gmail.com>
2024-10-11 23:46:13 +0200

    Added RoundingModeEnumRector to php84.php

--- config/set/php84.php
+++ config/set/php84.php
@@ -3,8 +3,9 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Php84\Rector\FuncCall\RoundingModeEnumRector;
 use Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rules([ExplicitNullableParamTypeRector::class]);
+    $rectorConfig->rules([ExplicitNullableParamTypeRector::class, RoundingModeEnumRector::class]);
 };
