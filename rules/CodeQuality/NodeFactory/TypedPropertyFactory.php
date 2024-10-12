d7bfc24bedc11374868f5a7ff3066eb847e80a8c
Tomas Votruba <tomas.vot@gmail.com>
2024-10-12 17:38:13 +0200

    [CodeQuality] Add DynamicDocBlockPropertyToNativePropertyRector (#5691)
    * [CodeQuality] Kick off DynamicDocBlockPropertyToNativePropertyRector
    
    * add nullable type with default
    
    * skip __get() and __set()
    
    * [ci-review] Rector Rectify
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

---
+++ rules/CodeQuality/NodeFactory/TypedPropertyFactory.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\CodeQuality\NodeFactory;
+
+use PhpParser\Node;
+use PhpParser\Node\ComplexType;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Name;
+use PhpParser\Node\NullableType;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\Property;
+use PhpParser\Node\Stmt\PropertyProperty;
+use PHPStan\PhpDocParser\Ast\PhpDoc\PropertyTagValueNode;
+use Rector\PHPStanStaticTypeMapper\Enum\TypeKind;
+use Rector\StaticTypeMapper\StaticTypeMapper;
+
+final readonly class TypedPropertyFactory
+{
+    public function __construct(
+        private StaticTypeMapper $staticTypeMapper,
+    ) {
+    }
+
+    public function createFromPropertyTagValueNode(
+        PropertyTagValueNode $propertyTagValueNode,
+        Class_ $class,
+        string $propertyName
+    ): Property {
+        $propertyProperty = new PropertyProperty($propertyName);
+        $propertyTypeNode = $this->createPropertyTypeNode($propertyTagValueNode, $class);
+
+        return new Property(Class_::MODIFIER_PRIVATE, [$propertyProperty], [], $propertyTypeNode);
+    }
+
+    public function createPropertyTypeNode(
+        PropertyTagValueNode $propertyTagValueNode,
+        Class_ $class,
+        bool $isNullable = true
+    ): Name|ComplexType|Identifier|null {
+        $propertyType = $this->staticTypeMapper->mapPHPStanPhpDocTypeNodeToPHPStanType(
+            $propertyTagValueNode->type,
+            $class
+        );
+
+        $typeNode = $this->staticTypeMapper->mapPHPStanTypeToPhpParserNode($propertyType, TypeKind::PROPERTY);
+
+        if ($isNullable && ! $typeNode instanceof NullableType && ! $typeNode instanceof ComplexType && $typeNode instanceof Node) {
+            return new NullableType($typeNode);
+        }
+
+        return $typeNode;
+    }
+}
