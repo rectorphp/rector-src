6740d3719407437f58ecd4ed84f195cc66dc30cc
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-10-16 11:19:13 +0700

    add e2e test for with php level with empty composer.json but has withPhpVersion() config

---
+++ e2e/with-php-level/rector.php
@@ -0,0 +1,9 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\ValueObject\PhpVersionFeature;
+
+return RectorConfig::configure()
+    ->withPaths([__DIR__ . '/src'])
+    ->withPhpVersion(PhpVersionFeature::ATTRIBUTES)
+    ->withPhpLevel(10);
