341178424d3f9477969fe5a832920588f9c16c05
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-10-23 21:15:37 +0700

    fix other test

--- rules-tests/TypeDeclaration/Rector/Closure/ClosureReturnTypeRector/config/configured_rule.php
+++ rules-tests/TypeDeclaration/Rector/Closure/ClosureReturnTypeRector/config/configured_rule.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\TypeDeclaration\Rector\Closure\ClosureReturnTypeRector;
+use Rector\ValueObject\PhpVersion;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(ClosureReturnTypeRector::class);
+    $rectorConfig->phpVersion(PhpVersion::PHP_82);
 };
