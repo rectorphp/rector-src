97f6f7b41740f890734537758d535e4d629a6824
Tomas Votruba <tomas.vot@gmail.com>
2024-11-12 16:01:38 +0100

    [deprecation] Remove deprecated PublicConstantVisibilityRector

--- rules-tests/Php71/Rector/ClassConst/PublicConstantVisibilityRector/config/configured_rule.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Rector\Config\RectorConfig;
-use Rector\Php71\Rector\ClassConst\PublicConstantVisibilityRector;
-
-return RectorConfig::configure()
-    ->withRules([PublicConstantVisibilityRector::class]);
