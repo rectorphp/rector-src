ffdb50b38e3301cb33d21292522ddc136c3092d8
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-11-15 10:17:42 +0700

    fix SmartPhpParserFactory setup

--- src/PhpDocParser/PhpParser/SmartPhpParserFactory.php
+++ src/PhpDocParser/PhpParser/SmartPhpParserFactory.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace Rector\PhpDocParser\PhpParser;
 
-use PhpParser\Lexer\Emulative;
 use PhpParser\NodeVisitor\NameResolver;
 use PhpParser\Parser;
 use PhpParser\ParserFactory;
+use PhpParser\PhpVersion;
 use PHPStan\Parser\CachedParser;
 use PHPStan\Parser\SimpleParser;
+use PHPStan\Parser\VariadicFunctionsVisitor;
+use PHPStan\Parser\VariadicMethodsVisitor;
 
 /**
  * Based on PHPStan-based PHP-Parser best practices:
@@ -33,17 +35,16 @@ final class SmartPhpParserFactory
     private function createNativePhpParser(): Parser
     {
         $parserFactory = new ParserFactory();
-        $lexerEmulative = new Emulative([
-            'usedAttributes' => ['comments', 'startLine', 'endLine', 'startTokenPos', 'endTokenPos'],
-        ]);
-
-        return $parserFactory->create(ParserFactory::PREFER_PHP7, $lexerEmulative);
+        return $parserFactory->createForVersion(PhpVersion::fromString('7.0'));
     }
 
     private function createPHPStanParser(Parser $parser): CachedParser
     {
         $nameResolver = new NameResolver();
-        $simpleParser = new SimpleParser($parser, $nameResolver);
+        $variadicMethodsVisitor = new VariadicMethodsVisitor();
+        $variadicFunctionsVisitor = new VariadicFunctionsVisitor();
+
+        $simpleParser = new SimpleParser($parser, $nameResolver, $variadicMethodsVisitor, $variadicFunctionsVisitor);
 
         return new CachedParser($simpleParser, 1024);
     }
