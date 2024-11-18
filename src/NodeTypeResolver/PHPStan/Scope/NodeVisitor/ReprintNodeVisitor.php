cd8ce7b570f4d8720b67ab8db6735bba8fc2fc7f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-11-18 09:46:56 +0700

    fix reprint boolean not on SimplifyEmptyTest

--- src/NodeTypeResolver/PHPStan/Scope/NodeVisitor/ReprintNodeVisitor.php
+++ src/NodeTypeResolver/PHPStan/Scope/NodeVisitor/ReprintNodeVisitor.php
@@ -6,6 +6,7 @@ namespace Rector\NodeTypeResolver\PHPStan\Scope\NodeVisitor;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\BinaryOp;
+use PhpParser\Node\Expr\BooleanNot;
 use PhpParser\NodeVisitorAbstract;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\NodeTypeResolver\PHPStan\Scope\Contract\NodeVisitor\ScopeResolverNodeVisitorInterface;
@@ -19,15 +20,19 @@ final class ReprintNodeVisitor extends NodeVisitorAbstract implements ScopeResol
         }
 
         if ($node instanceof BinaryOp) {
-            if ($node->left instanceof BinaryOp) {
+            if ($node->left instanceof BinaryOp  && $node->left->hasAttribute(AttributeKey::ORIGINAL_NODE)) {
                 $node->left->setAttribute(AttributeKey::ORIGINAL_NODE, null);
             }
 
-            if ($node->right instanceof BinaryOp) {
+            if ($node->right instanceof BinaryOp && $node->left->hasAttribute(AttributeKey::ORIGINAL_NODE)) {
                 $node->right->setAttribute(AttributeKey::ORIGINAL_NODE, null);
             }
 
             return $node;
+        }
+
+        if ($node instanceof BooleanNot && $node->expr instanceof BinaryOp && $node->expr->hasAttribute(AttributeKey::ORIGINAL_NODE)) {
+            $node->expr->setAttribute(AttributeKey::WRAPPED_IN_PARENTHESES, true);
         }
 
         return null;
