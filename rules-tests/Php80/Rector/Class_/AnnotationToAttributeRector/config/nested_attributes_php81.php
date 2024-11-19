9de17e14e9a6d135cd955e2fea61632e9e1542fb
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-11-19 23:16:32 +0700

    finally, unit test green

--- rules-tests/Php80/Rector/Class_/AnnotationToAttributeRector/config/nested_attributes_php81.php
+++ rules-tests/Php80/Rector/Class_/AnnotationToAttributeRector/config/nested_attributes_php81.php
@@ -2,11 +2,12 @@
 
 declare(strict_types=1);
 
-use Doctrine\ORM\Mapping\Table;
-use Doctrine\ORM\Mapping\UniqueConstraint;
 use Rector\Config\RectorConfig;
 use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
+use Rector\Php80\Rector\Property\NestedAnnotationToAttributeRector;
+use Rector\Php80\ValueObject\AnnotationPropertyToAttributeClass;
 use Rector\Php80\ValueObject\AnnotationToAttribute;
+use Rector\Php80\ValueObject\NestedAnnotationToAttribute;
 use Rector\Tests\Php80\Rector\Class_\AnnotationToAttributeRector\Source\GenericAnnotation;
 use Rector\Tests\Php80\Rector\Class_\AnnotationToAttributeRector\SourcePhp81\All;
 use Rector\Tests\Php80\Rector\Class_\AnnotationToAttributeRector\SourcePhp81\Length;
@@ -22,9 +23,12 @@ return static function (RectorConfig $rectorConfig): void {
         new AnnotationToAttribute(Length::class),
         new AnnotationToAttribute(NotNumber::class),
         new AnnotationToAttribute(GenericAnnotation::class),
+    ]);
 
-        // PHP 8.1 doctrine annotations
-        new AnnotationToAttribute(Table::class),
-        new AnnotationToAttribute(UniqueConstraint::class),
+    $rectorConfig->ruleWithConfiguration(NestedAnnotationToAttributeRector::class, [
+        new NestedAnnotationToAttribute('Doctrine\ORM\Mapping\Table', [
+            new AnnotationPropertyToAttributeClass('Doctrine\ORM\Mapping\Index', 'indexes'),
+            new AnnotationPropertyToAttributeClass('Doctrine\ORM\Mapping\UniqueConstraint', 'uniqueConstraints'),
+        ]),
     ]);
 };
