a96ccbbc1cb25d11187e7cd62366b3f6e5496d4b
GitHub Action <actions@github.com>
2024-11-19 07:59:49 +0000

    [ci-review] Rector Rectify

--- src/BetterPhpDocParser/PhpDocParser/StaticDoctrineAnnotationParser/ArrayParser.php
+++ src/BetterPhpDocParser/PhpDocParser/StaticDoctrineAnnotationParser/ArrayParser.php
@@ -203,11 +203,7 @@ final readonly class ArrayParser
         if ($key !== null) {
             if (is_numeric($key)) {
                 // use equal over identical on purpose to verify if it is an integer
-                if ((float) $key == (int) $key) {
-                    $key = (int) $key;
-                } else {
-                    $key = (float) $key;
-                }
+                $key = (float) $key == (int) $key ? (int) $key : (float) $key;
             }
 
             return new ArrayItemNode($value, $key);
