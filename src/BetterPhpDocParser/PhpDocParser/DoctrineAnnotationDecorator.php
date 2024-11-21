401d5593917e5ef33524b248a9be823c46232e02
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-11-21 20:25:15 +0700

    Fix regex space before star comment  (#6464)
    * Fix regex space before star comment
    
    * Fix regex space before star comment

--- src/BetterPhpDocParser/PhpDocParser/DoctrineAnnotationDecorator.php
+++ src/BetterPhpDocParser/PhpDocParser/DoctrineAnnotationDecorator.php
@@ -59,9 +59,9 @@ final readonly class DoctrineAnnotationDecorator implements PhpDocNodeDecoratorI
 
     /**
      * @var string
-     * @see https://regex101.com/r/3zXEh7/1
+     * @see https://regex101.com/r/3zXEh7/2
      */
-    private const STAR_COMMENT_REGEX = '#^\s*\*#ms';
+    private const STAR_COMMENT_REGEX = '#^[ \t]+\*#ms';
 
     public function __construct(
         private ClassAnnotationMatcher $classAnnotationMatcher,
