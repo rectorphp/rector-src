d894ad4ae8b50178f2d351f580ee3916f6c5e10d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-11-23 21:17:30 +0700

    final touch: more precise regex line

--- src/Console/Formatter/NumberLineDiffCleaner.php
+++ src/Console/Formatter/NumberLineDiffCleaner.php
@@ -13,14 +13,14 @@ class NumberLineDiffCleaner
      * @var string
      * @see https://regex101.com/r/zHJEfJ/1
      */
-    private const LINE_RANGE_REGEX = '#\@\@ (?<LINE_RANGE>\-\d+,\d+ \+\d+,\d+) \@\@#';
+    private const LINE_RANGE_REGEX = '#^\<fg=cyan\>@@ (?<LINE_RANGE>\-\d+,\d+ \+\d+,\d+) @@\<\/fg=cyan\>$#';
 
     public static function clean(string $diff): string
     {
         $diffLines = NewLineSplitter::split($diff);
 
         foreach ($diffLines as $key => $diffLine) {
-            $diffLines[$key] = Strings::replace($diffLine, self::LINE_RANGE_REGEX, fn(array $match): string => '@@ @@');
+            $diffLines[$key] = Strings::replace($diffLine, self::LINE_RANGE_REGEX, '<fg=cyan>@@ @@</fg=cyan>');
         }
 
         return implode(PHP_EOL, $diffLines);
