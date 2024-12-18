d9400918fafc874816608c130a58ec30ccbe890b
GitHub Action <actions@github.com>
2024-12-18 06:04:13 +0000

    [ci-review] Rector Rectify

--- src/PHPStan/NodeVisitor/ReIndexNodeAttributeVisitor.php
+++ src/PHPStan/NodeVisitor/ReIndexNodeAttributeVisitor.php
@@ -5,20 +5,6 @@ declare(strict_types=1);
 namespace Rector\PHPStan\NodeVisitor;
 
 use PhpParser\Node;
-use PhpParser\Node\Expr\CallLike;
-use PhpParser\Node\Expr\Closure;
-use PhpParser\Node\Expr\FuncCall;
-use PhpParser\Node\Expr\MethodCall;
-use PhpParser\Node\Expr\New_;
-use PhpParser\Node\Expr\NullsafeMethodCall;
-use PhpParser\Node\Expr\StaticCall;
-use PhpParser\Node\FunctionLike;
-use PhpParser\Node\MatchArm;
-use PhpParser\Node\Stmt\ClassMethod;
-use PhpParser\Node\Stmt\Function_;
-use PhpParser\Node\Stmt\If_;
-use PhpParser\Node\Stmt\Switch_;
-use PhpParser\Node\Stmt\TryCatch;
 use PhpParser\NodeVisitorAbstract;
 use Rector\Application\NodeAttributeReIndexer;
 
