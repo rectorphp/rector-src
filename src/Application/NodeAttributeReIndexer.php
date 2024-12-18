13756b7a2e1bb6bf3ab13c9b475a7952b6ed2c4b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-12-18 12:59:59 +0700

    Rollback reindex node attributes before refactor

--- src/Application/NodeAttributeReIndexer.php
+++ src/Application/NodeAttributeReIndexer.php
@@ -4,13 +4,7 @@ declare(strict_types=1);
 
 namespace Rector\Application;
 
-use PhpParser\Modifiers;
 use PhpParser\Node;
-use PhpParser\Node\ArrayItem;
-use PhpParser\Node\ClosureUse;
-use PhpParser\Node\DeclareItem;
-use PhpParser\Node\Expr;
-use PhpParser\Node\Expr\Array_;
 use PhpParser\Node\Expr\CallLike;
 use PhpParser\Node\Expr\Closure;
 use PhpParser\Node\Expr\FuncCall;
@@ -20,24 +14,11 @@ use PhpParser\Node\Expr\NullsafeMethodCall;
 use PhpParser\Node\Expr\StaticCall;
 use PhpParser\Node\FunctionLike;
 use PhpParser\Node\MatchArm;
-use PhpParser\Node\PropertyItem;
-use PhpParser\Node\StaticVar;
-use PhpParser\Node\Stmt;
 use PhpParser\Node\Stmt\ClassMethod;
-use PhpParser\Node\Stmt\Declare_;
-use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\Function_;
 use PhpParser\Node\Stmt\If_;
-use PhpParser\Node\Stmt\Property;
-use PhpParser\Node\Stmt\Static_;
 use PhpParser\Node\Stmt\Switch_;
 use PhpParser\Node\Stmt\TryCatch;
-use PhpParser\Node\Stmt\Use_;
-use PhpParser\Node\UseItem;
-use PHPStan\Analyser\MutatingScope;
-use Rector\Exception\ShouldNotHappenException;
-use Rector\NodeAnalyzer\ScopeAnalyzer;
-use Rector\NodeTypeResolver\PHPStan\Scope\PHPStanNodeScopeResolver;
 
 final class NodeAttributeReIndexer
 {
