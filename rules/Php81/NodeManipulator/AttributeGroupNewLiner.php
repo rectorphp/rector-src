7a7a91f388f6fc1fd59132284b0d0e073d804b0d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-12-19 15:33:12 +0700

    [Php81][php82] Add AttributeGroupNewLiner to make new line based on token on ReadOnlyPropertyRector and ReadOnlyClassRector

---
+++ rules/Php81/NodeManipulator/AttributeGroupNewLiner.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Php81\NodeManipulator;
+
+use PhpParser\Node\Param;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\Property;
+use Rector\ValueObject\Application\File;
+
+final class AttributeGroupNewLiner
+{
+    public function newLine(File $file, Property|Param|Class_ $node): void
+    {
+        $oldTokens = $file->getOldTokens();
+        $startTokenPos = $node->getStartTokenPos();
+
+        if (! isset($oldTokens[$startTokenPos])) {
+            return;
+        }
+
+        if ($oldTokens[$startTokenPos]->text !== '#[') {
+            return;
+        }
+
+        $iteration = 1;
+        $lastKey = array_key_last($node->attrGroups);
+
+        if ($lastKey === null) {
+            return;
+        }
+
+        $lastAttributeTokenPos = $node->attrGroups[$lastKey]->getEndTokenPos();
+
+        while (isset($oldTokens[$startTokenPos + $iteration])) {
+            if ($startTokenPos + $iteration === $lastAttributeTokenPos) {
+                $space = ltrim($oldTokens[$startTokenPos + $iteration + 1]->text ?? '', "\r\n");
+                $oldTokens[$startTokenPos + $iteration]->text = "]\n" . $space;
+                break;
+            }
+
+            ++$iteration;
+        }
+    }
+}
