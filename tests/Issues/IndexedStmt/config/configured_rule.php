a50d8a5861ff1d3f3af18482daa1ecee7f875dc7
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-12-19 11:22:29 +0700

    cs fix

--- tests/Issues/IndexedStmt/config/configured_rule.php
+++ tests/Issues/IndexedStmt/config/configured_rule.php
@@ -7,8 +7,5 @@ use Rector\Tests\Issues\IndexedStmt\Source\ChangeLastIndex1Rector;
 use Rector\Tests\Issues\IndexedStmt\Source\RemoveIndex1Rector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rules([
-        RemoveIndex1Rector::class,
-        ChangeLastIndex1Rector::class,
-    ]);
+    $rectorConfig->rules([RemoveIndex1Rector::class, ChangeLastIndex1Rector::class]);
 };
