66d93167af53e4116e73f6dc011f02ac23efbd6f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-01-01 23:32:36 +0700

    [DeadCode] Add ReplaceBlockToItsStmtsRector (#6641)
    * [DeadCode] Add ReplaceBlockToItsStmtsRector
    
    * add fixture for skip normal statments

--- src/Config/Level/DeadCodeLevel.php
+++ src/Config/Level/DeadCodeLevel.php
@@ -9,6 +9,7 @@ use Rector\Contract\Rector\RectorInterface;
 use Rector\DeadCode\Rector\Array_\RemoveDuplicatedArrayKeyRector;
 use Rector\DeadCode\Rector\Assign\RemoveDoubleAssignRector;
 use Rector\DeadCode\Rector\Assign\RemoveUnusedVariableAssignRector;
+use Rector\DeadCode\Rector\Block\ReplaceBlockToItsStmtsRector;
 use Rector\DeadCode\Rector\BooleanAnd\RemoveAndTrueRector;
 use Rector\DeadCode\Rector\Cast\RecastingRemovalRector;
 use Rector\DeadCode\Rector\ClassConst\RemoveUnusedPrivateClassConstantRector;
@@ -82,6 +83,7 @@ final class DeadCodeLevel
         RemoveUnusedNonEmptyArrayBeforeForeachRector::class,
         RemoveNullPropertyInitializationRector::class,
         RemoveUselessReturnExprInConstructRector::class,
+        ReplaceBlockToItsStmtsRector::class,
 
         RemoveTypedPropertyDeadInstanceOfRector::class,
         TernaryToBooleanOrFalseToBooleanAndRector::class,
