5394ff63991748813945bb7527ac48a104cfbb3f
Tomas Votruba <tomas.vot@gmail.com>
2025-01-06 10:31:25 +0100

    Add constructor fixture, just to verify alter behavior

--- tests/Issues/AddClassDependency/config/configured_rule.php
+++ tests/Issues/AddClassDependency/config/configured_rule.php
@@ -2,10 +2,8 @@
 
 declare(strict_types=1);
 
-use Rector\Symfony\DependencyInjection\Rector\Class_\GetBySymfonyStringToConstructorInjectionRector;
 use Rector\Config\RectorConfig;
+use Rector\Symfony\DependencyInjection\Rector\Class_\GetBySymfonyStringToConstructorInjectionRector;
 
 return RectorConfig::configure()
-    ->withRules([
-        GetBySymfonyStringToConstructorInjectionRector::class,
-    ]);
+    ->withRules([GetBySymfonyStringToConstructorInjectionRector::class]);
