7ec3ae3514546043861ab1615509c6c8ab9c26ff
Tomas Votruba <tomas.vot@gmail.com>
2025-01-19 08:19:55 +0000

    [removing] Add interface support to RemoveInterfaceRector (#6681)

--- rules/Removing/Rector/Class_/RemoveInterfacesRector.php
+++ rules/Removing/Rector/Class_/RemoveInterfacesRector.php
@@ -6,6 +6,7 @@ namespace Rector\Removing\Rector\Class_;
 
 use PhpParser\Node;
 use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\Interface_;
 use Rector\Contract\Rector\ConfigurableRectorInterface;
 use Rector\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\ConfiguredCodeSample;
@@ -48,31 +49,19 @@ CODE_SAMPLE
      */
     public function getNodeTypes(): array
     {
-        return [Class_::class];
+        return [Class_::class, Interface_::class];
     }
 
     /**
-     * @param Class_ $node
+     * @param Class_|Interface_ $node
      */
     public function refactor(Node $node): ?Node
     {
-        if ($node->implements === []) {
-            return null;
+        if ($node instanceof Class_) {
+            return $this->refactorClass($node);
         }
 
-        $isInterfacesRemoved = false;
-        foreach ($node->implements as $key => $implement) {
-            if ($this->isNames($implement, $this->interfacesToRemove)) {
-                unset($node->implements[$key]);
-                $isInterfacesRemoved = true;
-            }
-        }
-
-        if (! $isInterfacesRemoved) {
-            return null;
-        }
-
-        return $node;
+        return $this->refactorInterface($node);
     }
 
     /**
@@ -84,5 +73,46 @@ CODE_SAMPLE
 
         /** @var string[] $configuration */
         $this->interfacesToRemove = $configuration;
+    }
+
+    private function refactorClass(Class_ $class): ?Class_
+    {
+        if ($class->implements === []) {
+            return null;
+        }
+
+        $isInterfacesRemoved = false;
+        foreach ($class->implements as $key => $implement) {
+            if ($this->isNames($implement, $this->interfacesToRemove)) {
+                unset($class->implements[$key]);
+                $isInterfacesRemoved = true;
+            }
+        }
+
+        if (! $isInterfacesRemoved) {
+            return null;
+        }
+
+        return $class;
+    }
+
+    private function refactorInterface(Interface_ $interface): Interface_|null
+    {
+        $isInterfacesRemoved = false;
+
+        foreach ($interface->extends as $key => $extend) {
+            if (! $this->isNames($extend, $this->interfacesToRemove)) {
+                continue;
+            }
+
+            unset($interface->extends[$key]);
+            $isInterfacesRemoved = true;
+        }
+
+        if (! $isInterfacesRemoved) {
+            return null;
+        }
+
+        return $interface;
     }
 }
