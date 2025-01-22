fcd141bb95c7d81fb4409421a53e762e52a1ff39
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-01-23 02:58:16 +0700

    [NodeTypeResolver] Clean up wrap in parentheses check ReprintNodeVisitor (#6692)

--- src/NodeTypeResolver/PHPStan/Scope/NodeVisitor/ReprintNodeVisitor.php
+++ src/NodeTypeResolver/PHPStan/Scope/NodeVisitor/ReprintNodeVisitor.php
@@ -7,7 +7,6 @@ namespace Rector\NodeTypeResolver\PHPStan\Scope\NodeVisitor;
 use PhpParser\Node;
 use PhpParser\Node\Expr\BinaryOp;
 use PhpParser\Node\Expr\BinaryOp\Coalesce;
-use PhpParser\Node\Expr\BooleanNot;
 use PhpParser\NodeVisitorAbstract;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\NodeTypeResolver\PHPStan\Scope\Contract\NodeVisitor\ScopeResolverNodeVisitorInterface;
@@ -34,12 +33,6 @@ final class ReprintNodeVisitor extends NodeVisitorAbstract implements ScopeResol
             }
 
             return $node;
-        }
-
-        if ($node instanceof BooleanNot && $node->expr instanceof BinaryOp && $node->expr->getAttribute(
-            AttributeKey::ORIGINAL_NODE
-        ) instanceof Node) {
-            $node->expr->setAttribute(AttributeKey::WRAPPED_IN_PARENTHESES, true);
         }
 
         return null;
