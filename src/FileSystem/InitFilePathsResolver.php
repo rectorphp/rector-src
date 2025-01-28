72e7ff8ad2ea6f5e63d8730243a5fbb9eaa66656
Yogesh Vaishnav <yogeshvaishnav4037@gmail.com>
2025-01-28 17:07:01 +0530

    feat: exclude root node_modules path

--- src/FileSystem/InitFilePathsResolver.php
+++ src/FileSystem/InitFilePathsResolver.php
@@ -16,7 +16,7 @@ final class InitFilePathsResolver
      * @var string
      * @see https://regex101.com/r/XkQ6Pe/1
      */
-    private const DO_NOT_INCLUDE_PATHS_REGEX = '#(vendor|var|stubs|temp|templates|tmp|e2e|bin|build|Migrations|data(?:base)?|storage|migrations|writable)#';
+    private const DO_NOT_INCLUDE_PATHS_REGEX = '#(vendor|var|stubs|temp|templates|tmp|e2e|bin|build|Migrations|data(?:base)?|storage|migrations|writable|node_modules)#';
 
     /**
      * @return string[]
