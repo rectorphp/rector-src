b92a1d722d151dbaab720349df1cc3621effa5c0
Emilien Escalle <emilien.escalle@escemi.com>
2025-02-20 08:02:03 +0100

    feat(ChangesReporting): add GitHub output formatter
    Signed-off-by: Emilien Escalle <emilien.escalle@escemi.com>

--- tests/ValueObject/Reporting/FileDiffTest.php
+++ tests/ValueObject/Reporting/FileDiffTest.php
@@ -28,4 +28,34 @@ final class FileDiffTest extends TestCase
         );
         $this->assertNull($fileDiff->getFirstLineNumber());
     }
+
+    public function testGetLastLineNumberShouldReturnLastLineNumberRegardingHunk(): void
+    {
+        $fileDiff = new FileDiff(
+            'some/file.php',
+            '--- Original\n+++ New\n@@ -38,5 +39,6 @@\nreturn true;\n}\n',
+            'diff console formatted'
+        );
+        $this->assertSame(43, $fileDiff->getLastLineNumber());
+    }
+
+    public function testGetLastLineNumberShouldBeNullWhenHunkIsInvalid(): void
+    {
+        $fileDiff = new FileDiff(
+            'some/file.php',
+            '--- Original\n+++ New\n@@@@\nreturn true;\n}\n',
+            'diff console formatted'
+        );
+        $this->assertNull($fileDiff->getLastLineNumber());
+    }
+
+    public function testGetLastLineNumberShouldReturnFirstLineWhenUndefinedInHunk(): void
+    {
+        $fileDiff = new FileDiff(
+            'some/file.php',
+            '--- Original\n+++ New\n@@ -38 +39 @@\nreturn true;\n}\n',
+            'diff console formatted'
+        );
+        $this->assertSame(38, $fileDiff->getLastLineNumber());
+    }
 }
