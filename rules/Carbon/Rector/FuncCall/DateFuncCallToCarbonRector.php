1ddf2494ccee4e451cf4692e4408823f8610714d
Golluméo <p.mauriello54@gmail.com>
2025-02-21 13:35:47 +0100

    feat(carbon): transform date/time usage into Carbon calls
    - Add DateFuncCallToCarbonRector to replace date(), strtotime(), and time() - patterns
    - Handle parent Minus node for time() - (N) or time() - (60 * 60 * 24 * N)
    - Fix detectTimeUnit() to support both LNumber and Mul
    - Reverse TIME_UNITS order to detect subWeeks() before subSeconds()
    - Update fixtures to cover seconds, minutes, hours, days, and weeks

--- rules/Carbon/Rector/FuncCall/DateFuncCallToCarbonRector.php
+++ rules/Carbon/Rector/FuncCall/DateFuncCallToCarbonRector.php
@@ -6,13 +6,18 @@ namespace Rector\Carbon\Rector\FuncCall;
 
 use PhpParser\Node;
 use PhpParser\Node\Arg;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\BinaryOp\Minus;
+use PhpParser\Node\Expr\BinaryOp\Mul;
 use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\PropertyFetch;
 use PhpParser\Node\Expr\StaticCall;
 use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Scalar\LNumber;
 use PhpParser\Node\Scalar\String_;
 use Rector\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\Exception\PoorDocumentationException;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
@@ -21,6 +26,17 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
  */
 final class DateFuncCallToCarbonRector extends AbstractRector
 {
+    private const TIME_UNITS = [
+        ['weeks', 604800],
+        ['days', 86400],
+        ['hours', 3600],
+        ['minutes', 60],
+        ['seconds', 1],
+    ];
+
+    /**
+     * @throws PoorDocumentationException
+     */
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition('Convert date() function call to Carbon::now()->format(*)', [
@@ -34,7 +50,6 @@ class SomeClass
     }
 }
 CODE_SAMPLE
-
                 ,
                 <<<'CODE_SAMPLE'
 class SomeClass
@@ -54,41 +69,193 @@ CODE_SAMPLE
      */
     public function getNodeTypes(): array
     {
-        return [FuncCall::class];
+        return [Minus::class, FuncCall::class];
     }
 
     /**
      * @param FuncCall $node
+     * @return Node|null
      */
     public function refactor(Node $node): ?Node
     {
-        if (! $this->isName($node->name, 'date') && ! $this->isName($node->name, 'strtotime')) {
+        if ($node instanceof Minus) {
+            $left = $node->left;
+            if ($left instanceof FuncCall && $this->isName($left->name, 'time')) {
+                $timeUnit = $this->detectTimeUnit($node->right);
+                if ($timeUnit !== null) {
+                    return $this->createCarbonSubtract($timeUnit);
+                }
+            }
+
             return null;
         }
 
-        if ($node->isFirstClassCallable()) {
+        if (! $node instanceof FuncCall) {
             return null;
         }
 
-        if (count($node->getArgs()) !== 1) {
-            return null;
+        if ($this->isName($node->name, 'date') && isset($node->args[1]) && $node->args[1] instanceof Arg) {
+            $format = $this->getArgValue($node, 0);
+            if (! $format instanceof Expr) {
+                return null;
+            }
+
+            $timestamp = $node->args[1]->value;
+            if ($timestamp instanceof FuncCall
+                && $this->isName($timestamp->name, 'strtotime')
+                && isset($timestamp->args[0]) && $timestamp->args[0] instanceof Arg
+            ) {
+                $dateExpr = $timestamp->args[0]->value;
+                return $this->createCarbonParseFormat($dateExpr, $format);
+            }
         }
 
-        $firstArg = $node->getArgs()[0];
-        if (! $firstArg->value instanceof String_) {
-            return null;
+        if ($this->isName($node->name, 'date') && isset($node->args[0])) {
+            $format = $this->getArgValue($node, 0);
+            if ($format instanceof String_) {
+                return $this->createCarbonNowFormat($format);
+            }
         }
 
-        if ($this->isName($node->name, 'date')) {
-            $nowStaticCall = new StaticCall(new FullyQualified('Carbon\\Carbon'), 'now');
-            return new MethodCall($nowStaticCall, 'format', [new Arg($firstArg->value)]);
-        }
-
-        if ($this->isName($node->name, 'strtotime')) {
-            $parseCall = new StaticCall(new FullyQualified('Carbon\\Carbon'), 'parse', [new Arg($firstArg->value)]);
-            return new PropertyFetch($parseCall, 'timestamp');
+        if ($this->isName($node->name, 'strtotime') && isset($node->args[0])) {
+            $dateExpr = $this->getArgValue($node, 0);
+            if ($dateExpr !== null) {
+                return $this->createCarbonParseTimestamp($dateExpr);
+            }
         }
 
         return null;
+    }
+
+    /**
+     * @param FuncCall $node
+     * @param int $index
+     * @return Expr|null
+     */
+    private function getArgValue(FuncCall $node, int $index): ?Expr
+    {
+        if (!isset($node->args[$index]) || !$node->args[$index] instanceof Arg) {
+            return null;
+        }
+
+        return $node->args[$index]->value;
+    }
+
+    private function createCarbonNowFormat(String_ $format): MethodCall
+    {
+        $nowCall = new StaticCall(
+            new FullyQualified('Carbon\\Carbon'),
+            'now'
+        );
+
+        return new MethodCall($nowCall, 'format', [new Arg($format)]);
+    }
+
+    private function createCarbonParseTimestamp(Expr $dateExpr): PropertyFetch
+    {
+        $parseCall = new StaticCall(
+            new FullyQualified('Carbon\\Carbon'),
+            'parse',
+            [new Arg($dateExpr)]
+        );
+
+        return new PropertyFetch($parseCall, 'timestamp');
+    }
+
+    private function createCarbonParseFormat(Expr $dateExpr, Expr $format): MethodCall
+    {
+        $parseCall = new StaticCall(
+            new FullyQualified('Carbon\\Carbon'),
+            'parse',
+            [new Arg($dateExpr)]
+        );
+
+        return new MethodCall($parseCall, 'format', [new Arg($format)]);
+    }
+
+    /**
+     * @param array{unit: string, value: int} $timeUnit
+     * @return PropertyFetch
+     */
+    private function createCarbonSubtract(array $timeUnit): PropertyFetch
+    {
+        $nowCall = new StaticCall(
+            new FullyQualified('Carbon\\Carbon'),
+            'now'
+        );
+        $methodName = 'sub' . ucfirst($timeUnit['unit']);
+        $subtractCall = new MethodCall($nowCall, $methodName, [new Arg(new LNumber($timeUnit['value']))]);
+        return new PropertyFetch($subtractCall, 'timestamp');
+    }
+
+    /**
+     * @param Expr $node
+     * @return array{unit: string, value: int}|null
+     */
+    private function detectTimeUnit(Expr $node): ?array
+    {
+        $product = $this->calculateProduct($node);
+        if ($product === null) {
+            return null;
+        }
+
+        foreach (self::TIME_UNITS as [$unit, $seconds]) {
+            if ($product % $seconds === 0) {
+                return [
+                    'unit' => (string) $unit,
+                    'value' => (int) ($product / $seconds)
+                ];
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @param Expr $node
+     * @return int|null
+     */
+    private function calculateProduct(Expr $node): ?int
+    {
+        if ($node instanceof LNumber) {
+            return $node->value;
+        }
+
+        if (!$node instanceof Mul) {
+            return null;
+        }
+
+        $multipliers = $this->extractMultipliers($node);
+        if ($multipliers === []) {
+            return null;
+        }
+
+        return array_product($multipliers);
+    }
+
+    /**
+     * @param Node $node
+     * @return int[]
+     */
+    private function extractMultipliers(Node $node): array
+    {
+        $multipliers = [];
+        if (!$node instanceof Mul) {
+            return $multipliers;
+        }
+
+        if ($node->left instanceof LNumber) {
+            $multipliers[] = $node->left->value;
+        } elseif ($node->left instanceof Mul) {
+            $multipliers = array_merge($multipliers, $this->extractMultipliers($node->left));
+        }
+
+        if ($node->right instanceof LNumber) {
+            $multipliers[] = $node->right->value;
+        } elseif ($node->right instanceof Mul) {
+            $multipliers = array_merge($multipliers, $this->extractMultipliers($node->right));
+        }
+
+        return $multipliers;
     }
 }
