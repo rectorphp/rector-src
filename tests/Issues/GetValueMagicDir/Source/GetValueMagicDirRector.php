d033eaf33c3b433a364152554fa695c52fe8e333
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-02-24 18:02:02 +0700

    add more use case for inside array

--- tests/Issues/GetValueMagicDir/Source/GetValueMagicDirRector.php
+++ tests/Issues/GetValueMagicDir/Source/GetValueMagicDirRector.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace Rector\Tests\Issues\GetValueMagicDir\Source;
 
+use PhpParser\Builder;
 use PhpParser\Node;
+use PhpParser\Node\ArrayItem;
+use PhpParser\Node\Expr\Array_;
 use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Expr\New_;
 use PhpParser\Node\Scalar\String_;
 use Rector\FileSystem\FilePathHelper;
+use Rector\PhpParser\Node\NodeFactory;
 use Rector\PhpParser\Node\Value\ValueResolver;
 use Rector\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
@@ -16,7 +21,7 @@ final class GetValueMagicDirRector extends AbstractRector
 {
     public function __construct(
         private readonly ValueResolver $valueResolver,
-        private readonly FilePathHelper $filePathHelper
+        private readonly FilePathHelper $filePathHelper,
     ) {
     }
 
@@ -27,12 +32,18 @@ final class GetValueMagicDirRector extends AbstractRector
 
     public function getNodeTypes(): array
     {
-        return [FuncCall::class];
+        return [FuncCall::class, New_::class];
     }
 
-    public function refactor(Node $node): String_
+    public function refactor(Node $node): String_|Array_
     {
         $value = $this->valueResolver->getValue($node->args[0]->value);
-        return new String_($this->filePathHelper->relativePath($value));
+
+        if ($node instanceof FuncCall) {
+            return new String_($this->filePathHelper->relativePath($value));
+        }
+
+        $value['bar']['foo'] = $this->filePathHelper->relativePath($value['bar']['foo']);
+        return $this->nodeFactory->createArray($value);
     }
 }
