5a833e062eafffe01921dc6b2b674d173947777f
Tomas Votruba <tomas.vot@gmail.com>
2025-03-02 16:24:33 +0100

    [code-quality] Skip complex new instance with args assigns in InlineArrayReturnAssignRector (#6762)

---
+++ rules-tests/CodeQuality/Rector/ClassMethod/InlineArrayReturnAssignRector/Source/SomeAssignedObject.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Rector\Tests\CodeQuality\Rector\ClassMethod\InlineArrayReturnAssignRector\Source;
+
+final class SomeAssignedObject
+{
+    public function __construct(
+        string $name, int $age
+    ) {
+    }
+}
