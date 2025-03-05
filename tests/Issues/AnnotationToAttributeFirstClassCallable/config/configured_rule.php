5a87240627d2b0aad900876ddbf13e65fa035c07
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-03-05 18:57:12 +0700

    [Php80][Php81] Handle crash on combine AnnotationToAttributeRector + FirstClassCallableRector

---
+++ tests/Issues/AnnotationToAttributeFirstClassCallable/config/configured_rule.php
@@ -0,0 +1,14 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\Php81\Rector\Array_\FirstClassCallableRector;
+use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
+use Rector\Php80\ValueObject\AnnotationToAttribute;
+
+return RectorConfig::configure()
+    ->withConfiguredRule(AnnotationToAttributeRector::class, [
+        new AnnotationToAttribute('Symfony\Component\Serializer\Annotation\Context'),
+    ])
+    ->withRules([
+        FirstClassCallableRector::class,
+    ]);
