44fce60ea7c5eade8107c8134eb70ddfb01e6836
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-03-14 22:27:53 +0700

    [Php84] register AddEscapeArgumentRector

--- config/set/php84.php
+++ config/set/php84.php
@@ -3,9 +3,10 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Php84\Rector\FuncCall\AddEscapeArgumentRector;
 use Rector\Php84\Rector\FuncCall\RoundingModeEnumRector;
 use Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rules([ExplicitNullableParamTypeRector::class, RoundingModeEnumRector::class]);
+    $rectorConfig->rules([ExplicitNullableParamTypeRector::class, RoundingModeEnumRector::class, AddEscapeArgumentRector::class]);
 };
