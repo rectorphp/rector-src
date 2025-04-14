e6d7ff58e2f34563f91ff1f89f7feb45be398579
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-04-14 18:01:09 +0700

    allow indirect __toString() override-a

---
+++ rules-tests/Php83/Rector/ClassMethod/AddOverrideAttributeToOverriddenMethodsRector/Source/SomeClassImplementsToString.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Rector\Tests\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector\Source;
+
+use Stringable;
+
+abstract class SomeClassImplementsToString implements Stringable
+{
+}
