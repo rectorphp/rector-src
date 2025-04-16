c4690eb38d7b61920d23a4817e60cd3b8198286d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-04-16 18:20:18 +0700

    [Configuration] Fix root standalone registered rules verify on RectorConfigBuilder (#6840)
    * [Configuration] Fix root standalone registered rules verify on RectorConfigBuilder
    
    * set default false
    
    * add test for only collect root standalone registered rules
    
    * final touch: invoke
    
    * [ci-review] Rector Rectify
    
    * final touch: direct invoke()
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

---
+++ tests/Config/RectorConfigTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\Config;
+
+use Rector\Config\RectorConfig;
+use Rector\Configuration\Option;
+use Rector\Configuration\Parameter\SimpleParameterProvider;
+use Rector\Symfony\Set\TwigSetList;
+use Rector\Testing\PHPUnit\AbstractLazyTestCase;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromReturnNewRector;
+
+final class RectorConfigTest extends AbstractLazyTestCase
+{
+    public function test(): void
+    {
+        RectorConfig::configure()
+            ->withSets([
+                TwigSetList::TWIG_134
+            ])
+            ->withRules([
+                ReturnTypeFromReturnNewRector::class
+            ])(new RectorConfig());
+
+        // only collect root withRules()
+        $this->assertCount(1, SimpleParameterProvider::provideArrayParameter(Option::ROOT_STANDALONE_REGISTERED_RULES));
+    }
+}
