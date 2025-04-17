10266dcdfd5b35b55c8f3b24dade395ff4dd7290
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-04-17 13:10:44 +0700

    Bump to PHPStan ^2.1.12

--- tests/Config/RectorConfigTest.php
+++ tests/Config/RectorConfigTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Rector\Tests\Config;
 
+use PHPUnit\Framework\Attributes\RunClassInSeparateProcess;
 use Rector\Config\RectorConfig;
 use Rector\Configuration\Option;
 use Rector\Configuration\Parameter\SimpleParameterProvider;
@@ -11,6 +12,7 @@ use Rector\Symfony\Set\TwigSetList;
 use Rector\Testing\PHPUnit\AbstractLazyTestCase;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromReturnNewRector;
 
+#[RunClassInSeparateProcess]
 final class RectorConfigTest extends AbstractLazyTestCase
 {
     public function test(): void
