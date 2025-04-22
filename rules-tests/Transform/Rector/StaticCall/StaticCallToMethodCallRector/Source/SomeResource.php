bc3a50188be303409d4d2ee86c21f5d00de0c6f9
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-04-22 17:51:40 +0700

    [Transform] Do not call parent::construct() when parent __construct() is private (#6857)

---
+++ rules-tests/Transform/Rector/StaticCall/StaticCallToMethodCallRector/Source/SomeResource.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\Transform\Rector\StaticCall\StaticCallToMethodCallRector\Source;
+
+class SomeResource
+{
+    private function __construct()
+    {
+    }
+}
