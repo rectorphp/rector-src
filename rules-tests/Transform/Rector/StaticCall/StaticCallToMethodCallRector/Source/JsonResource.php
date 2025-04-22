56f41b76a6dd713e2ba22985b9b20c3a822b8eab
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-04-22 15:11:58 +0700

    Fix fixture Source test

--- rules-tests/Transform/Rector/StaticCall/StaticCallToMethodCallRector/Source/JsonResource.php
+++ rules-tests/Transform/Rector/StaticCall/StaticCallToMethodCallRector/Source/JsonResource.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Rector\Tests\Transform\Rector\StaticCall\StaticCallToMethodCallRector\Source;
 
+// for testing under php 8.0 to avoid flip flop ClassReflection cache
 class JsonResource
 {
     public $resource;
--- rules-tests/Transform/Rector/StaticCall/StaticCallToMethodCallRector/Source/JsonResource2.php
+++ rules-tests/Transform/Rector/StaticCall/StaticCallToMethodCallRector/Source/JsonResource2.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Rector\Tests\Transform\Rector\StaticCall\StaticCallToMethodCallRector\Source;
 
+// for testing under php 7.4 to avoid flip flop ClassReflection cache
 class JsonResource2
 {
     public $resource;
