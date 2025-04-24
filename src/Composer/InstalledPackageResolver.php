cfdaba59ccc1f2d45de54aadcb3a17500d052aee
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-04-24 22:51:23 +0700

    clean up

--- src/Composer/InstalledPackageResolver.php
+++ src/Composer/InstalledPackageResolver.php
@@ -84,9 +84,11 @@ final class InstalledPackageResolver
             if (isset($projectComposerJson['config']['vendor-dir']) &&
                 is_string($projectComposerJson['config']['vendor-dir'])
             ) {
-                return PathNormalizer::normalize(
-                    realpath($projectComposerJson['config']['vendor-dir']) ?: ''
-                ) === PathNormalizer::normalize($projectComposerJson['config']['vendor-dir'])
+                $realPathVendorDir = realpath($projectComposerJson['config']['vendor-dir']) ?: '';
+                $normalizedRealPathVendorDir = PathNormalizer::normalize($realPathVendorDir);
+                $normalizedVendorDir = PathNormalizer::normalize($projectComposerJson['config']['vendor-dir']);
+
+                return $normalizedRealPathVendorDir === $normalizedVendorDir
                     ? $projectComposerJson['config']['vendor-dir']
                     : $this->projectDirectory . '/' . $projectComposerJson['config']['vendor-dir'];
             }
