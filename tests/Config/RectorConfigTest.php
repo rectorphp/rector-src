e58976b9b686ec70b676db54e0308c2eace5a493
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-04-26 13:58:48 +0700

    Add back #[RunClassInSeparateProcess] on RectorConfigTest

--- tests/Config/RectorConfigTest.php
+++ tests/Config/RectorConfigTest.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace Rector\Tests\Config;
 
+use PHPUnit\Framework\Attributes\RunClassInSeparateProcess;
 use Rector\Configuration\Option;
 use Rector\Configuration\Parameter\SimpleParameterProvider;
 use Rector\Symfony\Set\TwigSetList;
 use Rector\Testing\PHPUnit\AbstractLazyTestCase;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromReturnNewRector;
 
+#[RunClassInSeparateProcess]
 final class RectorConfigTest extends AbstractLazyTestCase
 {
     public function test(): void
