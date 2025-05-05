f11e260fcdf53446269f8fe5d23c8507ced73a62
Tomas Votruba <tomas.vot@gmail.com>
2025-05-05 18:21:39 +0200

    add symfony 3-7 set providers to SetProviderCollector

--- src/Bridge/SetProviderCollector.php
+++ src/Bridge/SetProviderCollector.php
@@ -11,6 +11,11 @@ use Rector\Set\Contract\SetProviderInterface;
 use Rector\Set\SetProvider\CoreSetProvider;
 use Rector\Set\SetProvider\PHPSetProvider;
 use Rector\Set\ValueObject\ComposerTriggeredSet;
+use Rector\Symfony\Set\SetProvider\Symfony3SetProvider;
+use Rector\Symfony\Set\SetProvider\Symfony4SetProvider;
+use Rector\Symfony\Set\SetProvider\Symfony5SetProvider;
+use Rector\Symfony\Set\SetProvider\Symfony6SetProvider;
+use Rector\Symfony\Set\SetProvider\Symfony7SetProvider;
 use Rector\Symfony\Set\SetProvider\SymfonySetProvider;
 use Rector\Symfony\Set\SetProvider\TwigSetProvider;
 
@@ -37,6 +42,11 @@ final readonly class SetProviderCollector
             new CoreSetProvider(),
             new PHPUnitSetProvider(),
             new SymfonySetProvider(),
+            new Symfony3SetProvider(),
+            new Symfony4SetProvider(),
+            new Symfony5SetProvider(),
+            new Symfony6SetProvider(),
+            new Symfony7SetProvider(),
             new DoctrineSetProvider(),
             new TwigSetProvider(),
         ];
