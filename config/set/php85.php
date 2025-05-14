5d34b43644b923b20e275f8a550017d9b169380c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-05-15 05:46:50 +0700

    Revert "[StrContainsRector] also replace multibyte functions fro strpos and s…" (#6903)
    This reverts commit 5814187fd1a142d1c0a90d8d36d1a0f3a6306a0b.

--- config/set/php85.php
+++ config/set/php85.php
@@ -6,5 +6,9 @@ use Rector\Config\RectorConfig;
 use Rector\Php85\Rector\ArrayDimFetch\ArrayFirstLastRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rules([ArrayFirstLastRector::class]);
+    $rectorConfig->rules(
+        [
+            ArrayFirstLastRector::class,
+        ]
+    );
 };
