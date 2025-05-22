f34b319c3aa02cb039176441ffbadf096e6f51f9
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-05-23 05:26:31 +0700

    [Php83] Add DynamicClassConstFetchRector

--- config/set/php83.php
+++ config/set/php83.php
@@ -7,6 +7,7 @@ use Rector\Php83\Rector\Class_\ReadOnlyAnonymousClassRector;
 use Rector\Php83\Rector\ClassConst\AddTypeToConstRector;
 use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 use Rector\Php83\Rector\FuncCall\CombineHostPortLdapUriRector;
+use Rector\Php83\Rector\FuncCall\DynamicClassConstFetchRector;
 use Rector\Php83\Rector\FuncCall\RemoveGetClassGetParentClassNoArgsRector;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -16,5 +17,6 @@ return static function (RectorConfig $rectorConfig): void {
         CombineHostPortLdapUriRector::class,
         RemoveGetClassGetParentClassNoArgsRector::class,
         ReadOnlyAnonymousClassRector::class,
+        DynamicClassConstFetchRector::class,
     ]);
 };
