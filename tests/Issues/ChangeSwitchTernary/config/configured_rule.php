676dd83aa0a910f4c184abc69365056fabab23f5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-05-24 21:14:12 +0700

    [Test] Add test for crash after reprint Stmt

--- tests/Issues/ChangeSwitchTernary/config/configured_rule.php
+++ tests/Issues/ChangeSwitchTernary/config/configured_rule.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\CodeQuality\Rector\Expression\TernaryFalseExpressionToIfRector;
 use Rector\Config\RectorConfig;
 use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
+use Rector\Tests\Issues\ChangeSwitchTernary\Source\AnotherExpressionRector;
 
 return RectorConfig::configure()
-    ->withRules([ChangeSwitchToMatchRector::class, TernaryFalseExpressionToIfRector::class]);
+    ->withRules([ChangeSwitchToMatchRector::class, TernaryFalseExpressionToIfRector::class, AnotherExpressionRector::class]);
