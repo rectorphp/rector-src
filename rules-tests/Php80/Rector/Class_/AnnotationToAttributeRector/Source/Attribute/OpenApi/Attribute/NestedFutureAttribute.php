20c76767877daf5817d010595eaf3e539a268b73
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-06-03 07:18:40 +0700

    more stubs

--- rules-tests/Php80/Rector/Class_/AnnotationToAttributeRector/Source/Attribute/OpenApi/Attribute/NestedFutureAttribute.php
+++ rules-tests/Php80/Rector/Class_/AnnotationToAttributeRector/Source/Attribute/OpenApi/Attribute/NestedFutureAttribute.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Rector\Tests\Php80\Rector\Class_\AnnotationToAttributeRector\Source\Attribute\OpenApi\Attribute;
 
+#[\Attribute]
 final class NestedFutureAttribute
 {
 }
