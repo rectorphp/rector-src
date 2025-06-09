da52751a6d28053f1dfa87abe1645f2cbdc262be
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-06-09 17:02:53 +0700

    [Php80] Add more exact Expr node that always bool for switch(true) usage on ChangeSwitchToMatchRector (#6965)
    * [Php80] Add more exact Expr node that always bool for switch(true) usage on ChangeSwitchToMatchRector
    
    * [Php80] Add more exact Expr node that always bool for switch(true) usage on ChangeSwitchToMatchRector
    
    * [Php80] Add more exact Expr node that always bool for switch(true) usage on ChangeSwitchToMatchRector
    
    * [ci-review] Rector Rectify
    
    * re-use logic
    
    * re-use logic
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

--- rules/Php80/Rector/Switch_/ChangeSwitchToMatchRector.php
+++ rules/Php80/Rector/Switch_/ChangeSwitchToMatchRector.php
@@ -7,17 +7,16 @@ namespace Rector\Php80\Rector\Switch_;
 use PhpParser\Node;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\Assign;
-use PhpParser\Node\Expr\CallLike;
 use PhpParser\Node\Expr\Cast;
 use PhpParser\Node\Expr\Cast\Int_;
 use PhpParser\Node\Expr\Cast\String_;
-use PhpParser\Node\Expr\Instanceof_;
 use PhpParser\Node\Expr\Match_;
 use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\Return_;
 use PhpParser\Node\Stmt\Switch_;
 use PHPStan\Type\ObjectType;
 use Rector\Contract\PhpParser\Node\StmtsAwareInterface;
+use Rector\NodeAnalyzer\ExprAnalyzer;
 use Rector\Php80\NodeAnalyzer\MatchSwitchAnalyzer;
 use Rector\Php80\NodeFactory\MatchFactory;
 use Rector\Php80\NodeResolver\SwitchExprsResolver;
@@ -39,7 +38,8 @@ final class ChangeSwitchToMatchRector extends AbstractRector implements MinPhpVe
         private readonly SwitchExprsResolver $switchExprsResolver,
         private readonly MatchSwitchAnalyzer $matchSwitchAnalyzer,
         private readonly MatchFactory $matchFactory,
-        private readonly ValueResolver $valueResolver
+        private readonly ValueResolver $valueResolver,
+        private readonly ExprAnalyzer $exprAnalyzer
     ) {
     }
 
@@ -211,7 +211,9 @@ CODE_SAMPLE
 
     private function mirrorDynamicBoolExpr(Match_ $match): void
     {
-        if ($this->valueResolver->isTrue($match->cond)) {
+        // switch(true) already just use
+        // switch(false) is dead code that can be on purpose
+        if ($this->valueResolver->isTrueOrFalse($match->cond)) {
             return;
         }
 
@@ -222,15 +224,11 @@ CODE_SAMPLE
             }
 
             foreach ($arm->conds as $cond) {
-                if ($cond instanceof Instanceof_ || $cond instanceof CallLike) {
-                    $type = $this->nodeTypeResolver->getNativeType($cond);
-
-                    if ($type->isBoolean()->yes()) {
-                        // dont' stop lookup for dynamic conditions
-                        // continue verify other condition, in case of mixed condition
-                        $isChanged = true;
-                        continue;
-                    }
+                if ($this->exprAnalyzer->isBoolExpr($cond) || $this->exprAnalyzer->isCallLikeReturnNativeBool($cond)) {
+                    // dont' stop lookup for dynamic conditions
+                    // continue verify other condition, in case of mixed condition
+                    $isChanged = true;
+                    continue;
                 }
 
                 // return early here, as condition is mixed
