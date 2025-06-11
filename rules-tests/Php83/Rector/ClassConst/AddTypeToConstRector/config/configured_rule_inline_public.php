3dad3fc89f73cc52ee487187c648342f00521bbc
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-06-11 19:13:22 +0700

    [Alternative 2] Make AddTypeToConstRector configurable to set inline_public config to allow non-final class to have typed constant

---
+++ rules-tests/Php83/Rector/ClassConst/AddTypeToConstRector/config/configured_rule_inline_public.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php83\Rector\ClassConst\AddTypeToConstRector;
+use Rector\ValueObject\PhpVersion;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->ruleWithConfiguration(AddTypeToConstRector::class, [
+       AddTypeToConstRector::INLINE_PUBLIC => true,
+    ]);
+
+    $rectorConfig->phpVersion(PhpVersion::PHP_83);
+};
