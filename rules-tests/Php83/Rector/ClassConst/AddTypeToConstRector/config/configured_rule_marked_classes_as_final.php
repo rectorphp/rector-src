341fbe7020e075e807e2debb45b6c5b9cfafaa51
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-06-16 18:12:36 +0700

    [Alternative 1] Add withTreatClassesAsFinal() on RectorConfigBuilder so can be used in typed rules (#6974)
    * [Alternative 1] Add withTreatClassesAsFinal() on RectorConfigBuilder so can be used in typed rules
    
    * [ci-review] Rector Rectify
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

---
+++ rules-tests/Php83/Rector/ClassConst/AddTypeToConstRector/config/configured_rule_marked_classes_as_final.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php83\Rector\ClassConst\AddTypeToConstRector;
+use Rector\ValueObject\PhpVersion;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rule(AddTypeToConstRector::class);
+    $rectorConfig->treatClassesAsFinal(true);
+
+    $rectorConfig->phpVersion(PhpVersion::PHP_83);
+};
