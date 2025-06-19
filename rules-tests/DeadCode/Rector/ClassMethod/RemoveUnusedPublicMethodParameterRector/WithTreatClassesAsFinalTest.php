39bd9f8bd699922599fe7eb8a88949a97f959934
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-06-19 18:02:43 +0700

    [DeadCode] Skip abstract method on RemoveUnusedPublicMethodParameterRector

---
+++ rules-tests/DeadCode/Rector/ClassMethod/RemoveUnusedPublicMethodParameterRector/WithTreatClassesAsFinalTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\DeadCode\Rector\ClassMethod\RemoveUnusedPublicMethodParameterRector;
+
+use Iterator;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+final class WithTreatClassesAsFinalTest extends AbstractRectorTestCase
+{
+    #[DataProvider('provideData')]
+    public function test(string $filePath): void
+    {
+        $this->doTestFile($filePath);
+    }
+
+    public static function provideData(): Iterator
+    {
+        return self::yieldFilesFromDirectory(__DIR__ . '/FixtureTreatClassesAsFinal');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/configured_rule_with_treat_classes_as_final.php';
+    }
+}
---
+++ rules-tests/DeadCode/Rector/ClassMethod/RemoveUnusedPublicMethodParameterRector/config/configured_rule_with_treat_classes_as_final.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPublicMethodParameterRector;
+
+return RectorConfig::configure()
+    ->withRules([RemoveUnusedPublicMethodParameterRector::class])
+    ->withTreatClassesAsFinal();
