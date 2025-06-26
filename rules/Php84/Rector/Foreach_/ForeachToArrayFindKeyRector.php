8fda447edc388bbde9cc7eb09af69ae71d468d2d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-06-26 23:25:17 +0700

    [Php84] Skip non-array on ForeachToArray* rules

--- rules/Php84/Rector/Foreach_/ForeachToArrayFindKeyRector.php
+++ rules/Php84/Rector/Foreach_/ForeachToArrayFindKeyRector.php
@@ -180,6 +180,11 @@ CODE_SAMPLE
             return false;
         }
 
-        return $this->nodeComparator->areNodesEqual($assignment->expr, $foreach->keyVar);
+        if (! $this->nodeComparator->areNodesEqual($assignment->expr, $foreach->keyVar)) {
+            return false;
+        }
+
+        $type = $this->nodeTypeResolver->getNativeType($foreach->expr);
+        return $type->isArray()->yes();
     }
 }
