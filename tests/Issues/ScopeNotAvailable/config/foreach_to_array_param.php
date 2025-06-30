b08eb688434a11064a40a27610eb1114acf47b18
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-06-30 15:43:12 +0700

    rec

--- tests/Issues/ScopeNotAvailable/config/foreach_to_array_param.php
+++ tests/Issues/ScopeNotAvailable/config/foreach_to_array_param.php
@@ -1,12 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\ClassMethod\OptionalParametersAfterRequiredRector;
 use Rector\Config\RectorConfig;
+use Rector\Php84\Rector\Foreach_\ForeachToArrayFindRector;
 use Rector\ValueObject\PhpVersion;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rules([
-        \Rector\Php84\Rector\Foreach_\ForeachToArrayFindRector::class,
-        \Rector\CodeQuality\Rector\ClassMethod\OptionalParametersAfterRequiredRector::class,
-    ]);
+    $rectorConfig->rules([ForeachToArrayFindRector::class, OptionalParametersAfterRequiredRector::class]);
+
     $rectorConfig->phpVersion(PhpVersion::PHP_84);
 };
