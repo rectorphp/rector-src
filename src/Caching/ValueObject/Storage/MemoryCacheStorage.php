89dcff65a159207ad0c87c1b340f026784d1a207
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-07-08 16:55:42 +0700

    clean up

--- src/Caching/ValueObject/Storage/MemoryCacheStorage.php
+++ src/Caching/ValueObject/Storage/MemoryCacheStorage.php
@@ -31,7 +31,7 @@ final class MemoryCacheStorage implements CacheStorageInterface
      */
     public function load(string $key, string $variableKey): mixed
     {
-        if (!isset($this->storage[$key]) && ($this->hasNativeCacheEngine && apcu_exists($key))) {
+        if (!isset($this->storage[$key]) && $this->hasNativeCacheEngine && apcu_exists($key)) {
             $this->storage[$key] = new CacheItem($variableKey, apcu_fetch($key));
         }
 
