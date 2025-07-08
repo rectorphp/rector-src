2a1610509bdcb6f95e3c82bc32333282453b1f56
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-07-08 16:54:07 +0700

    try enable apcu extension on CI, then use default MemoryCacheStorage

--- e2e/parallel-kaizen-applied-rules/rector.php
+++ e2e/parallel-kaizen-applied-rules/rector.php
@@ -2,15 +2,12 @@
 
 declare(strict_types=1);
 
-use Rector\Caching\ValueObject\Storage\FileCacheStorage;
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodRector;
 use Rector\DeadCode\Rector\Property\RemoveUnusedPrivatePropertyRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->parallel(maxNumberOfProcess: 2, jobSize: 1);
-    $rectorConfig->cacheClass(FileCacheStorage::class);
-    $rectorConfig->cacheDirectory(sys_get_temp_dir() . '/rector');
 
     $rectorConfig->paths([
         __DIR__ . '/src/',
