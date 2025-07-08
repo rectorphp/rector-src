7d50ba9b484f8261d49af0d30de28d317d61b075
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-07-08 17:04:21 +0700

    set cache class to make reliable result

--- e2e/parallel-kaizen-applied-rules/rector.php
+++ e2e/parallel-kaizen-applied-rules/rector.php
@@ -2,12 +2,16 @@
 
 declare(strict_types=1);
 
+use Rector\Caching\ValueObject\Storage\MemoryCacheStorage;
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodRector;
 use Rector\DeadCode\Rector\Property\RemoveUnusedPrivatePropertyRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->parallel(maxNumberOfProcess: 2, jobSize: 1);
+
+    // this to make reliable result with local (that default to use FileCacheStorage) vs CI (that default to use MemoryCacheStorage)
+    $rectorConfig->cacheClass(MemoryCacheStorage::class);
 
     $rectorConfig->paths([
         __DIR__ . '/src/',
