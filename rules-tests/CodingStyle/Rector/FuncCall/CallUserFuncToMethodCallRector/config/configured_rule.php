851104219c0929768c38143b2e744609bdd7d421
Pablo Largo Mohedano <devnix.code@gmail.com>
2025-07-18 01:47:56 +0200

    Add `ClosureFromCallableToFirstClassCallableRector` rule

--- rules-tests/CodingStyle/Rector/FuncCall/CallUserFuncToMethodCallRector/config/configured_rule.php
+++ rules-tests/CodingStyle/Rector/FuncCall/CallUserFuncToMethodCallRector/config/configured_rule.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use Rector\CodingStyle\Rector\FuncCall\CallUserFuncToMethodCallRector;
 use Rector\Config\RectorConfig;
+use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
+    ->withPhpVersion(PhpVersion::PHP_81)
     ->withRules([CallUserFuncToMethodCallRector::class]);
