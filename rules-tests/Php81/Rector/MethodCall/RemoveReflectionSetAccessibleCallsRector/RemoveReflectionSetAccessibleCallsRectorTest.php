5b59464b0a17c34b1f492898660e25cedb02841b
NickSdot <catch.github.com.nicksdot@nicksdot.dev>
2025-07-23 09:29:18 +0700

    feat: added `RemoveDeprecatedReflectionSetAccessibleCallsRector`

---
+++ rules-tests/Php81/Rector/MethodCall/RemoveReflectionSetAccessibleCallsRector/RemoveReflectionSetAccessibleCallsRectorTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\Php81\Rector\MethodCall\RemoveReflectionSetAccessibleCallsRector;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+final class RemoveReflectionSetAccessibleCallsRectorTest extends AbstractRectorTestCase
+{
+    #[DataProvider('provideData')]
+    public function testRule(string $filePath): void
+    {
+        $this->doTestFile($filePath);
+    }
+
+    /**
+     * @return \Iterator<array<string>>
+     */
+    public static function provideData(): \Iterator
+    {
+        return self::yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/configured_rule.php';
+    }
+}
