279dc045e8fdd57241cb007c10739278625470b6
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-07-26 20:40:53 +0700

    [DeadCode] Skip Templated type on param and return on RemoveUselessParamTagRector and RemoveUselessReturnTagRector under IntersectionType

---
+++ tests/Issues/TemplatedTypeOnParamAndReturn/config/configured_rule.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
+
+return RectorConfig::configure()
+    ->withRules([
+        RemoveUselessParamTagRector::class,
+        RemoveUselessReturnTagRector::class
+    ]);
