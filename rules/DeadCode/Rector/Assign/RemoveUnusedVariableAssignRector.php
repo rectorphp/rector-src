e581023d93b16d9cfd795b093c1167ef553d8b3c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-08-05 11:05:02 +0700

    Update rules/DeadCode/Rector/Assign/RemoveUnusedVariableAssignRector.php

--- rules/DeadCode/Rector/Assign/RemoveUnusedVariableAssignRector.php
+++ rules/DeadCode/Rector/Assign/RemoveUnusedVariableAssignRector.php
@@ -154,7 +154,7 @@ CODE_SAMPLE
                 return false;
             }
 
-            return $this->isName($node, 'compact') || $this->isName($node, 'get_defined_vars');
+            return $this->isNames($node, ['compact', 'get_defined_vars']);
         });
     }
 
