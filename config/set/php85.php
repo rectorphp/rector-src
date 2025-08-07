c11c9d708ea5854710aacb53c0b344b7137db55a
Matthias Schmidt <matthias@mttsch.dev>
2025-08-07 06:10:06 +0200

    [Php85] Remove arg from get_defined_functions calls

--- config/set/php85.php
+++ config/set/php85.php
@@ -4,7 +4,17 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Php85\Rector\ArrayDimFetch\ArrayFirstLastRector;
+use Rector\Removing\Rector\FuncCall\RemoveFuncCallArgRector;
+use Rector\Removing\ValueObject\RemoveFuncCallArg;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rules([ArrayFirstLastRector::class]);
+
+    // https://wiki.php.net/rfc/deprecations_php_8_5#deprecate_the_exclude_disabled_parameter_of_get_defined_functions
+    $rectorConfig->ruleWithConfiguration(
+        RemoveFuncCallArgRector::class,
+        [
+            new RemoveFuncCallArg('get_defined_functions', 0),
+        ],
+    );
 };
