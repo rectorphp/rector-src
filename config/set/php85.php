19628294b909d73856fd65fa453103397a964f74
Matthias Schmidt <matthias@mttsch.dev>
2025-08-07 06:22:34 +0200

    [Php85] Remove deprecated arg from openssl_pkey_derive calls

--- config/set/php85.php
+++ config/set/php85.php
@@ -4,7 +4,16 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Php85\Rector\ArrayDimFetch\ArrayFirstLastRector;
+use Rector\Removing\Rector\FuncCall\RemoveFuncCallArgRector;
+use Rector\Removing\ValueObject\RemoveFuncCallArg;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rules([ArrayFirstLastRector::class]);
+
+    $rectorConfig->ruleWithConfiguration(
+        RemoveFuncCallArgRector::class,
+        [
+            new RemoveFuncCallArg('openssl_pkey_derive', 2),
+        ]
+    );
 };
