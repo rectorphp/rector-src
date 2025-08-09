ad470ba5736cb475692253770b0484317002b5a5
Matthias Schmidt <matthias@mttsch.dev>
2025-08-09 11:54:21 +0200

    [Php85] Rename deprecated SplObjectStorage methods (#7123)
    Co-authored-by: Abdul Malik Ikhsan <samsonasik@gmail.com>

--- config/set/php85.php
+++ config/set/php85.php
@@ -4,11 +4,23 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Php85\Rector\ArrayDimFetch\ArrayFirstLastRector;
+use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
+use Rector\Renaming\ValueObject\MethodCallRename;
 use Rector\Renaming\Rector\FuncCall\RenameFunctionRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rules([ArrayFirstLastRector::class]);
 
+    // https://wiki.php.net/rfc/deprecations_php_8_5#deprecate_splobjectstoragecontains_splobjectstorageattach_and_splobjectstoragedetach
+    $rectorConfig->ruleWithConfiguration(
+        RenameMethodRector::class,
+        [
+            new MethodCallRename('SplObjectStorage', 'contains', 'offsetExists'),
+            new MethodCallRename('SplObjectStorage', 'attach', 'offsetSet'),
+            new MethodCallRename('SplObjectStorage', 'detach', 'offsetUnset'),
+        ]
+    );
+  
     $rectorConfig->ruleWithConfiguration(
         RenameFunctionRector::class,
         [
