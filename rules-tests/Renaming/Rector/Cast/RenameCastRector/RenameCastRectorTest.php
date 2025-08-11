7843966f9f7b21da8559d56736fdd1afc78c7174
Matthias Schmidt <matthias@mttsch.dev>
2025-08-11 08:41:51 +0200

    [Renaming] Add `RenameCastRector` (#7117)
    While there is already `RealToFloatTypeCastRector` to handle the deprecation of the `(real)` cast, this generalized rector rule will allow to configure the cast renaming and thus also support other types of cast renamings.

---
+++ rules-tests/Renaming/Rector/Cast/RenameCastRector/RenameCastRectorTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Rector\Tests\Renaming\Rector\Cast\RenameCastRector;
+
+use Iterator;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+class RenameCastRectorTest extends AbstractRectorTestCase
+{
+    #[DataProvider('provideData')]
+    public function test(string $filePath): void
+    {
+        $this->doTestFile($filePath);
+    }
+
+    public static function provideData(): Iterator
+    {
+        return self::yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/configured_rule.php';
+    }
+}
