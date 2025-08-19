3e16e9a79a1c0e5efdd074b08c1e5b9d30e4c139
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-08-19 20:36:38 +0700

    [Renaming] Keep parentheses on RenameCastRector

--- rules-tests/Renaming/Rector/Cast/RenameCastRector/config/configured_rule.php
+++ rules-tests/Renaming/Rector/Cast/RenameCastRector/config/configured_rule.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use PhpParser\Node\Expr\Cast\Double;
+use PhpParser\Node\Expr\Cast\Int_;
 use Rector\Config\RectorConfig;
 use Rector\Renaming\Rector\Cast\RenameCastRector;
 use Rector\Renaming\ValueObject\RenameCast;
@@ -12,5 +13,6 @@ return static function (RectorConfig $rectorConfig): void {
         ->ruleWithConfiguration(RenameCastRector::class, [
             new RenameCast(Double::class, Double::KIND_REAL, Double::KIND_FLOAT),
             new RenameCast(Double::class, Double::KIND_DOUBLE, Double::KIND_FLOAT),
+            new RenameCast(Int_::class, Int_::KIND_INTEGER, Int_::KIND_INT),
         ]);
 };
