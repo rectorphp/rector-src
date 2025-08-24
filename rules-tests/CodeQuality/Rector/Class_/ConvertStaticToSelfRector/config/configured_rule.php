f78209f9384ab11eb515bcf702f08fc83b5b5900
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-08-24 08:55:03 +0700

    [CodeQuality] Skip @final doc with public class constant on ConvertStaticToSelfRector (#7170)
    * [CodeQuality] Skip @final doc with public class constant on ConvertStaticToSelfRector
    
    * reduce else
    
    * update config

--- rules-tests/CodeQuality/Rector/Class_/ConvertStaticToSelfRector/config/configured_rule.php
+++ rules-tests/CodeQuality/Rector/Class_/ConvertStaticToSelfRector/config/configured_rule.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\ConvertStaticToSelfRector;
 use Rector\Config\RectorConfig;
+use Rector\ValueObject\PhpVersionFeature;
 
 return RectorConfig::configure()
-    ->withRules([ConvertStaticToSelfRector::class]);
+    ->withRules([ConvertStaticToSelfRector::class])
+    ->withPhpVersion(PhpVersionFeature::FINAL_CLASS_CONSTANTS);
