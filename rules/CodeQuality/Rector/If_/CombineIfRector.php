cad4fb3591892e73b308fb632907013ebedc9360
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-08-27 22:23:04 +0700

    [CodeQuality] Handle parentheses inner if with assign on CombineIfRector

--- rules/CodeQuality/Rector/If_/CombineIfRector.php
+++ rules/CodeQuality/Rector/If_/CombineIfRector.php
@@ -98,6 +98,10 @@ CODE_SAMPLE
             $cond = $cond->right;
         }
 
+        if ($subIf->cond instanceof BinaryOp && ! $subIf->cond->left instanceof BinaryOp) {
+            $subIf->cond->left->setAttribute(AttributeKey::ORIGINAL_NODE, null);
+        }
+
         $node->cond = new BooleanAnd($node->cond, $subIf->cond);
 
         $node->stmts = $subIf->stmts;
