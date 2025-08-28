3bbaeb444fb61d4b4600b54a02f7736be7952c84
Arshid <arshid@Arshids-MacBook-Air.local>
2025-08-28 22:13:55 +0530

    Wrap chr() argument with % 256 to avoid deprecated out-of-range integers

---
+++ rules/Php85/Rector/FuncCall/ChrArgModuloRector.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Php85\Rector\FuncCall;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\BinaryOp\Mod;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Scalar\LNumber;
+use Rector\Rector\AbstractRector;
+use Rector\ValueObject\PhpVersionFeature;
+use Rector\VersionBonding\Contract\MinPhpVersionInterface;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+final class ChrArgModuloRector extends AbstractRector implements MinPhpVersionInterface
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Wrap chr() argument with % 256 to avoid deprecated out-of-range integers',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+echo chr(300);
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+echo chr(300 % 256);
+CODE_SAMPLE
+                ),
+            ]
+        );
+    }
+
+    public function getNodeTypes(): array
+    {
+        return [FuncCall::class];
+    }
+
+    /**
+     * @param FuncCall $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->isName($node, 'chr')) {
+            return null;
+        }
+
+        $args = $node->getArgs();
+
+        if (! isset($node->args[0])) {
+            return null;
+        }
+
+        $argExpr = $args[0]->value;
+
+        if ($argExpr instanceof Mod) {
+            return null;
+        }
+
+        $args[0]->value = new Mod($argExpr, new LNumber(256));
+        $node->args = $args;
+
+        return $node;
+    }
+
+    public function provideMinPhpVersion(): int
+    {
+        return PhpVersionFeature::DEPRECATE_OUTSIDE_INTERVEL_VAL_IN_CHR_FUNCTION;
+    }
+}
