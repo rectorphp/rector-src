913e1b37fb3a90e159940ba252983a8f3e794e82
TomasVotruba <924196+TomasVotruba@users.noreply.github.com>
2025-08-31 00:37:20 +0000

    [automated] Apply Coding Standard

--- src/NodeTypeResolver/NodeTypeResolver.php
+++ src/NodeTypeResolver/NodeTypeResolver.php
@@ -255,14 +255,6 @@ final class NodeTypeResolver
         return TypeCombinator::containsNull($nodeType);
     }
 
-    private function correctType(Type $type): Type
-    {
-        $type = $this->accessoryNonEmptyStringTypeCorrector->correct($type);
-        $type = $this->genericClassStringTypeCorrector->correct($type);
-
-        return $this->accessoryNonEmptyArrayTypeCorrector->correct($type);
-    }
-
     public function getNativeType(Expr $expr): Type
     {
         $scope = $expr->getAttribute(AttributeKey::SCOPE);
@@ -374,6 +366,14 @@ final class NodeTypeResolver
         }
 
         return $classReflection->hasTraitUse($objectType->getClassName());
+    }
+
+    private function correctType(Type $type): Type
+    {
+        $type = $this->accessoryNonEmptyStringTypeCorrector->correct($type);
+        $type = $this->genericClassStringTypeCorrector->correct($type);
+
+        return $this->accessoryNonEmptyArrayTypeCorrector->correct($type);
     }
 
     /**
