b684aff06bfe2b55b220d76af01a4d314e54c576
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-09-01 18:28:38 +0700

    SetList more removal

--- src/Configuration/RectorConfigBuilder.php
+++ src/Configuration/RectorConfigBuilder.php
@@ -31,8 +31,6 @@ use Rector\Set\Enum\SetGroup;
 use Rector\Set\SetManager;
 use Rector\Set\ValueObject\DowngradeLevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Rector\Symfony\Set\JMSSetList;
-use Rector\Symfony\Set\SensiolabsSetList;
 use Rector\Symfony\Set\SymfonySetList;
 use Rector\ValueObject\Configuration\LevelOverflow;
 use Rector\ValueObject\PhpVersion;
@@ -507,14 +505,6 @@ final class RectorConfigBuilder
 
         if ($phpunit || $all) {
             $this->sets[] = PHPUnitSetList::ANNOTATIONS_TO_ATTRIBUTES;
-        }
-
-        if ($jms || $all) {
-            $this->sets[] = JMSSetList::ANNOTATIONS_TO_ATTRIBUTES;
-        }
-
-        if ($sensiolabs || $all) {
-            $this->sets[] = SensiolabsSetList::ANNOTATIONS_TO_ATTRIBUTES;
         }
 
         if ($behat || $all) {
