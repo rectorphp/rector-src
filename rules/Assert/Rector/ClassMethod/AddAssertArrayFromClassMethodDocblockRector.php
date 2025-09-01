9df9d12f4e00278d03febde68609c225680dfb4f
Tomas Votruba <tomas.vot@gmail.com>
2025-09-01 14:20:07 +0200

    add ...

---
+++ rules/Assert/Rector/ClassMethod/AddAssertArrayFromClassMethodDocblockRector.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Rector\Assert\Rector\ClassMethod;
+
+use Rector\Rector\AbstractRector;
+
+final class AddAssertArrayFromClassMethodDocblockRector extends AbstractRector
+{
+    // @todo
+}
