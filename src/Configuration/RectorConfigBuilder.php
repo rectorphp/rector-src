a1d17e253cba75622b6d74f3b040a716956e1c48
Tomas Votruba <tomas.vot@gmail.com>
2025-09-01 17:08:45 +0200

    make use of internal set list

--- src/Configuration/RectorConfigBuilder.php
+++ src/Configuration/RectorConfigBuilder.php
@@ -31,6 +31,7 @@ use Rector\Set\Enum\SetGroup;
 use Rector\Set\SetManager;
 use Rector\Set\ValueObject\DowngradeLevelSetList;
 use Rector\Set\ValueObject\SetList;
+use Rector\Symfony\Set\SymfonyInternalSetList;
 use Rector\Symfony\Set\SymfonySetList;
 use Rector\ValueObject\Configuration\LevelOverflow;
 use Rector\ValueObject\PhpVersion;
@@ -504,15 +505,15 @@ final class RectorConfigBuilder
         }
 
         if ($fosRest || $all) {
-            $this->sets[] = __DIR__ . '/../../vendor/rector/rector-symfony/config/sets/fosrest/annotations-to-attributes.php';
+            $this->sets[] = SymfonyInternalSetList::FOS_REST_ANNOTATIONS_TO_ATTRIBUTES;
         }
 
         if ($jms || $all) {
-            $this->sets[] = __DIR__ . '/../../vendor/rector/rector-symfony/config/sets/jms/annotations-to-attributes.php';
+            $this->sets[] = SymfonyInternalSetList::JMS_ANNOTATIONS_TO_ATTRIBUTES;
         }
 
         if ($sensiolabs || $all) {
-            $this->sets[] = __DIR__ . '/../../vendor/rector/rector-symfony/config/sets/sensiolabs/annotations-to-attributes.php';
+            $this->sets[] = SymfonyInternalSetList::SENSIOLABS_ANNOTATIONS_TO_ATTRIBUTES;
         }
 
         if ($phpunit || $all) {
