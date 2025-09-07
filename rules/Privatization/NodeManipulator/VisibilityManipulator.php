6632c2f4f2be5e4d97c7ec9c283397df1898f2ac
Arshid <arshid@Arshids-MacBook-Air.local>
2025-09-08 00:39:37 +0530

    Support Param nodes in makeNonFinal()

--- rules/Privatization/NodeManipulator/VisibilityManipulator.php
+++ rules/Privatization/NodeManipulator/VisibilityManipulator.php
@@ -58,7 +58,7 @@ final class VisibilityManipulator
     /**
      * @api
      */
-    public function makeFinal(Class_ | ClassMethod | ClassConst $node): void
+    public function makeFinal(Class_ | ClassMethod | Param | ClassConst $node): void
     {
         $this->addVisibilityFlag($node, Visibility::FINAL);
     }
@@ -66,7 +66,7 @@ final class VisibilityManipulator
     /**
      * @api
      */
-    public function makeNonFinal(Class_ | ClassMethod $node): void
+    public function makeNonFinal(Class_ | ClassMethod | Param $node): void
     {
         if (! $node->isFinal()) {
             return;
