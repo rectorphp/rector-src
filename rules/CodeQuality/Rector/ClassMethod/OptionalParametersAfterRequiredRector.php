76a15e03c5dab3178e88d352f8856c4a07c7d001
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-09-07 14:23:57 +0700

    [CodeQuality] Fill reasonable default value on OptionalParametersAfterRequiredRector

--- rules/CodeQuality/Rector/ClassMethod/OptionalParametersAfterRequiredRector.php
+++ rules/CodeQuality/Rector/ClassMethod/OptionalParametersAfterRequiredRector.php
@@ -14,9 +14,13 @@ use PhpParser\Node\IntersectionType;
 use PhpParser\Node\Name;
 use PhpParser\Node\NullableType;
 use PhpParser\Node\Param;
+use PhpParser\Node\Scalar\Float_;
+use PhpParser\Node\Scalar\Int_;
+use PhpParser\Node\Scalar\String_;
 use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Function_;
 use PhpParser\Node\UnionType;
+use Rector\PhpParser\Node\Value\ValueResolver;
 use Rector\Rector\AbstractRector;
 use Rector\ValueObject\PhpVersionFeature;
 use Rector\VersionBonding\Contract\MinPhpVersionInterface;
@@ -28,6 +32,11 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
  */
 final class OptionalParametersAfterRequiredRector extends AbstractRector implements MinPhpVersionInterface
 {
+    public function __construct(
+        private readonly ValueResolver $valueResolver,
+    ) {
+    }
+
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition('Add null default value when a required parameter follows an optional one', [
@@ -84,39 +93,7 @@ CODE_SAMPLE
             $previousParam = $node->params[$key - 1] ?? null;
             if ($previousParam instanceof Param && $previousParam->default instanceof Expr) {
                 $hasChanged = true;
-
-                $param->default = new ConstFetch(new Name('null'));
-
-                if (! $param->type instanceof Node) {
-                    continue;
-                }
-
-                if ($param->type instanceof NullableType) {
-                    continue;
-                }
-
-                if ($param->type instanceof UnionType) {
-                    foreach ($param->type->types as $unionedType) {
-                        if ($unionedType instanceof Identifier && $this->isName($unionedType, 'null')) {
-                            continue 2;
-                        }
-                    }
-
-                    $param->type->types[] = new Identifier('null');
-                    continue;
-                }
-
-                if ($param->type instanceof IntersectionType) {
-                    $param->type = new UnionType([$param->type, new Identifier('null')]);
-
-                    continue;
-                }
-
-                if ($param->type instanceof ComplexType) {
-                    continue;
-                }
-
-                $param->type = new NullableType($param->type);
+                $this->processParam($param);
             }
         }
 
@@ -126,5 +103,88 @@ CODE_SAMPLE
     public function provideMinPhpVersion(): int
     {
         return PhpVersionFeature::NULLABLE_TYPE;
+    }
+
+    /**
+     * Look first found type reasonable value
+     *
+     * @param Node[] $types
+     */
+    private function mapReasonableParamValue(array $types): Expr
+    {
+        foreach ($types as $type) {
+            if ($this->isName($type, 'string')) {
+                return new String_('');
+            }
+
+            if ($this->isName($type, 'int')) {
+                return new Int_(0);
+            }
+
+            if ($this->isName($type, 'float')) {
+                return new Float_(0.0);
+            }
+
+            if ($this->isName($type, 'bool')) {
+                return $this->nodeFactory->createFalse();
+            }
+
+            if ($this->isName($type, 'array')) {
+                return $this->nodeFactory->createArray([]);
+            }
+        }
+
+        return new ConstFetch(new Name('null'));
+    }
+
+    private function processParam(Param $param): void
+    {
+        if (! $param->type instanceof Node) {
+            $param->default = new ConstFetch(new Name('null'));
+            return;
+        }
+
+        if ($param->type instanceof NullableType) {
+            $param->default = new ConstFetch(new Name('null'));
+            return;
+        }
+
+        if ($param->type instanceof IntersectionType) {
+            $param->default = new ConstFetch(new Name('null'));
+            $param->type = new UnionType([$param->type, new Identifier('null')]);
+            return;
+        }
+
+        if ($param->type instanceof UnionType) {
+            foreach ($param->type->types as $unionedType) {
+                if ($unionedType instanceof Identifier && $this->isName($unionedType, 'null')) {
+                    $param->default = new ConstFetch(new Name('null'));
+                    return;
+                }
+            }
+
+            $reasonableValue = $this->mapReasonableParamValue($param->type->types);
+            if ($this->valueResolver->isNull($reasonableValue)) {
+                $param->default = new ConstFetch(new Name('null'));
+                $param->type->types[] = new Identifier('null');
+                return;
+            }
+
+            $param->default = $reasonableValue;
+            return;
+        }
+
+        if ($param->type instanceof ComplexType) {
+            return;
+        }
+
+        $reasonableValue = $this->mapReasonableParamValue([$param->type]);
+        if ($this->valueResolver->isNull($reasonableValue)) {
+            $param->type = new NullableType($param->type);
+            $param->default = new ConstFetch(new Name('null'));
+            return;
+        }
+
+        $param->default = $reasonableValue;
     }
 }
