5d1a9e059950a5a0bf7c809438bf753bc7609ac1
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-09-10 18:54:30 +0700

    [AutoImport] Allow on FQCN current same class (part 2)

--- rules/CodingStyle/ClassNameImport/ClassNameImportSkipVoter/ClassLikeNameClassNameImportSkipVoter.php
+++ rules/CodingStyle/ClassNameImport/ClassNameImportSkipVoter/ClassLikeNameClassNameImportSkipVoter.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Rector\CodingStyle\ClassNameImport\ClassNameImportSkipVoter;
 
 use PhpParser\Node;
+use PHPStan\Analyser\Scope;
 use Rector\CodingStyle\ClassNameImport\ShortNameResolver;
 use Rector\CodingStyle\Contract\ClassNameImport\ClassNameImportSkipVoterInterface;
+use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\StaticTypeMapper\ValueObject\Type\FullyQualifiedObjectType;
 use Rector\ValueObject\Application\File;
 
@@ -33,9 +35,25 @@ final readonly class ClassLikeNameClassNameImportSkipVoter implements ClassNameI
             return false;
         }
 
+        $scope = $node->getAttribute(AttributeKey::SCOPE);
+        $namespace = $scope instanceof Scope ? $scope->getNamespace() : null;
+        $namespace = strtolower((string) $namespace);
+
         $shortNameLowered = $fullyQualifiedObjectType->getShortNameLowered();
+        $fullyQualifiedObjectTypeNamespace = strtolower(
+            substr($fullyQualifiedObjectType->getClassName(), 0, -strlen($fullyQualifiedObjectType->getShortName()) - 1)
+        );
+
         foreach ($classLikeNames as $classLikeName) {
-            if (strtolower($classLikeName) === $shortNameLowered) {
+            if (strtolower($classLikeName) !== $shortNameLowered) {
+                continue;
+            }
+
+            if ($namespace === '') {
+                return true;
+            }
+
+            if ($namespace !== $fullyQualifiedObjectTypeNamespace) {
                 return true;
             }
         }
