dde5d4255607822c39e83ca7e928a566e52bfe60
Tomas Votruba <tomas.vot@gmail.com>
2025-09-12 14:21:56 +0200

    [type-declaration-docblocks] add nesting support to DocblockReturnArrayFromDirectArrayInstanceRector (#7255)

--- rules/TypeDeclarationDocblocks/Rector/ClassMethod/DocblockReturnArrayFromDirectArrayInstanceRector.php
+++ rules/TypeDeclarationDocblocks/Rector/ClassMethod/DocblockReturnArrayFromDirectArrayInstanceRector.php
@@ -113,10 +113,7 @@ CODE_SAMPLE
             return null;
         }
 
-        $genericKeyType = $this->constantToGenericType($returnedType->getKeyType());
-        $genericItemType = $this->constantToGenericType($returnedType->getItemType());
-
-        $genericTypeNode = $this->createArrayGenericTypeNode($genericKeyType, $genericItemType);
+        $genericTypeNode = $this->createGenericArrayTypeFromConstantArrayType($returnedType);
 
         $returnTagValueNode = new ReturnTagValueNode($genericTypeNode, '');
         $phpDocInfo->addTagValueNode($returnTagValueNode);
@@ -151,10 +148,29 @@ CODE_SAMPLE
         return new MixedType();
     }
 
-    private function createArrayGenericTypeNode(Type $keyType, Type $itemType): GenericTypeNode
+    private function createGenericArrayTypeFromConstantArrayType(ConstantArrayType $constantArrayType): GenericTypeNode
+    {
+        $genericKeyType = $this->constantToGenericType($constantArrayType->getKeyType());
+
+        $itemType = $constantArrayType->getItemType();
+        if ($itemType instanceof ConstantArrayType) {
+            $genericItemType = $this->createGenericArrayTypeFromConstantArrayType($itemType);
+        } else {
+            $genericItemType = $this->constantToGenericType($itemType);
+        }
+
+        return $this->createArrayGenericTypeNode($genericKeyType, $genericItemType);
+    }
+
+    private function createArrayGenericTypeNode(Type $keyType, Type|GenericTypeNode $itemType): GenericTypeNode
     {
         $keyDocTypeNode = $this->staticTypeMapper->mapPHPStanTypeToPHPStanPhpDocTypeNode($keyType);
-        $itemDocTypeNode = $this->staticTypeMapper->mapPHPStanTypeToPHPStanPhpDocTypeNode($itemType);
+
+        if ($itemType instanceof Type) {
+            $itemDocTypeNode = $this->staticTypeMapper->mapPHPStanTypeToPHPStanPhpDocTypeNode($itemType);
+        } else {
+            $itemDocTypeNode = $itemType;
+        }
 
         return new GenericTypeNode(new IdentifierTypeNode('array'), [$keyDocTypeNode, $itemDocTypeNode]);
     }
