a64992508f6ecc25efc73ec738439a9ba7319131
Tomas Votruba <tomas.vot@gmail.com>
2025-09-13 11:30:18 +0200

    [type-declaration-docblocks] Add AddReturnDocblockDataProviderRector

---
+++ rules/TypeDeclarationDocblocks/Rector/Class_/AddReturnDocblockDataProviderRector.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\TypeDeclarationDocblocks\Rector\Class_;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Class_;
+use PHPStan\PhpDocParser\Ast\PhpDoc\ParamTagValueNode;
+use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfoFactory;
+use Rector\Comments\NodeDocBlock\DocBlockUpdater;
+use Rector\PhpParser\NodeFinder\LocalMethodCallFinder;
+use Rector\Privatization\TypeManipulator\TypeNormalizer;
+use Rector\Rector\AbstractRector;
+use Rector\StaticTypeMapper\StaticTypeMapper;
+use Rector\TypeDeclaration\NodeAnalyzer\CallTypesResolver;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\Tests\TypeDeclarationDocblocks\Rector\Class_\AddReturnDocblockDataProviderRector\AddReturnDocblockDataProviderRectorTest
+ */
+final class AddReturnDocblockDataProviderRector extends AbstractRector
+{
+    public function __construct(
+        private readonly PhpDocInfoFactory $phpDocInfoFactory,
+        private readonly DocBlockUpdater $docBlockUpdater,
+        private readonly StaticTypeMapper $staticTypeMapper,
+        private readonly CallTypesResolver $callTypesResolver,
+        private readonly LocalMethodCallFinder $localMethodCallFinder,
+        private readonly TypeNormalizer $typeNormalizer
+    ) {
+    }
+
+    public function getNodeTypes(): array
+    {
+        return [Class_::class];
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Add @return array docblock to array provider method', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+// @todo
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+// @todo
+CODE_SAMPLE
+            ),
+
+        ]);
+    }
+
+    /**
+     * @param Class_ $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        $hasChanged = false;
+
+        foreach ($node->getMethods() as $classMethod) {
+            if ($classMethod->getParams() === []) {
+                continue;
+            }
+
+            $classMethodPhpDocInfo = $this->phpDocInfoFactory->createFromNodeOrEmpty($classMethod);
+
+            $methodCalls = $this->localMethodCallFinder->match($node, $classMethod);
+            $classMethodParameterTypes = $this->callTypesResolver->resolveStrictTypesFromCalls($methodCalls);
+
+            foreach ($classMethod->getParams() as $parameterPosition => $param) {
+                if ($param->type === null || ! $this->isName($param->type, 'array')) {
+                    continue;
+                }
+
+                $parameterName = $this->getName($param);
+                $parameterTagValueNode = $classMethodPhpDocInfo->getParamTagValueByName($parameterName);
+
+                // already known, skip
+                if ($parameterTagValueNode instanceof ParamTagValueNode) {
+                    continue;
+                }
+
+                $resolvedParameterType = $classMethodParameterTypes[$parameterPosition] ?? null;
+                if (! $resolvedParameterType instanceof \PHPStan\Type\Type) {
+                    continue;
+                }
+
+                $normalizedResolvedParameterType = $this->typeNormalizer->generalizeConstantBoolTypes(
+                    $resolvedParameterType
+                );
+                $arrayDocTypeNode = $this->staticTypeMapper->mapPHPStanTypeToPHPStanPhpDocTypeNode(
+                    $normalizedResolvedParameterType
+                );
+
+                $paramTagValueNode = new ParamTagValueNode($arrayDocTypeNode, false, '$' . $parameterName, '', false);
+                $classMethodPhpDocInfo->addTagValueNode($paramTagValueNode);
+
+                $hasChanged = true;
+                $this->docBlockUpdater->updateRefactoredNodeWithPhpDocInfo($classMethod);
+            }
+        }
+
+        if (! $hasChanged) {
+            return null;
+        }
+
+        return $node;
+    }
+}
