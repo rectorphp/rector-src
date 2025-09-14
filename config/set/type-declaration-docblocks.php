93627abecfb822717e1bce0659075b0390c65865
Tomas Votruba <tomas.vot@gmail.com>
2025-09-14 15:15:35 +0200

    add AddReturnDocblockDataProviderRector to type-declaration-docblocks

--- config/set/type-declaration-docblocks.php
+++ config/set/type-declaration-docblocks.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnArrayDocblockBasedOnArrayMapRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnDocblockForScalarArrayFromAssignsRector;
+use Rector\TypeDeclarationDocblocks\Rector\Class_\AddReturnDocblockDataProviderRector;
 use Rector\TypeDeclarationDocblocks\Rector\Class_\ClassMethodArrayDocblockParamFromLocalCallsRector;
 use Rector\TypeDeclarationDocblocks\Rector\Class_\DocblockVarFromParamDocblockInConstructorRector;
 use Rector\TypeDeclarationDocblocks\Rector\ClassMethod\AddParamArrayDocblockFromDataProviderRector;
@@ -23,7 +24,9 @@ return static function (RectorConfig $rectorConfig): void {
         DocblockGetterReturnArrayFromPropertyDocblockVarRector::class,
         AddReturnDocblockForCommonObjectDenominatorRector::class,
         AddParamArrayDocblockFromDimFetchAccessRector::class,
-        AddParamArrayDocblockFromDataProviderRector::class,
         ClassMethodArrayDocblockParamFromLocalCallsRector::class,
+        // tests
+        AddParamArrayDocblockFromDataProviderRector::class,
+        AddReturnDocblockDataProviderRector::class,
     ]);
 };
