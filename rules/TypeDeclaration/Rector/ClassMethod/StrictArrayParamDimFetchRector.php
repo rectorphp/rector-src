3b1717892dd2502a9ecdd67c8e498fe118ed687a
Tomas Votruba <tomas.vot@gmail.com>
2025-10-05 15:33:37 +0200

    allow empty in StrictArrayParamDimFetchRector to make rule more practical (#7429)

--- rules/TypeDeclaration/Rector/ClassMethod/StrictArrayParamDimFetchRector.php
+++ rules/TypeDeclaration/Rector/ClassMethod/StrictArrayParamDimFetchRector.php
@@ -132,7 +132,6 @@ CODE_SAMPLE
 
         $isParamAccessedArrayDimFetch = false;
         $this->traverseNodesWithCallable($functionLike->stmts, function (Node $node) use (
-            $param,
             $paramName,
             &$isParamAccessedArrayDimFetch,
         ): int|null {
@@ -140,7 +139,7 @@ CODE_SAMPLE
                 return NodeVisitor::DONT_TRAVERSE_CURRENT_AND_CHILDREN;
             }
 
-            if ($this->shouldStop($node, $param, $paramName)) {
+            if ($this->shouldStop($node, $paramName)) {
                 // force set to false to avoid too early replaced
                 $isParamAccessedArrayDimFetch = false;
                 return NodeVisitor::STOP_TRAVERSAL;
@@ -209,16 +208,11 @@ CODE_SAMPLE
         return false;
     }
 
-    private function shouldStop(Node $node, Param $param, string $paramName): bool
+    private function shouldStop(Node $node, string $paramName): bool
     {
         $nodeToCheck = null;
 
-        if (! $param->default instanceof Expr && ($node instanceof Empty_ && $node->expr instanceof ArrayDimFetch && $node->expr->var instanceof Variable && $node->expr->var->name === $paramName)) {
-            return true;
-        }
-
-        if ($node instanceof FuncCall
-            && ! $node->isFirstClassCallable()
+        if ($node instanceof FuncCall && ! $node->isFirstClassCallable()
             && $this->isNames($node, ['is_array', 'is_string', 'is_int', 'is_bool', 'is_float'])) {
             $firstArg = $node->getArgs()[0];
             $nodeToCheck = $firstArg->value;
