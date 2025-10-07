653e44866180dfdfb813a97a6067955d361dd9dd
Tomas Votruba <tomas.vot@gmail.com>
2025-10-07 14:11:53 +0200

    rebuild autoload

--- preload.php
+++ preload.php
@@ -6,6 +6,12 @@ if (defined('__PHPSTAN_RUNNING__')) {
     return;
 }
 
+// edge case during Rector tests case, happens when
+// 1. phpstan autoload test case is triggered first,
+// 2. all php-parser classes are loaded,
+if (defined('PHPUNIT_COMPOSER_INSTALL') && class_exists(\PHPStan\Testing\PHPStanTestCase::class, false) && interface_exists(\PhpParser\Node::class, false)) {
+    return;
+}
 require_once __DIR__ . '/vendor/nikic/php-parser/lib/PhpParser/Node.php';
 require_once __DIR__ . '/src/Contract/PhpParser/Node/StmtsAwareInterface.php';
 require_once __DIR__ . '/vendor/nikic/php-parser/lib/PhpParser/NodeAbstract.php';
