7c2760f608dce158aafa482fa8a13599f811ca74
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-10-08 20:21:27 +0700

    ensure verify Node interface exists

--- build/target-repository/bootstrap.php
+++ build/target-repository/bootstrap.php
@@ -2,6 +2,8 @@
 
 declare(strict_types = 1);
 
+use PHPParser\Node;
+
 /**
  * The preload.php contains 2 dependencies
  *      - phpstan/phpdoc-parser
@@ -10,7 +12,13 @@ declare(strict_types = 1);
  * They need to be loaded early to avoid conflict version between rector prefixed vendor and Project vendor
  * For example, a project may use PHPParser v4, while Rector uses v5, that will error as class or logic are different.
  */
-if (defined('PHPUNIT_COMPOSER_INSTALL')) {
+if (
+    // verify PHPUnit is running
+    defined('PHPUNIT_COMPOSER_INSTALL')
+
+    // no need to preload if Node interface exists
+    && ! interface_exists(Node::class, false)
+) {
     require_once __DIR__ . '/preload.php';
 }
 
