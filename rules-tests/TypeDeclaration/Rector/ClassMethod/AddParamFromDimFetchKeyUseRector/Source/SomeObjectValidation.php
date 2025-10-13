222995398ae67eed14b18359912a3e44782c6518
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-10-13 15:54:42 +0700

    [TypeDeclaration] Skip with is_array() on AddParamFromDimFetchKeyUseRector (#7489)

---
+++ rules-tests/TypeDeclaration/Rector/ClassMethod/AddParamFromDimFetchKeyUseRector/Source/SomeObjectValidation.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\TypeDeclaration\Rector\ClassMethod\AddParamFromDimFetchKeyUseRector\Source;
+
+class SomeObjectValidation {
+    /**
+     * Array of validation error text messages.
+     *
+     * @var array<string, string>
+     */
+    public $validation_errors = [];
+}
