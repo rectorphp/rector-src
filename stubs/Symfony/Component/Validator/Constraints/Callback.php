7c8037e0c7cff4b2a30538d3aceb114a744b9dbf
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-10-20 01:25:50 +0700

    clean up stubs

--- stubs/Symfony/Component/Validator/Constraints/Callback.php
+++ stubs/Symfony/Component/Validator/Constraints/Callback.php
@@ -1,17 +1,6 @@
 <?php
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Symfony\Component\Validator\Constraints;
-
-use Symfony\Component\Validator\Constraint;
 
 if (class_exists('Symfony\Component\Validator\Constraints\Callback')) {
     return;
