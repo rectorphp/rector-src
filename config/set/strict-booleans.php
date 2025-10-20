cc09f94748836da3fd0ef3dcf311ecf4f3e5cb39
Tomas Votruba <tomas.vot@gmail.com>
2025-10-20 23:34:07 +0200

    [depre] Deprecate BooleanInBooleanNotRuleFixerRector as too strict and not practical

--- config/set/strict-booleans.php
+++ config/set/strict-booleans.php
@@ -3,14 +3,9 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Strict\Rector\BooleanNot\BooleanInBooleanNotRuleFixerRector;
 use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
 use Rector\Strict\Rector\Ternary\DisallowedShortTernaryRuleFixerRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rules([
-        BooleanInBooleanNotRuleFixerRector::class,
-        DisallowedEmptyRuleFixerRector::class,
-        DisallowedShortTernaryRuleFixerRector::class,
-    ]);
+    $rectorConfig->rules([DisallowedEmptyRuleFixerRector::class, DisallowedShortTernaryRuleFixerRector::class]);
 };
