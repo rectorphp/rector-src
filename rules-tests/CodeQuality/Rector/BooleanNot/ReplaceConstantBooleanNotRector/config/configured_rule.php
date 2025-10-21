6602925c051b7b30930ddbd938ef12d8af8dd20b
John Shaffer <jdsha@pm.me>
2025-10-21 11:07:18 -0500

    [code-quality] Add ReplaceConstantBooleanNotRector (#7527)
    * [code-quality] Add ReplaceConstantBooleanNotRector
    
    * Change constant lookups to valueResolver methods

---
+++ rules-tests/CodeQuality/Rector/BooleanNot/ReplaceConstantBooleanNotRector/config/configured_rule.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\BooleanNot\ReplaceConstantBooleanNotRector;
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withRules([ReplaceConstantBooleanNotRector::class]);
