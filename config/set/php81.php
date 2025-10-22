153fc786d301d3d9e4170b955df9bbac9ecf144d
Tomas Votruba <tomas.vot@gmail.com>
2025-10-23 00:53:10 +0200

    register NewInInitializerRector in PHP 8.1

--- config/set/php81.php
+++ config/set/php81.php
@@ -8,6 +8,7 @@ use Rector\Config\RectorConfig;
 use Rector\Php81\Rector\Array_\FirstClassCallableRector;
 use Rector\Php81\Rector\Class_\MyCLabsClassToEnumRector;
 use Rector\Php81\Rector\Class_\SpatieEnumClassToEnumRector;
+use Rector\Php81\Rector\ClassMethod\NewInInitializerRector;
 use Rector\Php81\Rector\FuncCall\NullToStrictIntPregSlitFuncCallLimitArgRector;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 use Rector\Php81\Rector\MethodCall\MyCLabsMethodCallToEnumConstRector;
@@ -34,5 +35,7 @@ return static function (RectorConfig $rectorConfig): void {
         FunctionLikeToFirstClassCallableRector::class,
         ClosureFromCallableToFirstClassCallableRector::class,
         RemoveReflectionSetAccessibleCallsRector::class,
+
+        NewInInitializerRector::class,
     ]);
 };
