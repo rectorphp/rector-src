9ad84582e11b6fb2a6104283095966344ab787c1
Tomas Votruba <tomas.vot@gmail.com>
2025-10-23 15:38:09 +0200

    improve rector class finder return only non abstract non deprecated

--- scripts/src/Finder/RectorClassFinder.php
+++ scripts/src/Finder/RectorClassFinder.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace Rector\Scripts\Finder;
 
 use Nette\Loaders\RobotLoader;
+use Rector\Configuration\Deprecation\Contract\DeprecatedInterface;
 
 final class RectorClassFinder
 {
     /**
      * @param string[] $dirs
-     * @return string[]
+     * @return class-string[]
      */
     public function find(array $dirs): array
     {
@@ -21,6 +22,25 @@ final class RectorClassFinder
         $robotLoader->setTempDirectory(sys_get_temp_dir() . '/rector-rules');
         $robotLoader->refresh();
 
-        return array_keys($robotLoader->getIndexedClasses());
+        /** @var array<class-string> $rectorClasses */
+        $rectorClasses = array_keys($robotLoader->getIndexedClasses());
+
+        $usableRectorClasses = [];
+
+        // remove deprecated and abstract classes
+        foreach ($rectorClasses as $rectorClass) {
+            $rectorClassReflection = new \ReflectionClass($rectorClass);
+            if ($rectorClassReflection->isAbstract()) {
+                continue;
+            }
+
+            if ($rectorClassReflection->implementsInterface(DeprecatedInterface::class)) {
+                continue;
+            }
+
+            $usableRectorClasses[] = $rectorClass;
+        }
+
+        return $usableRectorClasses;
     }
 }
