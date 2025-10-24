95e97ea7c4081b4ee285da8c6ad33b06cbd45d00
Tomas Votruba <tomas.vot@gmail.com>
2025-10-24 10:48:30 +0200

    [depre] Deprecate PropertyFetchToMethodCallRector not used and as too generic, use custom rule instead (#7558)

--- rules/Transform/ValueObject/PropertyFetchToMethodCall.php
+++ rules/Transform/ValueObject/PropertyFetchToMethodCall.php
@@ -7,6 +7,9 @@ namespace Rector\Transform\ValueObject;
 use PHPStan\Type\ObjectType;
 use Rector\Validation\RectorAssert;
 
+/**
+ * @deprecated as related rule is deprecated
+ */
 final readonly class PropertyFetchToMethodCall
 {
     /**
