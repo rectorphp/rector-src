bc57aa6675c3383250829bdd0b2937a543aae8d8
Tomas Votruba <tomas.vot@gmail.com>
2025-10-24 11:01:55 +0200

    note (#7560)

--- rules/Transform/Rector/MethodCall/MethodCallToFuncCallRector.php
+++ rules/Transform/Rector/MethodCall/MethodCallToFuncCallRector.php
@@ -17,6 +17,7 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 use Webmozart\Assert\Assert;
 
 /**
+ * @note used extensively https://github.com/search?q=MethodCallToFuncCallRector%3A%3Aclass&type=code
  * @see \Rector\Tests\Transform\Rector\MethodCall\MethodCallToFuncCallRector\MethodCallToFuncCallRectorTest
  */
 final class MethodCallToFuncCallRector extends AbstractRector implements ConfigurableRectorInterface
