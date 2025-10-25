09bdbf213af8fd352059a503a09d54677d0bf05a
Tomas Votruba <tomas.vot@gmail.com>
2025-10-25 12:34:18 +0200

    [depre] Deprecate MultiDimensionalArrayToArrayDestructRector as not part of any set, can create less readable and harder to analyse code

--- rules-tests/CodingStyle/Rector/Foreach_/MultiDimensionalArrayToArrayDestructRector/config/configured_rule.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Rector\CodingStyle\Rector\Foreach_\MultiDimensionalArrayToArrayDestructRector;
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()
-    ->withRules([MultiDimensionalArrayToArrayDestructRector::class]);
