1b91431bc8764e79fade6ff5d4ea089d966c858f
Orest Divintari <o.divintari@gmail.com>
2025-10-25 12:54:53 +0200

    Narrow object return type
    When the actual return type of a method is a subtype of the declared
    return type, we can replace the declared return type with the actual return type

---
+++ rules/TypeDeclaration/Rector/ClassMethod/NarrowObjectReturnTypeRector.php
@@ -0,0 +1,213 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\TypeDeclaration\Rector\ClassMethod;
+
+use Override;
+use PhpParser\Node;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Return_;
+use PHPStan\Type\ObjectType;
+use Rector\PhpParser\Node\BetterNodeFinder;
+use Rector\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * Narrows return types from generic object or parent class to specific class.
+ *
+ * @see \Rector\Tests\TypeDeclaration\Rector\ClassMethod\NarrowObjectReturnTypeRector\NarrowObjectReturnTypeRectorTest
+ */
+final class NarrowObjectReturnTypeRector extends AbstractRector
+{
+    public function __construct(
+        private readonly BetterNodeFinder $betterNodeFinder,
+    ) {
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Narrows return type from generic object or parent class to specific class',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+final class TalkFactory extends AbstractFactory
+{
+    protected function build(): object
+    {
+        return new ConferenceTalk();
+    }
+}
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+final class TalkFactory extends AbstractFactory
+{
+    protected function build(): ConferenceTalk
+    {
+        return new ConferenceTalk();
+    }
+}
+CODE_SAMPLE
+                ),
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+final class TalkFactory
+{
+    public function createConferenceTalk(): Talk
+    {
+        return new ConferenceTalk();
+    }
+}
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+final class TalkFactory
+{
+    public function createConferenceTalk(): ConferenceTalk
+    {
+        return new ConferenceTalk();
+    }
+}
+CODE_SAMPLE
+                ),
+            ],
+        );
+    }
+
+    #[Override]
+    public function getNodeTypes(): array
+    {
+        return [ClassMethod::class];
+    }
+
+    #[Override]
+    public function refactor(Node $node): ?Node
+    {
+        if (! $node instanceof ClassMethod) {
+            return null;
+        }
+
+        $returnType = $node->returnType;
+
+        if (! $returnType instanceof Identifier && ! $returnType instanceof FullyQualified) {
+            return null;
+        }
+
+        if ($node->isAbstract()) {
+            return null;
+        }
+
+        $actualReturnClass = $this->getActualReturnClass($node);
+
+        if ($actualReturnClass === null) {
+            return null;
+        }
+
+        $declaredType = $returnType->toString();
+
+        if ($declaredType === $actualReturnClass) {
+            return null;
+        }
+
+        if ($this->isDeclaredTypeFinal($declaredType)) {
+            return null;
+        }
+
+        if ($this->isActualTypeAnonymous($actualReturnClass)) {
+            return null;
+        }
+
+        if (! $this->isNarrowingValid($declaredType, $actualReturnClass)) {
+            return null;
+        }
+
+        $node->returnType = new FullyQualified($actualReturnClass);
+
+        return $node;
+    }
+
+    private function isDeclaredTypeFinal(string $declaredType): bool
+    {
+        if ($declaredType === 'object') {
+            return false;
+        }
+
+        $declaredObjectType = new ObjectType($declaredType);
+        $classReflection = $declaredObjectType->getClassReflection();
+
+        if ($classReflection === null) {
+            return false;
+        }
+
+        return $classReflection->isFinal();
+    }
+
+    private function isActualTypeAnonymous(string $actualType): bool
+    {
+        $actualObjectType = new ObjectType($actualType);
+        $classReflection = $actualObjectType->getClassReflection();
+
+        if ($classReflection === null) {
+            return false;
+        }
+
+        return $classReflection->isAnonymous();
+    }
+
+    private function isNarrowingValid(string $declaredType, string $actualType): bool
+    {
+        if ($declaredType === 'object') {
+            return true;
+        }
+
+        $actualObjectType = new ObjectType($actualType);
+        $declaredObjectType = new ObjectType($declaredType);
+
+        return $declaredObjectType->isSuperTypeOf($actualObjectType)
+            ->yes();
+    }
+
+    private function getActualReturnClass(ClassMethod $node): ?string
+    {
+        $returnStatements = $this->betterNodeFinder->findInstanceOf($node, Return_::class);
+
+        if ($returnStatements === []) {
+            return null;
+        }
+
+        $returnedClass = null;
+
+        foreach ($returnStatements as $returnStatement) {
+            if ($returnStatement->expr === null) {
+                return null;
+            }
+
+            $returnType = $this->nodeTypeResolver->getNativeType($returnStatement->expr);
+
+            if (! $returnType->isObject()->yes()) {
+                return null;
+            }
+
+            $classNames = $returnType->getObjectClassNames();
+
+            if (count($classNames) !== 1) {
+                return null;
+            }
+
+            $className = $classNames[0];
+
+            if ($returnedClass === null) {
+                $returnedClass = $className;
+            } elseif ($returnedClass !== $className) {
+                return null;
+            }
+        }
+
+        return $returnedClass;
+    }
+}
