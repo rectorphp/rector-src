4bc73cfe151b463467a8b2a744057bff47173dd0
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-10-27 22:23:15 +0700

    [Php84] Skip variable extraction with return early on ForeachToArrayAllRector (#7591)

--- rules/Php84/Rector/Foreach_/ForeachToArrayAllRector.php
+++ rules/Php84/Rector/Foreach_/ForeachToArrayAllRector.php
@@ -276,6 +276,10 @@ CODE_SAMPLE
             return false;
         }
 
+        if (! $foreach->valueVar instanceof Variable) {
+            return false;
+        }
+
         $type = $this->nodeTypeResolver->getNativeType($foreach->expr);
 
         return $type->isArray()
