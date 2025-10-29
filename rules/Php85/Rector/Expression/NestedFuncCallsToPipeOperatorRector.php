162ec68159e2212a27febe31f233b2641aefb5ad
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-10-29 20:42:23 +0700

    implemented

--- rules/Php85/Rector/Expression/NestedFuncCallsToPipeOperatorRector.php
+++ rules/Php85/Rector/Expression/NestedFuncCallsToPipeOperatorRector.php
@@ -11,6 +11,7 @@ use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\BinaryOp\Pipe;
 use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Stmt\Expression;
+use PhpParser\Node\Stmt\Return_;
 use PhpParser\Node\VariadicPlaceholder;
 use Rector\Rector\AbstractRector;
 use Rector\ValueObject\PhpVersionFeature;
@@ -57,17 +58,15 @@ CODE_SAMPLE
 
     public function getNodeTypes(): array
     {
-        return [Expression::class];
+        return [Expression::class, Return_::class];
     }
 
     /**
-     * @param Expression $node
+     * @param Expression|Return_ $node
      */
     public function refactor(Node $node): ?Node
     {
         $hasChanged = false;
-
-        // @todo handle return as well
 
         if ($node->expr instanceof Assign) {
             $assign = $node->expr;
@@ -102,33 +101,55 @@ CODE_SAMPLE
         return PhpVersionFeature::PIPE_OPERATOER;
     }
 
-    private function processNestedCalls(Node $node): ?Expr
+    private function buildPipeExpression(FuncCall $funcCall, Expr $expr): Pipe
     {
-        if (! $node instanceof FuncCall) {
-            return null;
+        // Recursively process inner call to unwrap all nested levels
+        if ($expr instanceof FuncCall) {
+            $processed = $this->processNestedCalls($expr, true);
+            $nestedInner = $processed instanceof Expr ? $processed : $expr;
+        } else {
+            $nestedInner = $expr;
         }
 
-        // Check if any argument is a function call
-        foreach ($node->args as $arg) {
-            if (! $arg instanceof Arg) {
-                return null;
-            }
-
-            if ($arg->value instanceof FuncCall) {
-                return $this->buildPipeExpression($node, $arg->value);
-            }
-        }
-
-        return null;
-    }
-
-    private function buildPipeExpression(FuncCall $outerCall, FuncCall $innerCall): Pipe
-    {
-        return new Pipe($innerCall, $this->createPlaceholderCall($outerCall));
+        return new Pipe($nestedInner, $this->createPlaceholderCall($funcCall));
     }
 
     private function createPlaceholderCall(FuncCall $funcCall): FuncCall
     {
         return new FuncCall($funcCall->name, [new VariadicPlaceholder()]);
+    }
+
+    private function processNestedCalls(Expr $expr, bool $deep = false): ?Expr
+    {
+        if (! $expr instanceof FuncCall) {
+            return null;
+        }
+
+        if ($expr->isFirstClassCallable()) {
+            return null;
+        }
+
+        if (count($expr->args) !== 1) {
+            return null;
+        }
+
+        // Check if any argument is a function call
+        foreach ($expr->args as $arg) {
+            if (! $arg instanceof Arg) {
+                return null;
+            }
+
+            if ($arg->value instanceof FuncCall) {
+                return $this->buildPipeExpression($expr, $arg->value);
+            }
+
+            // If we're deep in recursion and hit a non-FuncCall, this is the base
+            if ($deep) {
+                // Return a pipe with the base expression on the left
+                return new Pipe($arg->value, $this->createPlaceholderCall($expr));
+            }
+        }
+
+        return null;
     }
 }
