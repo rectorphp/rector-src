e99ea333bfd3eb543977d32d9c8e8d3a745ca2de
Tomas Votruba <tomas.vot@gmail.com>
2025-11-13 22:50:59 +0100

    skip internal or vendor class (#7626)

--- rules-tests/Unambiguous/Rector/Expression/FluentSettersToStandaloneCallMethodRector/Source/SomeSetterClass.php
+++ rules-tests/Unambiguous/Rector/Expression/FluentSettersToStandaloneCallMethodRector/Source/SomeSetterClass.php
@@ -2,7 +2,7 @@
 
 namespace Rector\Tests\Unambiguous\Rector\Expression\FluentSettersToStandaloneCallMethodRector\Source;
 
-final class SomeSetterClass
+class SomeSetterClass
 {
     private ?string $name = null;
 
