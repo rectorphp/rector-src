6a04e90ae197dc9efbefcb9454005ed7f83c9de6
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-11-19 10:41:02 +0700

    [DeadCode] Skip used from property fetch reference on loop on RemoveUnusedVariableAssignRector

--- rules/DeadCode/Rector/Assign/RemoveUnusedVariableAssignRector.php
+++ rules/DeadCode/Rector/Assign/RemoveUnusedVariableAssignRector.php
@@ -19,6 +19,7 @@ use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\Function_;
 use PhpParser\NodeVisitor;
+use Rector\Contract\PhpParser\Node\StmtsAwareInterface;
 use Rector\DeadCode\SideEffect\SideEffectNodeDetector;
 use Rector\NodeAnalyzer\VariableAnalyzer;
 use Rector\NodeManipulator\StmtsManipulator;
@@ -158,6 +159,22 @@ CODE_SAMPLE
         });
     }
 
+    private function collectAssignRefVariableNames(Stmt $stmt, array &$refVariableNames): void
+    {
+        if (! $stmt instanceof StmtsAwareInterface) {
+            return;
+        }
+
+        $this->traverseNodesWithCallable(
+            $stmt,
+            function (Node $subNode) use (&$refVariableNames): void {
+                if ($subNode instanceof AssignRef && $subNode->var instanceof Variable) {
+                    $refVariableNames[] = (string) $this->getName($subNode->var);
+                }
+            }
+        );
+    }
+
     /**
      * @param array<int, Stmt> $stmts
      * @return array<int, string>
@@ -169,6 +186,7 @@ CODE_SAMPLE
 
         foreach ($stmts as $key => $stmt) {
             if (! $stmt instanceof Expression) {
+                $this->collectAssignRefVariableNames($stmt, $refVariableNames);
                 continue;
             }
 
