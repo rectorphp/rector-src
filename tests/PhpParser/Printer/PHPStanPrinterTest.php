2d045f6fa5b4de0f9b56605388c6a71717c4ce7e
Tomas Votruba <tomas.vot@gmail.com>
2025-11-27 16:12:53 +0100

    [issue 9492] create reproducer for modified array_map args, as creates changed args on print

--- tests/PhpParser/Printer/PHPStanPrinterTest.php
+++ tests/PhpParser/Printer/PHPStanPrinterTest.php
@@ -8,7 +8,10 @@ use PhpParser\PrettyPrinter\Standard;
 use PHPStan\Parser\Parser;
 use PHPStan\Parser\RichParser;
 use Rector\Testing\PHPUnit\AbstractLazyTestCase;
+<<<<<<< HEAD
 use ReflectionProperty;
+=======
+>>>>>>> ea9a6ad8a5 ([issue 9492] create reproducer for modified array_map args, as creates changed args on print)
 
 /**
  * Test case for: https://github.com/rectorphp/rector/issues/9492
@@ -24,17 +27,30 @@ final class PHPStanPrinterTest extends AbstractLazyTestCase
         $stmts = $phpstanParser->parseFile(__DIR__ . '/Fixture/some_array_map.php');
 
         // get private property "parser"
+<<<<<<< HEAD
         $parserReflectionProperty = new ReflectionProperty(RichParser::class, 'parser');
+=======
+        $parserReflectionProperty = new \ReflectionProperty(RichParser::class, 'parser');
+>>>>>>> ea9a6ad8a5 ([issue 9492] create reproducer for modified array_map args, as creates changed args on print)
 
         /** @var \PhpParser\Parser $innerParser */
         $innerParser = $parserReflectionProperty->getValue($phpstanParser);
         $tokens = $innerParser->getTokens();
 
+<<<<<<< HEAD
         $standard = new Standard([]);
         $printerContents = $standard->printFormatPreserving($stmts, $stmts, $tokens);
 
         $newlineNormalizedContents = str_replace("\r\n", PHP_EOL, $printerContents);
 
         $this->assertStringEqualsFile(__DIR__ . '/Fixture/some_array_map.php', $newlineNormalizedContents);
+=======
+        $standardPrinter = new Standard([
+            'newline' => "\n",
+        ]);
+        $printerContents = $standardPrinter->printFormatPreserving($stmts, $stmts, $tokens);
+
+        $this->assertStringEqualsFile(__DIR__ . '/Fixture/some_array_map.php', $printerContents);
+>>>>>>> ea9a6ad8a5 ([issue 9492] create reproducer for modified array_map args, as creates changed args on print)
     }
 }
