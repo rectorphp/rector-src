e6244efcabb087a7f33fc31b3ce6444064377664
Peter Fox <peter.fox@peterfox.me>
2025-12-05 20:03:44 +0000

    working

---
+++ rules-tests/CodingStyle/Rector/Closure/StaticClosureRector/Source/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @param-closure-this \Rector\Tests\CodingStyle\Rector\Closure\StaticClosureRector\Source\BindObject $closure
+ * @param-closure-this \Rector\Tests\CodingStyle\Rector\Closure\StaticClosureRector\Source\BindObject $closureFinally
+ */
+function bind_on_object(Closure|null $closure = null, Closure|null $anotherClosure = null, Closure|null $closureFinally = null)
+{
+
+}
