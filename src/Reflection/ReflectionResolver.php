6efa09477442d9bfed0989fdd1d773bb0d3fa5a2
Tomas Votruba <tomas.vot@gmail.com>
2025-12-08 00:49:28 +0100

    [cleanup] Add unused public, remove unused methods and ignore rest (#7733)
    * [cleanup] Add unused public, remove unused methods and ignore rest
    
    * [ci-review] Rector Rectify
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

--- src/Reflection/ReflectionResolver.php
+++ src/Reflection/ReflectionResolver.php
@@ -17,8 +17,6 @@ use PhpParser\Node\Expr\StaticPropertyFetch;
 use PhpParser\Node\Name;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassLike;
-use PhpParser\Node\Stmt\ClassMethod;
-use PhpParser\Node\Stmt\Function_;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\ClassReflection;
 use PHPStan\Reflection\FunctionReflection;
@@ -213,34 +211,6 @@ final readonly class ReflectionResolver
         }
 
         // todo: support NullsafeMethodCall
-        return null;
-    }
-
-    public function resolveMethodReflectionFromClassMethod(ClassMethod $classMethod, Scope $scope): ?MethodReflection
-    {
-        $classReflection = $scope->getClassReflection();
-        if (! $classReflection instanceof ClassReflection) {
-            return null;
-        }
-
-        $className = $classReflection->getName();
-        $methodName = $this->nodeNameResolver->getName($classMethod);
-
-        return $this->resolveMethodReflection($className, $methodName, $scope);
-    }
-
-    public function resolveFunctionReflectionFromFunction(Function_ $function): ?FunctionReflection
-    {
-        $name = $this->nodeNameResolver->getName($function);
-        if ($name === null) {
-            return null;
-        }
-
-        $functionName = new Name($name);
-        if ($this->reflectionProvider->hasFunction($functionName, null)) {
-            return $this->reflectionProvider->getFunction($functionName, null);
-        }
-
         return null;
     }
 
