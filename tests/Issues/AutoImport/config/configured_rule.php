54d38a38b2422c35d95f343a751928f482d197a1
Tomas Votruba <tomas.vot@gmail.com>
2025-12-07 22:42:26 +0100

    tidy up

--- tests/Issues/AutoImport/config/configured_rule.php
+++ tests/Issues/AutoImport/config/configured_rule.php
@@ -12,17 +12,23 @@ use Rector\Renaming\Rector\Name\RenameClassRector;
 use Rector\Symfony\Symfony44\Rector\ClassMethod\ConsoleExecuteReturnIntRector;
 
 return static function (RectorConfig $rectorConfig): void {
+    /** enables @see \Rector\PostRector\Rector\NameImportingPostRector */
     $rectorConfig->importNames();
+
     $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
         'Some\Exception' => 'Some\Target\Exception',
         'DateTime' => 'DateTimeInterface',
         'Phalcon\Logger' => 'Phalcon\Logger\Logger',
     ]);
+
     $rectorConfig->rule(TernaryToNullCoalescingRector::class);
+
     $rectorConfig->ruleWithConfiguration(AnnotationToAttributeRector::class, [
         new AnnotationToAttribute('Doctrine\ORM\Mapping\Entity'),
     ]);
+
     $rectorConfig->rules([ConsoleExecuteReturnIntRector::class, RemoveUnusedPrivatePropertyRector::class]);
+
     $rectorConfig->ruleWithConfiguration(RenameFunctionRector::class, [
         'split' => 'explode',
     ]);
