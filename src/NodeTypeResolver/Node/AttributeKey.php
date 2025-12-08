e1d53b058ce5716671651d3e954c6b942d2f4f63
Tomas Votruba <tomas.vot@gmail.com>
2025-12-08 12:53:51 +0100

    [internal] rename FirstClassCallableRector to ArrayToFirstClassCallableRector to make behavior explicit (#7736)
    * rename FirstClassCallableRector to ArrayToFirstClassCallableRector to make feature explicit
    
    * [internal] move decorating node visitors to PhpParser/NodeVisitor, easier to find; extract attribute key from FunctionLikeToFirstClassCallableRector
    
    * cs

--- src/NodeTypeResolver/Node/AttributeKey.php
+++ src/NodeTypeResolver/Node/AttributeKey.php
@@ -282,4 +282,6 @@ final class AttributeKey
     public const CLASS_CONST_FETCH_NAME = 'class_const_fetch_name';
 
     public const PHP_VERSION_CONDITIONED = 'php_version_conditioned';
+
+    public const HAS_CLOSURE_WITH_VARIADIC_ARGS = 'has_closure_with_variadic_args';
 }
--- src/PhpParser/NodeVisitor/NameNodeVisitor.php
+++ src/PhpParser/NodeVisitor/NameNodeVisitor.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Rector\NodeTypeResolver\PHPStan\Scope\NodeVisitor;
+namespace Rector\PhpParser\NodeVisitor;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\ConstFetch;
