e040c5e3ea0f7b42510f8afab967591abd6e6b77
Tomas Votruba <tomas.vot@gmail.com>
2026-01-11 01:45:19 +0100

    [automated] Apply Coding Standard (#7818)
    Co-authored-by: TomasVotruba <924196+TomasVotruba@users.noreply.github.com>

--- src/Console/Command/SetupCICommand.php
+++ src/Console/Command/SetupCICommand.php
@@ -7,11 +7,11 @@ namespace Rector\Console\Command;
 use Nette\Utils\FileSystem;
 use OndraM\CiDetector\CiDetector;
 use Rector\Git\RepositoryHelper;
+use function sprintf;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use function sprintf;
 
 final class SetupCICommand extends Command
 {
