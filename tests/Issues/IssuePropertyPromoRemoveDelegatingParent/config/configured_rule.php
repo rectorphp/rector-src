3247d94490b19b8183b93eb20bc42be017ac9e54
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-01-21 22:47:19 +0700

    Rectify

--- tests/Issues/IssuePropertyPromoRemoveDelegatingParent/config/configured_rule.php
+++ tests/Issues/IssuePropertyPromoRemoveDelegatingParent/config/configured_rule.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\ClassMethod\RemoveParentDelegatingConstructorRector;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
