4b41b75838399394afbfa69a4db140628e1719e8
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-02-20 13:10:33 +0700

    [TypeDeclarationDocblocks] Typo fix RuleDefinition description on AddVarArrayDocblockFromDimFetchAssignRector

--- rules/TypeDeclarationDocblocks/Rector/Class_/AddVarArrayDocblockFromDimFetchAssignRector.php
+++ rules/TypeDeclarationDocblocks/Rector/Class_/AddVarArrayDocblockFromDimFetchAssignRector.php
@@ -37,7 +37,7 @@ final class AddVarArrayDocblockFromDimFetchAssignRector extends AbstractRector
 
     public function getRuleDefinition(): RuleDefinition
     {
-        return new RuleDefinition('Add @var array property docblock docblock from dim fetch on property fetch assignments', [
+        return new RuleDefinition('Add @var array property docblock from dim fetch on property assignment', [
             new CodeSample(
                 <<<'CODE_SAMPLE'
 final class SomeClass
