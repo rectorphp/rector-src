7849bc6071c22801783747887f02ee9049a03b94
Eduardo Rocha <22674892+hockdudu@users.noreply.github.com>
2026-03-21 13:34:38 +0100

    Add support for adding `#[Override]` attribute to methods implementing interfaces

---
+++ rules-tests/Php83/Rector/ClassMethod/AddOverrideAttributeToOverriddenMethodsRector/Source/AbstractParentWithInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector\Source;
+
+abstract class AbstractParentWithInterface implements ExampleFromInterface
+{
+
+}
