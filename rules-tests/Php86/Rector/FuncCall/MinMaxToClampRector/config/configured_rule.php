d021ef3778b2b06cc5270fd3400c78680002bd56
Caleb White <cdwhite3@pm.me>
2026-03-26 07:59:18 -0500

    feat: add min/max to clamp rector

--- rules-tests/Php86/Rector/FuncCall/MinMaxToClampRector/config/configured_rule.php
+++ rules-tests/Php86/Rector/FuncCall/MinMaxToClampRector/config/configured_rule.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Php86\Rector\FuncCall\MinMaxToClampRector;
+use Rector\ValueObject\PhpVersion;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rules([MinMaxToClampRector::class]);
+    $rectorConfig->rule(MinMaxToClampRector::class);
+
+    $rectorConfig->phpVersion(PhpVersion::PHP_86);
 };
