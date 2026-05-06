4c4855b099f99865806d388aa48eed2dbeb8a1fa
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-05-06 21:52:52 +0700

    Fix typo namespace Sourde -> Source in rules-tests/TypeDeclarationDocblocks

---
+++ rules-tests/TypeDeclarationDocblocks/Rector/ClassMethod/DocblockReturnArrayFromDirectArrayInstanceRector/Source/SomeObjectInDirectArray.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\TypeDeclarationDocblocks\Rector\ClassMethod\DocblockReturnArrayFromDirectArrayInstanceRector\Source;
+
+final class SomeObjectInDirectArray
+{
+}
