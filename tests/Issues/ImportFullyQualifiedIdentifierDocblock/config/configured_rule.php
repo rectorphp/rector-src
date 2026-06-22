b745b18311eb2e0e9eaf36acd24c5e970468b07a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-06-22 21:22:57 +0700

    [AutoImport] Avoid used import into param that skipped from removal rule

--- tests/Issues/ImportFullyQualifiedIdentifierDocblock/config/configured_rule.php
+++ tests/Issues/ImportFullyQualifiedIdentifierDocblock/config/configured_rule.php
@@ -3,8 +3,14 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedConstructorParamRector;
 use Rector\Tests\Issues\ImportFullyQualifiedIdentifierDocblock\Source\AddFullyQualifiedIdentifierDocblockRector;
 
 return RectorConfig::configure()
-    ->withRules([AddFullyQualifiedIdentifierDocblockRector::class])
-    ->withImportNames();
+    ->withRules([AddFullyQualifiedIdentifierDocblockRector::class, RemoveUnusedConstructorParamRector::class])
+    ->withImportNames()
+    ->withSkip([
+        RemoveUnusedConstructorParamRector::class => [
+            realpath(__DIR__ . '/../Fixture') . '/keep_import_used_in_param.php',
+        ],
+    ]);
