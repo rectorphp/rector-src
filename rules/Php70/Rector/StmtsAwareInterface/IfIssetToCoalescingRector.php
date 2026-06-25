0815da7a888492b7884bafb17c8e0233ad397cd3
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-06-25 15:43:25 +0700

    [Php70] Skip memoize item by key on IfIssetToCoalescingRector (#8083)
    * [Php70] Skip memoize item by key on IfIssetToCoalescingRector
    
    * fix

--- rules/Php70/Rector/StmtsAwareInterface/IfIssetToCoalescingRector.php
+++ rules/Php70/Rector/StmtsAwareInterface/IfIssetToCoalescingRector.php
@@ -6,6 +6,7 @@ namespace Rector\Php70\Rector\StmtsAwareInterface;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\BinaryOp\Coalesce;
 use PhpParser\Node\Expr\Isset_;
 use PhpParser\Node\Expr\Ternary;
@@ -110,6 +111,11 @@ CODE_SAMPLE
 
             $ifIsset = $previousStmt->cond;
             if (! $this->nodeComparator->areNodesEqual($ifOnlyStmt->expr, $ifIsset->vars[0])) {
+                continue;
+            }
+
+            if ($stmt->expr instanceof Assign &&
+                $this->nodeComparator->areNodesEqual($ifOnlyStmt->expr, $stmt->expr->var)) {
                 continue;
             }
 
