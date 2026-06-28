ff8fcf90ed8c9408ff619c92dcda6953dfb96c69
Tomas Votruba <tomas.vot@gmail.com>
2026-06-28 15:05:24 +0200

    [CodingStyle] Deprecate EnumCaseToPascalCaseRector as risky change
    It does not handle enum usage across the whole context. Better handled
    via IDE with full context, manually, or a custom rule.

--- rules-tests/CodingStyle/Rector/Enum_/EnumCaseToPascalCaseRector/config/configured_rule.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Rector\CodingStyle\Rector\Enum_\EnumCaseToPascalCaseRector;
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()
-    ->withRules([EnumCaseToPascalCaseRector::class]);
