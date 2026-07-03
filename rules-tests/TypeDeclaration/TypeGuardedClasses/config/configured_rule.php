e773ec890badad0fef8aa48896f01fcdf03cefe1
Tomas Votruba <tomas.vot@gmail.com>
2026-07-03 13:36:40 +0200

    [TypeDeclaration] Skip type-guarded trait methods in StrictArrayParamDimFetchRector

--- rules-tests/TypeDeclaration/TypeGuardedClasses/config/configured_rule.php
+++ rules-tests/TypeDeclaration/TypeGuardedClasses/config/configured_rule.php
@@ -7,10 +7,16 @@ use PHPStan\Type\MixedType;
 use Rector\Config\RectorConfig;
 use Rector\Tests\TypeDeclaration\TypeGuardedClasses\Source\GuardedRepository;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\StrictArrayParamDimFetchRector;
 use Rector\TypeDeclaration\ValueObject\AddReturnTypeDeclaration;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->typeGuardedClasses([GuardedRepository::class]);
+    $rectorConfig->typeGuardedClasses([
+        GuardedRepository::class,
+        'Rector\Tests\TypeDeclaration\TypeGuardedClasses\Fixture\SkipGuardedTrait',
+    ]);
+
+    $rectorConfig->rule(StrictArrayParamDimFetchRector::class);
 
     $rectorConfig->ruleWithConfiguration(AddReturnTypeDeclarationRector::class, [
         new AddReturnTypeDeclaration(
