173e8daa5e278ac4d85af8139c6796ba0ffe4410
Tomas Votruba <tomas.vot@gmail.com>
2026-07-30 23:45:31 +0200

    [TypeDeclarationDocblocks] Deprecate AddReturnDocblockDataProviderRector and AddParamArrayDocblockFromDataProviderRector
    Same reasoning as the data provider return docblock rule: docblock typing of
    data providers is not relevant to code quality, costs maintenance and lowers
    readability. Both rules are removed from the TypeDeclarationDocblocks level.

--- rules-tests/TypeDeclarationDocblocks/Rector/ClassMethod/AddParamArrayDocblockFromDataProviderRector/config/configured_rule.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Rector\Config\RectorConfig;
-use Rector\TypeDeclarationDocblocks\Rector\ClassMethod\AddParamArrayDocblockFromDataProviderRector;
-
-return RectorConfig::configure()
-    ->withRules([AddParamArrayDocblockFromDataProviderRector::class]);
