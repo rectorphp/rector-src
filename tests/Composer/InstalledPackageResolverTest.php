9d584ddbba90b89d331910251dbaaec7e951ab83
Tomas Votruba <tomas.vot@gmail.com>
2026-07-31 18:22:45 +0200

    [Composer] Prefer composer.json constraint over outdated installed.json version (#8242)

--- tests/Composer/InstalledPackageResolverTest.php
+++ tests/Composer/InstalledPackageResolverTest.php
@@ -27,4 +27,23 @@ final class InstalledPackageResolverTest extends TestCase
         $this->assertContainsOnlyInstancesOf(InstalledPackage::class, $installedPackages);
         $this->assertGreaterThan(77, count($installedPackages));
     }
+
+    public function testComposerJsonHasPriorityOverOutdatedInstalledJson(): void
+    {
+        $installedPackageResolver = new InstalledPackageResolver(
+            __DIR__ . '/Fixture/InstalledPackageResolver/outdated_installed_json'
+        );
+
+        // the "installed.json" is outdated and contains 11.5.2.0
+        $this->assertSame('10.5.0.0', $installedPackageResolver->resolvePackageVersion('phpunit/phpunit'));
+
+        // the installed version matches the "composer.json" constraint
+        $this->assertSame('7.2.0.0', $installedPackageResolver->resolvePackageVersion('symfony/console'));
+
+        // require-dev is respected as well
+        $this->assertSame('3.2.0.0', $installedPackageResolver->resolvePackageVersion('nette/utils'));
+
+        // not required in the "composer.json" at all
+        $this->assertSame('1.11.0.0', $installedPackageResolver->resolvePackageVersion('webmozart/assert'));
+    }
 }
