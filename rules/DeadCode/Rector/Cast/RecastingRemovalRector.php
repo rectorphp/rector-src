209ae3245ef2e8db65fceeb65a3dfec516bb2cc0
Tomas Votruba <tomas.vot@gmail.com>
2026-08-02 13:46:19 +0200

    [DeadCode] Skip RecastingRemovalRector on array dim fetch value (#8262)

--- rules/DeadCode/Rector/Cast/RecastingRemovalRector.php
+++ rules/DeadCode/Rector/Cast/RecastingRemovalRector.php
@@ -6,6 +6,7 @@ namespace Rector\DeadCode\Rector\Cast;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\ArrayDimFetch;
 use PhpParser\Node\Expr\Cast;
 use PhpParser\Node\Expr\Cast\Array_;
 use PhpParser\Node\Expr\Cast\Bool_;
@@ -143,6 +144,11 @@ CODE_SAMPLE
 
     private function shouldSkip(Expr $expr): bool
     {
+        // array dim fetch value can be anything, the type is often inaccurate
+        if ($expr instanceof ArrayDimFetch) {
+            return true;
+        }
+
         $type = $this->getType($expr);
 
         if ($type instanceof UnionType) {
