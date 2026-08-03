597c6b12b041a6a9b2c328b1a6c111fafb4f2d1b
Tomas Votruba <tomas.vot@gmail.com>
2026-08-03 14:36:47 +0200

    [CodingStyle] Deprecate CountArrayToEmptyArrayComparisonRector

--- rules-tests/CodingStyle/Rector/FuncCall/CountArrayToEmptyArrayComparisonRector/config/configured_rule.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Rector\CodingStyle\Rector\FuncCall\CountArrayToEmptyArrayComparisonRector;
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()
-    ->withRules([CountArrayToEmptyArrayComparisonRector::class]);
