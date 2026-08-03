2c77ccf8e6d3283717c5af6e5df1d1e81c29b1d5
Tomas Votruba <tomas.vot@gmail.com>
2026-08-03 17:14:31 +0200

    [Scripts] Add list-non-composer-based-rules.php script, skip deprecated rules in list-unused-rules.php

---
+++ scripts/list-non-composer-based-rules.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Scripts\Finder\RectorClassFinder;
+use Rector\Scripts\Finder\RectorSetFilesFinder;
+use Rector\Scripts\Resolver\UsedRectorClassResolver;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// 1. find all rector rules in core and in doctrine, phpunit and symfony packages
+$rectorClassFinder = new RectorClassFinder();
+
+$rectorClasses = $rectorClassFinder->find([
+    __DIR__ . '/../rules',
+    __DIR__ . '/../vendor/rector/rector-doctrine',
+    __DIR__ . '/../vendor/rector/rector-phpunit',
+    __DIR__ . '/../vendor/rector/rector-symfony',
+]);
+
+$symfonyStyle = new SymfonyStyle(new ArrayInput([]), new ConsoleOutput());
+$symfonyStyle->writeln(sprintf('<fg=green>Found Rector %d rules</>', count($rectorClasses)));
+
+// 2. find "composer-based.php" sets, that bind rules to the installed package version
+$rectorSetFilesFinder = new RectorSetFilesFinder();
+
+$rectorSetFiles = $rectorSetFilesFinder->find([
+    __DIR__ . '/../config/set',
+    __DIR__ . '/../vendor/rector/rector-symfony/config/sets',
+    __DIR__ . '/../vendor/rector/rector-doctrine/config/sets',
+    __DIR__ . '/../vendor/rector/rector-phpunit/config/sets',
+]);
+
+$composerBasedSetFiles = array_filter(
+    $rectorSetFiles,
+    static fn (string $rectorSetFile): bool => basename($rectorSetFile) === 'composer-based.php'
+);
+
+$symfonyStyle->writeln(sprintf('<fg=green>Found %d composer-based sets</>', count($composerBasedSetFiles)));
+$symfonyStyle->listing($composerBasedSetFiles);
+
+$usedRectorClassResolver = new UsedRectorClassResolver();
+$usedRectorRules = $usedRectorClassResolver->resolve($composerBasedSetFiles);
+
+$symfonyStyle->writeln(
+    sprintf('<fg=yellow>Found %d Rector rules used in composer-based sets</>', count($usedRectorRules))
+);
+
+$nonComposerBasedRectorRules = array_diff($rectorClasses, $usedRectorRules);
+
+$symfonyStyle->newLine();
+$symfonyStyle->listing($nonComposerBasedRectorRules);
+
+$symfonyStyle->writeln(
+    sprintf(
+        '<fg=yellow;options=bold>Found %d Rector rules not in any composer-based set, likely dead</>',
+        count($nonComposerBasedRectorRules)
+    )
+);
+$symfonyStyle->newLine();
