23fb7bbce01d7d65625937ebf34f0c6302222116
Tomas Votruba <tomas.vot@gmail.com>
2026-08-04 11:18:01 +0200

    [CodeQuality] Skip SimpleXMLElement on IssetOnPropertyObjectToPropertyExistsRector (#8284)

--- rules/CodeQuality/Rector/Isset_/IssetOnPropertyObjectToPropertyExistsRector.php
+++ rules/CodeQuality/Rector/Isset_/IssetOnPropertyObjectToPropertyExistsRector.php
@@ -221,6 +221,13 @@ CODE_SAMPLE
             return null;
         }
 
-        return $this->reflectionProvider->getClass($className);
+        $classReflection = $this->reflectionProvider->getClass($className);
+
+        // XML elements resolve properties from the XML document itself
+        if ($classReflection->is('SimpleXMLElement')) {
+            return null;
+        }
+
+        return $classReflection;
     }
 }
