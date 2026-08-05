74e1924244aedeb191e34021886eb7441082d142
Tomas Votruba <tomas.vot@gmail.com>
2026-08-06 01:01:00 +0200

    [EarlyReturn] Deprecate ChangeNestedForeachIfsToEarlyContinueRector

--- config/set/early-return.php
+++ config/set/early-return.php
@@ -3,12 +3,8 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\EarlyReturn\Rector\Foreach_\ChangeNestedForeachIfsToEarlyContinueRector;
 use Rector\EarlyReturn\Rector\If_\ChangeNestedIfsToEarlyReturnRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rules([
-        ChangeNestedForeachIfsToEarlyContinueRector::class,
-        ChangeNestedIfsToEarlyReturnRector::class,
-    ]);
+    $rectorConfig->rules([ChangeNestedIfsToEarlyReturnRector::class]);
 };
