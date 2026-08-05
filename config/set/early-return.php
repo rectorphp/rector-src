179416ac28654d5b34363dbb1eb730cb29a64d68
Tomas Votruba <tomas.vot@gmail.com>
2026-08-06 00:53:28 +0200

    [CodeQuality] Move 4 early-return rules to code quality set (#8301)

--- config/set/early-return.php
+++ config/set/early-return.php
@@ -4,21 +4,13 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\EarlyReturn\Rector\Foreach_\ChangeNestedForeachIfsToEarlyContinueRector;
-use Rector\EarlyReturn\Rector\If_\ChangeIfElseValueAssignToEarlyReturnRector;
 use Rector\EarlyReturn\Rector\If_\ChangeNestedIfsToEarlyReturnRector;
-use Rector\EarlyReturn\Rector\If_\RemoveAlwaysElseRector;
-use Rector\EarlyReturn\Rector\Return_\PreparedValueToEarlyReturnRector;
 use Rector\EarlyReturn\Rector\Return_\ReturnBinaryOrToEarlyReturnRector;
-use Rector\EarlyReturn\Rector\StmtsAwareInterface\ReturnEarlyIfVariableRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rules([
         ChangeNestedForeachIfsToEarlyContinueRector::class,
-        ChangeIfElseValueAssignToEarlyReturnRector::class,
         ChangeNestedIfsToEarlyReturnRector::class,
-        RemoveAlwaysElseRector::class,
-        PreparedValueToEarlyReturnRector::class,
         ReturnBinaryOrToEarlyReturnRector::class,
-        ReturnEarlyIfVariableRector::class,
     ]);
 };
