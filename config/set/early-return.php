cd5ecca14ccce52e364a79493c91664269b7ccfe
Tomas Votruba <tomas.vot@gmail.com>
2026-08-06 00:56:48 +0200

    [EarlyReturn] Deprecate ReturnBinaryOrToEarlyReturnRector

--- config/set/early-return.php
+++ config/set/early-return.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\EarlyReturn\Rector\Foreach_\ChangeNestedForeachIfsToEarlyContinueRector;
 use Rector\EarlyReturn\Rector\If_\ChangeNestedIfsToEarlyReturnRector;
-use Rector\EarlyReturn\Rector\Return_\ReturnBinaryOrToEarlyReturnRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rules([
         ChangeNestedForeachIfsToEarlyContinueRector::class,
         ChangeNestedIfsToEarlyReturnRector::class,
-        ReturnBinaryOrToEarlyReturnRector::class,
     ]);
 };
--- tests/Issues/ReplaceStmtToExpr/config/configured_rule.php
+++ tests/Issues/ReplaceStmtToExpr/config/configured_rule.php
@@ -5,13 +5,11 @@ declare(strict_types=1);
 use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
 use Rector\CodeQuality\Rector\If_\ObjectExplicitBoolCompareRector;
 use Rector\Config\RectorConfig;
-use Rector\EarlyReturn\Rector\Return_\ReturnBinaryOrToEarlyReturnRector;
 
 return RectorConfig::configure()
     ->withRules(
         [
             ObjectExplicitBoolCompareRector::class,
             FlipTypeControlToUseExclusiveTypeRector::class,
-            ReturnBinaryOrToEarlyReturnRector::class,
         ]
     );
