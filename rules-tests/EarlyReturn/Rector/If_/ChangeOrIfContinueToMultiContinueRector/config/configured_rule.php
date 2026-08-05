3211d43ed7073604bc7d6263dc40a168a60f659d
Tomas Votruba <tomas.vot@gmail.com>
2026-08-06 00:00:35 +0200

    [EarlyReturn] Deprecate ChangeOrIfContinueToMultiContinueRector (#8299)

--- rules-tests/EarlyReturn/Rector/If_/ChangeOrIfContinueToMultiContinueRector/config/configured_rule.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Rector\Config\RectorConfig;
-use Rector\EarlyReturn\Rector\If_\ChangeOrIfContinueToMultiContinueRector;
-
-return RectorConfig::configure()
-    ->withRules([ChangeOrIfContinueToMultiContinueRector::class]);
