a9870fe111210711895c697065baf8091e26f0e5
Tomas Votruba <tomas.vot@gmail.com>
2026-08-25 00:07:23 +0100

    Remove PHPUnit and PHPStan autoload compat test (#8367)

--- compat-tests/tests/Rector/MakeClassFinalRector/config/configured_rule.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Rector\Config\RectorConfig;
-use Rector\RectorCompatTests\Rector\MakeClassFinalRector;
-
-return RectorConfig::configure()
-    ->withRules([MakeClassFinalRector::class]);
