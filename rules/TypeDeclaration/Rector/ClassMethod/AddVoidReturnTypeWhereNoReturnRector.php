bb61cce8132fa22ee41bc7ee00349cd39a71e595
Tomas Votruba <tomas.vot@gmail.com>
2026-08-29 10:27:41 +0200

    cleanup

--- rules/TypeDeclaration/Rector/ClassMethod/AddVoidReturnTypeWhereNoReturnRector.php
+++ rules/TypeDeclaration/Rector/ClassMethod/AddVoidReturnTypeWhereNoReturnRector.php
@@ -9,6 +9,8 @@ use PhpParser\Node\Expr\Throw_;
 use PhpParser\Node\Identifier;
 use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Expression;
+use PHPStan\Reflection\ClassReflection;
+use Rector\PHPStan\ScopeFetcher;
 use Rector\Rector\AbstractRector;
 use Rector\Reflection\ClassModifierChecker;
 use Rector\TypeDeclaration\TypeInferer\SilentVoidResolver;
@@ -119,7 +121,17 @@ CODE_SAMPLE
             return ! $this->classModifierChecker->isInsideFinalClass($classMethod);
         }
 
-        return $this->classModifierChecker->isInsideAbstractClass($classMethod) && $classMethod->getStmts() === [];
+        $scope = ScopeFetcher::fetch($classMethod);
+        if (! $scope->isInClass()) {
+            return false;
+        }
+
+        $classReflection = $scope->getClassReflection();
+        if (! $classReflection instanceof ClassReflection) {
+            return false;
+        }
+
+        return $classReflection->isAbstract();
     }
 
     private function isNotFinalAndHasExceptionOnly(ClassMethod $classMethod): bool
