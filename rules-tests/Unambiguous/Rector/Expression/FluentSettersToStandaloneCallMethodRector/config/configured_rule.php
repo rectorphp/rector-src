96e88e1cde5ce25490b171d495eea71a80e7d8e9
Tomas Votruba <tomas.vot@gmail.com>
2026-09-08 21:25:31 +0200

    [Unambiguous] Deprecate FluentSettersToStandaloneCallMethodRector and RemoveReturnThisFromSetterClassMethodRector (#8473)

--- rules-tests/Unambiguous/Rector/Expression/FluentSettersToStandaloneCallMethodRector/config/configured_rule.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Rector\Config\RectorConfig;
-use Rector\Unambiguous\Rector\Expression\FluentSettersToStandaloneCallMethodRector;
-
-return RectorConfig::configure()
-    ->withRules([FluentSettersToStandaloneCallMethodRector::class]);
