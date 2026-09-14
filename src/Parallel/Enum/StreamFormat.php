ba7248392d40bad3edc4fcc65520d22d4925a45b
Bartłomiej Gajda <b.gajda@webard.me>
2026-09-14 12:16:23 +0200

    Give the worker the same NDJSON frame limit as the main process (#8489)

---
+++ src/Parallel/Enum/StreamFormat.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Parallel\Enum;
+
+final class StreamFormat
+{
+    public const int DEPTH = 512;
+
+    public const int MAX_LENGTH = 4 * 1024 * 1024;
+}
