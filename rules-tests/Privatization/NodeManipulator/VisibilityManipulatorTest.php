1d0224b3b250f90ae6acabbc9ab5ac7e5b4c4e1f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-09-21 14:26:47 +0700

    [Privatization] Remove final flag on Privatize*Rector from protected to private (#8509)

--- rules-tests/Privatization/NodeManipulator/VisibilityManipulatorTest.php
+++ rules-tests/Privatization/NodeManipulator/VisibilityManipulatorTest.php
@@ -7,6 +7,7 @@ namespace Rector\Tests\Privatization\NodeManipulator;
 use PhpParser\Node\Stmt\ClassMethod;
 use Rector\Privatization\NodeManipulator\VisibilityManipulator;
 use Rector\Testing\PHPUnit\AbstractLazyTestCase;
+use Rector\ValueObject\MethodName;
 use Rector\ValueObject\Visibility;
 
 final class VisibilityManipulatorTest extends AbstractLazyTestCase
@@ -20,5 +21,27 @@ final class VisibilityManipulatorTest extends AbstractLazyTestCase
 
         $visibilityManipulator->changeNodeVisibility($classMethod, Visibility::PROTECTED);
         $this->assertSame(Visibility::PROTECTED | Visibility::STATIC, $classMethod->flags);
+    }
+
+    public function testMakePrivateRemovesFinal(): void
+    {
+        $visibilityManipulator = $this->make(VisibilityManipulator::class);
+
+        $classMethod = new ClassMethod('run');
+        $classMethod->flags = Visibility::PROTECTED | Visibility::FINAL;
+
+        $visibilityManipulator->makePrivate($classMethod);
+        $this->assertSame(Visibility::PRIVATE, $classMethod->flags);
+    }
+
+    public function testMakePrivateKeepsFinalOnConstructor(): void
+    {
+        $visibilityManipulator = $this->make(VisibilityManipulator::class);
+
+        $classMethod = new ClassMethod(MethodName::CONSTRUCT);
+        $classMethod->flags = Visibility::PROTECTED | Visibility::FINAL;
+
+        $visibilityManipulator->makePrivate($classMethod);
+        $this->assertSame(Visibility::PRIVATE | Visibility::FINAL, $classMethod->flags);
     }
 }
